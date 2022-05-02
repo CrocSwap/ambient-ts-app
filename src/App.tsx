162eb72086136cd67d5afab10c39dc57a4ca046e
Ben Wolski <benwolski@gmail.com>
2022-05-02 13:50:27 -0400

    add rive animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Rive from '@rive-app/react-canvas';
+
+export const Simple = () => (
+  // <Rive src='./new_file.riv' />
+  // <Rive src='https://cdn.rive.app/animations/off_road_car_v7.riv' />
+  <Rive src='ambient.riv' />
+  // <Rive src='off_road_car_v7.riv' />
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <Simple />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
