453b1c94dc187abd9d45ece83d71e0e8b351e0b7
mhsmith321 <marty.smith01@yahoo.com>
2022-05-06 15:08:17 -0400

    delete function Simple() from App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,34 +6,11 @@ import './App.css';
 import HamburgerMenuIcon from '../animations/hamburger-menu-icon';
 import AmbientLogoIcon from '../animations/ambient-logo-icon';
 
-// TODO: move this function to its own file later
-function Simple() {
-  const { rive, RiveComponent } = useRive({
-    src: 'hamburger.riv',
-    stateMachines: 'Basic State Machine',
-    autoplay: true,
-  });
-
-  const switchInput = useStateMachineInput(
-    rive,
-    'Basic State Machine',
-    'Switch'
-  );
-
-  return (
-    <RiveComponent
-      style={{ height: '500px' }}
-      onClick={() => switchInput && switchInput.fire()}
-    />
-  );
-}
-
 /******* React Function *******/
 export default function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
         <HamburgerMenuIcon />
         <AmbientLogoIcon />
         <p>
