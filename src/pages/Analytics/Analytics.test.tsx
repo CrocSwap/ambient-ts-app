677e3a710590552ca94f66340509d5fda69b8c0b
mhsmith321 <marty.smith01@yahoo.com>
2022-05-09 13:55:32 -0400

    stub up basic smoke test in Analytics.test.tsx testing file

--- src/pages/Analytics/Analytics.test.tsx
+++ src/pages/Analytics/Analytics.test.tsx
@@ -1,1 +1,12 @@
-import { render } from "@testing-library/react";
+import {
+    render,
+    screen
+} from '@testing-library/react';
+
+import Analytics from './Analytics';
+
+test('renders Analytics() React function instance', () => {
+    render(<Analytics />);
+    const analyticsElement = screen.getByTestId('analytics');
+    expect(analyticsElement).toBeInTheDocument();
+});
