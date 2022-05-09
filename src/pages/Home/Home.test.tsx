a24fbd7db54c108899725aa73b01bf282e742164
mhsmith321 <marty.smith01@yahoo.com>
2022-05-09 13:35:13 -0400

    stub up a basic unit test in Home.test.tsx data file

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,1 +1,12 @@
+import {
+    render,
+    screen
+} from '@testing-library/react';
+
 import Home from './Home';
+
+test('renders learn react link', () => {
+    render(<Home />);
+    const homeElement = screen.getByTestId('home');
+    expect(homeElement).toBeInTheDocument();
+});
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main id={'home'}>
+        <main data-testid={'home'}>
         </main>
     );
 }
