c2d1b3a5cd7028e4e2fee6497be57916810a6a16
Jadessu <junioradessu@gmail.com>
2022-05-10 14:55:35 -0500

    change header tag on footer

--- src/App/components/PageFooter/PageFooter.tsx
+++ src/App/components/PageFooter/PageFooter.tsx
@@ -1,8 +1,8 @@
 export default function PageFooter() {
     return (
-        <header data-testid={'page-footer'}>
+        <footer data-testid={'page-footer'}>
             <h1>This is the Page Footer!</h1>
             <h3>PageFooter.tsx</h3>
-        </header>
+        </footer>
     );
 }
