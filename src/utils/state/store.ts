f4388e0f14fc5abd13234e3a6101b1232a1b0525
mhsmith321 <marty.smith01@yahoo.com>
2022-05-10 14:11:40 -0400

    import counterReducer into RTK store.ts file

--- src/utils/state/store.ts
+++ src/utils/state/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterReducer,
+    },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
