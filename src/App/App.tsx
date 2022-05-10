d8702bdd8bd38d07e55bec26d7e53b24e9202db2
mhsmith321 <marty.smith01@yahoo.com>
2022-05-10 10:54:40 -0400

    activate nested /market URL pathway in /trade URL pathway

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,22 +8,25 @@ import Home from '../pages/Home/Home';
 import Trade from '../pages/Trade/Trade';
 import Analytics from '../pages/Analytics/Analytics';
 import Portfolio from '../pages/Portfolio/Portfolio';
+import Market from '../pages/Trade/Market/Market';
 
 /** * **** Import Local Files *******/
 import './App.css';
 
 /** ***** React Function *******/
 export default function App() {
-  return (
-    <>
-      <PageHeader />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path='trade' element={<Trade />} />
-        <Route path='analytics' element={<Analytics />} />
-        <Route path='portfolio' element={<Portfolio />} />
-      </Routes>
-      <PageFooter />
-    </>
-  );
+    return (
+        <>
+            <PageHeader />
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path='trade' element={<Trade />}>
+                    <Route path='market' element={<Market />} />
+                </Route>
+                <Route path='analytics' element={<Analytics />} />
+                <Route path='portfolio' element={<Portfolio />} />
+            </Routes>
+            <PageFooter />
+        </>
+    );
 }
--- src/pages/Trade/Trade.tsx
+++ src/pages/Trade/Trade.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from 'react-router-dom';
+
 export default function Trade() {
     return (
         <main data-testid={'trade'}>
             <h1>This is Trade.tsx</h1>
+            <Outlet />
         </main>
     );
 }
