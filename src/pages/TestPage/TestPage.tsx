ae0d35164d2f2a376c5baf5dba4a971ee7a56a11
mhsmith321 <marty.smith01@yahoo.com>
2022-05-10 14:21:17 -0400

    code boilerplate logic in TestPage.jsx to test RTK

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,7 +1,22 @@
+import { RootState } from '../../utils/state/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../../utils/state/counterSlice';
+
 export default function TestPage() {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
         <main>
-            <h2>This is the test page!</h2>
+            <div>
+                <button aria-label='Increment value' onClick={() => dispatch(increment())}>
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+                    Decrement
+                </button>
+            </div>
         </main>
     );
 }
