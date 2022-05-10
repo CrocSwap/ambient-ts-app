8847e7571b581d13835b32500208c388375687d0
Jadessu <junioradessu@gmail.com>
2022-05-10 10:45:40 -0500

    setup rive hamburger menu

--- src/App/components/PageHeader/PageHeader.tsx
+++ src/App/components/PageHeader/PageHeader.tsx
@@ -6,16 +6,38 @@ import { useState } from 'react';
 /** ***** START: Import Local Files *******/
 import styles from './PageHeader.module.css';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
+import { useRive, useStateMachineInput } from 'rive-react';
 /** ***** END: Import Local Files *********/
 
 export default function PageHeader() {
+  // rive component
+  const STATE_MACHINE_NAME = 'Basic State Machine';
+  const INPUT_NAME = 'Switch';
+
+  const { rive, RiveComponent } = useRive({
+    src: './hamburger.riv',
+    stateMachines: STATE_MACHINE_NAME,
+    autoplay: true,
+  });
+
+  const onClickInput = useStateMachineInput(rive, STATE_MACHINE_NAME, INPUT_NAME);
+
+  // end of rive component
+
   return (
     <header data-testid={'page-header'} className={styles.primary_header}>
       <div className={styles.logo_container}>
         <h1>ambient</h1>
       </div>
-      <nav className={styles.primary_navigation}>
+      <div
+        className={styles.mobile_nav_toggle}
+        aria-controls='primary_navigation'
+        aria-expanded='false'
+      >
+        <RiveComponent onClick={() => onClickInput?.fire()} />
+        <span className='sr-only'>Menu</span>
+      </div>
+      <nav className={styles.primary_navigation} id='primary_navigation'>
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/trade'>Trade</NavLink>
         <NavLink to='/analytics'>Analytics</NavLink>
