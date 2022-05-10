cf4c2e5f99e9e5dc3b634ce6f4629c23b7bd4df0
mhsmith321 <marty.smith01@yahoo.com>
2022-05-10 13:54:06 -0400

    initialize a store.ts file for RTK with boilerplate code, also wrap <App/> with an RTK provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './utils/state/store';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
