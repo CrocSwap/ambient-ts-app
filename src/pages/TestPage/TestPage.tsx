de244256288113db0d77c8e9d77d0cab84a2e951
mhsmith321 <marty.smith01@yahoo.com>
2022-05-10 14:23:54 -0400

    annotate RTK code in TestPage.tsx page file

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -2,6 +2,11 @@ import { RootState } from '../../utils/state/store';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment } from '../../utils/state/counterSlice';
 
+//  Important!  This file currently has example code for how to interact
+//  ... with data from RTK using TypeScript syntax.  Please use it for
+//  ... reference.  Anyone who needs this file can delete it and this
+//  ... this note when needed.
+
 export default function TestPage() {
     const count = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch();
