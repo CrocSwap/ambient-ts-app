54d32991da287d5811a3c1ae084b4ee3c2746f74
mhsmith321 <marty.smith01@yahoo.com>
2022-05-10 10:50:04 -0400

    change tabWidth to 4

--- src/pages/Trade/Market/Market.tsx
+++ src/pages/Trade/Market/Market.tsx
@@ -1,7 +1,7 @@
 export default function Market() {
-  return (
-    <section data-testid={'market'}>
-      <h4>This is Market.tsx!</h4>
-    </section>
-  );
+    return (
+        <section data-testid={'market'}>
+            <h4>This is Market.tsx!</h4>
+        </section>
+    );
 }
