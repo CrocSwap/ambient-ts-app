5d5e24985994c4b83babff50ca258f66bc3ff4c8
Jadessu <junioradessu@gmail.com>
2022-05-10 15:04:08 -0500

    import social media icons to footer

--- src/App/components/PageFooter/PageFooter.tsx
+++ src/App/components/PageFooter/PageFooter.tsx
@@ -1,8 +1,12 @@
+import styles from './PageFooter.module.css';
+import { RiTable2, RiTwitchFill } from 'react-icons/ri';
+import { FaDiscord, FaGithub } from 'react-icons/fa';
+import { BsMedium } from 'react-icons/bs';
+
 export default function PageFooter() {
     return (
         <footer data-testid={'page-footer'}>
             <h1>This is the Page Footer!</h1>
-            <h3>PageFooter.tsx</h3>
         </footer>
     );
 }
