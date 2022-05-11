8a62396244eeb3d59effc8a18ada594d9a70372c
Jadessu <junioradessu@gmail.com>
2022-05-11 12:20:19 -0500

    create main layout jsx

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,9 +1,8 @@
 import styles from './MainLayout.module.css';
 
-export default function MainLayout() {
-    return (
-        <main data-testid={'trade'}>
-            <h1>This is Trade.tsx</h1>
-        </main>
-    );
+type Props = {
+    children?: React.ReactNode;
+};
+export default function MainLayout({ children }: Props) {
+    return <main data-testid={'main-layout'}>{children}</main>;
 }
