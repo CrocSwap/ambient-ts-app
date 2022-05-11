d16b8d21ea9255382bd11f40dff51c53e5748372
Jadessu <junioradessu@gmail.com>
2022-05-11 10:15:35 -0500

    create functions to handle popover

--- src/App/components/PageHeader/Account/Account.tsx
+++ src/App/components/PageHeader/Account/Account.tsx
@@ -5,5 +5,17 @@ import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { FiMoreHorizontal } from 'react-icons/fi';
 
 export default function Account() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handlePopoverClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const popoverId = open ? 'simple-popover' : undefined;
     return <div>Account</div>;
 }
