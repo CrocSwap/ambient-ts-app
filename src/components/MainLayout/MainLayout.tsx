a0897053fffd2c7b269a41080de9686a16cf3c0d
Jadessu <junioradessu@gmail.com>
2022-05-11 12:39:10 -0500

    move main layout to trade

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,9 +1,11 @@
 import styles from './MainLayout.module.css';
+import { useState } from 'react';
 
 type Props = {
     children?: React.ReactNode;
 };
 export default function MainLayout({ children }: Props) {
+    const [leftSidebar, setLeftSidebar] = useState<boolean>(true);
     return (
         <main data-testid={'main-layout'} className={styles.main_layout}>
             {children}
--- src/pages/Trade/Trade.tsx
+++ src/pages/Trade/Trade.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import styles from './Trade.module.css';
 import MainLayout from '../../components/MainLayout/MainLayout';
+import { FaTimes } from 'react-icons/fa';
 
 export default function Trade() {
     return (
         <main data-testid={'trade'}>
             {/* <h1>This is Trade.tsx</h1> */}
-            <MainLayout>
-                <div className={styles.left_col}>I am first</div>
-                <div className={styles.middle_col}>I am first</div>
-                <div className={styles.right_col}>I am first</div>
-            </MainLayout>
+            <main className={styles.main_layout}>
+                <div className={styles.left_col}>
+                    <FaTimes size={20} color='#bdbdbd' />
+                </div>
+                <div className={styles.middle_col}></div>
+                <div className={styles.right_col}></div>
+            </main>
             <Outlet />
         </main>
     );
