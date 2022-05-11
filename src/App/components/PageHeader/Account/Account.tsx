bb5a818b1b7ae9407d73fcef40d0c306d52fc0d3
Jadessu <junioradessu@gmail.com>
2022-05-11 10:16:18 -0500

    create popover content

--- src/App/components/PageHeader/Account/Account.tsx
+++ src/App/components/PageHeader/Account/Account.tsx
@@ -17,5 +17,35 @@ export default function Account() {
 
     const open = Boolean(anchorEl);
     const popoverId = open ? 'simple-popover' : undefined;
+
+    const popperContent = (
+        <div className={styles.popperContent}>
+            <div className={styles.more_row}>
+                <span>About</span>
+            </div>
+            <div className={styles.more_row}>
+                <span>Help Center</span>
+            </div>
+            <div className={styles.more_row}>
+                <span>Request Features</span>
+            </div>
+            <div className={styles.more_row}>
+                <span>Discord</span>
+            </div>
+            <div className={styles.more_row}>
+                <span>Language</span>
+            </div>
+            {/* <div className={styles.more_row} onClick={switchTheme}>
+                <span>{theme === "dark" ? "Light" : "Dark"} Theme</span>
+            </div> */}
+            <div className={styles.more_row}>
+                <span>Docs</span>
+            </div>
+            <div className={styles.more_row}>
+                <span>Legal & Privacy</span>
+            </div>
+            {/* <Button title="Logout"/> */}
+        </div>
+    );
     return <div>Account</div>;
 }
