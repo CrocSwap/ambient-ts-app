138a69ecc378896fbc06b9b21c268c1684277da0
Jadessu <junioradessu@gmail.com>
2022-05-11 11:51:58 -0500

    remove unused files

--- src/App/components/PageHeader/Account/Account.tsx
+++ src/App/components/PageHeader/Account/Account.tsx
@@ -1,52 +1,52 @@
 import styles from './Account.module.css';
-import React, { useState, useEffect } from 'react';
-import Popover from '@material-ui/core/Popover';
+// import React, { useState, useEffect } from 'react';
+// import Popover from '@material-ui/core/Popover';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { FiMoreHorizontal } from 'react-icons/fi';
 
 export default function Account() {
-    const [anchorEl, setAnchorEl] = useState(null);
+    // const [anchorEl, setAnchorEl] = useState(null);
 
-    const handlePopoverClick = (event: React.ChangeEvent<any>) => {
-        setAnchorEl(event.currentTarget);
-    };
+    // const handlePopoverClick = (event: React.ChangeEvent<any>) => {
+    //     setAnchorEl(event.currentTarget);
+    // };
 
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
+    // const handlePopoverClose = () => {
+    //     setAnchorEl(null);
+    // };
 
-    const open = Boolean(anchorEl);
-    const popoverId = open ? 'simple-popover' : undefined;
+    // const open = Boolean(anchorEl);
+    // const popoverId = open ? 'simple-popover' : undefined;
 
-    const popperContent = (
-        <div className={styles.popperContent}>
-            <div className={styles.more_row}>
-                <span>About</span>
-            </div>
-            <div className={styles.more_row}>
-                <span>Help Center</span>
-            </div>
-            <div className={styles.more_row}>
-                <span>Request Features</span>
-            </div>
-            <div className={styles.more_row}>
-                <span>Discord</span>
-            </div>
-            <div className={styles.more_row}>
-                <span>Language</span>
-            </div>
-            {/* <div className={styles.more_row} onClick={switchTheme}>
-                <span>{theme === "dark" ? "Light" : "Dark"} Theme</span>
-            </div> */}
-            <div className={styles.more_row}>
-                <span>Docs</span>
-            </div>
-            <div className={styles.more_row}>
-                <span>Legal & Privacy</span>
-            </div>
-            {/* <Button title="Logout"/> */}
-        </div>
-    );
+    // const popperContent = (
+    //     <div className={styles.popperContent}>
+    //         <div className={styles.more_row}>
+    //             <span>About</span>
+    //         </div>
+    //         <div className={styles.more_row}>
+    //             <span>Help Center</span>
+    //         </div>
+    //         <div className={styles.more_row}>
+    //             <span>Request Features</span>
+    //         </div>
+    //         <div className={styles.more_row}>
+    //             <span>Discord</span>
+    //         </div>
+    //         <div className={styles.more_row}>
+    //             <span>Language</span>
+    //         </div>
+    //         <div className={styles.more_row} onClick={switchTheme}>
+    //             <span>{theme === "dark" ? "Light" : "Dark"} Theme</span>
+    //         </div>
+    //         <div className={styles.more_row}>
+    //             <span>Docs</span>
+    //         </div>
+    //         <div className={styles.more_row}>
+    //             <span>Legal & Privacy</span>
+    //         </div>
+    //         <Button title="Logout"/>
+    //     </div>
+    // );
     return (
         <div className={styles.account_container}>
             <div className={styles.ethereum_icon}>
@@ -65,7 +65,10 @@ export default function Account() {
 
             <div>username</div>
 
-            <div className={styles.more} aria-describedby={popoverId} onClick={handlePopoverClick}>
+            {/* <div className={styles.more} aria-describedby={popoverId} onClick={handlePopoverClick}>
+                <FiMoreHorizontal size={20} color='#ffffff' />
+            </div> */}
+            <div className={styles.more}>
                 <FiMoreHorizontal size={20} color='#ffffff' />
             </div>
             {/* <Popover
