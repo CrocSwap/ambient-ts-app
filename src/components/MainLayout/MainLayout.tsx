4371bbc4c07b5666d7a61e5da46fae6339bb7051
Jadessu <junioradessu@gmail.com>
2022-05-11 12:17:53 -0500

    setup main layout files

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,9 @@
+import styles from './MainLayout.module.css';
+
+export default function MainLayout() {
+    return (
+        <main data-testid={'trade'}>
+            <h1>This is Trade.tsx</h1>
+        </main>
+    );
+}
