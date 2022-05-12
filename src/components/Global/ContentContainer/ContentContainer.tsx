2e6f38ebb6a9d7e63c12ae8069ba95bdefaff826
Jadessu <junioradessu@gmail.com>
2022-05-12 09:10:32 -0500

    create content container jsx

--- src/components/Global/ContentContainer/ContentContainer.tsx
+++ src/components/Global/ContentContainer/ContentContainer.tsx
@@ -2,8 +2,21 @@ import styles from 'ContentContainer.module.css';
 
 interface ContentContainerProps {
     children: React.ReactNode;
+    customWidth: boolean;
+    customWidthAuto: boolean;
 }
 
 export default function ContentContainer(props: ContentContainerProps) {
-    return <div className={styles.row}>{props.children}</div>;
+    const { customWidth, customWidthAuto } = props;
+
+    const customWidthStyle = customWidth ? styles.customWidth_container : null;
+    const customWidthAutoStyle = customWidthAuto ? styles.customWidthAuto : styles.container;
+
+    return (
+        <main className={`$ ${customWidthStyle} ${customWidthAutoStyle}`}>
+            <section className={`${styles.window} `}>
+                <div className={styles.main_content}>{props.children}</div>
+            </section>
+        </main>
+    );
 }
