01b6d313af9909b254fec121d3924ef9bb286eb3
mhsmith321 <marty.smith01@yahoo.com>
2022-05-12 10:24:51 -0400

    build URL pathway to render Chart.tsx page file

--- src/pages/Chart/Chart.test.tsx
+++ src/pages/Chart/Chart.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import Chart from './Chart';
 
-test('renders Analytics() React function instance', () => {
+test('renders Chart() React function instance', () => {
     render(<Chart />);
     const chartElement = screen.getByTestId('chart');
     expect(chartElement).toBeInTheDocument();
