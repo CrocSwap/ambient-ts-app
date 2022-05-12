bc9101e4b5916b87df36dc892a8e6eba6f78b695
Jadessu <junioradessu@gmail.com>
2022-05-12 10:22:09 -0500

    add extra info details

--- src/components/Swap/ExtraInfo/ExtraInfo.tsx
+++ src/components/Swap/ExtraInfo/ExtraInfo.tsx
@@ -9,5 +9,26 @@ interface ExtraInfoProps {
 export default function ExtraInfo(props: ExtraInfoProps) {
     const [showExtraDetails, setShowExtraDetails] = useState<boolean>(false);
 
+    const extraInfoDetails = (
+        <div className={styles.extra_details}>
+            <div className={styles.extra_row}>
+                <span>Spot Price</span>
+                <span>0.6969 ETH per AMBI</span>
+            </div>
+            <div className={styles.extra_row}>
+                <span>Price Limit after Slippage and Fee</span>
+                <span>0.6861 ETH per AMBI</span>
+            </div>
+            <div className={styles.extra_row}>
+                <span>Slippage Tolerance</span>
+                <span>5%</span>
+            </div>
+            <div className={styles.extra_row}>
+                <span>Liquidity Provider Fee</span>
+                <span>0.3%</span>
+            </div>
+        </div>
+    );
+
     return <div className={styles.row}>{props.children}</div>;
 }
