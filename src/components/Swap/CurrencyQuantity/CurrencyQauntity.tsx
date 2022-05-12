d96faa5bb79063eb4b8e1bddb99b1570dfae910e
Jadessu <junioradessu@gmail.com>
2022-05-12 09:59:27 -0500

    create currency quantity jsx

--- src/components/Swap/CurrencyQuantity/CurrencyQauntity.tsx
+++ src/components/Swap/CurrencyQuantity/CurrencyQauntity.tsx
@@ -1,9 +1,29 @@
 import styles from 'Row.module.css';
 
-interface RowProps {
-    children: React.ReactNode;
+interface CurrencyQuantityProps {
+    disable?: boolean;
+    fieldId: string;
 }
 
-export default function CurrencyQuantity(props: RowProps) {
-    return <div className={styles.row}>{props.children}</div>;
+export default function CurrencyQuantity(props: CurrencyQuantityProps) {
+    const { disable, fieldId } = props;
+
+    return (
+        <div className={styles.token_amount}>
+            <input
+                id={`${fieldId}-quantity`}
+                className={styles.currency_quantity}
+                type='text'
+                inputMode='decimal'
+                autoComplete='off'
+                autoCorrect='off'
+                min='0'
+                minLength={1}
+                pattern='^[0-9]*[.,]?[0-9]*$'
+                placeholder='0.0'
+                disabled={disable}
+                required
+            />
+        </div>
+    );
 }
