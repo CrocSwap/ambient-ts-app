ff5359fc18692cd2da94ca3f7cd0b3546ade5220
Jadessu <junioradessu@gmail.com>
2022-05-12 10:21:51 -0500

    set stat efor show extra details

--- src/components/Swap/ExtraInfo/ExtraInfo.tsx
+++ src/components/Swap/ExtraInfo/ExtraInfo.tsx
@@ -1,9 +1,13 @@
 import styles from 'ExtraInfo.module.css';
-
+import { useState } from 'react';
+import { FaGasPump } from 'react-icons/fa';
+import { RiArrowDownSLine } from 'react-icons/ri';
 interface ExtraInfoProps {
     children: React.ReactNode;
 }
 
 export default function ExtraInfo(props: ExtraInfoProps) {
+    const [showExtraDetails, setShowExtraDetails] = useState<boolean>(false);
+
     return <div className={styles.row}>{props.children}</div>;
 }
