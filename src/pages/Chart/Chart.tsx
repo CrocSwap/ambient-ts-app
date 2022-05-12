f64de58a4a7c69bc7580fd768d8d048faab66c08
Ben Wolski <benwolski@gmail.com>
2022-05-12 11:20:17 -0400

    test commit to test deploy to netlify

--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -1,7 +1,7 @@
 export default function Chart() {
     return (
         <main data-testid='chart'>
-            <h2>This is Chart.tsx!</h2>
+            <h2>This is Chart.tsx</h2>
         </main>
     );
 }
