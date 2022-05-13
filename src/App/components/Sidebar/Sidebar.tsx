2bdbd8e41f78eb3d53e25c2ffaebbf955bbd6d3e
Jadessu <junioradessu@gmail.com>
2022-05-13 11:37:41 -0500

    add styling to sidebar

--- src/App/components/Sidebar/Sidebar.tsx
+++ src/App/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import styles from './Sidebar.module.css';
 
 interface SidebarProps {
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 export default function Sidebar(props: SidebarProps) {
-    return <div className={styles.Sidebar}>{props.children}</div>;
+    return (
+        <div className={styles.sidebar_container}>
+            <div className={styles.sidebar_content}></div>
+            {props.children}
+        </div>
+    );
 }
