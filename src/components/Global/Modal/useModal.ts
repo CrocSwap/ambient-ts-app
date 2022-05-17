fe2ecad33d641b57b0f4fbcadbcdc4d4fd57c7d5
mhsmith321 <marty.smith01@yahoo.com>
2022-05-17 14:38:51 -0400

    annotate useModal.ts custom hook file

--- src/components/Global/Modal/useModal.ts
+++ src/components/Global/Modal/useModal.ts
@@ -1,8 +1,13 @@
 import { useState } from 'react';
+
 export const useModal = (initialMode = false) => {
+    // create a useState hook to track if the modal should be rendered
     const [isModalOpen, setIsModalOpen] = useState<boolean>(initialMode);
+
+    // click handlers to to open and close the modal
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
 
+    // return all data and functions needed for local use
     return [isModalOpen, openModal, closeModal] as const;
 };
