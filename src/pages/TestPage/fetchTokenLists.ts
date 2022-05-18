9a23cc3cb32f633a6ddfef41ad3205756eb2da7b
mhsmith321 <marty.smith01@yahoo.com>
2022-05-18 17:10:32 -0400

    change Promise.allSettled() to Promise.all() in fetchTokenLists.ts

--- src/pages/TestPage/fetchTokenLists.ts
+++ src/pages/TestPage/fetchTokenLists.ts
@@ -14,7 +14,11 @@ export function fetchTokenLists() {
                 .then((response) => ({ ...response, uri })),
         );
         // when all promises have been resolved, send to local storage
-        Promise.allSettled(tokenLists).then((results) => {
+        // IMPORTANT!  because of how `Promise.all` works, if a single
+        // ... promise is rejected, the function will not execute, if
+        // ... this is acting strangely, try changing Promise.all() to
+        // ... Promise.allSettled()
+        Promise.all(tokenLists).then((results) => {
             localStorage.setItem('allTokenLists', JSON.stringify(results));
         });
     }
