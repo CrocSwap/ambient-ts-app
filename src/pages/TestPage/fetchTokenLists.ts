c08fadecdc910dc4d6a835514714f2e1c58a1d85
mhsmith321 <marty.smith01@yahoo.com>
2022-05-18 17:07:09 -0400

    annotate code with comments in fetchTokenLists.ts file

--- src/pages/TestPage/fetchTokenLists.ts
+++ src/pages/TestPage/fetchTokenLists.ts
@@ -1,17 +1,19 @@
 import { tokenListURIs } from '../../utils/data/tokenURIs';
 
 export function fetchTokenLists() {
+    // check if the token lists are already in local storage, currently
+    // ... this is limited because there could feasibly be an empty
+    // ... value, this will be accounted for later when additional
+    // ... functionality periodically refreshes the list
     if (!window.localStorage.allTokenLists) {
-        console.log('fetching lists...');
+        // create an array of promises to fetch all token lists in the URIs file
+        // middleware will add the source URI to every return
         const tokenLists = Object.values(tokenListURIs).map((uri) =>
             fetch(uri)
-                .then((response) => {
-                    console.log(response);
-                    return response.json();
-                })
+                .then((response) => response.json())
                 .then((response) => ({ ...response, uri })),
         );
-
+        // when all promises have been resolved, send to local storage
         Promise.allSettled(tokenLists).then((results) => {
             localStorage.setItem('allTokenLists', JSON.stringify(results));
         });
