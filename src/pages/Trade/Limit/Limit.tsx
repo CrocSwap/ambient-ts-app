ab0a59e3254366430392a1ac245e14c9caac9d78
Jadessu <junioradessu@gmail.com>
2022-05-20 14:20:26 -0500

    create limit rate jsx file

--- src/pages/Trade/Limit/Limit.tsx
+++ src/pages/Trade/Limit/Limit.tsx
@@ -1,7 +1,16 @@
+import ContentContainer from '../../../components/Global/ContentContainer/ContentContainer';
+import LimitCurrencyConverter from '../../../components/Trade/Limit/LimitCurrencyConverter/LimitCurrencyConverter';
+import LimitDenominationSwitch from '../../../components/Trade/Limit/LimitDenominationSwitch/LimitDenominationSwitch';
+import LimitHeader from '../../../components/Trade/Limit/LimitHeader/LimitHeader';
+
 export default function Limit() {
     return (
         <section data-testid={'limit'}>
-            <h4>This is Limit.tsx!</h4>
+            <ContentContainer isOnTradeRoute>
+                <LimitHeader />
+                <LimitDenominationSwitch />
+                <LimitCurrencyConverter />
+            </ContentContainer>
         </section>
     );
 }
