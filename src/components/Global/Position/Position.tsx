c3e92244576df7f5dec71212046782a18c9115a2
Jadessu <junioradessu@gmail.com>
2022-05-23 14:33:42 -0500

    create positions folder and files

--- src/components/Global/Position/Position.tsx
+++ src/components/Global/Position/Position.tsx
@@ -1,9 +1,21 @@
 import styles from './Position.module.css';
 
-interface PositionProps {
-    children: React.ReactNode;
-}
+export default function Position() {
+    return (
+        <div className={styles.position_container}>
+            <div className={styles.position_info}>
+                <div className={styles.id}>0xfa05...db35</div>
+                <div className={styles.range}>2100.00 3200.00</div>
+                <div className={styles.apy}>35.65%</div>
+                <div className={styles.range_sign}>In Range</div>
+            </div>
 
-export default function Position(props: PositionProps) {
-    return <div className={styles.Position}>{props.children}</div>;
+            <div className={styles.options_container}>
+                <button className={styles.options}>Harvest</button>
+                <button className={styles.options}>Edit</button>
+                <button className={styles.options}>Remove</button>
+                <button className={styles.options}>Details</button>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/Trade/Positions/Positions.jsx
@@ -0,0 +1,5 @@
+import styles from './Positions.module.css';
+
+export default function Positions() {
+    return <div className={styles.row}></div>;
+}
