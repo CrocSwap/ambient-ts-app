53fb97ae7c4b7e4906ad7642565edd0cccb7eb45
Jadessu <junioradessu@gmail.com>
2022-05-24 10:47:13 -0500

    create limit order table

---
+++ src/components/Trade/LimitOrders/LimitOrders.tsx
@@ -0,0 +1,30 @@
+import LimitOrder from '../../LimitOrder/LimitOrder';
+import styles from './LimitOrders.module.css';
+
+export default function LimitOrders() {
+    const exampleLimitOrders = [1, 2, 3];
+
+    const LimitOrdersDisplay = exampleLimitOrders.map((order, idx) => <LimitOrder key={idx} />);
+
+    const LimitOrdersHeader = (
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Price</th>
+                <th>From</th>
+                <th>To</th>
+                <th></th>
+            </tr>
+        </thead>
+    );
+
+    return (
+        <div className={styles.posiitons_table_display}>
+            <table>
+                {LimitOrdersHeader}
+
+                <tbody>{LimitOrdersDisplay}</tbody>
+            </table>
+        </div>
+    );
+}
