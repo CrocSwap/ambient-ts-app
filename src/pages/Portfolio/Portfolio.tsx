8e4eed1c2632989935f72d9dff944cae6d880880
Jadessu <junioradessu@gmail.com>
2022-05-24 08:29:38 -0500

    finalize layout

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,10 +1,7 @@
-import Positions from '../../components/Trade/Positions/Positions';
-
 export default function Portfolio() {
     return (
         <main data-testid={'portfolio'}>
             <h1>This is Portfolio.tsx</h1>
-            <Positions />
         </main>
     );
 }
