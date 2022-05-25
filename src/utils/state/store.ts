fa3861380ec008a39e67489c9c16aa0b3128e95d
mhsmith321 <marty.smith01@yahoo.com>
2022-05-25 15:17:42 -0400

    remove counterSlice.ts from the app

--- src/utils/state/store.ts
+++ src/utils/state/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
 import tradeDataReducer from './tradeDataSlice';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         tradeData: tradeDataReducer,
     },
 });
