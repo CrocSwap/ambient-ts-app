d82e9e515c2ab14953f645c6cbd3f1f57df1e0b5
mhsmith321 <marty.smith01@yahoo.com>
2022-05-25 13:07:59 -0400

    refactor logic in initializeLocalStorage.ts to remove unnecessary `window` verbiage

--- src/App/functions/initializeLocalStorage.ts
+++ src/App/functions/initializeLocalStorage.ts
@@ -4,5 +4,8 @@ export default function initializeLocalStorage() {
     console.log('ran function initializeLocalStorage()');
 
     // fetch token lists from URIs if none are in local storage
-    if (!window.localStorage.allTokenLists) fetchTokenLists();
+    if (!localStorage.allTokenLists) fetchTokenLists();
+
+    // create the `user` object and send to local storage
+    if (!localStorage.user) localStorage.setItem('user', JSON.stringify({}));
 }
