f8433e4acaefda8cdf86749a946dcf666dd29476
mhsmith321 <marty.smith01@yahoo.com>
2022-05-25 15:14:42 -0400

    import tradeDataReducer into store.ts and put in reducer object

--- src/utils/state/store.ts
+++ src/utils/state/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+import tradeDataReducer from './tradeDataSlice';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
+        tradeData: tradeDataReducer,
     },
 });
 
