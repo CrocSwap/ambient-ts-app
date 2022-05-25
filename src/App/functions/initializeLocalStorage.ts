7aad712a9773bd45219b6f8de583904c1a4f8611
mhsmith321 <marty.smith01@yahoo.com>
2022-05-25 13:10:54 -0400

    add code to initializeLocalStorage.ts to pull user object from local storage

--- src/App/functions/initializeLocalStorage.ts
+++ src/App/functions/initializeLocalStorage.ts
@@ -8,4 +8,8 @@ export default function initializeLocalStorage() {
 
     // create the `user` object and send to local storage
     if (!localStorage.user) localStorage.setItem('user', JSON.stringify({}));
+
+    // retrieve the user object from local storage
+    // putting it there then pulling it back is necessary to prevent overwrites
+    const userData = JSON.parse(localStorage.getItem('user') as string);
 }
