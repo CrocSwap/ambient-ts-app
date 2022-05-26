7c873e42ce4c5c721b481aea6593a1a741a591b2
Jadessu <junioradessu@gmail.com>
2022-05-26 14:52:50 -0500

    create token info files

--- src/components/RangeDetails/RangeDetails.tsx
+++ src/components/RangeDetails/RangeDetails.tsx
@@ -1,8 +1,10 @@
+import RemoveRangeHeader from '../RemoveRange/RemoveRangeHeader/RemoveRangeHeader';
 import styles from './RangeDetails.module.css';
 
 export default function RangeDetails() {
     return (
         <div className={styles.remove_Range_container}>
+            <RemoveRangeHeader />
             <div className={styles.main_content}></div>
         </div>
     );
---
+++ src/components/RangeDetails/TokenInfo/TokenInfo.tsx
@@ -0,0 +1,5 @@
+import styles from './TokenIngo.module.css';
+
+export default function TokenIngo() {
+    return <div className={styles.token_info_container}></div>;
+}
