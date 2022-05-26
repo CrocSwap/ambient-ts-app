488a30c5e1b61639512c38ca5d602f7c465e2575
Jadessu <junioradessu@gmail.com>
2022-05-26 14:40:22 -0500

    create range detials files

---
+++ src/components/RangeDetails/RangeDetails.tsx
@@ -0,0 +1,9 @@
+import styles from './RangeDetails.module.css';
+
+export default function RangeDetails() {
+    return (
+        <div className={styles.remove_Range_container}>
+            <div className={styles.main_content}></div>
+        </div>
+    );
+}
