e66e787fd83260b19caca8e30fe955bd577bfd1e
Tankut Koray <tkoray@proven.com.tr>
2022-05-27 11:53:52 +0300

    Refreshing chart

--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -1,24 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import * as d3fc from 'd3fc';
 import './Chart.css';
+import { CartesianChart, Scale } from '@d3fc/d3fc-chart/src/cartesian';
 
 export default function Chart() {
     const d3Container = useRef(null);
+    const [data, setData] = useState(d3fc.randomFinancial()(50));
+    const [targets, setTargets] = useState([
+        {
+            name: 'high',
+            value: 98.42,
+        },
+        {
+            name: 'low',
+            value: 97.2,
+        },
+    ]);
+    console.log('Helooo');
+    const [chart, setChart] = useState<CartesianChart<Scale, Scale>>();
 
     useEffect(() => {
-        const data = d3fc.randomFinancial()(50);
-        const targets = [
-            {
-                name: 'high',
-                value: 98.42,
-            },
-            {
-                name: 'low',
-                value: 97.2,
-            },
-        ];
-
+        console.log('init chart');
         const yExtent = d3fc.extentLinear().accessors([(d: any) => d.high, (d: any) => d.low]);
         const xExtent = d3fc.extentDate().accessors([(d: any) => d.date]);
 
@@ -37,6 +40,28 @@ export default function Chart() {
 
         const valueFormatter = d3.format('.2f');
 
+        const drag = d3.drag().on('drag', function (event, d: any) {
+            const plotY = d3.pointer(event, d3.select('.plot-chart').select('svg'))[1];
+            console.log('y: ', plotY, ' invert: ', yScale.invert(plotY));
+
+            d.value = yScale.invert(d3.pointer(event)[1] - 26);
+            setTargets(d);
+        });
+        horizontalLine.decorate((selection: any) => {
+            selection.enter().select('g.left-handle').append('text').attr('x', 5).attr('y', -5);
+            selection.enter().select('line').attr('stroke', 'red').call(drag);
+            selection
+                .select('g.left-handle text')
+                .text((d: any) => d.name + ' - ' + valueFormatter(d.value));
+            selection
+                .on('mouseover', (event: any, data: any) => {
+                    d3.select(event.currentTarget).select('line').style('cursor', 'ns-resize');
+                })
+                .on('mouseout', (event: any, data: any) => {
+                    d3.select(event.currentTarget).select('line').style('cursor', 'default');
+                });
+        });
+
         const xAxisJoin = d3fc.dataJoin('g', 'x-axis');
         const yAxisJoin = d3fc.dataJoin('g', 'y-axis');
 
@@ -53,12 +78,19 @@ export default function Chart() {
                 }
             });
 
-        const chart = d3fc.chartCartesian(xScale, yScale).svgPlotArea(multi);
-
-        chart.xDomain(xExtent(data));
-        chart.yDomain(yExtent(data));
-        d3.select(d3Container.current).datum(data).call(chart);
+        const chrt = d3fc.chartCartesian(xScale, yScale).svgPlotArea(multi);
+        chrt.xDomain(xExtent(data));
+        chrt.yDomain(yExtent(data));
+        d3.select(d3Container.current).datum(data).call(chrt);
+        // setChart(chrt);
     }, []);
+
+    useEffect(() => {
+        console.log('Redraw');
+        if (chart != null) {
+            d3.select(d3Container.current).datum(data).call(chart);
+        }
+    }, [targets, data, chart]);
 
     return <div ref={d3Container} className='main_layout' data-testid={'chart'}></div>;
 }
