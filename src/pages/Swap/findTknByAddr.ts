45baf1796f0682dfe470380ee02f43aea9ef92a8
mhsmith321 <marty.smith01@yahoo.com>
2022-05-27 17:05:36 -0400

    delete findTknByAddr.ts file now that logic has been recreated in processTokens.ts file

--- src/pages/Swap/findTknByAddr.ts
+++
@@ -1,6 +0,0 @@
-import { TokenIF } from '../../utils/interfaces/TokenIF';
-
-export const findTknByAddr = (addr: string, tokens: Array<TokenIF>) => {
-    const tk = tokens.find((tkn: TokenIF) => tkn.address === addr);
-    return tk;
-};
