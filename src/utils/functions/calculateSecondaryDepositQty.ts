bb81733426c0e32c78c70a7522e617e1edffde05
Ben Wolski <ben@crocodilelabs.io>
2022-05-27 13:19:57 -0500

    add calculateSecondaryDepositQty.ts util function

---
+++ src/utils/functions/calculateSecondaryDepositQty.ts
@@ -0,0 +1,53 @@
+import { toDisplayPrice } from '@crocswap-libs/sdk';
+
+export const calculateSecondaryDepositQty = (
+    poolPriceNonDisplay: number, // the 'scaled' or 'wei' price of the pool
+    tokenADecimals: number,
+    tokenBDecimals: number,
+    primaryInputValueStr: string, // the token quantity entered by the user
+    isTokenAPrimary: boolean,
+    isTokenABase: boolean,
+    isAmbientPosition: boolean,
+    depositSkew?: number,
+) => {
+    const baseDecimals = isTokenABase ? tokenADecimals : tokenBDecimals;
+    const quoteDecimals = !isTokenABase ? tokenADecimals : tokenBDecimals;
+
+    const poolDisplayPrice = toDisplayPrice(poolPriceNonDisplay, baseDecimals, quoteDecimals);
+
+    const isPrimaryTokenBase =
+        (isTokenAPrimary && isTokenABase) || (!isTokenAPrimary && !isTokenABase);
+
+    console.log({ isAmbientPosition });
+    console.log({ poolDisplayPrice });
+    console.log({ isTokenAPrimary });
+    console.log({ isTokenABase });
+    console.log({ depositSkew });
+    console.log({ isPrimaryTokenBase });
+    console.log({ primaryInputValueStr });
+
+    let secondaryQuantity;
+
+    const primInputValueNum = parseFloat(primaryInputValueStr);
+
+    if (isAmbientPosition) {
+        if (isPrimaryTokenBase) {
+            secondaryQuantity = primInputValueNum / poolDisplayPrice;
+        } else {
+            secondaryQuantity = primInputValueNum / (1 / poolDisplayPrice);
+        }
+    } else {
+        if (depositSkew) {
+            if (isPrimaryTokenBase) {
+                secondaryQuantity = (primInputValueNum / poolDisplayPrice) * depositSkew;
+            } else {
+                secondaryQuantity = (primInputValueNum / (1 / poolDisplayPrice)) * depositSkew;
+            }
+        }
+    }
+
+    // if function calculates NaN or zero, return null instead
+    const output = secondaryQuantity || null;
+    // return secondary quantity
+    return output;
+};
