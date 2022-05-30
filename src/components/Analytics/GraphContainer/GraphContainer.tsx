5a0da824da600b432cd21c4d1a7e000d1052a60a
Jadessu <junioradessu@gmail.com>
2022-05-30 15:24:52 -0500

    create analytics info content

--- src/components/Analytics/GraphContainer/GraphContainer.tsx
+++ src/components/Analytics/GraphContainer/GraphContainer.tsx
@@ -1,5 +1,38 @@
 import styles from './GraphContainer.module.css';
 
 export default function GraphContainer() {
+    const timeFrame = (
+        <div className={styles.time_frame_container}>
+            <div className={styles.title}>Ambient Analytics</div>
+            <div className={styles.right_side}>
+                <span>Timeframe</span>
+                <button>1m</button>
+                <button>5m</button>
+                <button>15m</button>
+                <button>1h</button>
+                <button>4h</button>
+                <button>1d</button>
+            </div>
+        </div>
+    );
+
+    const analyticsInfo = (
+        <div className={styles.info_container}>
+            <div className={styles.info_content}>
+                <div className={styles.info_title}>Total TVL</div>
+                <div className={styles.info_value}>$1,000,000</div>
+            </div>
+
+            <div className={styles.info_content}>
+                <div className={styles.info_title}>24h Volume</div>
+                <div className={styles.info_value}>$1,000,000</div>
+            </div>
+
+            <div className={styles.info_content}>
+                <div className={styles.info_title}>24h Fees</div>
+                <div className={styles.info_value}>$1,000,000</div>
+            </div>
+        </div>
+    );
     return <div className={styles.GraphContainer}></div>;
 }
