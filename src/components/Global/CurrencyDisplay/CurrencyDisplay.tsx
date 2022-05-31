bb44ed4361c97a90d77c5159dcb276bf07e9b681
Jadessu <junioradessu@gmail.com>
2022-05-31 11:36:51 -0500

    create currency display styling

--- src/components/Global/CurrencyDisplay/CurrencyDisplay.tsx
+++ src/components/Global/CurrencyDisplay/CurrencyDisplay.tsx
@@ -1,9 +1,35 @@
-import styles from './CurrencyDisplau.module.css';
+import styles from './CurrencyDisplay.module.css';
 
-interface CurrencyDisplauProps {
-    children: React.ReactNode;
+// we will eventually get the data passed down as props through token data.
+// For now, token data is hardcoded
+interface CurrencyDisplayProps {
+    // tokenData: React.ReactNode;
+    amount: number;
+    logoLocal: string;
+    logoAltText: string;
+    symbol: string;
 }
 
-export default function CurrencyDisplau(props: CurrencyDisplauProps) {
-    return <div className={styles.CurrencyDisplau}>{props.children}</div>;
+export default function CurrencyDisplay(props: CurrencyDisplayProps) {
+    const { amount, logoAltText, logoLocal, symbol } = props;
+
+    const currencyBox = (
+        <div className={styles.modalSwapbox}>
+            <div className={styles.swapbox_top}>
+                <div className={styles.token_select}>
+                    <img
+                        src={logoLocal}
+                        width='25px'
+                        className={styles.token_list_img}
+                        alt={logoAltText}
+                    />
+                    <span className={styles.token_list_text}>{symbol}</span>
+                </div>
+                <div className={styles.token_amount}>
+                    <span className={styles.currency_quantity}>{amount}</span>
+                </div>
+            </div>
+        </div>
+    );
+    return <div className={styles.CurrencyDisplau}>{currencyBox}</div>;
 }
