573aeea8b0f9a5e629a7c4d21f4e48818fe0178d
Jadessu <junioradessu@gmail.com>
2022-06-01 10:55:01 -0500

    create jsx for token select

--- src/components/Global/TokenSelect/TokenSelect.tsx
+++ src/components/Global/TokenSelect/TokenSelect.tsx
@@ -1,9 +1,34 @@
 import styles from './TokenSelect.module.css';
+import { RiStarFill, RiStarLine } from 'react-icons/ri';
+import { CgUnavailable } from 'react-icons/cg';
 
 interface TokenSelectProps {
-    children: React.ReactNode;
+    icon: string;
+    qty: number;
+    symbol: string;
+    name: string;
 }
 
 export default function TokenSelect(props: TokenSelectProps) {
-    return <div className={styles.TokenSelect}>{props.children}</div>;
+    const { icon, qty, symbol, name } = props;
+    function getRandomInt() {
+        return Math.floor(Math.random() * 18000);
+    }
+
+    const fakeQty = getRandomInt();
+
+    const noTokenImage = <CgUnavailable size={20} />;
+
+    return (
+        <div className={styles.modal_content}>
+            <div className={styles.modal_tokens_info}>
+                <RiStarFill size={20} className={styles.star_filled} />
+                <RiStarLine size={20} className={styles.star_line} />
+                {icon ? <img src={icon} alt='' width='27px' /> : noTokenImage}
+                <span className={styles.modal_token_symbol}>{symbol}</span>
+                <span className={styles.modal_token_name}>{name}</span>
+            </div>
+            <div className={styles.modal_tokens_amount}>{fakeQty}</div>
+        </div>
+    );
 }
