f529f0f88c6c263cada61afa16a6e786e71a8bf9
mhsmith321 <marty.smith01@yahoo.com>
2022-06-02 13:05:54 -0400

    reorganize webpack import statements in LimitHeader.tsx component file

--- src/components/Trade/Limit/LimitHeader/LimitHeader.tsx
+++ src/components/Trade/Limit/LimitHeader/LimitHeader.tsx
@@ -1,6 +1,11 @@
-import styles from './LimitHeader.module.css';
+// START: Import React and Dongles
 import { FiSettings } from 'react-icons/fi';
+
+// START: Import React Functional Components
 import ContentHeader from '../../../Global/ContentHeader/ContentHeader';
+
+// START: Import Local Files
+import styles from './LimitHeader.module.css';
 import { TokenIF } from '../../../../utils/interfaces/TokenIF';
 
 interface LimitHeaderPropsIF {
