9373382b179c34d13732bf80a91b10c5cb311514
mhsmith321 <marty.smith01@yahoo.com>
2022-06-02 10:31:02 -0400

    delete Market.tsx and related files

--- src/components/Trade/Range/RangeCurrencyConverter/RangeCurrencyConverter.tsx
+++ src/components/Trade/Range/RangeCurrencyConverter/RangeCurrencyConverter.tsx
@@ -1,10 +1,10 @@
 import { ChangeEvent, SetStateAction } from 'react';
 import styles from './RangeCurrencyConverter.module.css';
 import RangeCurrencySelector from '../RangeCurrencySelector/RangeCurrencySelector';
-import { calculateSecondaryDepositQty } from '../../../../utils/functions/calculateSecondaryDepositQty';
+// import { calculateSecondaryDepositQty } from '../../../../utils/functions/calculateSecondaryDepositQty';
 import { TokenIF } from '../../../../utils/interfaces/TokenIF';
-import { useAppDispatch } from '../../../../utils/hooks/reduxToolkit';
-import { setIsTokenAPrimary, setPrimQty } from '../../../../utils/state/rangeDataSlice';
+// import { useAppDispatch } from '../../../../utils/hooks/reduxToolkit';
+// import { setIsTokenAPrimary, setPrimQty } from '../../../../utils/state/rangeDataSlice';
 
 interface RangeCurrencyConverterProps {
     isWithdrawTokenAFromDexChecked: boolean;
@@ -25,50 +25,52 @@ interface RangeCurrencyConverterProps {
 export default function RangeCurrencyConverter(props: RangeCurrencyConverterProps) {
     const {
         isLiq,
-        poolPriceNonDisplay,
-        tokenPair,
-        isTokenABase,
-        depositSkew,
+        // poolPriceNonDisplay,
+        // tokenPair,
+        // isTokenABase,
+        // depositSkew,
         isWithdrawTokenAFromDexChecked,
         setIsWithdrawTokenAFromDexChecked,
         isWithdrawTokenBFromDexChecked,
         setIsWithdrawTokenBFromDexChecked,
     } = props;
 
-    const dispatch = useAppDispatch();
+    // const dispatch = useAppDispatch();
 
     const handleChangeQtyTokenA = (evt: ChangeEvent<HTMLInputElement>) => {
-        const qtyTokenB = calculateSecondaryDepositQty(
-            poolPriceNonDisplay,
-            tokenPair.dataTokenA.decimals,
-            tokenPair.dataTokenB.decimals,
-            evt.target.value,
-            true,
-            isTokenABase,
-            false,
-            depositSkew,
-        )?.toString();
-        const fieldToUpdate = document.getElementById('buy-range-quantity') as HTMLInputElement;
-        fieldToUpdate.value = typeof qtyTokenB === 'string' ? qtyTokenB : '';
-        dispatch(setPrimQty(evt.target.value));
-        dispatch(setIsTokenAPrimary(true));
+        // const qtyTokenB = calculateSecondaryDepositQty(
+        //     poolPriceNonDisplay,
+        //     tokenPair.dataTokenA.decimals,
+        //     tokenPair.dataTokenB.decimals,
+        //     evt.target.value,
+        //     true,
+        //     isTokenABase,
+        //     false,
+        //     depositSkew,
+        // )?.toString();
+        // const fieldToUpdate = document.getElementById('buy-range-quantity') as HTMLInputElement;
+        // fieldToUpdate.value = typeof qtyTokenB === 'string' ? qtyTokenB : '';
+        // dispatch(setPrimQty(evt.target.value));
+        // dispatch(setIsTokenAPrimary(true));
+        console.log(evt);
     };
 
     const handleChangeQtyTokenB = (evt: ChangeEvent<HTMLInputElement>) => {
-        const qtyTokenA = calculateSecondaryDepositQty(
-            poolPriceNonDisplay,
-            tokenPair.dataTokenA.decimals,
-            tokenPair.dataTokenB.decimals,
-            evt.target.value,
-            false,
-            isTokenABase,
-            false,
-            depositSkew,
-        )?.toString();
-        const fieldToUpdate = document.getElementById('sell-range-quantity') as HTMLInputElement;
-        fieldToUpdate.value = typeof qtyTokenA === 'string' ? qtyTokenA : '';
-        dispatch(setPrimQty(evt.target.value));
-        dispatch(setIsTokenAPrimary(false));
+        // const qtyTokenA = calculateSecondaryDepositQty(
+        //     poolPriceNonDisplay,
+        //     tokenPair.dataTokenA.decimals,
+        //     tokenPair.dataTokenB.decimals,
+        //     evt.target.value,
+        //     false,
+        //     isTokenABase,
+        //     false,
+        //     depositSkew,
+        // )?.toString();
+        // const fieldToUpdate = document.getElementById('sell-range-quantity') as HTMLInputElement;
+        // fieldToUpdate.value = typeof qtyTokenA === 'string' ? qtyTokenA : '';
+        // dispatch(setPrimQty(evt.target.value));
+        // dispatch(setIsTokenAPrimary(false));
+        console.log(evt);
     };
     // props for <RangeCurrencyConverter/> React element
     const rangeCurrencySelectorProps = {
--- src/pages/Trade/Market/Market.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import Market from './Market';
-
-test('renders Market() React function instance', () => {
-    render(<Market />);
-    const marketElement = screen.getByTestId('market');
-    expect(marketElement).toBeInTheDocument();
-});
