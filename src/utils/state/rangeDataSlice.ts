c037e8d149783db6adf6f3f4c186070c254de134
mhsmith321 <marty.smith01@yahoo.com>
2022-06-02 12:22:09 -0400

    add commented-out list of token (ETH/DAI/USDC) to rangeDataSlice.ts to simplify development testing

--- src/utils/state/rangeDataSlice.ts
+++ src/utils/state/rangeDataSlice.ts
@@ -36,3 +36,7 @@ export const { setAddressTokenA, setAddressTokenB, setDenomInBase, toggleDenomIn
     rangeDataSlice.actions;
 
 export default rangeDataSlice.reducer;
+
+// ETH:   0x0000000000000000000000000000000000000000
+// DAI:   0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa
+// USDC:  0xb7a4F3E9097C08dA09517b5aB877F7a917224ede
