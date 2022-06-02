971fbb7b3d73247402031209cb1d43dcd15946f6
mhsmith321 <marty.smith01@yahoo.com>
2022-06-02 11:34:03 -0400

    pull current transaction data from Trade.tsx into Limit.tsx via useTradeData() hook

--- src/pages/Trade/Limit/Limit.tsx
+++ src/pages/Trade/Limit/Limit.tsx
@@ -1,13 +1,21 @@
+// START: Import React and Dongles
+import { motion } from 'framer-motion';
+
+// START: Import React Functional Components
 import ContentContainer from '../../../components/Global/ContentContainer/ContentContainer';
 import LimitButton from '../../../components/Trade/Limit/LimitButton/LimitButton';
 import LimitCurrencyConverter from '../../../components/Trade/Limit/LimitCurrencyConverter/LimitCurrencyConverter';
 import LimitDenominationSwitch from '../../../components/Trade/Limit/LimitDenominationSwitch/LimitDenominationSwitch';
 import LimitExtraInfo from '../../../components/Trade/Limit/LimitExtraInfo/LimitExtraInfo';
 import LimitHeader from '../../../components/Trade/Limit/LimitHeader/LimitHeader';
-import { motion } from 'framer-motion';
 import DividerDark from '../../../components/Global/DividerDark/DividerDark';
 
+// START: Import Local Files
+import { useTradeData } from '../Trade';
+
 export default function Limit() {
+    const { tradeData } = useTradeData();
+    console.log(tradeData);
     return (
         <motion.section
             initial={{ width: 0 }}
