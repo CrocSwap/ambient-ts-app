1cbdfd2e74723ca89fcb8f310bb8b42554c91840
Jadessu <junioradessu@gmail.com>
2022-06-02 15:02:42 -0500

    build switch buttons for range switch

---
+++ src/components/Trade/Range/ConfirmRangeModal/SelectedRange/SelectedRange.tsx
@@ -0,0 +1,32 @@
+import styles from './SelectedRange.module.css';
+import { useState } from 'react';
+
+export default function SelectedRange() {
+    const [reverseDisplay, setReverseDisplay] = useState(false);
+
+    const tokenAShortName = 'ETH';
+    const tokenBShortName = 'DAI';
+
+    const switchButtons = (
+        <div className={styles.button_container}>
+            <button
+                onClick={() => {
+                    setReverseDisplay(!reverseDisplay);
+                }}
+                className={reverseDisplay ? styles.active_button : styles.non_active_button}
+            >
+                {tokenAShortName}
+            </button>
+            <button
+                onClick={() => {
+                    setReverseDisplay(!reverseDisplay);
+                }}
+                className={reverseDisplay ? styles.non_active_button : styles.active_button}
+            >
+                {tokenBShortName}
+            </button>
+        </div>
+    );
+
+    return <div></div>;
+}
