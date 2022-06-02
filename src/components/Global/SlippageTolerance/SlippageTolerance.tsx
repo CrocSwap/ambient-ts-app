a184f37271939b7b9c8ecb42a0102543a36649dc
Jadessu <junioradessu@gmail.com>
2022-06-02 10:49:39 -0500

    add slippage tolerance jsx

--- src/components/Global/SlippageTolerance/SlippageTolerance.tsx
+++ src/components/Global/SlippageTolerance/SlippageTolerance.tsx
@@ -1,5 +1,17 @@
 import styles from './SlippageTolerance.module.css';
 
 export default function SlippageTolerance() {
-    return <div className={styles.row}></div>;
+    return (
+        <div className={styles.slippage_tolerance_container}>
+            <div className={styles.slippage_title}>Slippage tolerance</div>
+            <div className={styles.slippage_box}>
+                <div className={styles.slippage_content}>
+                    <input type='text ' placeholder='0.5' />
+                    <button>0.1%</button>
+                    <button>0.1%</button>
+                    <button>0.1%</button>
+                </div>
+            </div>
+        </div>
+    );
 }
