c9040508c5f54bf0795423c2664354d1cf88ede9
mhsmith321 <marty.smith01@yahoo.com>
2022-06-02 14:10:25 -0400

    refactor RangePriceInfo.tsx to hold individual content items from the JSX of the return statement into fragments held in variables

--- src/components/Trade/Range/RangePriceInfo/RangePriceInfo.tsx
+++ src/components/Trade/Range/RangePriceInfo/RangePriceInfo.tsx
@@ -15,36 +15,58 @@ interface IRangePriceInfoProps {
 export default function RangePriceInfo(props: IRangePriceInfoProps) {
     const { tokenPair, spotPriceDisplay, maxPriceDisplay, minPriceDisplay } = props;
 
+    const apy = <span className={styles.apy}> Est.APY | 35.65%</span>;
+
+    const minimumPrice = (
+        <div className={styles.price_display}>
+            <span className={styles.price_title}>Min Price</span>
+            <span className={styles.min_price}>{minPriceDisplay}</span>
+        </div>
+    );
+
+    const currentPrice = (
+        <div className={styles.price_display}>
+            <span className={styles.price_title}>Current Price</span>
+            <span className={styles.current_price}>{spotPriceDisplay}</span>
+        </div>
+    );
+
+    const maximumPrice = (
+        <div className={styles.price_display}>
+            <span className={styles.price_title}>Max Price</span>
+            <span className={styles.max_price}>{maxPriceDisplay}</span>
+        </div>
+    );
+
+    const collateralTokenA = (
+        <div className={styles.collateral_display}>
+            <span className={styles.collateral_title}>
+                {tokenPair.dataTokenA.symbol} Collateral
+            </span>
+            <span className={styles.collateral_amount}>1.69</span>
+        </div>
+    );
+
+    const collateralTokenB = (
+        <div className={styles.collateral_display}>
+            <span className={styles.collateral_title}>
+                {tokenPair.dataTokenB.symbol} Collateral
+            </span>
+            <span className={styles.collateral_amount}>5,000.00</span>
+        </div>
+    );
+
     return (
         <div className={styles.price_info_container}>
-            <span className={styles.apy}> Est.APY | 35.65%</span>
+            {apy}
             <div className={styles.price_info_content}>
-                <div className={styles.price_display}>
-                    <span className={styles.price_title}>Min Price</span>
-                    <span className={styles.min_price}>{minPriceDisplay}</span>
-                </div>
-                <div className={styles.price_display}>
-                    <span className={styles.price_title}>Current Price</span>
-                    <span className={styles.current_price}>{spotPriceDisplay}</span>
-                </div>
-                <div className={styles.price_display}>
-                    <span className={styles.price_title}>Max Price</span>
-                    <span className={styles.max_price}>{maxPriceDisplay}</span>
-                </div>
+                {minimumPrice}
+                {currentPrice}
+                {maximumPrice}
             </div>
             <div className={styles.collateral_container}>
-                <div className={styles.collateral_display}>
-                    <span className={styles.collateral_title}>
-                        {tokenPair.dataTokenA.symbol} Collateral
-                    </span>
-                    <span className={styles.collateral_amount}>1.69</span>
-                </div>
-                <div className={styles.collateral_display}>
-                    <span className={styles.collateral_title}>
-                        {tokenPair.dataTokenB.symbol} Collateral
-                    </span>
-                    <span className={styles.collateral_amount}>5,000.00</span>
-                </div>
+                {collateralTokenA}
+                {collateralTokenB}
             </div>
         </div>
     );
