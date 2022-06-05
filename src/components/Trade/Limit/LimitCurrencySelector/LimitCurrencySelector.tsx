bf786d29114c586dad9df31048283f817fa547de
mhsmith321 <marty.smith01@yahoo.com>
2022-06-05 19:42:42 -0400

    update LimitCurrencyConverter.tsx to use global TokenPairIF interface

--- src/components/Trade/Limit/LimitCurrencySelector/LimitCurrencySelector.tsx
+++ src/components/Trade/Limit/LimitCurrencySelector/LimitCurrencySelector.tsx
@@ -9,6 +9,7 @@ import Toggle from '../../../Global/Toggle/Toggle';
 // START: Import Local Files
 import styles from './LimitCurrencySelector.module.css';
 import { TokenIF } from '../../../../utils/interfaces/TokenIF';
+import { TokenPairIF } from '../../../../utils/interfaces/TokenPairIF';
 import Modal from '../../../../components/Global/Modal/Modal';
 import TokenSelectContainer from '../../../Global/TokenSelectContainer/TokenSelectContainer';
 import { useModal } from '../../../../components/Global/Modal/useModal';
@@ -16,10 +17,7 @@ import { getAmbientTokens } from '../../../../tempdata';
 
 // interface for component props
 interface LimitCurrencySelectorProps {
-    tokenPair: {
-        dataTokenA: TokenIF;
-        dataTokenB: TokenIF;
-    };
+    tokenPair: TokenPairIF;
     tokensBank: Array<TokenIF>;
     chainId: string;
     fieldId: string;
