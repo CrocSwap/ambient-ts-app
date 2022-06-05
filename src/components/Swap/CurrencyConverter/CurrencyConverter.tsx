e376714571590dd3e8190a007aa8b7e9248070f6
mhsmith321 <marty.smith01@yahoo.com>
2022-06-05 19:34:37 -0400

    update CurrencyConverter.tsx file to use global TokenPairIF interface

--- src/components/Swap/CurrencyConverter/CurrencyConverter.tsx
+++ src/components/Swap/CurrencyConverter/CurrencyConverter.tsx
@@ -1,15 +1,13 @@
 import { ChangeEvent, SetStateAction } from 'react';
 import styles from './CurrencyConverter.module.css';
 import CurrencySelector from '../CurrencySelector/CurrencySelector';
+import { TokenPairIF } from '../../../utils/interfaces/TokenPairIF';
 import { TokenIF } from '../../../utils/interfaces/TokenIF';
 import { setAddressTokenA, setAddressTokenB } from '../../../utils/state/tradeDataSlice';
 import { useAppDispatch } from '../../../utils/hooks/reduxToolkit';
 
 interface CurrencyConverterProps {
-    tokenPair: {
-        dataTokenA: TokenIF;
-        dataTokenB: TokenIF;
-    };
+    tokenPair: TokenPairIF;
     tokensBank: Array<TokenIF>;
     chainId: string;
     isLiq: boolean;
