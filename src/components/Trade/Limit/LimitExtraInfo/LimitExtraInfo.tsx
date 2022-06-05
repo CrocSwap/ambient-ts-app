a8e063b4bfad4753d3f33f3769a21893b8879410
mhsmith321 <marty.smith01@yahoo.com>
2022-06-05 19:43:25 -0400

    update LimitExtraInfo.tsx to use global TokenPairIF interface

--- src/components/Trade/Limit/LimitExtraInfo/LimitExtraInfo.tsx
+++ src/components/Trade/Limit/LimitExtraInfo/LimitExtraInfo.tsx
@@ -5,14 +5,11 @@ import { RiArrowDownSLine } from 'react-icons/ri';
 
 // START: Import Local Files
 import styles from './LimitExtraInfo.module.css';
-import { TokenIF } from '../../../../utils/interfaces/TokenIF';
+import { TokenPairIF } from '../../../../utils/interfaces/TokenPairIF';
 
 // interface for component props
 interface LimitExtraInfoPropsIF {
-    tokenPair: {
-        dataTokenA: TokenIF;
-        dataTokenB: TokenIF;
-    };
+    tokenPair: TokenPairIF;
     poolPriceDisplay?: number;
     slippageTolerance?: number;
     liquidityProviderFee?: number;
