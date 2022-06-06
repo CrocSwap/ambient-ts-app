d647d340a96a10f23cb76b0b5f2e582421d649e4
mhsmith321 <marty.smith01@yahoo.com>
2022-06-05 21:29:54 -0400

    reorganize webpack import statements at the top of RangeHeader.tsx component file

--- src/components/Trade/Range/RangeHeader/RangeHeader.tsx
+++ src/components/Trade/Range/RangeHeader/RangeHeader.tsx
@@ -1,13 +1,19 @@
-
-import styles from './RangeHeader.module.css';
+// START: Import React and Dongles
 import { FiSettings } from 'react-icons/fi';
+
+// START: Import React Functional Components
 import ContentHeader from '../../../Global/ContentHeader/ContentHeader';
+
+// START: Import Local Files
+import styles from './RangeHeader.module.css';
 import { TokenPairIF } from '../../../../utils/interfaces/exports';
 
+// interface for component props
 interface RangeHeaderPropsIF {
     tokenPair: TokenPairIF;
 }
 
+// central react functional component
 export default function RangeHeader(props: RangeHeaderPropsIF) {
     const { tokenPair } = props;
 
