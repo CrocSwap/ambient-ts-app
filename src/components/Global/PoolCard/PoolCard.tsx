dd75075d4063d3e167b967d9f1b6af2127adb0aa
Jadessu <junioradessu@gmail.com>
2022-06-06 15:26:33 -0500

    finish pool card

--- src/components/Global/PoolCard/PoolCard.tsx
+++ src/components/Global/PoolCard/PoolCard.tsx
@@ -1,5 +1,44 @@
 import styles from './PoolCard.module.css';
 
 export default function PoolCard() {
-    return <div className={styles.row}></div>;
+    return (
+        <div className={styles.pool_card}>
+            <div className={styles.row}>
+                <div>
+                    <img
+                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png'
+                        alt=''
+                    />
+                    <img
+                        src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png'
+                        alt=''
+                    />
+                </div>
+                <div className={styles.tokens_name}>ETH / USDC</div>
+            </div>
+
+            <div className={styles.row}>
+                <div></div>
+                <div>
+                    <div className={styles.row_title}>APY</div>
+                    <div className={styles.apy}>35.68%</div>
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div></div>
+                <div>
+                    <div className={styles.row_title}>Vol.</div>
+                    <div className={styles.vol}>$62m</div>
+                </div>
+            </div>
+
+            <div className={styles.row}>
+                <div className={styles.price}>$2,681.00</div>
+                <div>
+                    <div className={styles.row_title}>24h</div>
+                    <div className={styles.hours}>1.54%</div>
+                </div>
+            </div>
+        </div>
+    );
 }
