5401ff198e0ee57d2ae65c57057ee08111f0aefb
mhsmith321 <marty.smith01@yahoo.com>
2022-06-06 15:25:02 -0400

    refactor initializeLocalStorage.ts to address issue with app crashing on first initialization due to race condition

--- src/App/functions/initializeLocalStorage.ts
+++ src/App/functions/initializeLocalStorage.ts
@@ -14,12 +14,9 @@ export default function initializeLocalStorage() {
     // fetch token lists from URIs if none are in local storage
     if (!localStorage.allTokenLists) fetchTokenLists();
 
-    // create the `user` object and send to local storage
-    if (!localStorage.user) localStorage.setItem('user', JSON.stringify({}));
-
     // retrieve the user object from local storage
     // putting it there then pulling it back is necessary to prevent overwrites
-    const user = JSON.parse(localStorage.getItem('user') as string);
+    const user = localStorage.user ? JSON.parse(localStorage.getItem('user') as string) : {};
 
     // if user object does not have active token lists, initialize with ambient
     if (!user.activeTokenLists || !user.activeTokenLists.length) {
