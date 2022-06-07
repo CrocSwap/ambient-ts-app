ad60a473126ac8fd54c3c36cf17f544693be5ed9
Jadessu <junioradessu@gmail.com>
2022-06-07 14:19:00 -0500

    create edit route files

---
+++ src/pages/Trade/Edit/Edit.tsx
@@ -0,0 +1,9 @@
+import styles from './Edit.module.css';
+
+// interface EditProps {
+//     children: React.ReactNode;
+// }
+
+export default function Edit() {
+    return <div className={styles.Edit}></div>;
+}
