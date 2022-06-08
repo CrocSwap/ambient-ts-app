7ac14b26f5dd776ea6c28ce88d43a4e8e44faee7
mhsmith321 <marty.smith01@yahoo.com>
2022-06-08 16:05:08 -0400

    refactor structure of getRandomInt() function call in TokenSelect.tsx file

--- src/components/Global/TokenSelect/TokenSelect.tsx
+++ src/components/Global/TokenSelect/TokenSelect.tsx
@@ -1,9 +1,8 @@
 import styles from './TokenSelect.module.css';
 import { RiStarFill, RiStarLine } from 'react-icons/ri';
 import { CgUnavailable } from 'react-icons/cg';
-import { setAddressTokenA, setAddressTokenB } from '../../../utils/state/tradeDataSlice';
+import { setTokenA, setTokenB } from '../../../utils/state/tradeDataSlice';
 import { useAppDispatch } from '../../../utils/hooks/reduxToolkit';
-// import { TokenIF } from '../../../utils/interfaces/TokenIF';
 import { SetStateAction } from 'react';
 import { TokenIF, TokenPairIF } from '../../../utils/interfaces/exports';
 
@@ -17,32 +16,38 @@ interface TokenSelectProps {
 
 export default function TokenSelect(props: TokenSelectProps) {
     const { token, tokenToUpdate, closeModal, tokenPair, setIsReversalInProgress } = props;
-    function getRandomInt() {
-        return Math.floor(Math.random() * 18000);
-    }
 
     const dispatch = useAppDispatch();
 
-    const fakeQty = getRandomInt();
+    const getRandomInt = () => Math.floor(Math.random() * 18000);
 
     const noTokenImage = <CgUnavailable size={20} />;
 
+    // TODO: @Emily refactor the control flow here for simplicity
+    // TODO: ... we also risk race conditions depending on the
+    // TODO: ... sequence of dispatches
     const handleClick = (): void => {
         if (tokenToUpdate === 'A') {
             if (tokenPair.dataTokenB.address === token.address) {
                 setIsReversalInProgress(true);
-                dispatch(setAddressTokenA(token.address));
-                dispatch(setAddressTokenB(tokenPair.dataTokenA.address));
+                // dispatch(setAddressTokenA(token.address));
+                dispatch(setTokenA(token));
+                // dispatch(setAddressTokenB(tokenPair.dataTokenA.address));
+                dispatch(setTokenB(tokenPair.dataTokenA));
             } else {
-                dispatch(setAddressTokenA(token.address));
+                // dispatch(setAddressTokenA(token.address));
+                dispatch(setTokenA(token));
             }
         } else if (tokenToUpdate === 'B') {
             if (tokenPair.dataTokenA.address === token.address) {
                 setIsReversalInProgress(true);
-                dispatch(setAddressTokenB(token.address));
-                dispatch(setAddressTokenA(tokenPair.dataTokenB.address));
+                // dispatch(setAddressTokenB(token.address));
+                dispatch(setTokenB(token));
+                // dispatch(setAddressTokenA(tokenPair.dataTokenB.address));
+                dispatch(setTokenA(tokenPair.dataTokenB));
             } else {
-                dispatch(setAddressTokenB(token.address));
+                // dispatch(setAddressTokenB(token.address));
+                dispatch(setTokenB(token));
             }
         } else {
             console.warn('Error in TokenSelect.tsx, failed to find proper dispatch function.');
@@ -59,7 +64,7 @@ export default function TokenSelect(props: TokenSelectProps) {
                 <span className={styles.modal_token_symbol}>{token.symbol}</span>
                 <span className={styles.modal_token_name}>{token.name}</span>
             </div>
-            <div className={styles.modal_tokens_amount}>{fakeQty}</div>
+            <div className={styles.modal_tokens_amount}>{getRandomInt()}</div>
         </div>
     );
 }
