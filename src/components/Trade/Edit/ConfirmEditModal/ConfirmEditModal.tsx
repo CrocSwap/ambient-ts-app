0601a710733678dafad9bc74f61594ebbb44121b
Jadessu <junioradessu@gmail.com>
2022-06-09 16:07:08 -0500

    made changes

---
+++ src/components/Trade/Edit/ConfirmEditModal/ConfirmEditModal.tsx
@@ -0,0 +1,29 @@
+import styles from './ConfirmEditModal.module.css';
+import Button from '../../../Global/Button/Button';
+import CurrencyDisplayContainer from '../CurrencyDisplayContainer/CurrencyDisplayContainer';
+import Divider from '../../../Global/Divider/Divider';
+import EditPriceInfo from '../EditPriceInfo/EditPriceInfo';
+
+interface ConfirmEditModalProps {
+    onClose: () => void;
+}
+
+export default function ConfirmEditModal(props: ConfirmEditModalProps) {
+    const closeButton = <Button title='Close' action={props.onClose} />;
+
+    const fullTxDetails = (
+        <div>
+            <CurrencyDisplayContainer />
+            <Divider />
+            <EditPriceInfo />
+        </div>
+    );
+
+    const modal = (
+        <div className={styles.modal_container}>
+            <section className={styles.modal_content}>{fullTxDetails}</section>
+            <footer className={styles.modal_footer}>{closeButton}</footer>
+        </div>
+    );
+    return <>{modal}</>;
+}
