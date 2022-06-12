23f6484288f53f74ddd83fcc8265df3b438544e1
Tankut Koray <tkoray@proven.com.tr>
2022-06-12 17:47:21 +0300

    New structure for chart

--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -1,21 +1,24 @@
-import { DetailedHTMLProps, HTMLAttributes, useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import * as d3fc from 'd3fc';
+import { DetailedHTMLProps, HTMLAttributes, useEffect, useRef, useState } from 'react';
 import './Chart.css';
-import { CartesianChart, Scale } from '@d3fc/d3fc-chart/src/cartesian';
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace JSX {
         interface IntrinsicElements {
-            ['d3fc-group']: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
-            ['d3fc-svg']: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+            'd3fc-group': DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+            'd3fc-svg': DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
         }
     }
 }
 
 export default function Chart() {
     const d3Container = useRef(null);
+    const d3PlotArea = useRef(null);
+    const d3Xaxis = useRef(null);
+    const d3Yaxis = useRef(null);
+
     const [data, setData] = useState(d3fc.randomFinancial()(50));
     const [targets, setTargets] = useState([
         {
@@ -31,23 +34,35 @@ export default function Chart() {
 
     useEffect(() => {
         console.log('update chart');
+        const container = d3Container.current;
         const yExtent = d3fc.extentLinear().accessors([(d: any) => d.high, (d: any) => d.low]);
         const xExtent = d3fc.extentDate().accessors([(d: any) => d.date]);
 
         const xScale = d3.scaleTime();
         const yScale = d3.scaleLinear();
 
-        const gridlines = d3fc.annotationSvgGridline();
-        const candlestick = d3fc.seriesSvgCandlestick().decorate((selection: any) => {
-            selection
-                .enter()
-                .style('fill', (d: any) => (d.close > d.open ? '#7371FC' : '#CDC1FF'))
-                .style('stroke', (d: any) => (d.close > d.open ? '#7371FC' : '#CDC1FF'));
-        });
+        xScale.domain(xExtent(data));
+        yScale.domain(yExtent(data));
+
+        // axes
+        const xAxis = d3fc.axisBottom().scale(xScale);
+        const yAxis = d3fc.axisRight().scale(yScale);
+
+        const gridlines = d3fc.annotationSvgGridline().xScale(xScale).yScale(yScale);
+        const candlestick = d3fc
+            .seriesSvgCandlestick()
+            .decorate((selection: any) => {
+                selection
+                    .enter()
+                    .style('fill', (d: any) => (d.close > d.open ? '#7371FC' : '#CDC1FF'))
+                    .style('stroke', (d: any) => (d.close > d.open ? '#7371FC' : '#CDC1FF'));
+            })
+            .xScale(xScale)
+            .yScale(yScale);
 
         const barSeries = d3fc
             .autoBandwidth(d3fc.seriesSvgBar())
-            // .crossValue(d:any => d.tag)
+            // .crossValue(d: any => d.tag)
             .align('left')
             .orient('horizontal')
             .key((d: any) => d.high)
@@ -94,47 +109,77 @@ export default function Chart() {
         });
 
         const xAxisJoin = d3fc.dataJoin('g', 'x-axis');
+        const gridJoin = d3fc.dataJoin('g', 'grid');
+        const candleJoin = d3fc.dataJoin('g', 'candle');
         const yAxisJoin = d3fc.dataJoin('g', 'y-axis');
+        const barJoin = d3fc.dataJoin('g', 'bar');
 
-        const multi = d3fc
-            .seriesSvgMulti()
-            .series([gridlines, candlestick, horizontalLine, barSeries])
-            .mapping((dta: any, index: number, series: any[]) => {
-                switch (series[index]) {
-                    case gridlines:
-                    case candlestick:
-                    case barSeries:
-                        return dta;
-                    case horizontalLine:
-                        return targets;
-                }
-            });
+        // const multi = d3fc
+        //     .seriesSvgMulti()
+        //     .series([gridlines, candlestick, horizontalLine, barSeries])
+        //     .mapping((dta: any, index: number, series: any[]) => {
+        //         switch (series[index]) {
+        //             case gridlines:
+        //             case candlestick:
+        //             case barSeries:
+        //                 return dta;
+        //             case horizontalLine:
+        //                 return targets;
+        //         }
+        //     });
 
-        const chart = d3fc.chartCartesian(xScale, yScale).svgPlotArea(multi);
-        chart.xDomain(xExtent(data));
-        chart.yDomain(yExtent(data));
-        d3.select(d3Container.current).datum(data).call(chart);
+        // handle the plot area measure event in order to compute the scale ranges
+        d3.select(d3PlotArea.current).on('measure', function (event: any) {
+            xScale.range([0, event.detail.width]);
+            yScale.range([event.detail.height, 0]);
+        });
+
+        d3.select(d3PlotArea.current).on('draw', function (event: any) {
+            const svg = d3.select(event.target).select('svg');
+
+            gridJoin(svg, [data]).call(gridlines);
+
+            candleJoin(svg, [data]).call(candlestick);
+
+            barJoin(svg, [data]).call(barSeries);
+        });
+
+        d3.select(d3Xaxis.current).on('draw', function (event: any) {
+            d3.select(event.target).select('svg').call(xAxis);
+        });
+
+        d3.select(d3Yaxis.current).on('draw', function (event: any) {
+            d3.select(event.target).select('svg').call(yAxis);
+        });
+        const nd = d3.select('#group').node()! as any;
+        nd.requestRedraw();
+        // const chart = d3fc.chartCartesian(xScale, yScale).svgPlotArea(multi);
+        // chart.xDomain(xExtent(data));
+        // chart.yDomain(yExtent(data));
+        // d3.select(d3Container.current).datum(data).call(chart);
     }, [targets, data]);
 
     return (
         <div ref={d3Container} className='main_layout' data-testid={'chart'}>
             <d3fc-group
                 id='group'
+                className='hellooo'
                 style={{ display: 'flex', height: '100%', width: '100%', flexDirection: 'column' }}
                 auto-resize
             >
                 <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-                    <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-                        <d3fc-svg className='close-axis' style={{ width: '3em' }}></d3fc-svg>
+                    <div style={{ flex: 1, display: 'flex', flexDirection: 'row' }}>
                         <d3fc-svg
+                            ref={d3PlotArea}
                             className='plot-area'
                             style={{ flex: 1, overflow: 'hidden' }}
                         ></d3fc-svg>
-                        <d3fc-svg className='open-axis' style={{ width: '3em' }}></d3fc-svg>
+                        <d3fc-svg ref={d3Yaxis} style={{ width: '3em' }}></d3fc-svg>
                     </div>
                     <d3fc-svg
+                        ref={d3Xaxis}
                         className='x-axis'
-                        style={{ height: '2em', marginLeft: '3em', marginRight: '3em' }}
+                        style={{ height: '2em', marginRight: '3em' }}
                     ></d3fc-svg>
                 </div>
             </d3fc-group>
