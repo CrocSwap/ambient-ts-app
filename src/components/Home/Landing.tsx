f7cc7ffa8473478d7de7b9f90fd302ad874ef0b4
Jadessu <junioradessu@gmail.com>
2022-06-13 12:47:18 -0500

    build landing page structure

--- src/components/Home/Landing.tsx
+++ src/components/Home/Landing.tsx
@@ -1,5 +1,17 @@
 import styles from './Landing.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Landing() {
-    return <div className={styles.row}></div>;
+    return (
+        <div className={styles.main_container}>
+            <div className={styles.content_container}>
+                <div className={styles.ambient}>ambient</div>
+                <Link to={'/trade'}>
+                    <button className={styles.action_button}>
+                        <span>Trade now</span>
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,36 +1,12 @@
+import Landing from '../../components/Home/Landing';
 import styles from './Home.module.css';
 
 export default function Home() {
-    const ambientText = (
-        <div className={styles.text_container}>
-            <div className={`${styles.sign} ${styles.light}`} id='one'>
-                A
-            </div>
-            <div className={`${styles.sign} ${styles.light}`} id='two'>
-                M
-            </div>
-            <div className={`${styles.sign} ${styles.light}`} id='three'>
-                B
-            </div>
-            <div className={`${styles.sign} ${styles.light}`} id='four'>
-                I
-            </div>
-            <div className={`${styles.sign} ${styles.non_light}`} id='four'>
-                E
-            </div>
-            <div className={`${styles.sign} ${styles.non_light}`} id='four'>
-                N
-            </div>
-            <div className={`${styles.sign} ${styles.non_light}`} id='four'>
-                T
-            </div>
-        </div>
-    );
-
     return (
         <main data-testid={'home'} className={styles.home_container}>
             {/* {ambientImage} */}
-            {ambientText}
+            {/* {ambientText} */}
+            <Landing />
         </main>
     );
 }
