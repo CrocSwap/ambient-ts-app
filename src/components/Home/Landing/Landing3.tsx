38b462e8572c63d4acb596cc109f2846d82f11d3
Jadessu <junioradessu@gmail.com>
2022-06-15 13:10:47 -0500

    create landing 3 files

---
+++ src/components/Home/Landing/Landing3.tsx
@@ -0,0 +1,9 @@
+import styles from './Landing3.module.css';
+
+export default function Landing3() {
+    return (
+        <div className={styles.main_container}>
+            <div className={styles.content_container}></div>
+        </div>
+    );
+}
