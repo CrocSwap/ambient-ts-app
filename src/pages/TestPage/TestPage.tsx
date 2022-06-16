4393e6db0fe7615fa68ed7148b54827e800fff82
mhsmith321 <marty.smith01@yahoo.com>
2022-06-16 13:51:38 -0400

    create logic in TestPage.tsx to parse allTokenLists from storage and map over it to make data objects to make active list toggles

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,51 +1,20 @@
-import SnackbarComponent from '../../components/Global/SnackbarComponent/SnackbarComponent';
-import { useState } from 'react';
-import TooltipComponent from '../../components/Global/TooltipComponent/TooltipComponent';
-import PoolCard from '../../components/Global/PoolCard/PoolCard';
-import Stats from '../../components/Home/Stats/AmbientStats';
-// import Carousel from '../../components/Global/Carousel/Carousel';
-// import CarouselItem from '../../components/Global/Carousel/CarouselItem/CarouselItem';
-import Landing2 from '../../components/Home/Landing/Landing2';
-import Landing from '../../components/Home/Landing/Landing';
-import Landing3 from '../../components/Home/Landing/Landing3';
-import Landing4 from '../../components/Home/Landing/Landing4';
-import Landing5 from '../../components/Home/Landing/Landing5';
-import Landing6 from '../../components/Home/Landing/Landing6';
+import { TokenListIF } from '../../utils/interfaces/exports';
 
 export default function TestPage() {
-    const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
+
+    const allTokenListMeta = JSON.parse(localStorage.getItem('allTokenLists') as string)
+        .map((list:TokenListIF) => (
+            {
+                name: list.name,
+                uri: list.uri,
+                isDefault: list.default
+            }
+        ));
+        console.log(allTokenListMeta);
 
     return (
         <main>
-            <button onClick={() => setOpenSnackbar(true)}>open me</button>
-            <h3>This is TestPage.tsx</h3>
-            <SnackbarComponent
-                severity='success'
-                setOpenSnackbar={setOpenSnackbar}
-                openSnackbar={openSnackbar}
-            >
-                I am snackbar
-            </SnackbarComponent>
-            <TooltipComponent title='Snackbar title' />
-            <PoolCard />
-            <Stats />
-            {/* <Carousel>
-                <CarouselItem height={'526px'}>
-                    <Landing />
-                </CarouselItem>
-                <CarouselItem height={'526px'}>
-                    <Landing2 />
-                </CarouselItem>
-                <CarouselItem height={'526px'}>
-                    <Landing />
-                </CarouselItem>
-            </Carousel> */}
-            <Landing />
-            <Landing2 />
-            <Landing3 />
-            <Landing4 />
-            <Landing5 />
-            <Landing6 />
+
         </main>
     );
 }
