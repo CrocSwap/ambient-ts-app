e0869ce93f369f5a8d9de6e31f4eeacadca31660
Jadessu <junioradessu@gmail.com>
2022-06-16 14:42:27 -0500

    create jsx for token list card

--- src/components/Global/TokenListCard/TokenListCard.tsx
+++ src/components/Global/TokenListCard/TokenListCard.tsx
@@ -8,5 +8,44 @@ interface TokenListCardProps {
 }
 
 export default function TokenListCard(props: TokenListCardProps) {
-    return <div className={styles.row}>{props.children}</div>;
+    const listIsActive = false;
+    const [isChecked, setIsChecked] = useState(listIsActive);
+
+    const cardBackground = isChecked ? 'red' : '#363535';
+
+    function handleToggleAction() {
+        // handleAlert()
+        setIsChecked(!isChecked);
+    }
+
+    return (
+        <div className={styles.token_list_card_container}>
+            <div
+                className={styles.token_list_card_content}
+                style={{ backgroundColor: cardBackground }}
+            >
+                <div className={styles.left_content}>
+                    <img
+                        src='https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F40A-crocodile-512.png'
+                        alt='token list icon'
+                        width='40px'
+                    />
+                    <div className={styles.token_list_card_name}>
+                        <span> CrocSwap Interface</span>
+                        <div className={styles.token_count}>{21} tokens</div>
+                    </div>
+                </div>
+                <div className={styles.right_content}>
+                    <Toggle
+                        isOn={isChecked}
+                        handleToggle={handleToggleAction}
+                        buttonColor={isChecked ? 'blue' : '#565a69'}
+                        Width={50}
+                        id='ambientId'
+                        onColor={isChecked ? 'black' : '#212429'}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
