fb15dc5e4eb33515c95f7fcfb540bd4cb5988af8
mhsmith321 <marty.smith01@yahoo.com>
2022-06-16 14:11:08 -0400

    refactor TestPage.tsx to generate <li> elements mapping over token lists instead of data objects

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,22 +1,28 @@
 import { TokenListIF } from '../../utils/interfaces/exports';
+import styles from './TestPage.module.css';
 
 export default function TestPage() {
 
-    const { activeTokenLists } = JSON.parse(localStorage.getItem('user') as string);
+    // const { activeTokenLists } = JSON.parse(localStorage.getItem('user') as string);
 
     const allTokenListMeta = JSON.parse(localStorage.getItem('allTokenLists') as string)
         .map((list:TokenListIF) => (
-            {
-                name: list.name,
-                uri: list.uri,
-                isActive: activeTokenLists.includes(list.uri)
-            }
+            // {
+            //     name: list.name,
+            //     uri: list.uri,
+            //     isActive: activeTokenLists.includes(list.uri)
+            // }
+            <li key={`token-list-toggle-${list.uri}`} className={styles.token_list_li}>
+                <h4>{list.name}</h4>
+                
+            </li>
         ));
-    console.log(allTokenListMeta);
 
     return (
-        <main>
-
+        <main className={styles.test_page_main}>
+            <ul className={styles.token_list_selects_ul}>
+                {allTokenListMeta}
+            </ul>
         </main>
     );
 }
