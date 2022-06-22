de2da28a19e15fa59e2abc44e77bb6b7e1a9f9d8
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2022-06-22 14:29:51 +0300

    pool row token images added

--- src/components/Pools/PoolRow.tsx
+++ src/components/Pools/PoolRow.tsx
@@ -1,6 +1,6 @@
 import { Badge } from '@material-ui/core';
 import { PoolData } from '../../state/pools/reducer';
-import { feeTierPercent } from '../../utils';
+import { feeTierPercent, isAddress } from '../../utils';
 import { formatDollarAmount } from '../../utils/numbers';
 import styles from './Pool.module.css';
 
@@ -11,13 +11,45 @@ interface PoolProps {
 
 export default function PoolRow(props: PoolProps) {
     const poolData = props.pool;
+
+    function getTokenLogoURL(address: string) {
+        const checkSummed = isAddress(address);
+        return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${checkSummed}/logo.png`;
+    }
+
+    const tokenImages = (
+        <>
+            <td data-column='tokens' className={styles.tokens}>
+                <img
+                    src={getTokenLogoURL(poolData.token0.address)}
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = '/static/media/ambient_logo.55c57a31.svg';
+                    }}
+                    alt='token'
+                    width='30px'
+                />
+                <img
+                    src={getTokenLogoURL(poolData.token1.address)}
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = '/static/media/ambient_logo.55c57a31.svg';
+                    }}
+                    alt='token'
+                    width='30px'
+                />
+                {poolData.token0.symbol}/{poolData.token1.symbol}
+            </td>
+        </>
+    );
+
     return (
         <tr>
             <td data-column='id' className={styles.pool_id}>
                 {props.index}
             </td>
             <td data-column='name' className={styles.pool_range}>
-                {poolData.token0.symbol}/{poolData.token1.symbol}
+                {tokenImages}
                 <Badge>{feeTierPercent(poolData.feeTier)}</Badge>
             </td>
             <td></td>
