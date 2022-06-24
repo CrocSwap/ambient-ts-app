12818824077e8bdf770ca3d216898720a795eb0f
Ben Wolski <benwolski@gmail.com>
2022-06-24 13:43:46 -0400

    add dispatch to RTK on submit

--- src/components/Global/SlippageTolerance/SlippageTolerance.tsx
+++ src/components/Global/SlippageTolerance/SlippageTolerance.tsx
@@ -1,13 +1,21 @@
 import styles from './SlippageTolerance.module.css';
 
-export default function SlippageTolerance() {
+interface TransactionSettingsProps {
+    setSlippageInput: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function SlippageTolerance(props: TransactionSettingsProps) {
     return (
         <div className={styles.slippage_tolerance_container}>
             <div className={styles.slippage_title}>Slippage tolerance</div>
             <div className={styles.slippage_box}>
                 <div className={styles.slippage_content}>
                     <div className={styles.input_container}>
-                        <input type='text ' placeholder='0.5' />
+                        <input
+                            onChange={(e) => props.setSlippageInput(parseFloat(e.target.value))}
+                            type='text '
+                            placeholder='0.5'
+                        />
                         {/* <div>%</div> */}
                     </div>
                     <button>0.1%</button>
--- src/components/Global/TransactionSettings/TransactionSettings.tsx
+++ src/components/Global/TransactionSettings/TransactionSettings.tsx
@@ -2,16 +2,33 @@ import styles from './TransactionSettings.module.css';
 
 import SlippageTolerance from '../SlippageTolerance/SlippageTolerance';
 
+import { useAppDispatch, useAppSelector } from '../../../utils/hooks/reduxToolkit';
+import { useState } from 'react';
+import { setSlippageTolerance } from '../../../utils/state/tradeDataSlice';
+
 interface TransactionSettingsProps {
     onClose: () => void;
 }
 
 export default function TransactionSettings(props: TransactionSettingsProps) {
+    const dispatch = useAppDispatch();
+
+    const tradeData = useAppSelector((state) => state.tradeData);
+
+    const slippageToleranceInRTK = tradeData.slippageTolerance;
+
+    const [slippageInput, setSlippageInput] = useState(slippageToleranceInRTK);
+
+    const handleClose = () => {
+        props.onClose();
+        dispatch(setSlippageTolerance(slippageInput));
+    };
+
     return (
         <div className={styles.settings_container}>
             <div className={styles.settings_title}>Settings</div>
-            <SlippageTolerance />
-            <button onClick={props.onClose}>Submit</button>
+            <SlippageTolerance setSlippageInput={setSlippageInput} />
+            <button onClick={handleClose}>Submit</button>
         </div>
     );
 }
