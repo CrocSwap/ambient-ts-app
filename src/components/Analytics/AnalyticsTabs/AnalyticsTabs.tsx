4243f009a984ce8d0827fb12a6b43dd2ce5bed60
Tankut Koray <tkoray@proven.com.tr>
2022-06-30 16:19:13 +0300

    Fixed unused import

--- src/components/Analytics/AnalyticsTabs/AnalyticsTabs.tsx
+++ src/components/Analytics/AnalyticsTabs/AnalyticsTabs.tsx
@@ -11,7 +11,6 @@ import { notEmpty } from '../../../utils';
 import { useAllPoolData } from '../../../state/pools/hooks';
 import { TokenData } from '../../../state/tokens/models';
 import { PoolData } from '../../../state/pools/models';
-import { useAppSelector } from '../../../utils/hooks/reduxToolkit';
 
 export default function AnalyticsTabs() {
     const [activeTab, setActiveTab] = useState('tab1');
