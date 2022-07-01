dd9b4d64053ed29c29eecaa046b4e6e1271aff78
Ben Wolski <ben@crocodilelabs.io>
2022-07-01 00:00:29 -0400

    add button to add buy token to metamask

--- src/components/Global/TransactionSubmitted/TransactionSubmitted.tsx
+++ src/components/Global/TransactionSubmitted/TransactionSubmitted.tsx
@@ -1,14 +1,26 @@
 import styles from './TransactionSubmitted.module.css';
 import Animation from '../../Global/Animation/Animation';
 import completed from '../../../assets/animations/completed.json';
+import addTokenToWallet from './addTokenToWallet';
+import Button from '../../Global/Button/Button';
 
 interface TransactionSubmittedProps {
     hash: string;
+    tokenBAddress: string;
+    tokenBSymbol: string;
+    tokenBDecimals: number;
+    tokenBImage: string;
 }
 
 export default function TransactionSubmitted(props: TransactionSubmittedProps) {
-    const { hash } = props;
+    const { hash, tokenBAddress, tokenBSymbol, tokenBDecimals, tokenBImage } = props;
     const EthersanTx = `https://kovan.etherscan.io/tx/${hash}`;
+
+    const logoURI = tokenBImage;
+
+    const handleAddToMetamask = async () => {
+        await addTokenToWallet(tokenBAddress, tokenBSymbol, tokenBDecimals, logoURI);
+    };
 
     return (
         <div className={styles.transaction_submitted}>
@@ -20,6 +32,12 @@ export default function TransactionSubmitted(props: TransactionSubmittedProps) {
                 <a href={EthersanTx} target='_blank' rel='noreferrer'>
                     View on Etherscan
                 </a>
+                <Button
+                    title={`Add ${tokenBSymbol} to Metamask`}
+                    // action={props.onClickFn}
+                    action={handleAddToMetamask}
+                    disabled={false}
+                ></Button>
             </p>
         </div>
     );
