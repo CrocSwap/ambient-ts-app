0ed5dc7200362270f36bbe0c62a6175ecca138e3
Jadessu <junioradessu@gmail.com>
2022-07-01 15:46:41 -0500

    refactor scrolling on choose token modal

--- src/components/Global/TokenSelectContainer/TokenSelectContainer.tsx
+++ src/components/Global/TokenSelectContainer/TokenSelectContainer.tsx
@@ -36,35 +36,35 @@ export default function TokenSelectContainer(props: TokenSelectContainerPropsIF)
         showManageTokenListContent,
         setShowManageTokenListContent,
         activeTokenListsChanged,
-        indicateActiveTokenListsChanged
+        indicateActiveTokenListsChanged,
     } = props;
 
-    const [
-        matchingImportedTokens,
-        matchingSearchableTokens,
-        setSearchInput
-    ] = useSearch(tokensBank, searchableTokens, chainId);
+    const [matchingImportedTokens, matchingSearchableTokens, setSearchInput] = useSearch(
+        tokensBank,
+        searchableTokens,
+        chainId,
+    );
 
-    const handleClickSearchable = (tkn:TokenIF) => {
+    const handleClickSearchable = (tkn: TokenIF) => {
         // look inside tokensBank to see if clicked token is already imported
-        const importedTokenAddresses = tokensBank.map((token: TokenIF) => token.address)
+        const importedTokenAddresses = tokensBank.map((token: TokenIF) => token.address);
         const newImportedTokensArray = importedTokenAddresses.includes(tkn.address)
-            // TRUE: make new array with it removed
-            ? tokensBank.filter((token:TokenIF) => token.address !== tkn.address)
-            // FALSE: make new array with it added
-            : [tkn, ...tokensBank];
+            ? // TRUE: make new array with it removed
+              tokensBank.filter((token: TokenIF) => token.address !== tkn.address)
+            : // FALSE: make new array with it added
+              [tkn, ...tokensBank];
         // sync local storage and local state inside App.tsx with new array
         const userData = JSON.parse(localStorage.getItem('user') as string);
         userData.tokens = newImportedTokensArray;
         localStorage.setItem('user', JSON.stringify(userData));
         setImportedTokens(newImportedTokensArray);
-    }
+    };
 
     const tokenListContent = (
         <>
             <h3>Your Tokens</h3>
-            {matchingImportedTokens
-                .map((token:TokenIF, idx:number) => {
+            <div className={styles.tokens_container}>
+                {matchingImportedTokens.map((token: TokenIF, idx: number) => {
                     return (
                         <TokenSelect
                             key={idx}
@@ -75,9 +75,10 @@ export default function TokenSelectContainer(props: TokenSelectContainerPropsIF)
                             reverseTokens={reverseTokens}
                         />
                     );
-            })}
+                })}
+            </div>
             {matchingSearchableTokens.length ? <h3>Searched Tokens</h3> : null}
-            {matchingSearchableTokens.map((tkn:TokenIF, idx:number) => (
+            {matchingSearchableTokens.map((tkn: TokenIF, idx: number) => (
                 <TokenSelectSearchable
                     key={`tss_${idx}`}
                     token={tkn}
@@ -103,12 +104,14 @@ export default function TokenSelectContainer(props: TokenSelectContainerPropsIF)
 
     return (
         <div className={styles.token_select_container}>
-            {showManageTokenListContent
-                ? <TokenList
+            {showManageTokenListContent ? (
+                <TokenList
                     activeTokenListsChanged={activeTokenListsChanged}
                     indicateActiveTokenListsChanged={indicateActiveTokenListsChanged}
-                    />
-                : tokenListContainer}
+                />
+            ) : (
+                tokenListContainer
+            )}
         </div>
     );
 }
