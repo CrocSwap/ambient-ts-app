6541d27093bf51911b7e80735b948314351dee82
mhsmith321 <marty.smith01@yahoo.com>
2022-07-06 12:27:34 -0400

    annotate func removeToken() in TokenSelectContainer.tsx component file

--- src/components/Global/TokenSelectContainer/TokenSelectContainer.tsx
+++ src/components/Global/TokenSelectContainer/TokenSelectContainer.tsx
@@ -87,13 +87,23 @@ export default function TokenSelectContainer(props: TokenSelectContainerPropsIF)
         chooseToken(tkn);
     };
 
+    // function to remove a token from local state and local storage
     const removeToken = (token: TokenIF) => {
-        console.log({tokensBank});
+        // get the list of imported tokens as currently held in local state
+        // should be identical to data in local storage if the app updates both in parallel
         const newTokensList = tokensBank.filter((tkn: TokenIF) =>
-                (tkn.address !== token.address)
-                || ((tkn.address === token.address) && (tkn.chainId !== parseInt(chainId)))
-            );
-        console.log({newTokensList});
+            // return tokens different from the current address
+            (tkn.address !== token.address)
+            // if address matches, still return the token if it's not on the current chain
+            // native tokens will have the same address on multiple chains
+            || ((tkn.address === token.address) && (tkn.chainId !== parseInt(chainId)))
+        );
+        // send new filtered tokens array to local state
+        setImportedTokens(newTokensList);
+        // send new filtered tokens array to local storage
+        const userData = JSON.parse(localStorage.getItem('user') as string);
+        userData.tokens = newTokensList;
+        localStorage.setItem('user', JSON.stringify(userData));
     }
 
     const tokenListContent = (
