b72609ed40ea5200f7b9e7e70cd69755e4a0c72e
Ben Wolski <benwolski@gmail.com>
2022-07-06 13:10:35 -0400

    add support for multiple NFT images

--- src/components/Portfolio/PortfolioBanner/PortfolioBanner.tsx
+++ src/components/Portfolio/PortfolioBanner/PortfolioBanner.tsx
@@ -1,8 +1,8 @@
 import styles from './PortfolioBanner.module.css';
-import nft1 from '../../../assets/images/Temporary/nft/nft1.png';
-import nft2 from '../../../assets/images/Temporary/nft/nft2.png';
-import nft3 from '../../../assets/images/Temporary/nft/nft3.png';
-import avatarImage from '../../../assets/images/Temporary/nft/avatar.png';
+// import nft1 from '../../../assets/images/Temporary/nft/nft1.png';
+// import nft2 from '../../../assets/images/Temporary/nft/nft2.png';
+// import nft3 from '../../../assets/images/Temporary/nft/nft3.png';
+// import avatarImage from '../../../assets/images/Temporary/nft/avatar.png';
 import Moralis from 'moralis';
 import { useEffect, useState } from 'react';
 
@@ -17,25 +17,35 @@ export default function PortfolioBanner(props: PortfolioBannerPropsIF) {
     const { ensName, connectedAccount } = props;
     const ensNameAvailable = ensName !== '';
 
-    const [nftAvatarImageURL, setNFTAvatarImageURL] = useState(avatarImage);
+    const [imageData, setImageData] = useState<string[]>([]);
 
     async function getNFTs() {
-        // const options = { chain: 'eth', address: connectedAccount };
         const userEthNFTs = (
             await Moralis.Web3API.account.getNFTs({ address: connectedAccount })
         ).result?.filter((nft) => nft.contract_type === 'ERC1155');
 
         if (userEthNFTs) {
-            const metadataOfFirstNFT = userEthNFTs[0].metadata;
-            if (metadataOfFirstNFT) {
-                const parsedMetadata = JSON.parse(metadataOfFirstNFT);
-                const imageURL = parsedMetadata.image;
-                const imageUrlNoProtocol = imageURL.substring(12);
-                const imageGatewayURL = 'https://ipfs.io/ipfs/' + imageUrlNoProtocol;
-                setNFTAvatarImageURL(imageGatewayURL);
-            } else {
-                setNFTAvatarImageURL(avatarImage);
-            }
+            console.log({ userEthNFTs });
+            const imageLocalURLs: string[] = [];
+            userEthNFTs.forEach((nft) => {
+                const metadata = nft.metadata;
+                // console.log({ metadata });
+                if (metadata) {
+                    const parsedMetadata = JSON.parse(metadata);
+                    const imageURL = parsedMetadata.image;
+                    const imageUrlNoProtocol = imageURL.substring(12);
+                    const imageGatewayURL = 'https://ipfs.io/ipfs/' + imageUrlNoProtocol;
+                    // console.log({ imageGatewayURL });
+                    fetch(imageGatewayURL)
+                        .then((response) => response.blob())
+                        .then((image) => {
+                            // Create a local URL of that image
+                            const localUrl = URL.createObjectURL(image);
+                            imageLocalURLs.push(localUrl);
+                        });
+                }
+            });
+            setImageData(imageLocalURLs);
         }
     }
 
@@ -49,7 +59,7 @@ export default function PortfolioBanner(props: PortfolioBannerPropsIF) {
         <div className={styles.rectangle_container}>
             {/* <div className={styles.background}></div> */}
             <div className={styles.account_container}>
-                <img src={nftAvatarImageURL} alt='avatar' />
+                {imageData[0] ? <img src={imageData[0]} alt='avatar' /> : null}
                 <div className={styles.account_names}>
                     <span className={styles.name}>
                         {ensNameAvailable ? ensName : truncatedAccountAddress}
@@ -62,9 +72,9 @@ export default function PortfolioBanner(props: PortfolioBannerPropsIF) {
             </div>
 
             <div className={styles.nft_container}>
-                <img src={nft1} alt='nft' />
-                <img src={nft2} alt='nft' />
-                <img src={nft3} alt='nft' />
+                {imageData[1] ? <img src={imageData[1]} alt='nft' /> : null}
+                {imageData[2] ? <img src={imageData[2]} alt='nft' /> : null}
+                {imageData[3] ? <img src={imageData[3]} alt='nft' /> : null}
             </div>
         </div>
     );
