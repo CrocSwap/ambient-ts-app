64dd508e4f779584a46ff0abb0f2f924f1bdb74c
Ben Wolski <benwolski@gmail.com>
2022-07-06 13:56:55 -0400

    move NFT image state to App.tsx

--- src/components/Portfolio/PortfolioBanner/PortfolioBanner.tsx
+++ src/components/Portfolio/PortfolioBanner/PortfolioBanner.tsx
@@ -3,55 +3,19 @@ import styles from './PortfolioBanner.module.css';
 // import nft2 from '../../../assets/images/Temporary/nft/nft2.png';
 // import nft3 from '../../../assets/images/Temporary/nft/nft3.png';
 // import avatarImage from '../../../assets/images/Temporary/nft/avatar.png';
-import Moralis from 'moralis';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 import truncateAddress from '../../../utils/truncateAddress';
 
 interface PortfolioBannerPropsIF {
     ensName: string;
     connectedAccount: string;
+    imageData: string[];
 }
 
 export default function PortfolioBanner(props: PortfolioBannerPropsIF) {
-    const { ensName, connectedAccount } = props;
+    const { ensName, connectedAccount, imageData } = props;
     const ensNameAvailable = ensName !== '';
-
-    const [imageData, setImageData] = useState<string[]>([]);
-
-    async function getNFTs() {
-        const userEthNFTs = (
-            await Moralis.Web3API.account.getNFTs({ address: connectedAccount })
-        ).result?.filter((nft) => nft.contract_type === 'ERC1155');
-
-        if (userEthNFTs) {
-            console.log({ userEthNFTs });
-            const imageLocalURLs: string[] = [];
-            userEthNFTs.forEach((nft) => {
-                const metadata = nft.metadata;
-                // console.log({ metadata });
-                if (metadata) {
-                    const parsedMetadata = JSON.parse(metadata);
-                    const imageURL = parsedMetadata.image;
-                    const imageUrlNoProtocol = imageURL.substring(12);
-                    const imageGatewayURL = 'https://ipfs.io/ipfs/' + imageUrlNoProtocol;
-                    // console.log({ imageGatewayURL });
-                    fetch(imageGatewayURL)
-                        .then((response) => response.blob())
-                        .then((image) => {
-                            // Create a local URL of that image
-                            const localUrl = URL.createObjectURL(image);
-                            imageLocalURLs.push(localUrl);
-                        });
-                }
-            });
-            setImageData(imageLocalURLs);
-        }
-    }
-
-    useEffect(() => {
-        if (connectedAccount) getNFTs();
-    }, [connectedAccount]);
 
     const truncatedAccountAddress = truncateAddress(connectedAccount, 18);
 
