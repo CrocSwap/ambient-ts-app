1f1e88a47baf3f279b596cfdc820a72a8031d2d5
mhsmith321 <marty.smith01@yahoo.com>
2022-07-07 15:22:04 -0400

    add logic to fetchList.ts to determine if the list already exists in local storage by URI

--- src/components/Global/TokenList/fetchList.ts
+++ src/components/Global/TokenList/fetchList.ts
@@ -1,3 +1,5 @@
+import { TokenListIF } from '../../../utils/interfaces/exports';
+
 export default function getList(listURI:string) {
     console.log('user wants list from: ' + listURI);
     const list = fetch(listURI)
@@ -5,12 +7,21 @@ export default function getList(listURI:string) {
         .then(response => (
             {
                 ...response,
-                listURI,
+                uri: listURI,
                 dateRetrieved: new Date().toISOString(),
                 default: false,
                 userImported: true,
             }
         ));
     Promise.resolve(list)
-        .then(resolvedList => console.log(resolvedList));
+        .then(resolvedList => {
+            console.log(resolvedList);
+            const allTokenLists = JSON.parse(localStorage.getItem('allTokenLists') as string)
+            if (allTokenLists.map((tokenList: TokenListIF) => tokenList.uri).includes(resolvedList.uri)) {
+                console.log('already got it, boss!')
+            } else {
+                console.log('nope, new to me!')
+            }
+            console.log(allTokenLists);
+        });
 }
