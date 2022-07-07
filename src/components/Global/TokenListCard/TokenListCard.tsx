3c989338d32f74d80dff178826aef38af8f53ad5
Jadessu <junioradessu@gmail.com>
2022-07-07 10:52:35 -0500

    create menu component using materialUI'

--- src/components/Global/TokenListCard/TokenListCard.tsx
+++ src/components/Global/TokenListCard/TokenListCard.tsx
@@ -1,6 +1,8 @@
 import styles from './TokenListCard.module.css';
 import Toggle from '../../Global/Toggle/Toggle';
 import { TokenListIF } from '../../../utils/interfaces/exports';
+import { MenuItem, Menu } from '@material-ui/core';
+import { useState } from 'react';
 
 interface TokenListProps {
     list: TokenListIF;
@@ -10,9 +12,19 @@ interface TokenListProps {
 }
 
 export default function TokenListCard(props: TokenListProps) {
+    const [anchorEl, setAnchorEl] = useState(null);
     const { list, listIsActive, toggleActiveState } = props;
 
     const cardBackground = listIsActive ? '#7371FC ' : '';
+
+    const handleClick = (event: React.ChangeEvent<any>) => {
+        console.log('handleClick', event.currentTarget);
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        console.log('handleClose');
+        setAnchorEl(null);
+    };
 
     return (
         <div className={styles.token_list_card_container}>
@@ -25,7 +37,26 @@ export default function TokenListCard(props: TokenListProps) {
                     <div className={styles.token_list_card_name}>
                         <span> {list?.name}</span>
                         <div className={styles.token_count}>{list?.tokens?.length} tokens</div>
+
+                        <div
+                            aria-controls='list settings'
+                            aria-haspopup='true'
+                            onClick={handleClick}
+                            className={styles.menu}
+                        >
+                            Open Menu
+                        </div>
                     </div>
+                    <Menu
+                        id='simple-menu'
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Item 1</MenuItem>
+                        <MenuItem onClick={handleClose}>Item 2</MenuItem>
+                    </Menu>
                 </div>
                 <div className={styles.right_content}>
                     <Toggle
