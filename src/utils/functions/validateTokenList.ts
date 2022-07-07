bc104d393637ff1f365f1675750cffa521cfe219
mhsmith321 <marty.smith01@yahoo.com>
2022-07-07 17:52:07 -0400

    commit code and push for Ben to test

--- src/utils/functions/validateTokenList.ts
+++ src/utils/functions/validateTokenList.ts
@@ -1,5 +1,10 @@
+import Ajv from 'ajv';
 import { TokenListIF } from '../interfaces/TokenListIF';
+import * as tokenListSchema from '../data/tokenListSchema.json';
 
 export default function validateTokenList(tokenList: TokenListIF) {
     console.log(tokenList);
+    console.log(tokenListSchema);
+    const tokenListValidator = new Ajv({ allErrors: true }).compile(tokenListSchema);
+    console.log(tokenListValidator(tokenList));
 }
