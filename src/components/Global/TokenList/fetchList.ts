d2beb8ccfac886cca95a619f206feba5eec1953f
mhsmith321 <marty.smith01@yahoo.com>
2022-07-07 16:10:11 -0400

    annotate code in fetchList.ts function file

--- src/components/Global/TokenList/fetchList.ts
+++ src/components/Global/TokenList/fetchList.ts
@@ -1,9 +1,13 @@
 import { TokenListIF } from '../../../utils/interfaces/exports';
 
 export default function getList(listURI:string) {
+    // trim whitespace from start and end of input string
     const sanitizedInput = listURI.trim();
+    // fetch a token list from the outside URI
     const list = fetch(sanitizedInput)
+        // format response object as a JSON
         .then(response => response.json())
+        // middleware to add metadata to the list used by the Ambient app
         .then(response => (
             {
                 ...response,
@@ -13,13 +17,20 @@ export default function getList(listURI:string) {
                 userImported: true,
             }
         ))
+        // error logging in console
         .catch(err => console.error(err));
+
+    // resolve the promise object
     Promise.resolve(list)
         .then(resolvedList => {
+            // get allTokenLists from local storage
             const allTokenLists = JSON.parse(localStorage.getItem('allTokenLists') as string);
+            // determine if a token list from the URI exists in local storage already
             const listExistsInLocalStorage = allTokenLists
                 .map((tokenList: TokenListIF) => tokenList.uri)
                 .includes(resolvedList.uri);
+            // if the list is already in local storage, overwrite it
+            // ... if not, add it to the bank of token lists
             if (listExistsInLocalStorage) {
                 console.log('already got it, boss!');
                 const otherLists = allTokenLists.filter((tokenList: TokenListIF) => tokenList.uri !== resolvedList.uri);
@@ -31,5 +42,6 @@ export default function getList(listURI:string) {
                 throw new Error('could not determine if token list is already in local storage');
             };
         })
+        // error logging in console
         .catch(err => console.error(err));
 }
