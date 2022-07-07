f0409e96aa5cd8e0cd7b055c47e9de98750b063d
mhsmith321 <marty.smith01@yahoo.com>
2022-07-07 15:07:20 -0400

    add logic to fetchList.ts to fetch a list from a URI, resolve the promise, and use middleware to apply internal Ambient app values

--- src/components/Global/TokenList/fetchList.ts
+++ src/components/Global/TokenList/fetchList.ts
@@ -1,3 +1,15 @@
 export default function getList(listURI:string) {
     console.log('user wants list from: ' + listURI);
+    const list = fetch(listURI)
+        .then(response => response.json())
+        .then(response => (
+            {
+                ...response,
+                listURI,
+                dateRetrieved: new Date().toISOString(),
+                default: false
+            }
+        ));
+    Promise.resolve(list)
+        .then(resolvedList => console.log(resolvedList));
 }
