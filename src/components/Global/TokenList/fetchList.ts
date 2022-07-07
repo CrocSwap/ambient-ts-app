78d5d8d81c7b73cb4571ecebef86e8ed2c22e837
mhsmith321 <marty.smith01@yahoo.com>
2022-07-07 15:38:46 -0400

    complete logic to get an outside token list into local storage

--- src/components/Global/TokenList/fetchList.ts
+++ src/components/Global/TokenList/fetchList.ts
@@ -12,16 +12,24 @@ export default function getList(listURI:string) {
                 default: false,
                 userImported: true,
             }
-        ));
+        ))
+        .catch(err => console.error(err));
     Promise.resolve(list)
         .then(resolvedList => {
             console.log(resolvedList);
-            const allTokenLists = JSON.parse(localStorage.getItem('allTokenLists') as string)
-            if (allTokenLists.map((tokenList: TokenListIF) => tokenList.uri).includes(resolvedList.uri)) {
-                console.log('already got it, boss!')
+            const allTokenLists = JSON.parse(localStorage.getItem('allTokenLists') as string);
+            const listExistsInLocalStorage = allTokenLists
+                .map((tokenList: TokenListIF) => tokenList.uri)
+                .includes(resolvedList.uri);
+            if (listExistsInLocalStorage) {
+                console.log('already got it, boss!');
+            } else if (!listExistsInLocalStorage) {
+                console.log('nope, new to me!');
+                // console.log([...allTokenLists, resolvedList]);
+                localStorage.setItem('allTokenLists', JSON.stringify([...allTokenLists, resolvedList]));
             } else {
-                console.log('nope, new to me!')
-            }
-            console.log(allTokenLists);
-        });
+                throw new Error('could not determine if token list is already in local storage');
+            };
+        })
+        .catch(err => console.error(err));
 }
