53d76a45165893af9852913cb250c5893f6c1d25
Ben Wolski <benwolski@gmail.com>
2022-07-07 18:44:56 -0400

    add ajv-formats

--- src/utils/functions/validateTokenList.ts
+++ src/utils/functions/validateTokenList.ts
@@ -1,10 +1,14 @@
 import Ajv from 'ajv';
 import { TokenListIF } from '../interfaces/TokenListIF';
-import * as tokenListSchema from '../data/tokenListSchema.json';
+import tokenListSchema from '../data/tokenListSchema.json';
+import addFormats from 'ajv-formats';
 
 export default function validateTokenList(tokenList: TokenListIF) {
-    console.log(tokenList);
-    console.log(tokenListSchema);
-    const tokenListValidator = new Ajv({ allErrors: true }).compile(tokenListSchema);
-    console.log(tokenListValidator(tokenList));
+    console.log({ tokenList });
+    console.log({ tokenListSchema });
+    const ajv = new Ajv({ allErrors: true });
+    addFormats(ajv);
+    const tokenListValidator = ajv.compile(tokenListSchema);
+    const listPassesValidation = tokenListValidator(tokenList);
+    console.log({ listPassesValidation });
 }
