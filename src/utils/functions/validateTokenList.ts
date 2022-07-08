821e6415133943bc762aaf3028ac23eb1391ec9a
mhsmith321 <marty.smith01@yahoo.com>
2022-07-08 15:54:54 -0400

    commit to allow merge from origin

--- src/utils/functions/validateTokenList.ts
+++ src/utils/functions/validateTokenList.ts
@@ -1,6 +1,6 @@
 import Ajv from 'ajv';
 import { TokenListIF } from '../interfaces/TokenListIF';
-import * as tokenListSchema from '../data/tokenListSchema.json';
+import tokenListSchema from '../data/tokenListSchema.json';
 
 export default function validateTokenList(tokenList: TokenListIF) {
     console.log(tokenList);
