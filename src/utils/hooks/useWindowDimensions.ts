f4ecac9c22b7f8b9001c94e017ca63a9e1833417
Jadessu <junioradessu@gmail.com>
2022-07-11 19:44:48 -0500

    create useWindowsize hook

--- src/utils/hooks/useWindowDimensions.ts
+++ src/utils/hooks/useWindowDimensions.ts
@@ -1,39 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-// See: https://usehooks-ts.com/react-hook/use-event-listener
-import { useEventListener } from '../useEventListener';
-// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
-
-interface WindowSize {
-    width: number;
-    height: number;
+// Define general type for useWindowSize hook, which includes width and height
+interface Size {
+    width: number | undefined;
+    height: number | undefined;
 }
 
-function useWindowSize(): WindowSize {
-    const [windowSize, setWindowSize] = useState<WindowSize>({
-        width: 0,
-        height: 0,
+export default function useWindowSize(): Size {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState<Size>({
+        width: undefined,
+        height: undefined,
     });
-
-    const handleSize = () => {
-        setWindowSize({
-            width: window.innerWidth,
-            height: window.innerHeight,
-        });
-    };
-
-    useEventListener('resize', handleSize);
-
-    // Set size at the first client-side load
-    useIsomorphicLayoutEffect(() => {
-        handleSize();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
+    useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        // Add event listener
+        window.addEventListener('resize', handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
     return windowSize;
 }
 
-export default useWindowSize;
-
-// Taken from https://usehooks-ts.com/react-hook/use-window-size: -Jr
+// Taken from https://usehooks.com/useWindowSize/: -Jr
