a894255d187c9a8a855d314c74bf76ed293bc18c
Jadessu <junioradessu@gmail.com>
2022-07-13 16:19:13 -0500

    create menu control functionality

--- src/components/Transaction/TransactionCard.tsx
+++ src/components/Transaction/TransactionCard.tsx
@@ -1,6 +1,22 @@
 import styles from './TransactionCard.module.css';
-
+import { MenuItem, Menu } from '@material-ui/core';
+import { useStyles } from '../../utils/functions/styles';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { useState } from 'react';
 export default function TransactionCard() {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const handleClick = (
+        event: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>,
+    ) => {
+        console.log('handleClick', event.currentTarget);
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        console.log('handleClose');
+        setAnchorEl(null);
+    };
+    const classes = useStyles();
+
     const buttonsDisplay = (
         <div className={styles.buttons_container}>
             <button className={styles.details_button}>Edit</button>
