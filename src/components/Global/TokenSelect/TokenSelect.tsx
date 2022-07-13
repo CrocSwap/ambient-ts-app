25c104625473b5f6741e8a7420cf4c5b7d4c5858
mhsmith321 <marty.smith01@yahoo.com>
2022-07-13 15:13:57 -0400

    minor refactor to starIcon in TokenSelect.tsx file

--- src/components/Global/TokenSelect/TokenSelect.tsx
+++ src/components/Global/TokenSelect/TokenSelect.tsx
@@ -9,7 +9,7 @@ import { TokenIF } from '../../../utils/interfaces/exports';
 import uriToHttp from '../../../utils/functions/uriToHttp';
 import { removeToken } from '../../Global/TokenSelectContainer/removeToken';
 
-interface TokenSelectProps {
+interface TokenSelectPropsIF {
     token: TokenIF;
     tokensBank: Array<TokenIF>;
     chainId: string;
@@ -17,7 +17,7 @@ interface TokenSelectProps {
     chooseToken: (tok: TokenIF) => void;
 }
 
-export default function TokenSelect(props: TokenSelectProps) {
+export default function TokenSelect(props: TokenSelectPropsIF) {
     const { token, chooseToken, tokensBank, chainId, setImportedTokens } = props;
     const [showDelete, setShowDelete] = useState(false);
     const [toggleDeleteOn, setToggleDeleteOn] = useState(false);
@@ -28,14 +28,7 @@ export default function TokenSelect(props: TokenSelectProps) {
 
     // As much as I dislike directing using svgs in code, this is the only way we can style the fill on hover...unless we want to bring in two different SVGS.
     const starIcon = (
-        <div className={styles.star_icon}>
-            <svg
-                width='23'
-                height='23'
-                viewBox='0 0 23 23'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-            >
+        <svg width='23' height='23' viewBox='0 0 23 23' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path
                 d='M11.5 1.58301L14.7187 8.10384L21.9166 9.15593L16.7083 14.2288L17.9375 21.3955L11.5 18.0101L5.06248 21.3955L6.29165 14.2288L1.08331 9.15593L8.28123 8.10384L11.5 1.58301Z'
                 stroke='#BDBDBD'
@@ -44,8 +37,7 @@ export default function TokenSelect(props: TokenSelectProps) {
                 strokeLinejoin='round'
                 className={styles.star_svg}
             />
-            </svg>
-        </div>
+        </svg>
     );
 
     const deleteIcon = (
@@ -94,7 +86,9 @@ export default function TokenSelect(props: TokenSelectProps) {
                 Remove {token.symbol} from your list
                 {toggleButtons}
             </div>
-            {starIcon}
+            <div className={styles.star_icon}>
+                {starIcon}
+            </div>
             <div className={styles.modal_content} onClick={() => chooseToken(token)}>
                 <div className={styles.modal_tokens_info}>
                     {token.logoURI ? <img src={uriToHttp(token.logoURI)} alt={`logo for token ${token.name}`} width='27px' /> : noTokenImage}
