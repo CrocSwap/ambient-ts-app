c9f45707d5112293a63845656b89fd01946f26fa
Jadessu <junioradessu@gmail.com>
2022-07-14 13:04:01 -0500

    add styling to header container

--- src/components/Transaction/TransactionCardHeader.tsx
+++ src/components/Transaction/TransactionCardHeader.tsx
@@ -1,5 +1,18 @@
 import styles from './TransactionCardHeader.module.css';
 
 export default function TransactionCardHeader() {
-    return <div className={styles.row}></div>;
+    return (
+        <div className={styles.header_container}>
+            <div className={styles.header_container_items}>
+                <p>ID</p>
+                <p>Wallet</p>
+                <p>Price</p>
+                <p>Type</p>
+                <p>Side</p>
+                <p>ETH</p>
+                <p>USDC</p>
+            </div>
+            <div className={styles.menu}>Menu</div>
+        </div>
+    );
 }
