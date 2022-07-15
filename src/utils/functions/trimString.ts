cb6990765a688effb76201b905e87aa4ae1e8147
mhsmith321 <emily.drake.smith@gmail.com>
2022-07-15 18:18:30 -0400

    refactor code in trimString.ts file

--- src/utils/functions/trimString.ts
+++ src/utils/functions/trimString.ts
@@ -2,13 +2,18 @@
 // first arg is the string to shorten
 // second arg is how many characters to retain from ends
 
-export const trimString = (input:string, length:number) => {
-    // get `length` number of characters from the start of the string
-    const start = input.slice(0, length);
-    // get `length` number of characters from the end of the string
-    const end = input.slice(-length);
-    // concatenate start and end with ellipsis
-    const shortString = start + '...' + end;
+export const trimString = (
+    input:string,
+    startLength:number,
+    endLength:number,
+    separator:string
+) => {
+    // get `startLength` number of characters from the start of the string
+    const start = input.slice(0, startLength);
+    // get `endLength` number of characters from the end of the string
+    const end = input.slice(-endLength);
+    // concatenate start and end with user-specified separator
+    const shortString = start + separator + end;
     // return shortened string
     return shortString;
 }
