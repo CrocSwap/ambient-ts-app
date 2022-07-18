4cf658cabdc3bf0b68371376e3020a1baf7f14ad
mhsmith321 <emily.drake.smith@gmail.com>
2022-07-18 13:21:47 -0400

    throw error in useCustomToken.ts file if on-chain query fails to find a valid token

--- src/components/Global/TokenList/useCustomToken.ts
+++ src/components/Global/TokenList/useCustomToken.ts
@@ -29,6 +29,9 @@ export const useCustomToken = (
                 console.log(token);
                 Promise.resolve(token).then((tkn) => {
                     console.log(tkn);
+                    if (!tkn[0].decimals) {
+                        throw new Error('Data returned from chain does not appear to represent a valid token. Check that you are on the correct chain for the contract address used. If so, please log an issue referencing the file useCustomToken.ts, your current chain, and the contract address used.');
+                    }
                     const customToken = new Token(
                         tkn[0].name,
                         tkn[0].address,
@@ -40,7 +43,7 @@ export const useCustomToken = (
                     );
                     console.log(customToken);
                     setMatchingTokens([customToken]);
-                });
+                }).catch(err => console.warn(err));
             };
         } else {
             setMatchingTokens([]);
