ffe81c53bd2b96ab73e092099e99144d9a68aba5
mhsmith321 <emily.drake.smith@gmail.com>
2022-07-18 12:20:50 -0400

    fix typo in Token.ts class declaration constructor

--- src/utils/classes/token.ts
+++ src/utils/classes/token.ts
@@ -10,7 +10,7 @@ export default class Token {
     constructor(
         name = 'Unknown Token',
         address: string,
-        symbol: '???',
+        symbol = '???',
         decimals: number,
         chainId: number,
         logoURI: string,  
