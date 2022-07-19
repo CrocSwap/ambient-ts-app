da1b3fc6456fd855c0af19fdb468fe59337431de
Jadessu <junioradessu@gmail.com>
2022-07-19 11:53:05 -0500

    create temp token logo images

--- src/components/Global/Account/Range/RangeCard.tsx
+++ src/components/Global/Account/Range/RangeCard.tsx
@@ -1,4 +1,14 @@
 import styles from './RangeCard.module.css';
 export default function RangeCard() {
+    const tokenLogos = (
+        <div className={styles.token_logos}>
+            <img src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png' alt='' />
+            <img
+                src='https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png'
+                alt=''
+            />
+        </div>
+    );
+
     return <div></div>;
 }
