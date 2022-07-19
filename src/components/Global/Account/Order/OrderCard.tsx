4299a1e46bbe80ef3733dddf2779db136968648a
Jadessu <junioradessu@gmail.com>
2022-07-19 14:14:40 -0500

    create column layout

--- src/components/Global/Account/Order/OrderCard.tsx
+++ src/components/Global/Account/Order/OrderCard.tsx
@@ -16,5 +16,25 @@ export default function OrderCard() {
             <img src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png' alt='' />
         </div>
     );
+
+    const tokenQtyColumn = (
+        <div className={styles.token_qty_column}>
+            <p>T1 Qty</p>
+            <p>T2 Qty</p>
+        </div>
+    );
+    const accountColumn = (
+        <div className={styles.account_column}>
+            <p>0xcD...134</p>
+            <p>0xcD...134</p>
+        </div>
+    );
+
+    const buyLimitColumn = (
+        <div className={styles.buy_limit_column}>
+            <p>Buy</p>
+            <p>Limit</p>
+        </div>
+    );
     return <div>OrderCard</div>;
 }
