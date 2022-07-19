a02007a73c48c763b49e8623f723ec82f9826a99
Jadessu <junioradessu@gmail.com>
2022-07-19 14:13:49 -0500

    create token logos and qty

--- src/components/Global/Account/Order/OrderCard.tsx
+++ src/components/Global/Account/Order/OrderCard.tsx
@@ -1,3 +1,20 @@
+import styles from './OrderCard.module.css';
 export default function OrderCard() {
+    const tokenLogos = (
+        <div className={styles.token_logos}>
+            <img src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png' alt='' />
+            <img
+                src='https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png'
+                alt=''
+            />
+        </div>
+    );
+
+    const tokenQty = (
+        <div className={styles.token_qty}>
+            T1 Qty
+            <img src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png' alt='' />
+        </div>
+    );
     return <div>OrderCard</div>;
 }
