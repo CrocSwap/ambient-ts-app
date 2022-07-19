ab433ca5355de76b9add5de8e8c45657bc80d035
Ben Wolski <ben@crocodilelabs.io>
2022-07-19 01:29:45 -0400

    truncate wallet addresses on position card

--- src/utils/functions/trimString.ts
+++ src/utils/functions/trimString.ts
@@ -8,14 +8,14 @@ export default function trimString(
     input: string,
     startLength: number,
     endLength: number,
-    separator = '...'
+    separator = '...',
 ) {
     // return input unchanged if shorter than the number of characters to use
     if (input.length <= startLength + endLength) return input;
     // get `startLength` number of characters from the start of the string
     const start = input.slice(0, startLength);
     // get `endLength` number of characters from the end of the string
-    const end = input.slice(-endLength);
+    const end = endLength > 0 ? input.slice(-endLength) : '';
     // concatenate start and end with user-specified separator if provided
     const shortString = start + separator + end;
     // return shortened string
