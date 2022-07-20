6adf47e3933409e5ff8950b1f53a35947fc1e90a
Jadessu <junioradessu@gmail.com>
2022-07-20 09:46:48 -0500

    change layout for sidebar to only show 0n 1020px devices

--- src/components/Global/Account/Transaction/TransactionsTable.tsx
+++ src/components/Global/Account/Transaction/TransactionsTable.tsx
@@ -1,8 +1,8 @@
-import styles from './Transactions.module.css';
+import styles from './TransactionsTable.module.css';
 import TransactionCard from './TransactionCard';
 import TransactionCardHeader from './TransactionCardHeader';
 
-export default function Transactions() {
+export default function TransactionsTable() {
     const items = [1, 2, 3, 4, 5, 6];
 
     const ItemContent = items.map((item, idx) => <TransactionCard key={idx} />);
