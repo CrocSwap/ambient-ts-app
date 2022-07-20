3507e2e442a631f14f4fcb785048b29a5232f051
mhsmith321 <emily.drake.smith@gmail.com>
2022-07-20 13:01:51 -0400

    stub-up useState() hooks in useSlippage.ts to track four slippage values

--- src/App/useSlippage.ts
+++ src/App/useSlippage.ts
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+
 export const useSlippage = () => {
     console.log('ran useSlippage() hook');
+
+    const [slipMintVolatile, setSlipMintVolatile] = useState<number>();
+    const [slipMintStable, setSlipMintStable] = useState<number>();
+
+
+    const [slipSwapVolatile, setSlipSwapVolatile] = useState<number>();
+    const [slipSwapStable, setSlipSwapStable] = useState<number>();
+    
 }
