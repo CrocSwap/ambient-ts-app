9a23be94f0c10cb32dcffe78f8eb08057642b9a4
Jadessu <junioradessu@gmail.com>
2022-07-20 16:34:20 -0500

    build a genereal menu component to render menu in table based on tab

--- src/components/Global/DropdownMenu/DropdownMenu.tsx
+++ src/components/Global/DropdownMenu/DropdownMenu.tsx
@@ -9,12 +9,15 @@ interface DropdownMenuProps {
 export default function DropdownMenu(props: DropdownMenuProps) {
     const [open, setOpen] = useState(false);
 
+    // There is a mouse leave issue. I am currently getting around it by wrapping the children in a div and applying the mouse leave to that div. This still requires the user to enter the children before that function fires.
+
+    // DropdownMenuContainer is also translating on the x axis to fix the alignment that the extra wrapper causes here.
     return (
-        <div className={styles.nav_item}>
+        <div className={styles.dropdown_control}>
             <div className={styles.menu_icon} onClick={() => setOpen(!open)}>
                 {props.title}
             </div>
-            {open && props.children}
+            <div onMouseLeave={() => setOpen(false)}>{open && props.children}</div>
         </div>
     );
 }
