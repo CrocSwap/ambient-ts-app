b419c237b88d0e04b0236de9de0ce0dcfca68c9d
Jadessu <junioradessu@gmail.com>
2022-07-21 09:47:47 -0500

    create order card 2 jsx

--- src/components/Global/Account/Order/OrderCard2.tsx
+++ src/components/Global/Account/Order/OrderCard2.tsx
@@ -1,5 +1,39 @@
 import styles from './OrderCard2.module.css';
 
 export default function OrderCard2() {
-    return <div>order</div>;
+    return (
+        <div className={styles.main_container}>
+            <div className={styles.row_container}>
+                {/* ------------------------------------------------------ */}
+                <div className={styles.column_account}>
+                    <p>0xaBcD...1234</p>
+                    <p>0xAbCd...9876</p>
+                </div>
+                <p className={styles.account_sing}>0xaBcD...1234</p>
+                <p className={styles.account_sing}>0xAbCd...9876</p>
+
+                {/* ------------------------------------------------------ */}
+                <p className={styles.price}>Price</p>
+                {/* ------------------------------------------------------ */}
+
+                <div className={styles.type_column}>
+                    <p>Buy</p>
+                    <p>Limit</p>
+                </div>
+                <p className={styles.side_sing}>Buy</p>
+                <p className={styles.type_sing}>Limit</p>
+                {/* ------------------------------------------------------ */}
+                <div className={styles.column_qty}>
+                    <p>T1 Qty</p>
+                    <p>T2 Qty</p>
+                </div>
+                <p className={styles.qty_sing}>T1 Qty</p>
+                <p className={styles.qty_sing}>T2 Qty</p>
+                {/* ------------------------------------------------------ */}
+                <div className={styles.status}>STAT</div>
+            </div>
+
+            <div className={styles.menu_container}>...</div>
+        </div>
+    );
 }
