4c6129e71b6d2aaaddbb367b3d353c3e40ca9c0d
Jadessu <junioradessu@gmail.com>
2022-07-21 14:20:23 -0500

    change styling for input

--- src/components/Global/TokenList/CustomTokens.tsx
+++ src/components/Global/TokenList/CustomTokens.tsx
@@ -18,17 +18,28 @@ export default function CustomTokens(props: CustomTokenPropsIF) {
 
     const dispatch = useAppDispatch();
 
-    const [setSearchInput, tokenAlreadyImported, setTokenAlreadyImported, foundTokens, errorText] = useCustomToken(chainId);
+    const [setSearchInput, tokenAlreadyImported, setTokenAlreadyImported, foundTokens, errorText] =
+        useCustomToken(chainId);
 
     const [importedTokens, setImportedTokens] = useState<TokenIF[]>(
-        JSON.parse(localStorage.getItem('user') as string).tokens.filter((tkn: TokenIF) => tkn.fromList === 'custom')
+        JSON.parse(localStorage.getItem('user') as string).tokens.filter(
+            (tkn: TokenIF) => tkn.fromList === 'custom',
+        ),
     );
 
     function deleteToken(sadToken: TokenIF) {
         setTokenAlreadyImported(false);
-        setImportedTokens(importedTokens.filter((token: TokenIF) => (token.address !== sadToken.address || token.chainId !== sadToken.chainId)));
+        setImportedTokens(
+            importedTokens.filter(
+                (token: TokenIF) =>
+                    token.address !== sadToken.address || token.chainId !== sadToken.chainId,
+            ),
+        );
         const user = JSON.parse(localStorage.getItem('user') as string);
-        user.tokens = user.tokens.filter((token: TokenIF) => (token.address !== sadToken.address || token.chainId !== sadToken.chainId));
+        user.tokens = user.tokens.filter(
+            (token: TokenIF) =>
+                token.address !== sadToken.address || token.chainId !== sadToken.chainId,
+        );
         localStorage.setItem('user', JSON.stringify(user));
     }
 
@@ -43,7 +54,9 @@ export default function CustomTokens(props: CustomTokenPropsIF) {
                 closeModal();
                 break;
             default:
-                console.warn(`Problem in function importToken() in CustomTokens.tsx file. Did not recognize whether to update Token A or Token B in Redux Toolkit.  Received value <<<<<${tokenToUpdate}>>>>> of type <<<<<${typeof tokenToUpdate}>>>>>, recognized values are 'A' and 'B' (type: string). App will not close modal, refer to CustomTokens.tsx for troubleshooting.`);
+                console.warn(
+                    `Problem in function importToken() in CustomTokens.tsx file. Did not recognize whether to update Token A or Token B in Redux Toolkit.  Received value <<<<<${tokenToUpdate}>>>>> of type <<<<<${typeof tokenToUpdate}>>>>>, recognized values are 'A' and 'B' (type: string). App will not close modal, refer to CustomTokens.tsx for troubleshooting.`,
+                );
                 break;
         }
         setTokenAlreadyImported(true);
@@ -58,31 +71,35 @@ export default function CustomTokens(props: CustomTokenPropsIF) {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.3 }}
-            className={styles.search_input}
+            className={styles.search_container}
         >
-            <input type='text' placeholder='0x000' onChange={(e) => setSearchInput(e.target.value.trim().toLowerCase())} />
+            <div className={styles.search_input}>
+                <input
+                    type='text'
+                    placeholder='0x000'
+                    onChange={(e) => setSearchInput(e.target.value.trim().toLowerCase())}
+                />
+            </div>
+
             <p className={styles.query_error_text}>{errorText}</p>
             <Divider />
             <div className={styles.custom_tokens_header}>
                 <span>0 Custom Tokens</span>
                 <span className={styles.clear_all_button}>Clear all</span>
             </div>
-            {
-                importedTokens.map((token: TokenIF) => (
-                    <div key={`imported_token_${token.address}`}>
-                        <h4>{token.name}</h4>
-                        <button onClick={() => deleteToken(token)}>Delete</button>
-                    </div>
-                ))
-            }
-            { tokenAlreadyImported ||
+            {importedTokens.map((token: TokenIF) => (
+                <div key={`imported_token_${token.address}`}>
+                    <h4>{token.name}</h4>
+                    <button onClick={() => deleteToken(token)}>Delete</button>
+                </div>
+            ))}
+            {tokenAlreadyImported ||
                 foundTokens.map((token: TokenIF) => (
                     <div key={`found_token_${token.address}`}>
                         <h4>{token.name}</h4>
                         <button onClick={() => importToken(token)}>Import</button>
                     </div>
-                ))
-            }
+                ))}
             <div className={styles.custom_tokens_footer}>
                 Tip: Custom tokens are stored locally in your browser
             </div>
