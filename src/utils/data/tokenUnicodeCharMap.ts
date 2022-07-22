4a34ea36c68d953c48dca4c2b44f2ca9f1c7f92f
Ben Wolski <ben@crocodilelabs.io>
2022-07-22 13:34:12 -0400

    add mapping between token symbol and unicode character

---
+++ src/utils/data/tokenUnicodeCharMap.ts
@@ -0,0 +1,8 @@
+export default [
+    { symbol: 'ambi', character: 'ⓐ', UTF16: '0x24D0' },
+    { symbol: 'btc', character: '₿', UTF16: '0x20BF' },
+    { symbol: 'eth', character: 'Ξ', UTF16: '0x039E' },
+    { symbol: 'usdc', character: '$', UTF16: '0x0024' },
+    { symbol: 'dai', character: '$', UTF16: '0x0024' },
+    { symbol: 'usdt', character: '$', UTF16: '0x0024' },
+];
