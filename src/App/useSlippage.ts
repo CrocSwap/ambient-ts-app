cf74245deb506cd44095fe6b80d92e027f587bb1
mhsmith321 <emily.drake.smith@gmail.com>
2022-07-22 16:03:54 -0400

    fix issue of control flow in useSlippage.ts when app tries to access user data from local storage before initialization

--- src/App/useSlippage.ts
+++ src/App/useSlippage.ts
@@ -2,34 +2,43 @@ import { useEffect, useState } from 'react';
 
 export const useSlippage = () => {
     const userData = JSON.parse(localStorage.getItem('user') as string);
+    console.log(userData);
 
     const [slipSwapStable, setSlipSwapStable] = useState('2');
     useEffect(() => {
-        console.log(slipSwapStable);
-        console.log(userData);
-        userData.slippage.swap.stable = slipSwapStable;
-        localStorage.setItem('user', JSON.stringify(userData));
+        console.log({slipSwapStable});
+        if (userData?.slippage) {
+            console.log(userData.slippage.swap.stable);
+            userData.slippage.swap.stable = slipSwapStable;
+            localStorage.setItem('user', JSON.stringify(userData));
+        }
     }, [slipSwapStable]);
 
     const [slipSwapVolatile, setSlipSwapVolatile] = useState('4');
     useEffect(() => {
         console.log({slipSwapVolatile});
-        userData.slippage.swap.volatile = slipSwapVolatile;
-        localStorage.setItem('user', JSON.stringify(userData));
+        if (userData?.slippage) {
+            userData.slippage.swap.volatile = slipSwapVolatile;
+            localStorage.setItem('user', JSON.stringify(userData));
+        }
     }, [slipSwapVolatile]);
 
     const [slipMintStable, setSlipMintStable] = useState('3');
     useEffect(() => {
         console.log({slipMintStable});
-        userData.slippage.mint.stable = slipMintStable;
-        localStorage.setItem('user', JSON.stringify(userData));
+        if (userData?.slippage) {
+            userData.slippage.mint.stable = slipMintStable;
+            localStorage.setItem('user', JSON.stringify(userData));
+        }
     }, [slipMintStable]);
 
     const [slipMintVolatile, setSlipMintVolatile] = useState('7');
     useEffect(() => {
         console.log({slipMintVolatile});
-        userData.slippage.mint.volatile = slipMintVolatile;
-        localStorage.setItem('user', JSON.stringify(userData));
+        if (userData?.slippage) {
+            userData.slippage.mint.volatile = slipMintVolatile;
+            localStorage.setItem('user', JSON.stringify(userData));
+        }
     }, [slipMintVolatile]);
 
     return [
