857d0bd5a50885d5dd4cd3c420ea2fe0f55cb660
Jadessu <junioradessu@gmail.com>
2022-07-24 16:45:44 -0500

    fix merge conflict

--- src/components/Trade/TradeTabs/Transactions/TransactionCard2.tsx
+++ src/components/Trade/TradeTabs/Transactions/TransactionCard2.tsx
@@ -5,22 +5,6 @@ import WalletAndId from '../../../Global/Tabs/WalletAndID/WalletAndId';
 import styles from './TransactionCard2.module.css';
 
 export default function TransactionCard2() {
-    const token1 = (
-        <section className={styles.qty_sing}>
-            <p>T1 Qty</p>
-            <img
-                src='https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png'
-                alt=''
-            />
-        </section>
-    );
-
-    const token2 = (
-        <section className={styles.qty_sing}>
-            <p>T2 Qty</p>
-            <img src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png' alt='' />
-        </section>
-    );
     return (
         <div className={styles.main_container}>
             <div className={styles.row_container}>
@@ -29,7 +13,7 @@ export default function TransactionCard2() {
                     <p>0xcD...1234</p>
                     <p>0xCd...9876</p>
                 </section>
-                <section className={styles.account_sing}>0xcD...1234</section>
+               
                 <section className={styles.account_sing}>0xCd...9876</section> */}
                 <WalletAndId />
 
