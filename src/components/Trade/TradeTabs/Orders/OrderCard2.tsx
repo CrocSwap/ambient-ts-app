e69e51cbcdb7c13f3fab3f872a2813b40cfa31f2
Jadessu <junioradessu@gmail.com>
2022-07-25 09:51:51 -0500

    clean up files

--- src/components/Trade/TradeTabs/Orders/OrderCard2.tsx
+++ src/components/Trade/TradeTabs/Orders/OrderCard2.tsx
@@ -13,46 +13,14 @@ export default function OrderCard2() {
         <div className={styles.main_container}>
             <div className={styles.row_container}>
                 {/* ------------------------------------------------------ */}
-                {/* <section className={styles.column_account}>
-                    <p>0xcD...1234</p>
-                    <p>0xCd...9876</p>
-                </section>
-                <section className={styles.account_sing}>0xcD...1234</section>
-                <section className={styles.account_sing}>0xCd...9876</section> */}
+
                 <WalletAndId ownerId={tempOwnerId} posHash={tempPosHash} />
 
                 {/* ------------------------------------------------------ */}
-                {/* <section className={styles.price}>Price</section> */}
                 <Price priceType='priceBuy' />
                 {/* ------------------------------------------------------ */}
-
-                {/* <section className={styles.type_column}>
-                    <p>Buy</p>
-                    <p>Limit</p>
-                </section>
-                <section className={styles.side_sing}>Buy</section>
-                <section className={styles.type_sing}>Limit</section> */}
-
                 <OrderTypeSide type='order' side='sell' />
                 {/* ------------------------------------------------------ */}
-                {/* <section className={styles.column_qty}>
-                    <div>
-                        <p>T1 Qty</p>
-                        <img
-                            src='https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png'
-                            alt=''
-                        />
-                    </div>
-                    <div>
-                        <p>T2 Qty</p>
-                        <img
-                            src='https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png'
-                            alt=''
-                        />
-                    </div>
-                </section>
-                {token1}
-                {token2} */}
                 <TokenQty />
                 {/* ------------------------------------------------------ */}
                 <div className={styles.status}>
