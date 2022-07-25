5106418f31f68a5d274abc578356d8fe7e23aa46
Jadessu <junioradessu@gmail.com>
2022-07-25 11:30:33 -0500

    style denominaiton switch

--- src/components/Trade/Reposition/RepositionDenominationSwitch/RepositionDenominationSwitch.tsx
+++ src/components/Trade/Reposition/RepositionDenominationSwitch/RepositionDenominationSwitch.tsx
@@ -6,13 +6,7 @@ import { toggleDidUserFlipDenom } from '../../../../utils/state/tradeDataSlice';
 
 // interface for props
 interface RepositionDenominationSwitchPropsIF {
-    // tokenPair: TokenPairIF;
-    // displayForBase: boolean;
-    // poolPriceDisplay: number;
-    // isOnTradeRoute?: boolean;
-    // isTokenABase: boolean;
     denominationsInBase: boolean;
-    // setDenominationsInBase: React.Dispatch<React.SetStateAction<boolean>>;
     quoteTokenSymbol: string;
     baseTokenSymbol: string;
 }
@@ -22,44 +16,20 @@ interface RepositionDenominationSwitchPropsIF {
 // TODO   ... end, please remove the value from props
 
 export default function RepositionDenominationSwitch(props: RepositionDenominationSwitchPropsIF) {
-    // const { tokenPair, isTokenABase, poolPriceDisplay, didUserFlipDenom } = props;
     const { denominationsInBase, baseTokenSymbol, quoteTokenSymbol } = props;
     const dispatch = useAppDispatch();
-
-    // TODO:  @Junior, if both buttons have the same action of reversing the current
-    // TODO:  ... value of `toggleDenomination`, let's do just one button with two
-    // TODO   ... <div> elements nested inside of it
-
-    // const moreExpensiveToken = poolPriceDisplay < 1
-    //     ? (isTokenABase ? 'A' : 'B')
-    //     : (isTokenABase ? 'B' : 'A');
-
-    // const tokenToHighlight = moreExpensiveToken === 'A'
-    //     ? (didUserFlipDenom ? 'B' : 'A')
-    //     : (didUserFlipDenom ? 'A' : 'B');
-
-    // temp data
-    // const tokenToHighlight = 'A';
-
-    // For now, I have made this a static file but the code can essentially be taken from DenominationSwitch.tsx and just pass the props fro edit
-    // TODO: Eventually, we will want just one denomination switch throughout the entire app instead of rewriting the same code for each component
 
     return (
         <div className={styles.denomination_switch}>
             <div>Denomination</div>
             <button
-                className={
-                    // tokenToHighlight === 'A' ? styles.active_button : styles.non_active_button
-                    denominationsInBase ? styles.active_button : styles.non_active_button
-                }
-                // onClick={() => setDenominationsInBase(!denominationsInBase)}
+                className={denominationsInBase ? styles.active_button : styles.non_active_button}
                 onClick={() => dispatch(toggleDidUserFlipDenom())}
             >
                 {baseTokenSymbol}
             </button>
             <button
                 className={!denominationsInBase ? styles.active_button : styles.non_active_button}
-                // onClick={() => setDenominationsInBase(!denominationsInBase)}
                 onClick={() => dispatch(toggleDidUserFlipDenom())}
             >
                 {quoteTokenSymbol}
