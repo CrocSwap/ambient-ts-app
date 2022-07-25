afc50d2d77b1777224976e4c1746559d16c6b636
Jadessu <junioradessu@gmail.com>
2022-07-25 08:52:51 -0500

    create price component

---
+++ src/components/Global/Tabs/Price/Price.tsx
@@ -0,0 +1,31 @@
+import styles from './PriceSide.module.css';
+export default function PriceSide() {
+    const priceBuy = <p className={styles.buy_style}>Price</p>;
+    const priceSell = <p className={styles.sell_style}>Price</p>;
+
+    const minMaxBuy = (
+        <div className={styles.min_max}>
+            <p className={styles.buy_style}>Min</p>
+            <p className={styles.buy_style}>Max</p>
+        </div>
+    );
+    const minMaxAdd = (
+        <div className={styles.min_max}>
+            <p className={styles.add_style}>Min</p>
+            <p className={styles.add_style}>Max</p>
+        </div>
+    );
+
+    const priceData = {
+        priceBuy: priceBuy,
+        priceSell: priceSell,
+        minMaxBuy: minMaxBuy,
+        minMaxAdd: minMaxAdd,
+    };
+
+    return (
+        <>
+            <section className={styles.price}>{priceData.priceBuy}</section>
+        </>
+    );
+}
