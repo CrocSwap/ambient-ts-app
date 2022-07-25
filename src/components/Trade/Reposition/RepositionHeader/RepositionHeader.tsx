0cb18b8d902b685583ec62a31e6e6886b36f4850
Jadessu <junioradessu@gmail.com>
2022-07-25 11:25:06 -0500

    create reposition ehader files

---
+++ src/components/Trade/Reposition/RepositionHeader/RepositionHeader.tsx
@@ -0,0 +1,6 @@
+import { dialogActionsClasses } from '@mui/material';
+import styles from './RepositionHeader.module.css';
+
+export default function RepositionHeader() {
+    return <div>Repositon header</div>;
+}
