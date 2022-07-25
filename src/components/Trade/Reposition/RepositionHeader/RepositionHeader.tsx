dd0d2c7e293788652a5530bc717d68376fbcf9f0
Jadessu <junioradessu@gmail.com>
2022-07-25 11:26:53 -0500

    create reposition header jsx

--- src/components/Trade/Reposition/RepositionHeader/RepositionHeader.tsx
+++ src/components/Trade/Reposition/RepositionHeader/RepositionHeader.tsx
@@ -1,6 +1,24 @@
-import { dialogActionsClasses } from '@mui/material';
-import styles from './RepositionHeader.module.css';
+import ContentHeader from '../../../Global/ContentHeader/ContentHeader';
+import { RiCloseFill } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
+import trimString from '../../../../utils/functions/trimString';
 
-export default function RepositionHeader() {
-    return <div>Repositon header</div>;
+interface RepositionHeaderPropsIF {
+    positionHash?: string;
+}
+export default function RepositionHeader(props: RepositionHeaderPropsIF) {
+    const { positionHash } = props;
+    const navigate = useNavigate();
+
+    return (
+        <ContentHeader>
+            <div>
+                Edit Position:{' '}
+                {positionHash ? trimString(positionHash, 4, 4, '…') : ' 0xFD05...A3FF'}
+            </div>
+            <div onClick={() => navigate(-1)}>
+                <RiCloseFill />
+            </div>
+        </ContentHeader>
+    );
 }
