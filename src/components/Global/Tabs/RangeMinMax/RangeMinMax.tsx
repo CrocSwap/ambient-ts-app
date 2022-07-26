150b49316ea78a3bcb12971397b1d0622e45db80
Jadessu <junioradessu@gmail.com>
2022-07-26 13:42:14 -0500

    create range min max component

--- src/components/Global/Tabs/RangeMinMax/RangeMinMax.tsx
+++ src/components/Global/Tabs/RangeMinMax/RangeMinMax.tsx
@@ -1,5 +1,18 @@
 import styles from './RangeMinMax.module.css';
 
 export default function RangeMinMax() {
-    return <div>rangeminmax</div>;
+    return (
+        <>
+            <section className={styles.range_column}>
+                <p>Min</p>
+                <p>Max</p>
+            </section>
+            <section className={styles.range_sing}>
+                <p>Min</p>
+            </section>
+            <section className={styles.range_sing}>
+                <p>Max</p>
+            </section>
+        </>
+    );
 }
