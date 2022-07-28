039613a61ec137a74c36ba259471d057a0b6f9c7
Ben Wolski <benwolski@gmail.com>
2022-07-28 16:59:23 -0400

    update testTokenMap

---
+++ src/utils/data/testTokenMap.ts
@@ -0,0 +1,18 @@
+export const testTokenMap = new Map([
+    [
+        '0x0000000000000000000000000000000000000000_0x5', // 'ETH' on Goerli
+        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2_0x1', // 'ETH' on Mainnet
+    ],
+    [
+        '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60_0x5', // 'DAI' on Goerli
+        '0x6b175474e89094c44da98b954eedeac495271d0f_0x1', // 'DAI' on Mainnet
+    ],
+    [
+        '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c_0x5', // 'USDC' on Goerli
+        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48_0x1', // 'USDC' on Mainnet
+    ],
+    [
+        '0xc04b0d3107736c32e19f1c62b2af67be61d63a05_0x5', // 'WBTC' on Goerli
+        '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599_0x1', // 'WBTC' on Mainnet
+    ],
+]);
