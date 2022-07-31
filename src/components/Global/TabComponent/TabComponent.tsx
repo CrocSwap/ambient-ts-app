269e54c19b459652c39eaa93964667ebbc51107e
Jadessu <junioradessu@gmail.com>
2022-07-31 14:00:15 -0500

    create typescript data

--- src/components/Global/TabComponent/TabComponent.tsx
+++ src/components/Global/TabComponent/TabComponent.tsx
@@ -3,10 +3,16 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ItemEnterAnimation } from '../../../utils/others/FramerMotionAnimations';
 
-interface TabComponentProps {
-    children: React.ReactNode;
+type tabData = {
+    label: string;
+    content: React.ReactNode;
+};
+
+interface TabProps {
+    data: tabData[];
+    rightTabOptions?: React.ReactNode;
 }
 
-export default function TabComponent(props: TabComponentProps) {
-    return <div className={styles.row}>{props.children}</div>;
+export default function TabComponent(props: TabProps) {
+    return <div className={styles.row}></div>;
 }
