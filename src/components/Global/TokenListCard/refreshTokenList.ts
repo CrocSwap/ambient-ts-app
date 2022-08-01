9c94c8e6a5446011abeca818dab5b0e38c30ac0d
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-01 15:52:50 -0400

    update refreshTokenList.ts to transform input with uriToHttp() function

--- src/components/Global/TokenListCard/refreshTokenList.ts
+++ src/components/Global/TokenListCard/refreshTokenList.ts
@@ -1,3 +1,5 @@
+import uriToHttp from '../../../utils/functions/uriToHttp'; 
+
 export default function refreshTokenList(uri:string) {
-    console.log('user clicked button to refresh: ', uri);
+    console.log('user clicked button to refresh: ', uriToHttp(uri));
 }
