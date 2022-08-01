a6df0d28e48c1b8d03ed37431970c478cf1ea6bb
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-01 15:57:54 -0400

    add logic to refreshTokenList.ts to fetch a token list from a URI

--- src/components/Global/TokenListCard/refreshTokenList.ts
+++ src/components/Global/TokenListCard/refreshTokenList.ts
@@ -2,4 +2,17 @@ import uriToHttp from '../../../utils/functions/uriToHttp';
 
 export default function refreshTokenList(uri:string) {
     console.log('user clicked button to refresh: ', uriToHttp(uri));
+
+    const newList = fetch(uriToHttp(uri))
+        .then((response) => response.json())
+        .then((response) => (
+            {
+                ...response,
+                uri,
+                dateRetrieved: new Date().toISOString(),
+                userImported: false
+            }
+        ));
+    
+    console.log(newList);
 }
