c20f349cf0908f98ccee01e38ee994094e53be71
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-01 16:20:18 -0400

    add logic to refreshTokenList.ts to update token list in allTokenLists in local storage

--- src/components/Global/TokenListCard/refreshTokenList.ts
+++ src/components/Global/TokenListCard/refreshTokenList.ts
@@ -1,7 +1,10 @@
 import uriToHttp from '../../../utils/functions/uriToHttp'; 
+import { TokenListIF } from '../../../utils/interfaces/exports';
 
 export default function refreshTokenList(uri:string) {
     console.log('user clicked button to refresh: ', uriToHttp(uri));
+
+    const allTokenLists = JSON.parse(localStorage.getItem('allTokenLists') as string);
 
     const newList = fetch(uriToHttp(uri))
         .then((response) => response.json())
@@ -15,5 +18,11 @@ export default function refreshTokenList(uri:string) {
         ));
 
     Promise.resolve(newList)
-        .then((list) => console.log(list))
+        .then((list) => replaceList(list))
+
+    function replaceList(list: TokenListIF) {
+        const tokenListIndex = allTokenLists.findIndex((tokenList: TokenListIF) => tokenList.uri === uri);
+        allTokenLists.splice(tokenListIndex, 1, list);
+        localStorage.setItem('allTokenLists', JSON.stringify(allTokenLists));
+    }
 }
