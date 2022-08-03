46e1831fd8b88ce4a6d60f37e595727f79717c3f
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-03 13:12:04 -0400

    send token address to local storage on star click

--- src/components/Global/TokenSelect/clickStar.ts
+++ src/components/Global/TokenSelect/clickStar.ts
@@ -2,4 +2,7 @@ import { TokenIF } from '../../../utils/interfaces/exports';
 
 export default function clickStar(token: TokenIF) {
     console.log('user clicked the star: ', token);
+    const user = JSON.parse(localStorage.getItem('user') as string);
+    user.favePools.push(token.address);
+    localStorage.setItem('user', JSON.stringify(user));
 }
