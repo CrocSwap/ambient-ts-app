20dd2ceb2e01bc1dd782d439d39e4888d141c97d
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2022-08-04 17:19:57 +0300

    Top Range Table

--- src/components/TopRanges/TopRange.tsx
+++ src/components/TopRanges/TopRange.tsx
@@ -1,18 +1,30 @@
-import styles from './TopRange.module.css';
+// import styles from './TopRange.module.css';
 
 export default function TopRange() {
-    return (
-        <tr>
-            <td data-column='TopRange ID' className={styles.topRange_id}>
-                0xfs05...db35
-            </td>
-            <td data-column='Range' className={styles.topRange_range}>
-                2100.00 3200.00
-            </td>
-            <td data-column='APY' className={styles.apy}>
-                35.65%
-            </td>
-            <td data-column='Range Status'></td>
-        </tr>
-    );
+    // const tempOwnerId = '0xa2b398145b7fc8fd9a01142698f15d329ebb5ff5090cfcc8caae440867ab9919';
+    // const tempPosHash = '0x01e650abfc761c6a0fc60f62a4e4b3832bb1178b';
+
+    return <></>;
+    // return (
+    //     <div className={styles.main_container}>
+    //     <div className={styles.tokens_container}>
+    //         <AccountTokensDisplay />
+    //     </div>
+
+    //     <div className={styles.row_container}>
+    //         <AccountPoolDisplay />
+    //         <WalletAndId ownerId={tempOwnerId} posHash={tempPosHash} />
+    //         <Price priceType='priceBuy' />
+    //         <OrderTypeSide type='order' side='sell' />
+    //         <TokenQty />
+    //         <div className={styles.status}>
+    //             <OpenOrderStatus isFilled />
+    //         </div>
+    //     </div>
+
+    //     <div className={styles.menu_container}>
+    //         <OrdersMenu userPosition={false} />
+    //     </div>
+    // </div>
+    // );
 }
---
+++ src/components/TopRanges/TopRangeCardHeader.tsx
@@ -0,0 +1,22 @@
+import styles from './TopRangeCardHeader.module.css';
+
+export default function TopRangeCardHeader() {
+    return (
+        <div className={styles.main_container}>
+            <div className={styles.row_container}>
+                <p></p>
+                <p>Pool</p>
+                <p>ID</p>
+                <p className={styles.wallet}>Wallet</p>
+                <p className={styles.price}>Range</p>
+                <p className={styles.token}>ETH</p>
+                <p className={styles.token}>USDC</p>
+                <p>APY</p>
+                <p>Status</p>
+                <p></p>
+            </div>
+
+            <div></div>
+        </div>
+    );
+}
