259c24960a61d3df23220c7d8658893c54c6d672
Jadessu <junioradessu@gmail.com>
2022-08-04 14:50:25 -0500

    create navigation for trade buttons

--- src/pages/Trade/Trade.tsx
+++ src/pages/Trade/Trade.tsx
@@ -18,8 +18,8 @@ import truncateDecimals from '../../utils/data/truncateDecimals';
 import getUnicodeCharacter from '../../utils/functions/getUnicodeCharacter';
 // import TradeTabs from '../../components/Trade/TradeTabs/TradeTabs';
 import TradeTabs2 from '../../components/Trade/TradeTabs/TradeTabs2';
-import { Dispatch, SetStateAction } from 'react';
-import { motion } from 'framer-motion';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { motion, AnimateSharedLayout } from 'framer-motion';
 
 interface ITradeProps {
     account: string;
@@ -78,6 +78,53 @@ export default function Trade(props: ITradeProps) {
             : truncateDecimals(poolPriceDisplay, 2);
 
     // These would be move to their own components, presumably the graph component
+
+    const [activerOverlayButton, setActiveOverlayButton] = useState('Curve');
+    const chartOverlayButtonData = [
+        { name: 'Volume' },
+        { name: 'TVL' },
+        { name: 'Fee Rate' },
+        { name: 'Heatmap' },
+        { name: 'Liquidity Profile' },
+        { name: 'Curve' },
+        { name: 'Depth' },
+    ];
+
+    function handleOverlayButtonClick(name: string) {
+        setActiveOverlayButton(name);
+    }
+
+    const chartOverlayButtons = chartOverlayButtonData.map((button, idx) => (
+        <motion.div
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className={styles.settings_container}
+            key={idx}
+        >
+            <button
+                onClick={() => handleOverlayButtonClick(button.name)}
+                className={
+                    button.name === activerOverlayButton
+                        ? styles.active_button
+                        : styles.non_active_button
+                }
+            >
+                {button.name}
+
+                {button.name === activerOverlayButton && (
+                    <motion.div
+                        layoutId='outline'
+                        className={styles.outline}
+                        initial={false}
+                        // animate={{ borderColor: 'red' }}
+                        transition={spring}
+                    />
+                )}
+            </button>
+        </motion.div>
+    ));
     const tokenInfo = (
         <div className={styles.token_info_container}>
             <div className={styles.tokens_info} onClick={() => dispatch(toggleDidUserFlipDenom())}>
@@ -100,12 +147,7 @@ export default function Trade(props: ITradeProps) {
                     {denomInTokenA ? tokenBSymbol : tokenASymbol}
                 </span>
             </div>
-
-            <div className={styles.settings_container}>
-                <span>Liquidity Profile</span>
-                <button>Total</button>
-                <button>Cumulative</button>
-            </div>
+            <div className={styles.chart_overlay_container}>{chartOverlayButtons}</div>
         </div>
     );
 
@@ -206,45 +248,50 @@ export default function Trade(props: ITradeProps) {
         //     exit={{ x: window.innerWidth, transition: { duration: 0.4 } }}
         //     data-testid={'trade'}
         // >
-        <main className={styles.main_layout}>
-            <div className={styles.middle_col}>
-                <div className={`${styles.graph_style} ${expandGraphStyle}`}>
-                    {tokenInfo}
-                    {timeFrameContent}
-                    {chartImage}
+        <AnimateSharedLayout>
+            <main className={styles.main_layout}>
+                <div className={styles.middle_col}>
+                    <div className={`${styles.graph_style} ${expandGraphStyle}`}>
+                        {tokenInfo}
+                        {timeFrameContent}
+                        {chartImage}
+                    </div>
+
+                    <motion.div
+                        animate={{
+                            height: props.expandTradeTable ? '100%' : '30%',
+                            transition: {
+                                duration: 0.5,
+                                type: 'spring',
+                                damping: 10,
+                            },
+                        }}
+
+                        // className={` ${expandTradeTableStyle}`}
+                    >
+                        <TradeTabs2
+                            account={props.account}
+                            isAuthenticated={props.isAuthenticated}
+                            isWeb3Enabled={props.isWeb3Enabled}
+                            lastBlockNumber={props.lastBlockNumber}
+                            chainId={props.chainId}
+                            switchTabToTransactions={props.switchTabToTransactions}
+                            setSwitchTabToTransactions={props.setSwitchTabToTransactions}
+                            currentTxActiveInTransactions={props.currentTxActiveInTransactions}
+                            setCurrentTxActiveInTransactions={
+                                props.setCurrentTxActiveInTransactions
+                            }
+                            isShowAllEnabled={props.isShowAllEnabled}
+                            setIsShowAllEnabled={props.setIsShowAllEnabled}
+                            expandTradeTable={props.expandTradeTable}
+                            setExpandTradeTable={props.setExpandTradeTable}
+                        />
+                    </motion.div>
                 </div>
+                {mainContent}
+            </main>
+        </AnimateSharedLayout>
 
-                <motion.div
-                    animate={{
-                        height: props.expandTradeTable ? '100%' : '30%',
-                        transition: {
-                            duration: 0.5,
-                            type: 'spring',
-                            damping: 10,
-                        },
-                    }}
-
-                    // className={` ${expandTradeTableStyle}`}
-                >
-                    <TradeTabs2
-                        account={props.account}
-                        isAuthenticated={props.isAuthenticated}
-                        isWeb3Enabled={props.isWeb3Enabled}
-                        lastBlockNumber={props.lastBlockNumber}
-                        chainId={props.chainId}
-                        switchTabToTransactions={props.switchTabToTransactions}
-                        setSwitchTabToTransactions={props.setSwitchTabToTransactions}
-                        currentTxActiveInTransactions={props.currentTxActiveInTransactions}
-                        setCurrentTxActiveInTransactions={props.setCurrentTxActiveInTransactions}
-                        isShowAllEnabled={props.isShowAllEnabled}
-                        setIsShowAllEnabled={props.setIsShowAllEnabled}
-                        expandTradeTable={props.expandTradeTable}
-                        setExpandTradeTable={props.setExpandTradeTable}
-                    />
-                </motion.div>
-            </div>
-            {mainContent}
-        </main>
         // </motion.main>
     );
 }
@@ -254,3 +301,9 @@ type ContextType = { tradeData: TradeDataIF; navigationMenu: JSX.Element };
 export function useTradeData() {
     return useOutletContext<ContextType>();
 }
+
+const spring = {
+    type: 'spring',
+    stiffness: 500,
+    damping: 30,
+};
