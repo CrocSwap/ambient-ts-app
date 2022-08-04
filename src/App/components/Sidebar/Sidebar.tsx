92b5bbb107f7d999e00fd0f23632e96f5da2b843
Jadessu <junioradessu@gmail.com>
2022-08-04 11:20:14 -0500

    fix typescript errors

--- src/App/components/Sidebar/Sidebar.tsx
+++ src/App/components/Sidebar/Sidebar.tsx
@@ -131,7 +131,7 @@ export default function Sidebar(props: SidebarPropsIF) {
                     {searchContainer}
 
                     {topItems.map((item, idx) => (
-           <SidebarAccordion
+                        <SidebarAccordion
                             showSidebar={showSidebar}
                             idx={idx}
                             item={item}
