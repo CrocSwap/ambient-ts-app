8430bc1dc44a0c3e86a071c28d86cabcec58f949
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-10 11:14:31 -0400

    add logic to useAppChain.ts to update chain in Moralis after user changes network in the app

--- src/App/hooks/useAppChain.ts
+++ src/App/hooks/useAppChain.ts
@@ -6,8 +6,8 @@ import {
     useState
 } from 'react';
 import { useChain } from 'react-moralis';
-import { lookupChain } from '@crocswap-libs/sdk/dist/context';
 import { ChainSpec } from '@crocswap-libs/sdk';
+import { lookupChain } from '@crocswap-libs/sdk/dist/context';
 
 export const useAppChain = (
     defaultChain: string
@@ -15,26 +15,38 @@ export const useAppChain = (
     ChainSpec,
     Dispatch<SetStateAction<string>>
 ] => {
-    const { chainId } = useChain();
+    // chain from connected wallet via Moralis
+    const { chainId, switchNetwork } = useChain();
 
+    // value tracking the current chain the app is set to use
+    // initializes on the default chain parameter 
     const [ currentChain, setCurrentChain ] = useState(defaultChain);
     useEffect(() => console.log({currentChain}), [currentChain]);
+
+    // change the network in Moralis after user changes in the app
+    useEffect(() => {
+        if (chainId !== currentChain) switchNetwork(currentChain);
+    }, [currentChain]);
 
     // if the chain in metamask changes, update the value in the app to match
     // gatekeeping ensures this only runs when the user changes the chain in metamask
     useEffect(() => {
+        // only take action if Moralis and app have different chains
         if (chainId !== currentChain) {
             chainId ?? console.log(`updating chain to: ${chainId ?? defaultChain}`);
             setCurrentChain(chainId ?? defaultChain);
         }
     }, [chainId]);
 
+    // data from the SDK about the current chain
+    // refreshed every time the the value of currentChain is updated
     const chainData = useMemo(() => {
         let chn;
         try {
             chn = lookupChain(currentChain);
         } catch (err) {
             console.warn(err);
+            setCurrentChain(defaultChain);
             chn = lookupChain(defaultChain);
         }
         return chn;
