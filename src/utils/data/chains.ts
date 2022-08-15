75409f1ecdebb6bb70f85a0adf2fe95b89a54978
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-15 15:40:21 -0400

    refactor map format in chains.ts file

--- src/utils/data/chains.ts
+++ src/utils/data/chains.ts
@@ -2,46 +2,47 @@
 // all chains supported by Moralis are here regardless of Ambient support
 // keys === name of the network (human readable)
 // values === chain ids Moralis will recognize
-export const allMoralisChainIds = new Map()
-    .set(
+export const allMoralisChainIds = new Map([
+    [
         'ethereum',
         ['eth', 'mainnet', '0x1']
-    )
-    .set(
+    ],
+    [
         'goerli',
         ['goerli', '0x5']
-    )
-    .set(
+    ],
+    [
         'binance mainnet',
         ['bsc', 'binance', 'binance smart chain', '0x38']
-    )
-    .set(
+    ],
+    [
         'binance testnet',
         ['bsc testnet', 'binance testnet', 'binance smart chain testnet', '0x61']
-    )
-    .set(
+    ],
+    [
         'polygon',
         ['matic', 'polygon', '0x89']
-    )
-    .set(
+    ],
+    [
         'mumbai',
         ['mumbai', 'matic testnet', 'polygon testnet', '0x13881']
-    )
-    .set(
+    ],
+    [
         'avalanche mainnet',
         ['avalanche', '0xa86a']
-    )
-    .set(
+    ],
+    [
         'avalanche testnet',
         ['avalanche testnet', '0xa869']
-    )
-    .set(
+    ],
+    [
         'fantom',
-        ['fantom', '0xfa'])
-    .set(
+        ['fantom', '0xfa']],
+    [
         'cronos',
         ['cronos', '0x19']
-    );
+    ],
+]);
 
 // chains supported by the Ambient app
 // this is the single source of truth for chains Ambient supports
