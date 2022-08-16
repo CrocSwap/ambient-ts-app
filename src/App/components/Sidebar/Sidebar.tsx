1e7ed73a1fcb1ba1af1b1f79a8edaa805d976c24
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-16 03:46:21 -0400

    annotate function search() in Sidebar.tsx component file

--- src/App/components/Sidebar/Sidebar.tsx
+++ src/App/components/Sidebar/Sidebar.tsx
@@ -118,33 +118,45 @@ export default function Sidebar(props: SidebarPropsIF) {
     ];
 
     function search(text: string) {
+        //  format input as an array of single-character strings
         const inputAsArray = text.toLowerCase().split('');
-        const separators = [' ', '/', '\\'];
+        //  recognized separator characters
+        const separators = [' ', '/', '\\', ','];
 
-        while (separators.includes(inputAsArray[0])) {
-            inputAsArray.shift();
-        }
+        // remove any leading separator characters including whitespace
+        while (separators.includes(inputAsArray[0])) inputAsArray.shift();
 
-        const outputArray = [];
+        // array to hold isolated strings to be searched
+        const outputArray: string[] = [];
+        // value to accumulate strings before being pushed to `outputArray`
         let builtString = '';
 
-        while (inputAsArray.length && outputArray.length < 2) {
-            const character = inputAsArray.shift();
-            if (!separators.includes(character as string)) {
-                builtString += character;
-            } else {
-                outputArray.push(builtString);
-                builtString = '';
-            }
+        const pushAndResetString = () => {
+            // if character is a separator, push current value of builtString to output
+            builtString.length && outputArray.push(builtString);
+            // reset value of builtString
+            builtString = '';
         }
 
-        builtString.length && outputArray.push(builtString);
+        while (inputAsArray.length && outputArray.length < 2) {
+            // remove first character from array (mutated) and hold in a variable
+            const character = inputAsArray.shift();
+            // accumulate character or break if it is a separator
+            separators.includes(character as string)
+                ? pushAndResetString()
+                : builtString += character;
+        }
 
+        // add second search string to the output array if there is one
+        pushAndResetString();
+
+        // format output array of raw strings into tuples
         const outputTuples = outputArray.map((searchText: string) => [
             searchText.startsWith('0x') ? 'address' : 'name',
             searchText
         ]);
 
+        // log searches in console
         console.log(outputTuples);
     }
 
