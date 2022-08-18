90d318a6c54501d9bfea8f803dd1755b37551f09
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-18 17:15:02 -0400

    add pseudocode to useFavePools.ts file

--- src/App/hooks/useFavePools.ts
+++ src/App/hooks/useFavePools.ts
@@ -5,11 +5,17 @@ export const useFavePools = () => {
     const [ favePools, setFavePools ] = useState(userData.favePools);
 
     const addPoolToFaves = () => {
+        //  make a new array of all fave pools with new addition and expansion operator
+        //  update local state with the new array
         setFavePools([]);
+        // assign the value to userData.favePools and write to local storage
     }
 
     const removePoolFromFaves = () => {
+        //  make a new array of all fave pools with pool removed via Array.filter()
+        //  update local state with the new array
         setFavePools([]);
+        // assign the value to userData.favePools and write to local storage
     }
 
     return [
