16e09fd2b294f10796b9a9ea1bcd646bb72196de
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-20 11:53:10 -0400

    complete logic in useTermsOfService.ts to accept a user agreement

--- src/App/hooks/useTermsOfService.ts
+++ src/App/hooks/useTermsOfService.ts
@@ -1,22 +1,38 @@
 import { useEffect, useState } from 'react';
 
-export const useTermsOfService = () => {
+export const useTermsOfService = (): [
+    string,
+    (date: string) => void
+] => {
     const [ userData, setUserData ] = useState(JSON.parse(localStorage.getItem('user') as string));
-    const [ userAgreement, setUserAgreement ] = useState(userData?.termsOfService);
+    const [ agreement, setAgreement ] = useState(userData?.termsOfService?.agreed);
+    const [ agreementDate, setAgreementDate ] = useState(userData?.termsOfService?.date);
     useEffect(() => {
         function getUserData() {
             localStorage.user
                 ? setUserData(JSON.parse(localStorage.getItem('user') as string))
-                : getUserData();
+                : setTimeout(() => getUserData(), 500);
         }
         getUserData();
     }, []);
     useEffect(() => {
-        userData && setUserAgreement(userData.termsOfService);
+        userData && setAgreement(userData.termsOfService);
     }, [userData]);
-    useEffect(() => console.log(userAgreement), [userAgreement]);
+    useEffect(() => console.log(agreement), [agreement]);
 
     const tosText = 'Doug will have to put something here.'
 
-    return [tosText];
+    const agreeToS = (date: string) => {
+        const details = {
+            agreed: true,
+            date: date
+        }
+        setAgreement(details.agreed);
+        setAgreementDate(details.date);
+        const newUserData = userData;
+        newUserData.termsOfService = details;
+        localStorage.setItem('user', JSON.stringify(newUserData));
+    }
+
+    return [tosText, agreeToS];
 }
--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,12 +1,15 @@
 import { useTermsOfService } from '../../App/hooks/useTermsOfService';
 
 export default function TestPage() {
-    const [tosText] = useTermsOfService();
+    const [tosText, agreeToS] = useTermsOfService();
 
     return (
         <main>
             <h1>Hi there!</h1>
             <p>{tosText}</p>
+            <button onClick={() => agreeToS(new Date().toISOString())}>
+                Agree to ToS
+            </button>
         </main>
     );
 }
