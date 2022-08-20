7f8c11ad409a3766b5bf00a15268f10c3ed2e721
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-20 02:17:06 -0400

    reorganize webpack import statements at the top of LimitRage.tsx component file

--- src/components/Trade/Limit/LimitRate/LimitRate.tsx
+++ src/components/Trade/Limit/LimitRate/LimitRate.tsx
@@ -1,17 +1,10 @@
+import { Dispatch, SetStateAction } from 'react';
 import styles from './LimitRate.module.css';
-// import { RiArrowDownSLine } from 'react-icons/ri';
-// import Toggle from '../../../Global/Toggle/Toggle';
 import { useAppDispatch } from '../../../../utils/hooks/reduxToolkit';
-
 import { TokenIF, TokenPairIF } from '../../../../utils/interfaces/exports';
-// import { useState, useEffect } from 'react';
-// import Modal from '../../../../components/Global/Modal/Modal';
-// import TokenSelectContainer from '../../../Global/TokenSelectContainer/TokenSelectContainer';
-// import { useModal } from '../../../../components/Global/Modal/useModal';
-// import { getAmbientTokens } from '../../../../tempdata';
 import { setLimitPrice } from '../../../../utils/state/tradeDataSlice';
 
-interface LimitRateProps {
+interface LimitRatePropsIF {
     tokenPair: TokenPairIF;
     tokensBank: Array<TokenIF>;
     fieldId: string;
@@ -19,42 +12,15 @@ interface LimitRateProps {
     sellToken?: boolean;
     disable?: boolean;
     reverseTokens: () => void;
-    setLimitRate: React.Dispatch<React.SetStateAction<string>>;
+    setLimitRate: Dispatch<SetStateAction<string>>;
     poolPriceNonDisplay: number | undefined;
     insideTickDisplayPrice: number;
-    // updateOtherQuantity: (evt: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function LimitRate(props: LimitRateProps) {
-    const {
-        fieldId,
-        //  tokenPair,
-        //   tokensBank,
-        //   chainId,
-        disable,
-        setLimitRate,
-        // insideTickDisplayPrice,
-        //   reverseTokens
-    } = props;
+export default function LimitRate(props: LimitRatePropsIF) {
+    const { fieldId, disable, setLimitRate } = props;
+
     const dispatch = useAppDispatch();
-    // const [isChecked, setIsChecked] = useState<boolean>(false);
-
-    // const [isModalOpen, openModal, closeModal] = useModal();
-    // const tempTokenList = getAmbientTokens();
-
-    // const tokenSelectModalOrNull = isModalOpen ? (
-    //     <Modal onClose={closeModal} title='Select Token'>
-    //         <TokenSelectContainer
-    //             tokenPair={tokenPair}
-    //             tokensBank={tokensBank}
-    //             tokenToUpdate={'B'}
-    //             chainId={chainId}
-    //             tokenList={tempTokenList}
-    //             closeModal={closeModal}
-    //             reverseTokens={reverseTokens}
-    //         />
-    //     </Modal>
-    // ) : null;
 
     const handleLimitChange = (value: string) => {
         dispatch(setLimitPrice(value));
@@ -80,20 +46,6 @@ export default function LimitRate(props: LimitRateProps) {
             />
         </div>
     );
-
-    // const withdrawTokensContent = (
-    //     <span className={styles.surplus_toggle}>
-    //         Withdraw tokens
-    //         <div className={styles.toggle_container}>
-    //             <Toggle
-    //                 isOn={isChecked}
-    //                 handleToggle={() => setIsChecked(!isChecked)}
-    //                 Width={36}
-    //                 id='tokens_withdrawal'
-    //             />
-    //         </div>
-    //     </span>
-    // );
 
     return (
         <div className={styles.swapbox}>
