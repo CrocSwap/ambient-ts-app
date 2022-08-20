97352991636690e7fea0fedc180e286a182114bf
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-20 00:33:23 -0400

    reorganize webpack import statements at the top of CarouselItem.tsx component file

--- src/components/Global/Carousel/CarouselItem/CarouselItem.tsx
+++ src/components/Global/Carousel/CarouselItem/CarouselItem.tsx
@@ -1,12 +1,13 @@
+import { ReactNode } from 'react';
 import styles from './CarouselItem.module.css';
 
-interface CarouselItemProps {
-    children: React.ReactNode;
+interface CarouselItemPropsIF {
+    children: ReactNode;
     width?: number | string;
     height?: number | string;
 }
 
-export default function CarouselItem(props: CarouselItemProps) {
+export default function CarouselItem(props: CarouselItemPropsIF) {
     const { children, width } = props;
     return (
         <div className={styles.carousel_item} style={{ width: width, height: '526px' }}>
