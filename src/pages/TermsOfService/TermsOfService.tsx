e4e6f866402df6c6d7f747a3a01d851d875bd6c5
mhsmith321 <emily.drake.smith@gmail.com>
2022-08-24 17:39:53 -0400

    make <h1> content on TermsOfService.tsx page less silly

--- src/pages/TermsOfService/TermsOfService.tsx
+++ src/pages/TermsOfService/TermsOfService.tsx
@@ -3,7 +3,7 @@ import styles from './TermsOfService.module.css';
 export default function TermsOfService() {
     return (
         <main>
-            <h1 className={styles.tos_title}>Terms... Of... SERVICE!!!!!!</h1>
+            <h1 className={styles.tos_title}>Terms of Service</h1>
             <section className={styles.tos_section}>
                 <h3 className={styles.tos_section_heading}>First Section</h3>
                 <p className={styles.tos_section_paragraph}>
