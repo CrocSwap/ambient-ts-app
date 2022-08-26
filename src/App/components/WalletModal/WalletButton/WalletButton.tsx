5ea85ee3cee74ccb8816da971b33d061914a811e
Jadessu <junioradessu@gmail.com>
2022-08-26 15:17:38 -0500

    create wallet button layout

--- src/App/components/WalletModal/WalletButton/WalletButton.tsx
+++ src/App/components/WalletModal/WalletButton/WalletButton.tsx
@@ -1,5 +1,14 @@
 import styles from './WalletButton.module.css';
 
 export default function WalletButton() {
-    return <div>wallet button</div>;
+    return (
+        <button className={styles.container}>
+            <img
+                className={styles.icon}
+                src='https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png'
+                alt=''
+            />
+            <div className={styles.wallet_name}></div>
+        </button>
+    );
 }
