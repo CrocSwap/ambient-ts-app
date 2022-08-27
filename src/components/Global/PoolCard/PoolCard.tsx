eb10ec4730656b70cc3553ba58617578c92c196a
Ben Wolski <benwolski@gmail.com>
2022-08-26 20:28:49 -0400

    add APY for Top Pools on Home

--- src/components/Global/PoolCard/PoolCard.tsx
+++ src/components/Global/PoolCard/PoolCard.tsx
@@ -6,7 +6,12 @@ import { CrocEnv, toDisplayPrice } from '@crocswap-libs/sdk';
 import { querySpotPrice } from '../../../App/functions/querySpotPrice';
 import getUnicodeCharacter from '../../../utils/functions/getUnicodeCharacter';
 import { lookupChain } from '@crocswap-libs/sdk/dist/context';
-import { get24hChange, getPoolTVL, getPoolVolume } from '../../../App/functions/getPoolStats';
+import {
+    get24hChange,
+    getPoolStatsFresh,
+    // getPoolTVL,
+    // getPoolVolume,
+} from '../../../App/functions/getPoolStats';
 import { formatAmount } from '../../../utils/numbers';
 import PoolCardSkeleton from './PoolCardSkeleton/PoolCardSkeleton';
 
@@ -106,6 +111,8 @@ export default function PoolCard(props: PoolCardProps) {
 
     const [poolVolume, setPoolVolume] = useState<string | undefined>(undefined);
     const [poolTvl, setPoolTvl] = useState<string | undefined>(undefined);
+    const [poolApy, setPoolApy] = useState<string | undefined>(undefined);
+
     const [poolPriceChangePercent, setPoolPriceChangePercent] = useState<string | undefined>(
         undefined,
     );
@@ -117,18 +124,16 @@ export default function PoolCard(props: PoolCardProps) {
     useEffect(() => {
         (async () => {
             if (tokenAAddress && tokenBAddress && poolIndex && chainId) {
-                const tvlResult = await getPoolTVL(
+                const poolStats = await getPoolStatsFresh(
+                    chainId,
                     tokenAAddress,
                     tokenBAddress,
                     poolIndex,
-                    chainId,
                 );
-                const volumeResult = await getPoolVolume(
-                    tokenAAddress,
-                    tokenBAddress,
-                    poolIndex,
-                    chainId,
-                );
+
+                const tvlResult = poolStats?.tvl;
+                const volumeResult = poolStats?.volume;
+                const apyResult = poolStats?.apy;
 
                 if (tvlResult) {
                     const tvlString = formatAmount(tvlResult);
@@ -137,6 +142,13 @@ export default function PoolCard(props: PoolCardProps) {
                 if (volumeResult) {
                     const volumeString = formatAmount(volumeResult);
                     setPoolVolume(volumeString);
+                }
+                if (apyResult) {
+                    const apyString = apyResult.toLocaleString(undefined, {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                    });
+                    setPoolApy(apyString);
                 }
 
                 try {
@@ -202,7 +214,7 @@ export default function PoolCard(props: PoolCardProps) {
             <div></div>
             <div>
                 <div className={styles.row_title}>APY</div>
-                <div className={styles.apy}>{poolPriceDisplay === undefined ? '…' : '35.34'}</div>
+                <div className={styles.apy}>{poolApy === undefined ? '…' : `${poolApy}`}</div>
             </div>
         </>
     );
