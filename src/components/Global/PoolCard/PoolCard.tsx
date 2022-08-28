8bf54127487121d148a850fb052cc7e196c8b771
Ben Wolski <benwolski@gmail.com>
2022-08-28 18:07:26 -0400

    add percentage symbol to APY in PoolCard

--- src/components/Global/PoolCard/PoolCard.tsx
+++ src/components/Global/PoolCard/PoolCard.tsx
@@ -214,7 +214,7 @@ export default function PoolCard(props: PoolCardProps) {
             <div></div>
             <div>
                 <div className={styles.row_title}>APY</div>
-                <div className={styles.apy}>{poolApy === undefined ? '…' : `${poolApy}`}</div>
+                <div className={styles.apy}>{poolApy === undefined ? '…' : `${poolApy}%`}</div>
             </div>
         </>
     );
