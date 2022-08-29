ee8ed9bc09255aafa7e52e9e26afeedf96b95f62
Ben Wolski <benwolski@gmail.com>
2022-08-29 09:37:54 -0400

    fix DEX stats query on homepage

--- src/components/Home/Stats/AmbientStats.tsx
+++ src/components/Home/Stats/AmbientStats.tsx
@@ -26,7 +26,7 @@ const getDexStatsFresh = async () => {
     return fetch(
         'https://809821320828123.de:5000/dex_stats_fresh?' +
             new URLSearchParams({
-                lookback: '9999999999999',
+                lookback: '10000000',
             }),
     )
         .then((response) => response?.json())
