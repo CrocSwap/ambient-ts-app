d8c93efd1d63c5e4b757defa9ced2726fd54a647
Ben Wolski <benwolski@gmail.com>
2022-08-30 12:43:28 -0400

    add favorite pools data to sidebar

--- src/components/Global/Sidebar/FavoritePools/FavoritePoolsCard.tsx
+++ src/components/Global/Sidebar/FavoritePools/FavoritePoolsCard.tsx
@@ -1,5 +1,8 @@
 import styles from './FavoritePoolsCard.module.css';
 import { PoolIF } from '../../../../utils/interfaces/exports';
+import { getPoolStatsFresh } from '../../../../App/functions/getPoolStats';
+import { useEffect, useState } from 'react';
+import { formatAmount } from '../../../../utils/numbers';
 
 interface FavoritePoolsCardIF {
     pool: PoolIF;
@@ -7,11 +10,34 @@ interface FavoritePoolsCardIF {
 
 export default function FavoritePoolsCard(props: FavoritePoolsCardIF) {
     const { pool } = props;
+
+    const [poolVolume, setPoolVolume] = useState<string | undefined>();
+    const [poolTvl, setPoolTvl] = useState<string | undefined>();
+
+    useEffect(() => {
+        (async () => {
+            const poolStatsFresh = await getPoolStatsFresh(
+                pool.chainId,
+                pool.base.address,
+                pool.quote.address,
+                pool.poolId,
+            );
+            const volume = poolStatsFresh?.volume;
+            const volumeString = volume ? '$' + formatAmount(volume) : undefined;
+            setPoolVolume(volumeString);
+            const tvl = poolStatsFresh?.tvl;
+            const tvlString = tvl ? '$' + formatAmount(tvl) : undefined;
+            setPoolTvl(tvlString);
+        })();
+    }, [JSON.stringify(pool)]);
+
     return (
         <div className={styles.container}>
-            <div>{pool.base.address} / {pool.quote.address}</div>
-            <div>Volume</div>
-            <div>TVL</div>
+            <div>
+                {pool.base.symbol} / {pool.quote.symbol}
+            </div>
+            <div>{poolVolume ?? '…'}</div>
+            <div>{poolTvl ?? '…'}</div>
         </div>
     );
 }
