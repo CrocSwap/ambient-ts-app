63aa84d3d022e4474f3b8bf72e8d8298111969ce
Ben Wolski <benwolski@gmail.com>
2022-08-30 17:59:04 -0400

    update <TopTokens>

--- src/App/mockData.ts
+++ src/App/mockData.ts
@@ -125,7 +125,56 @@ export const topTokens = [
         id: 1,
     },
     {
+        name: 'WBTC',
+        base: {
+            name: 'Native Ether',
+            address: '0x0000000000000000000000000000000000000000',
+            symbol: 'ETH',
+            decimals: 18,
+            chainId: 0x5,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+        },
+        quote: {
+            name: 'Wrapped BTC',
+            address: '0xC04B0d3107736C32e19F1c62b2aF67BE61d63a05',
+            symbol: 'WBTC',
+            decimals: 8,
+            chainId: 0x5,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png',
+        },
+        chainId: '0x5',
+        poolId: 36000,
+        speed: 0.5,
+        id: 3,
+    },
+    {
         name: 'DAI',
+        base: {
+            name: 'USDCoin',
+            address: '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C',
+            symbol: 'USDC',
+            decimals: 6,
+            chainId: 0x5,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+        },
+        quote: {
+            name: 'Dai Stablecoin',
+            address: '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60',
+            symbol: 'DAI',
+            decimals: 18,
+            chainId: 0x5,
+            logoURI: 'https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png',
+        },
+        chainId: '0x5',
+        poolId: 36000,
+        speed: -2,
+        id: 4,
+    },
+    {
+        name: 'USDC',
         base: {
             name: 'USDCoin',
             address: '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C',
