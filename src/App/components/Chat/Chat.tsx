a9b3357612bad6eb3468a400993bc133dc94fab1
Jadessu <junioradessu@gmail.com>
2022-08-30 14:54:39 -0500

    create ai chat system

--- src/App/components/Chat/Chat.tsx
+++ src/App/components/Chat/Chat.tsx
@@ -4,8 +4,10 @@ import { MdClose, MdOpenInFull } from 'react-icons/md';
 import { GoUnmute, GoMute } from 'react-icons/go';
 import { BsEmojiSmile } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import { devpun } from './devpun';
 
 import notificationSound from '../../../assets/audio/message.wav';
+import ambientLogo from '../../../assets/images/logos/ambient_logo.svg';
 interface ChatPropsIF {
     ensName: string;
     connectedAccount: string;
@@ -15,35 +17,70 @@ interface ChatPropsIF {
 type MessageType = {
     message: string;
     time: string;
+    isUser: boolean;
 };
 
 interface MessageItemPropsIF {
     name: string;
     message: string;
     time: string;
-}
-function MessageItem(props: MessageItemPropsIF) {
-    return (
-        <div className={styles.message_item_container}>
-            <h2 className={styles.name}>{props.name}</h2>
-            <div className={styles.message_content}>
-                <div className={styles.avatar} />
-                <div className={styles.message_text}>{props.message}</div>
-            </div>
-            <div className={styles.time}>{props.time}</div>
-        </div>
-    );
+    idx: number;
+    isUser: boolean;
 }
 
 export default function Chat(props: ChatPropsIF) {
     const [showChatBot, setShowChatBot] = useState(false);
     const [message, setMessage] = useState('');
-    const [messagesArray] = useState<MessageType[]>([]);
+    const [messagesArray, setMessagesArray] = useState<MessageType[]>([]);
+    const [aiMessageIsAJoke, setAiMessageIsAJoke] = useState(false);
     const [soundIsMuted, setSoundIsMuted] = useState(false);
 
     useEffect(() => {
         props.fullScreen ? setShowChatBot(true) : null;
     }, [props.fullScreen]);
+
+    function MessageItem(props: MessageItemPropsIF) {
+        const messageYes = 'The sky is blue. Grass is green? a. Do you know the color of the Cloud';
+
+        const sentences = props.message.split(/[?]/);
+
+        const [answerBlur, setAnswerBlur] = useState(true);
+
+        const jokeContent = (
+            <div className={styles.joke_container}>
+                <div className={styles.question}>{sentences[0]}</div>
+                <div
+                    onClick={() => setAnswerBlur(!answerBlur)}
+                    className={answerBlur ? styles.answer_blur : styles.answer}
+                >
+                    {sentences[1]}
+                </div>
+            </div>
+        );
+
+        return (
+            <div
+                className={`${styles.message_item_container} ${
+                    !props.isUser && styles.example_right_side
+                }`}
+            >
+                <h2 className={styles.name}>{props.isUser ? props.name : 'Ambi'}</h2>
+
+                <div className={styles.message_content}>
+                    {props.isUser ? (
+                        <div className={styles.avatar} />
+                    ) : (
+                        <img src={ambientLogo} alt='ambient' />
+                    )}
+
+                    <div className={styles.message_text}>
+                        {aiMessageIsAJoke ? jokeContent : props.message}
+                    </div>
+                </div>
+                <div className={styles.time}>{props.time}</div>
+            </div>
+        );
+    }
 
     const chatButton = (
         <div
@@ -88,6 +125,38 @@ export default function Chat(props: ChatPropsIF) {
         </div>
     );
 
+    const [fakeMessageTyping, setFakeMessageTyping] = useState(true);
+
+    const AiMessagesArray = [
+        ` Hi there ${
+            props.ensName ? props.ensName : 'friend'
+        }. My name is Ambi. I am here to help guide you through our chat Interface. Try typing next in the input below and press enter`,
+
+        'Nice! As you can see, there is sound associated with the chat system. For your convenience, you can keep it muted or unmuted by clicking the sound icon on the upper right side. Type next once you are done ',
+
+        'Nice! Try to play around with the UI a little. If you have any suggestions for improvement, let Junior know!',
+
+        'You are still here...I have nothing else for you at the moment but you can try entering the word "joke" to get a joke.',
+    ];
+    const [currentMessage, setCurrentMessage] = useState(0);
+    const [aiMessage, setAiMessage] = useState(AiMessagesArray[currentMessage]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFakeMessageTyping(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [showChatBot]);
+
+    const typingAnimmation = (
+        <div className={styles.loader}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    );
+
     const chatHeader = (
         <div className={styles.chat_header}>
             <h3>Chat</h3>
@@ -97,6 +166,9 @@ export default function Chat(props: ChatPropsIF) {
                         {props.ensName ? props.ensName : props.connectedAccount}
                     </h2>
                     <div className={styles.avatar} />
+                    <div onClick={() => setSoundIsMuted(!soundIsMuted)}>
+                        {soundIsMuted ? <GoUnmute /> : <GoMute />}
+                    </div>
                 </div>
             ) : (
                 <div className={styles.chat_header_right}>
@@ -115,17 +187,48 @@ export default function Chat(props: ChatPropsIF) {
     );
     const audio = new Audio(notificationSound);
     audio.muted = soundIsMuted;
+
+    const randomJoke = Math.floor(Math.random() * devpun.length);
+    const handleNextMessage = () => {
+        setCurrentMessage((prev) => {
+            if (prev === AiMessagesArray.length - 1) {
+                setAiMessageIsAJoke(true);
+                return 0;
+            } else {
+                return prev + 1;
+            }
+        });
+    };
+
     const handleMessage = (event: React.KeyboardEvent<HTMLInputElement>) => {
         const date = new Date();
         const time = date.toLocaleTimeString();
         if (event.key === 'Enter') {
             event.preventDefault();
 
-            messagesArray?.push({ message, time });
+            messagesArray?.push({ message, time, isUser: true });
 
             setMessage('');
             audio.volume = 0.1;
             audio.play();
+            if (message?.toLowerCase() === 'joke') {
+                setAiMessageIsAJoke(true);
+            }
+            handleNextMessage();
+            setTimeout(handleComputerResponse, 2000);
+        }
+        function handleComputerResponse() {
+            const newMessage = {
+                message: aiMessageIsAJoke
+                    ? devpun[randomJoke].text
+                    : AiMessagesArray[currentMessage],
+                time: 'time',
+                isUser: false,
+            };
+
+            const newMessageArray = [...messagesArray, newMessage];
+
+            setMessagesArray(newMessageArray);
         }
     };
 
@@ -146,15 +249,19 @@ export default function Chat(props: ChatPropsIF) {
 
     const messagesDisplay = (
         <div className={styles.messages_container}>
+            {/* {exampleRightSideMessage} */}
             {messagesArray &&
                 messagesArray.map((message, idx) => (
                     <MessageItem
                         key={idx}
+                        idx={idx}
                         message={message.message}
                         time={message.time}
                         name={props.ensName ? props.ensName : props.connectedAccount}
+                        isUser={message.isUser}
                     />
                 ))}
+            {/* { showNextMessage &&  exampleRightSideMessage} */}
         </div>
     );
 
