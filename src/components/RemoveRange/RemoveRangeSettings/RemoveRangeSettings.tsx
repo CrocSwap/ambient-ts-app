7b4dedefb032b0e105ba0fb2df52034b5936572f
Jadessu <junioradessu@gmail.com>
2022-08-31 15:14:34 -0500

    refactgor remove range settings and created animated page component

--- src/components/RemoveRange/RemoveRangeSettings/RemoveRangeSettings.tsx
+++ src/components/RemoveRange/RemoveRangeSettings/RemoveRangeSettings.tsx
@@ -10,6 +10,8 @@ interface RemoveRangeSettingsPropsIF {
 export default function RemoveRangeSettings(props: RemoveRangeSettingsPropsIF) {
     const { showSettings, setShowSettings } = props;
 
+    console.log(showSettings);
+
     const wrapperStyle = showSettings ? styles.settings_wrapper_active : styles.settings_wrapper;
     return (
         <div className={styles.settings_container}>
@@ -17,6 +19,8 @@ export default function RemoveRangeSettings(props: RemoveRangeSettingsPropsIF) {
                 <div className={styles.close_icon} onClick={() => setShowSettings(false)}>
                     <RiCloseLine size={20} />
                 </div>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, veniam obcaecati.
+                Sit perspiciatis natus porro nesciunt quasi, voluptatem atque, aut corrupti.
             </div>
         </div>
     );
