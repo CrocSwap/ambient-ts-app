f42cfb9a041a9d7d3468a56091454702adcc5d96
mhsmith321 <emily.drake.smith@gmail.com>
2022-09-06 11:31:17 -0400

    add logic to useUrlParams.ts to return parameters as a Map

--- src/pages/Swap/useUrlParams.ts
+++ src/pages/Swap/useUrlParams.ts
@@ -1,15 +1,18 @@
-import { stringify } from 'querystring';
 import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const useUrlParams = () => {
     const { params } = useParams() ?? '';
-    const parsedParams = useMemo(() => {
+    
+    const paramsMap = useMemo(() => {
         const fixedParams = params ?? '';
-        const paramsArray = fixedParams.split('&')
+        const map = new Map();
+        fixedParams.split('&')
             .filter((par => par.includes('=')))
-            .map(par => par.split('='));
-        return paramsArray;
+            .map(par => par.split('='))
+            .forEach(par => paramsMap.set(par[0], par[1]));
+        return map;
     }, []);
-    console.log(parsedParams);
+
+    console.log(paramsMap);
 }
