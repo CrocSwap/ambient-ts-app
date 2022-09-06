b22579ea45442002a2f464b3065cf2fda9e11b00
Jadessu <junioradessu@gmail.com>
2022-09-06 10:06:14 -0500

    create harvest position settings

---
+++ src/components/HarvestPosition/HarvestPositionSettings/HarvestPositionSettings.tsx
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction } from 'react';
+import styles from './HarvestPositionSettings.module.css';
+import { RiCloseLine } from 'react-icons/ri';
+
+interface HarvestPositionSettingsPropsIF {
+    showSettings: boolean;
+    setShowSettings: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function HarvestPositionSettings(props: HarvestPositionSettingsPropsIF) {
+    const { showSettings, setShowSettings } = props;
+
+    // console.log(showSettings);
+
+    return (
+        <div className={styles.settings_container}>
+            {showSettings && (
+                <div className={styles.close_button} onClick={() => setShowSettings(false)}>
+                    <RiCloseLine size={25} />
+                </div>
+            )}
+            <h4>Remove Range Settings</h4>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, veniam obcaecati. Sit
+            perspiciatis natus porro nesciunt quasi, voluptatem atque, aut corrupti.
+        </div>
+    );
+}
