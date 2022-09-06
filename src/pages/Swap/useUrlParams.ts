6ccf0024c29b7d4d0a48d3e262335a6c05d84da9
mhsmith321 <emily.drake.smith@gmail.com>
2022-09-06 13:46:51 -0400

    annotate code in useUrlParameters.ts file

--- src/pages/Swap/useUrlParams.ts
+++ src/pages/Swap/useUrlParams.ts
@@ -3,25 +3,39 @@ import { useParams } from 'react-router-dom';
 import swapParams from '../../utils/classes/swapParams';
 
 export const useUrlParams = (module: string) => {
+    // get URL parameters, empty string if undefined
     const { params } = useParams() ?? '';
 
+    // parse parameter string into [key, value] tuples
+    // useMemo() with empty dependency array runs once on initial render
     const parsedParams = useMemo(() => {
+        // get URL parameters or empty string if undefined
         const fixedParams = params ?? '';
+        // split params string at every ampersand
         const paramsArray = fixedParams.split('&')
+            // remove any values missing an = symbol
             .filter(par => par.includes('='))
+            // split substrings at = symbols to make [key, value] tuples
             .map(par => par.split('='))
+            // remove empty strings created by extra = symbols
             .map(par => par.filter(e => e !== ''))
+            // remove tuples with trisomy issues
             .filter(par => par.length === 2);
+        // return array of parameters
         return paramsArray;
     }, []);
 
+    // router to feed parameters into the correct object constructor
     const makeParams = (input: string[][]) => {
         switch (module) {
+            // swap module
             case 'swap':
                 return new swapParams(input);
+            // default pathway (considered an error pathway)
             default: return;
         }
     }
 
+    // return parameters object
     return makeParams(parsedParams);
 }
