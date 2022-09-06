f110d80c177ee6e3ddc56c731717348a16cc09d7
mhsmith321 <emily.drake.smith@gmail.com>
2022-09-06 12:23:11 -0400

    add logic to useUrlParams.ts to filter out unrecognized parameters

--- src/pages/Swap/useUrlParams.ts
+++ src/pages/Swap/useUrlParams.ts
@@ -3,12 +3,17 @@ import { useParams } from 'react-router-dom';
 
 export const useUrlParams = () => {
     const { params } = useParams() ?? '';
+
+    const recognizedParams = ['chain', 'tokenA', 'tokenB'];
+
     const parsedParams = useMemo(() => {
         const fixedParams = params ?? '';
         const paramsArray = fixedParams.split('&')
-            .filter((par => par.includes('=')))
-            .map(par => par.split('='));
+            .filter(par => par.includes('='))
+            .map(par => par.split('='))
+            .filter(par => recognizedParams.includes(par[0]));
         return paramsArray;
     }, []);
+
     console.log(parsedParams);
 }
