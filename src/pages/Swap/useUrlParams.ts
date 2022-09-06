d6f7b18a992f6f63b12c65c8e019d51e8a030085
mhsmith321 <emily.drake.smith@gmail.com>
2022-09-06 11:08:16 -0400

    memoize logic in useUrlParams.ts to deconstruct URL parameters only on initial render

--- src/pages/Swap/useUrlParams.ts
+++ src/pages/Swap/useUrlParams.ts
@@ -1,8 +1,11 @@
+import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const useUrlParams = () => {
     const { params } = useParams() ?? '';
-    const fixedParams = params ?? '';
-    const paramsArray = fixedParams.split('&');
-    console.log({paramsArray});
+    const paramsArray = useMemo(() => {
+        const fixedParams = params ?? '';
+        return fixedParams.split('&');
+    }, []);
+    console.log(paramsArray);
 }
