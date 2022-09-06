1984ca80ff4618a7f2cde53aa16e383ccce8bf8b
mhsmith321 <emily.drake.smith@gmail.com>
2022-09-06 11:33:26 -0400

    remove Map logic from useUrlParams.ts due to initialization issues

--- src/pages/Swap/useUrlParams.ts
+++ src/pages/Swap/useUrlParams.ts
@@ -3,16 +3,12 @@ import { useParams } from 'react-router-dom';
 
 export const useUrlParams = () => {
     const { params } = useParams() ?? '';
-    
-    const paramsMap = useMemo(() => {
+    const parsedParams = useMemo(() => {
         const fixedParams = params ?? '';
-        const map = new Map();
-        fixedParams.split('&')
+        const paramsArray = fixedParams.split('&')
             .filter((par => par.includes('=')))
-            .map(par => par.split('='))
-            .forEach(par => paramsMap.set(par[0], par[1]));
-        return map;
+            .map(par => par.split('='));
+        return paramsArray;
     }, []);
-
-    console.log(paramsMap);
+    console.log(parsedParams);
 }
