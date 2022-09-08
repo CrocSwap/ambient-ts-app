f8695277da4c0e179b92131e40a7a34cb7d847aa
Jadessu <junioradessu@gmail.com>
2022-09-08 09:06:08 -0500

    clean up order card header

--- src/components/Trade/TradeTabs/Orders/OrderCardHeader.tsx
+++ src/components/Trade/TradeTabs/Orders/OrderCardHeader.tsx
@@ -2,19 +2,46 @@ import { Dispatch, SetStateAction, useMemo } from 'react';
 import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 import styles from './OrderCardHeader.module.css';
 
-interface OrderCardHeaderPropsIF {
+type ColumnData = {
+    name: string;
+    sortable: boolean;
+    className: string;
+};
+
+interface OrderCardHeaderWithSortPropsIF {
     data: {
         name: string;
         sortable: boolean;
+        className: string;
     };
+    sortBy: string;
+    reverseSort: boolean;
+}
+interface OrderCardHeaderPropsIF {
+    // data: {
+    //     name: string;
+    //     sortable: boolean;
+    //     className: string;
+    // };
+
     sortBy: string;
     setSortBy: Dispatch<SetStateAction<string>>;
     reverseSort: boolean;
     setReverseSort: Dispatch<SetStateAction<boolean>>;
+
+    columnHeaders: ColumnData[];
 }
 
 export default function OrderCardHeader(props: OrderCardHeaderPropsIF) {
-    const { data, sortBy, setSortBy, reverseSort, setReverseSort } = props;
+    const {
+        // data,
+
+        sortBy,
+        setSortBy,
+        reverseSort,
+        setReverseSort,
+        columnHeaders,
+    } = props;
 
     function handleClick(name: string) {
         console.clear();
@@ -43,29 +70,62 @@ export default function OrderCardHeader(props: OrderCardHeaderPropsIF) {
         </div>
     );
 
-    const arrow = useMemo(() => {
-        if (sortBy !== data.name.toLowerCase()) {
-            return arrowPlaceholder;
-        } else if (!reverseSort) {
-            return <FaAngleDown />;
-        } else if (reverseSort) {
-            return <FaAngleUp />;
-        } else {
-            return arrowPlaceholder;
-        }
-    }, [sortBy, reverseSort]);
+    function OrderCardHeaderWithSort(props: OrderCardHeaderWithSortPropsIF) {
+        const { data, sortBy, reverseSort } = props;
 
-    // TODO:   @Junior we need to make <div> wrapping the arrow icon a fixed
-    // TODO:   ... width so the parent element does not resize based on whether
-    // TODO:   ... or not it is being rendered
+        const arrow = useMemo(() => {
+            if (sortBy !== data.name.toLowerCase()) {
+                return arrowPlaceholder;
+            } else if (!reverseSort) {
+                return <FaAngleDown />;
+            } else if (reverseSort) {
+                return <FaAngleUp />;
+            } else {
+                return arrowPlaceholder;
+            }
+        }, [sortBy, reverseSort]);
+
+        // TODO:   @Junior we need to make <div> wrapping the arrow icon a fixed
+        // TODO:   ... width so the parent element does not resize based on whether
+        // TODO:   ... or not it is being rendered
+
+        return (
+            <section
+                onClick={() => handleClick(data.name.toLowerCase())}
+                className={`${styles.sortable_container} ${styles[data.className]}`}
+            >
+                <p>{data.name}</p>
+                <div className={styles.arrow_wrapper}>{arrow}</div>
+            </section>
+        );
+    }
+
+    const columnHeaderContent = (
+        <>
+            {columnHeaders.map((header) => (
+                <OrderCardHeaderWithSort
+                    key={`orderDataHeaderField${header.name}`}
+                    data={header}
+                    sortBy={sortBy}
+                    reverseSort={reverseSort}
+                />
+            ))}
+        </>
+    );
 
     return (
-        <div
-            className={styles.order_card_header}
-            onClick={() => handleClick(data.name.toLowerCase())}
-        >
-            <p>{data.name}</p>
-            <div className={styles.arrow_wrapper}>{arrow}</div>
+        // <div
+        //     className={styles.order_card_header}
+        //     onClick={() => handleClick(data.name.toLowerCase())}
+        // >
+        //     <p className={styles.headerClassName}>{data.name}
+
+        //     </p>
+        //     <div className={styles.arrow_wrapper}>{arrow}</div>
+        // </div>
+        <div className={styles.main_container}>
+            <div className={styles.row_container}>{columnHeaderContent}</div>
+            <div></div>
         </div>
     );
 }
