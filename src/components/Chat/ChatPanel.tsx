48b079c109951923833565a2c905995d76cec2ea
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2022-09-08 16:04:25 +0300

    clear warning

--- src/components/Chat/ChatPanel.tsx
+++ src/components/Chat/ChatPanel.tsx
@@ -111,6 +111,7 @@ export default function ChatPanel(props: ChatProps) {
                     className={`
                     ${styles.main_body}
                     `}
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
                     onClick={(e: any) => e.stopPropagation()}
                 >
                     <div
