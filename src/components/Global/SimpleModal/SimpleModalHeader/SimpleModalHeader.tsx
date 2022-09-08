048427ef751e326280ab9c991e24d92b5918c358
Jadessu <junioradessu@gmail.com>
2022-09-08 14:23:42 -0500

    create simple modal header files

---
+++ src/components/Global/SimpleModal/SimpleModalHeader/SimpleModalHeader.tsx
@@ -0,0 +1,8 @@
+import styles from './SimpleModalHeader.module.css';
+
+interface SimpleModalHeaderPropsIF {
+    title: 'string';
+}
+export default function SimpleModalHeader(props: SimpleModalHeaderPropsIF) {
+    return <header></header>;
+}
