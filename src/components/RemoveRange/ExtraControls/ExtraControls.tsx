ea0aea65d1d5f9800a9f8de8c0bb41d3b42efd74
Jadessu <junioradessu@gmail.com>
2022-09-09 15:08:48 -0500

    create full extra controls

--- src/components/RemoveRange/ExtraControls/ExtraControls.tsx
+++ src/components/RemoveRange/ExtraControls/ExtraControls.tsx
@@ -1,11 +1,20 @@
 import styles from './ExtraControls.module.css';
 import Toggle2 from '../../Global/Toggle/Toggle2';
+import { MdAccountBalanceWallet } from 'react-icons/md';
+import ambientLogo from '../../../assets/images/logos/ambient_logo.svg';
+
 export default function ExtraControls() {
     const exchangeBalanceControl = (
         <section className={styles.wallet_container}>
             <div className={styles.wallet_container_left}>
-                <div>Wallet</div>
-                <div>logo</div>
+                <div className={styles.wallet_amount}>
+                    <MdAccountBalanceWallet size={15} color='#EBEBFF' />
+                    Wallet
+                </div>
+                <div className={styles.exchange_text}>
+                    <img src={ambientLogo} width='15' alt='' />
+                    0.00
+                </div>
             </div>
 
             <Toggle2
@@ -16,6 +25,23 @@ export default function ExtraControls() {
             />
         </section>
     );
+    const gaslesssTransactionControl = (
+        <section className={styles.gasless_container}>
+            <div className={styles.gasless_text}>Enable Gasless Transaction</div>
 
-    return <div className={styles.main_container}>{exchangeBalanceControl}</div>;
+            <Toggle2
+                isOn={false}
+                handleToggle={() => console.log('toggled')}
+                id='gasless_transaction_toggle'
+                disabled={true}
+            />
+        </section>
+    );
+
+    return (
+        <div className={styles.main_container}>
+            {exchangeBalanceControl}
+            {gaslesssTransactionControl}
+        </div>
+    );
 }
