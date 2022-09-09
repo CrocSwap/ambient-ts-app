a411c039bdec6e159849b8a152347fc3d27db90a
Jadessu <junioradessu@gmail.com>
2022-09-09 12:30:36 -0500

    create remove range header jsx and files

--- src/components/RemoveRange/RemoveRangeHeader/RemoveRangeHeader.tsx
+++ src/components/RemoveRange/RemoveRangeHeader/RemoveRangeHeader.tsx
@@ -1,5 +1,19 @@
 import styles from './RemoveRangeHeader.module.css';
+import { BiArrowBack } from 'react-icons/bi';
 
-export default function RemoveRangeHeader() {
-    return <div>remove</div>;
+import { VscClose } from 'react-icons/vsc';
+interface RemoveRangeHeaderPropsIF {
+    title: string;
+    onClose: () => void;
+}
+export default function RemoveRangeHeader(props: RemoveRangeHeaderPropsIF) {
+    return (
+        <header className={styles.header_container}>
+            <div></div>
+            <h2>{props.title}</h2>
+            <div onClick={props.onClose}>
+                <VscClose size={22} />
+            </div>
+        </header>
+    );
 }
