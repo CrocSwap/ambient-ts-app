d85c6684fedd5609a4c415581654df88197aff25
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2022-09-13 10:05:18 +0300

    clear warning

--- src/components/Chat/MessagePanel/InputBox/MessageInput.tsx
+++ src/components/Chat/MessagePanel/InputBox/MessageInput.tsx
@@ -29,7 +29,7 @@ export default function MessageInput(props: MessageInputProps) {
 
     const [message, setMessage] = useState('');
     const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-    const { user, account, enableWeb3, isWeb3Enabled, isAuthenticated } = useMoralis();
+    const { isWeb3Enabled, isAuthenticated } = useMoralis();
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleEmojiClick = (event: any, emoji: any) => {
@@ -65,10 +65,6 @@ export default function MessageInput(props: MessageInputProps) {
         setMessage(e.target.value);
     };
 
-    const accountProps = {
-        isAuthenticated: isAuthenticated,
-        isWeb3Enabled: isWeb3Enabled,
-    };
     return (
         <div className={styles.input_box}>
             <div className={styles.input}>
