c502ab18d3af8ab100db0b83758a9af55523e867
Jadessu <junioradessu@gmail.com>
2022-09-13 09:18:36 -0500

    create icon with tooltip

--- src/components/Global/IconWithTooltip/IconWithTooltip.tsx
+++ src/components/Global/IconWithTooltip/IconWithTooltip.tsx
@@ -1,11 +1,36 @@
 import { ReactNode } from 'react';
+import { DefaultTooltip } from '../StyledTooltip/StyledTooltip';
 import styles from './IconWithTooltip.module.css';
 
 interface IconWithTooltipPropsIF {
     children: ReactNode;
+    symbolTitle: string;
+    placement?:
+        | 'right'
+        | 'bottom-end'
+        | 'bottom-start'
+        | 'bottom'
+        | 'left-end'
+        | 'left-start'
+        | 'left'
+        | 'right-end'
+        | 'right-start'
+        | 'top-end'
+        | 'top-start'
+        | 'top'
+        | undefined;
 }
 
 export default function IconWithTooltip(props: IconWithTooltipPropsIF) {
-    const { children } = props;
-    return <div className={styles.row}>{children}</div>;
+    const { children, symbolTitle, placement } = props;
+    return (
+        <DefaultTooltip
+            title={symbolTitle}
+            enterDelay={400}
+            leaveDelay={200}
+            placement={placement ? placement : 'bottom'}
+        >
+            <div>{children}</div>
+        </DefaultTooltip>
+    );
 }
