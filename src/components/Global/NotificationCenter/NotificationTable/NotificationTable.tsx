4311c6a71864af35a46a1b5edc6834ff210c7bc8
Jadessu <junioradessu@gmail.com>
2022-09-14 10:56:03 -0500

    create notification table files

--- src/components/Global/NotificationCenter/NotificationTable/NotificationTable.tsx
+++ src/components/Global/NotificationCenter/NotificationTable/NotificationTable.tsx
@@ -1,11 +1,24 @@
-import { ReactNode } from 'react';
-import styles from './Row.module.css';
-
-interface RowPropsIF {
-    children: ReactNode;
+import styles from './NotificationTable.module.css';
+import { Dispatch, SetStateAction } from 'react';
+interface NotificationTableProps {
+    showNotificationTable: boolean;
+    setShowNotificationTable: Dispatch<SetStateAction<boolean>>;
 }
+const NotificationTable = (props: NotificationTableProps) => {
+    const { showNotificationTable } = props;
 
-export default function Row(props: RowPropsIF) {
-    const { children } = props;
-    return <div className={styles.row}>{children}</div>;
-}
+    if (!showNotificationTable) return null;
+    return (
+        <div className={styles.container}>
+            <section className={styles.header}>Notifications</section>
+
+            <section className={styles.content}></section>
+
+            <section className={styles.footer}>
+                <button>Clear all</button>
+            </section>
+        </div>
+    );
+};
+
+export default NotificationTable;
