9fb486dd6677e618e1c8a99d1a147e790a235be1
Jadessu <junioradessu@gmail.com>
2022-09-14 10:57:46 -0500

    create notification center jsx and styling

--- src/components/Global/NotificationCenter/NotificationCenter.tsx
+++ src/components/Global/NotificationCenter/NotificationCenter.tsx
@@ -1,11 +1,43 @@
-import { ReactNode } from 'react';
 import styles from './NotificationCenter.module.css';
+import { motion, AnimatePresence, useAnimation } from 'framer-motion';
+import React, { useEffect, useRef, Dispatch, SetStateAction } from 'react';
+import NotificationTable from './NotificationTable/NotificationTable';
+import ActivityIndicator from './ActivityIndicator/ActivityIndicator';
+
+const animStates = {
+    hidden: { scale: 0, opacity: 0 },
+    visible: { scale: 1, opacity: 1 },
+    pop: { scale: [1.3, 1], opacity: 1 },
+    hover: { scale: 1.1 },
+    pressed: { scale: 0.95 },
+};
 
 interface NotificationCenterPropsIF {
-    children: ReactNode;
+    value: number;
+    pending: boolean;
+
+    showNotificationTable: boolean;
+    setShowNotificationTable: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function NotificationCenter(props: NotificationCenterPropsIF) {
-    const { children } = props;
-    return <div className={styles.row}>{children}</div>;
-}
+const NotificationCenter = (props: NotificationCenterPropsIF) => {
+    const { value, pending, showNotificationTable, setShowNotificationTable } = props;
+
+    return (
+        <div className={styles.container}>
+            <ActivityIndicator
+                value={value}
+                pending={pending}
+                showNotificationTable={showNotificationTable}
+                setShowNotificationTable={setShowNotificationTable}
+            />
+
+            <NotificationTable
+                showNotificationTable={showNotificationTable}
+                setShowNotificationTable={setShowNotificationTable}
+            />
+        </div>
+    );
+};
+
+export default NotificationCenter;
