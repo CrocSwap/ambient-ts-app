a731186c588f25a6132c45a45a238d16958da20f
Jadessu <junioradessu@gmail.com>
2022-09-16 13:53:34 -0500

    seperate main variant into its own file

--- src/App/components/PageHeader/NetworkSelector/NewNetworkSelector.tsx
+++ src/App/components/PageHeader/NetworkSelector/NewNetworkSelector.tsx
@@ -11,52 +11,58 @@ const itemVariants: Variants = {
     closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
 };
 
+const mainVariant: Variants = {
+    open: {
+        clipPath: 'inset(0% 0% 0% 0% round 10px)',
+        transition: {
+            type: 'spring',
+            bounce: 0,
+            duration: 0.7,
+            delayChildren: 0.3,
+            staggerChildren: 0.05,
+        },
+    },
+    closed: {
+        clipPath: 'inset(10% 50% 90% 50% round 10px)',
+        transition: {
+            type: 'spring',
+            bounce: 0,
+            duration: 0.3,
+        },
+    },
+};
+
 export default function NewNetworkSelector() {
     const [isOpen, setIsOpen] = useState(false);
 
+    const dropdownMenuArrow = (
+        <motion.div
+            variants={{
+                open: { rotate: 180 },
+                closed: { rotate: 0 },
+            }}
+            transition={{ duration: 0.2 }}
+            style={{ originY: 0.55 }}
+        >
+            <svg width='15' height='15' viewBox='0 0 20 20'>
+                <path d='M0 7 L 20 7 L 10 16' />
+            </svg>
+        </motion.div>
+    );
+
     return (
-        <motion.nav initial={false} animate={isOpen ? 'open' : 'closed'} className='menu'>
+        <motion.nav initial={false} animate={isOpen ? 'open' : 'closed'} className={styles.menu}>
             <motion.button
                 whileTap={{ scale: 0.97 }}
                 onClick={() => setIsOpen(!isOpen)}
                 className={styles.main_button}
             >
                 Menu
-                <motion.div
-                    variants={{
-                        open: { rotate: 180 },
-                        closed: { rotate: 0 },
-                    }}
-                    transition={{ duration: 0.2 }}
-                    style={{ originY: 0.55 }}
-                >
-                    <svg width='15' height='15' viewBox='0 0 20 20'>
-                        <path d='M0 7 L 20 7 L 10 16' />
-                    </svg>
-                </motion.div>
+                {dropdownMenuArrow}
             </motion.button>
 
             <motion.ul
-                variants={{
-                    open: {
-                        clipPath: 'inset(0% 0% 0% 0% round 10px)',
-                        transition: {
-                            type: 'spring',
-                            bounce: 0,
-                            duration: 0.7,
-                            delayChildren: 0.3,
-                            staggerChildren: 0.05,
-                        },
-                    },
-                    closed: {
-                        clipPath: 'inset(10% 50% 90% 50% round 10px)',
-                        transition: {
-                            type: 'spring',
-                            bounce: 0,
-                            duration: 0.3,
-                        },
-                    },
-                }}
+                variants={mainVariant}
                 style={{ pointerEvents: isOpen ? 'auto' : 'none' }}
                 className={styles.main_container}
             >
