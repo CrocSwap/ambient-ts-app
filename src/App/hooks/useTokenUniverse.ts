19d4bbe698949f27b54b1b9002b1d9d2ce29a2b2
mhsmith321 <emily.drake.smith@gmail.com>
2022-09-16 15:01:59 -0400

    stub up code in useTokenUniverse.ts to hold the current token universe in state

--- src/App/hooks/useTokenUniverse.ts
+++ src/App/hooks/useTokenUniverse.ts
@@ -1,3 +1,10 @@
+import { useEffect, useState } from 'react';
+
 export const useTokenUniverse = (chain: string) => {
     console.log(`ran useTokenUniverse() hook on chain: ${chain}`);
+
+    const [tokens, setTokens] = useState();
+
+    useEffect(() => console.log(tokens), tokens);
+    useEffect(() => setTokens(tokens), []);
 }
