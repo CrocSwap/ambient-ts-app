553883c4a79f76839dc411e7be0b194bb8efcc8d
Jadessu <junioradessu@gmail.com>
2022-09-21 14:13:48 -0500

    create disable confirmation modal toggle

--- src/components/Global/ConfirmationModalControl/ConfirmationModalControl.tsx
+++ src/components/Global/ConfirmationModalControl/ConfirmationModalControl.tsx
@@ -1,5 +1,17 @@
+import Toggle2 from '../Toggle/Toggle2';
 import styles from './ConfirmationModalControl.module.css';
+import { useState } from 'react';
 
 export default function ConfirmationModalControl() {
-    return <div className={styles.main_container}></div>;
+    const [disableConfirmationModal, setDisableConfirmationModal] = useState(false);
+    return (
+        <div className={styles.main_container}>
+            <Toggle2
+                isOn={disableConfirmationModal}
+                disabled={false}
+                handleToggle={() => setDisableConfirmationModal(!disableConfirmationModal)}
+                id='disabled_confirmation_modal_toggle'
+            />
+        </div>
+    );
 }
