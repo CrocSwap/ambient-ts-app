a016fdddb95ecad296af37879467e6972defc807
Jadessu <junioradessu@gmail.com>
2022-09-26 10:57:31 -0500

    create jsx structure for transaction accordion

--- src/components/Trade/TradeTabs/Transactions/TransactionAccordions/TransactionAccordions.tsx
+++ src/components/Trade/TradeTabs/Transactions/TransactionAccordions/TransactionAccordions.tsx
@@ -1,5 +1,53 @@
 import styles from './TransactionAccordions.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import { RiArrowUpSFill } from 'react-icons/ri';
 
-export default function TransactionAccordions() {
-    return <div></div>;
+interface TransactionAccordionsPropsIF {
+    i: number;
+    expanded: number | boolean;
+    setExpanded: any;
+}
+export default function TransactionAccordions(props: TransactionAccordionsPropsIF) {
+    const { i, expanded, setExpanded } = props;
+    const isOpen = i === expanded;
+    return (
+        <motion.div
+            className={`${styles.main_container} ${isOpen ? styles.open_border : ''}`}
+            initial={false}
+            animate={{ border: isOpen ? '1px solid rgba( 255, 255, 255, 0.18 )' : '' }}
+        >
+            <motion.header
+                initial={false}
+                animate={{
+                    backgroundColor: isOpen ? '#171d27' : '#12171f',
+                    color: isOpen ? '#EBEBFF' : '#bdbdbd',
+                }}
+                onClick={() => setExpanded(isOpen ? false : i)}
+            >
+                {/* I am header */}
+                {'headerTitle'}
+                {isOpen ? <RiArrowUpSFill /> : <IoMdArrowDropdown />}
+            </motion.header>
+            <AnimatePresence initial={false}>
+                {isOpen && (
+                    <motion.section
+                        key='content'
+                        initial='collapsed'
+                        animate='open'
+                        exit='collapsed'
+                        className={styles.accordion_content}
+                        variants={{
+                            open: { opacity: 1, height: 'auto' },
+                            collapsed: { opacity: 0, height: 0 },
+                        }}
+                        transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+                    >
+                        {/* <ContentPlaceholder /> */}
+                        <div>I am the children of the transaction</div>
+                    </motion.section>
+                )}
+            </AnimatePresence>
+        </motion.div>
+    );
 }
