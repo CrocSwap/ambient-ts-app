e9a3b6e6a40d188f183de30bdb022603e2974b85
Jadessu <junioradessu@gmail.com>
2022-10-06 14:50:48 -0500

    create a handleChangeStep function

--- src/pages/InitPool/InitPool.tsx
+++ src/pages/InitPool/InitPool.tsx
@@ -1,16 +1,25 @@
 import styles from './InitPool.module.css';
 import { useUrlParams } from './useUrlParams';
-
+import { useState } from 'react';
 export default function InitPool() {
     const newPoolData = useUrlParams();
     console.log(newPoolData);
 
-    const progressSteps = [
+    const progressStepsData = [
         { id: 1, name: 'Choose tokens & weights' },
         { id: 2, name: 'Set pool fees' },
         { id: 3, name: 'Set initial liquidity' },
         { id: 4, name: 'Confirm  pool creation' },
     ];
+
+    const [progressStep, setProgressStep] = useState(0);
+
+    const handleChangeStep = (e: any) => {
+        e.target.value === 'prev' && progressStep > 0 && setProgressStep(progressStep - 1);
+        e.target.value === 'next' &&
+            progressStep < progressStepsData.length &&
+            setProgressStep(progressStep + 1);
+    };
 
     return (
         <main className={styles.main}>
