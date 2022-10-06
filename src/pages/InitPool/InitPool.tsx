5c02b98a86e8a9887df5427d6c48f132217d2378
Jadessu <junioradessu@gmail.com>
2022-10-06 14:47:45 -0500

    create progress steps data

--- src/pages/InitPool/InitPool.tsx
+++ src/pages/InitPool/InitPool.tsx
@@ -5,6 +5,13 @@ export default function InitPool() {
     const newPoolData = useUrlParams();
     console.log(newPoolData);
 
+    const progressSteps = [
+        { id: 1, name: 'Choose tokens & weights' },
+        { id: 2, name: 'Set pool fees' },
+        { id: 3, name: 'Set initial liquidity' },
+        { id: 4, name: 'Confirm  pool creation' },
+    ];
+
     return (
         <main className={styles.main}>
             <div className={styles.init_pool_container}>
