4af659b946518902ef9bf0bb6ee8ba13a144a9bc
Jadessu <junioradessu@gmail.com>
2022-10-06 15:24:47 -0500

    create step style

--- src/components/InitPool/InitPoolSteps/InitPoolSteps.tsx
+++ src/components/InitPool/InitPoolSteps/InitPoolSteps.tsx
@@ -1,5 +1,6 @@
 import styles from './InitPoolSteps.module.css';
 import { useState, Dispatch, SetStateAction } from 'react';
+import { act } from 'react-dom/test-utils';
 
 interface InitPoolStepsPropsIF {
     progressStepsData: {
@@ -8,6 +9,49 @@ interface InitPoolStepsPropsIF {
     }[];
     progressStep: number;
     setProgressStep: Dispatch<SetStateAction<number>>;
+}
+
+interface InitStepPropsIF {
+    step: {
+        id: number;
+        name: string;
+    };
+    progressStep: number;
+    index: number;
+}
+function Step(props: InitStepPropsIF) {
+    const { step, progressStep, index } = props;
+
+    const activeStyle = (
+        <div className={styles.step_active}>
+            <div />{' '}
+        </div>
+    );
+    const nonActiveStyle = <div className={styles.step_non_active} />;
+    const completedStyle = <div className={styles.step_completed} />;
+
+    return (
+        <div>
+            {index <= progressStep ? (
+                index === progressStep ? (
+                    <div className={styles.step_content}>
+                        {activeStyle}
+                        <p>{step.name}</p>
+                    </div>
+                ) : (
+                    <div className={styles.step_content}>
+                        {completedStyle}
+                        <p>{step.name}</p>
+                    </div>
+                )
+            ) : (
+                <div className={styles.step_content}>
+                    {nonActiveStyle}
+                    <p>{step.name}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 export default function InitPoolSteps(props: InitPoolStepsPropsIF) {
     const { progressStepsData, progressStep, setProgressStep } = props;
@@ -20,22 +64,13 @@ export default function InitPoolSteps(props: InitPoolStepsPropsIF) {
     };
 
     const stepsDisplay = (
-        <div>
-            {progressStepsData.map((_, index) => (
+        <div className={styles.steps_display_container}>
+            {progressStepsData.map((step, index) => (
                 <>
-                    {index <= progressStep ? (
-                        index === progressStep ? (
-                            <p style={{ color: '#db7777' }}>I am active</p>
-                        ) : (
-                            <p style={{ color: 'green' }}>I am completed</p>
-                        )
-                    ) : (
-                        <p style={{ color: 'grey' }}>I am not active</p>
-                    )}
+                    <Step step={step} index={index} progressStep={progressStep} />
                 </>
             ))}
-            <hr />
-            Step: {progressStep}
+
             <button value='prev' onClick={handleChangeStep}>
                 Previous step
             </button>
@@ -44,5 +79,5 @@ export default function InitPoolSteps(props: InitPoolStepsPropsIF) {
             </button>
         </div>
     );
-    return <div className={styles.container}>yes</div>;
+    return <div className={styles.container}>{stepsDisplay}</div>;
 }
