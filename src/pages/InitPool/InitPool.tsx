ef1850b00f681694910ecb22c8b6c46c6dc87d72
mhsmith321 <emily.drake.smith@gmail.com>
2022-10-11 13:14:44 -0400

    organizational changes to InitPool.tsx component file

--- src/pages/InitPool/InitPool.tsx
+++ src/pages/InitPool/InitPool.tsx
@@ -1,24 +1,37 @@
+// START: Import React and Dongles
 import { useEffect, useState } from 'react';
-import { CrocEnv } from '@crocswap-libs/sdk';
-import styles from './InitPool.module.css';
-import { useUrlParams } from './useUrlParams';
-import ContentContainer from '../../components/Global/ContentContainer/ContentContainer';
-import Button from '../../components/Global/Button/Button';
-import { useAppSelector } from '../../utils/hooks/reduxToolkit';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { VscClose } from 'react-icons/vsc';
-import InitPoolExtraInfo from '../../components/InitPool/InitPoolExtraInfo/InitPoolExtraInfo';
+import { CrocEnv } from '@crocswap-libs/sdk';
 
-interface InitPoolPropsIf {
+// START: Import JSX Components
+import InitPoolExtraInfo from '../../components/InitPool/InitPoolExtraInfo/InitPoolExtraInfo';
+import ContentContainer from '../../components/Global/ContentContainer/ContentContainer';
+import Button from '../../components/Global/Button/Button';
+
+// START: Import Local Files
+import styles from './InitPool.module.css';
+import { useUrlParams } from './useUrlParams';
+import { useAppSelector } from '../../utils/hooks/reduxToolkit';
+
+// interface for props
+interface InitPoolPropsIF {
     crocEnv: CrocEnv|undefined;
     showSidebar: boolean;
 }
-export default function InitPool(props: InitPoolPropsIf) {
+
+// react functional component
+export default function InitPool(props: InitPoolPropsIF) {
     const { crocEnv, showSidebar } = props;
 
+    // URL parameters
     const newPoolData = useUrlParams();
+
+    // data on token pair from RTK
+    // may not match params but we'll fix that later with token universe
     const { tokenA, tokenB } = useAppSelector((state) => state.tradeData);
 
+    // function to programmatically navigate the user
     const navigate = useNavigate();
 
     // DO NOT combine these hooks with useMemo()
@@ -43,49 +56,6 @@ export default function InitPool(props: InitPoolPropsIf) {
     // re-run hook if a new crocEnv is created
     // this will happen if the user switches chains
     }, [crocEnv]);
-
-    const initialPoolPriceDisplay = (
-        <div className={styles.pool_price_container}>
-            <span>Initial Price</span>
-            <section>
-                <input
-                    id={'initial-pool-price-quantity'}
-                    className={styles.currency_quantity}
-                    placeholder='e.g. 1500 (ETH/TokenX)'
-                    type='string'
-                    inputMode='decimal'
-                    autoComplete='off'
-                    autoCorrect='off'
-                    min='0'
-                    minLength={1}
-                    pattern='^[0-9]*[.,]?[0-9]*$'
-                    required
-                />
-            </section>
-        </div>
-    );
-
-    const poolDisplay = (
-        <div className={styles.pool_display_container}>
-            <div className={styles.pool_display}>
-                <div>
-                    <img src={tokenA.logoURI} alt='token a' />
-                    <h3>{tokenA.symbol}</h3>
-                </div>
-                <p>{tokenA.name}</p>
-            </div>
-
-            <div className={styles.pool_display}>
-                <div>
-                    <img src={tokenB.logoURI} alt='token b' />
-                    <h3>{tokenB.symbol}</h3>
-                </div>
-                <p>{tokenB.name}</p>
-            </div>
-            {initialPoolPriceDisplay}
-            <InitPoolExtraInfo />
-        </div>
-    );
 
     return (
         <main
@@ -116,7 +86,41 @@ export default function InitPool(props: InitPoolPropsIf) {
                         <header>
                             <h1>INITIALIZE POOL</h1>
                         </header>
-                        {poolDisplay}
+                        <div className={styles.pool_display_container}>
+                            <div className={styles.pool_display}>
+                                <div>
+                                    <img src={tokenA.logoURI} alt='token a' />
+                                    <h3>{tokenA.symbol}</h3>
+                                </div>
+                                <p>{tokenA.name}</p>
+                            </div>
+                            <div className={styles.pool_display}>
+                                <div>
+                                    <img src={tokenB.logoURI} alt='token b' />
+                                    <h3>{tokenB.symbol}</h3>
+                                </div>
+                                <p>{tokenB.name}</p>
+                            </div>
+                            <div className={styles.pool_price_container}>
+                                <span>Initial Price</span>
+                                <section>
+                                    <input
+                                        id={'initial-pool-price-quantity'}
+                                        className={styles.currency_quantity}
+                                        placeholder='e.g. 1500 (ETH/TokenX)'
+                                        type='string'
+                                        inputMode='decimal'
+                                        autoComplete='off'
+                                        autoCorrect='off'
+                                        min='0'
+                                        minLength={1}
+                                        pattern='^[0-9]*[.,]?[0-9]*$'
+                                        required
+                                    />
+                                </section>
+                            </div>
+                            <InitPoolExtraInfo />
+                        </div>
                         <footer>
                             <Button title='Next' action={() => console.log('completed')} />
                         </footer>
