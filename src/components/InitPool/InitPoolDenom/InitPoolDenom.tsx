880a61c4fb5e5a339e82973dde9cf9372fc11ce9
Ben Wolski <benwolski@gmail.com>
2022-10-18 14:25:27 -0400

    add denom switching functionality to initPool

--- src/components/InitPool/InitPoolDenom/InitPoolDenom.tsx
+++ src/components/InitPool/InitPoolDenom/InitPoolDenom.tsx
@@ -1,31 +1,35 @@
 // START: Import Local Files
 import styles from './InitPoolDenom.module.css';
 // import { TokenPairIF } from '../../../utils/interfaces/exports';
-import { useAppDispatch, useAppSelector } from '../../../utils/hooks/reduxToolkit';
-import { toggleDidUserFlipDenom } from '../../../utils/state/tradeDataSlice';
+import { useAppSelector } from '../../../utils/hooks/reduxToolkit';
+// import { toggleDidUserFlipDenom } from '../../../utils/state/tradeDataSlice';
+import { Dispatch, SetStateAction } from 'react';
 
 // interface for props
-// interface InitPoolDenomPropsIF {
-//     // tokenPair: TokenPairIF;
-//     // denominationsInBase: boolean;
-//     // poolPriceDisplay: number | undefined;
-//     // isOnTradeRoute?: boolean;
-//     // isTokenABase: boolean;
-//     // didUserFlipDenom: boolean;
-// }
+interface InitPoolDenomPropsIF {
+    setIsDenomBase: Dispatch<SetStateAction<boolean>>;
+    isDenomBase: boolean;
+    invertInitialPrice: () => void;
+    // tokenPair: TokenPairIF;
+    // denominationsInBase: boolean;
+    // poolPriceDisplay: number | undefined;
+    // isOnTradeRoute?: boolean;
+    // isTokenABase: boolean;
+    // didUserFlipDenom: boolean;
+}
 
 // TODO:  @Emily poolPriceDisplay is passed here as a prop for the purpose of managing
 // TODO:  ... which token to initialize the display too, if it's not necessary in the
 // TODO   ... end, please remove the value from props
 
-export default function InitPoolDenom() {
+export default function InitPoolDenom(props: InitPoolDenomPropsIF) {
     // const { tokenPair, isTokenABase, poolPriceDisplay, didUserFlipDenom } = props;
-
-    const dispatch = useAppDispatch();
+    const { isDenomBase, setIsDenomBase, invertInitialPrice } = props;
+    // const dispatch = useAppDispatch();
 
     const tradeData = useAppSelector((state) => state.tradeData);
 
-    const isDenomBase = tradeData.isDenomBase;
+    // const isDenomBase = tradeData.isDenomBase;
     const baseTokenSymbol = tradeData.baseToken.symbol;
     const quoteTokenSymbol = tradeData.quoteToken.symbol;
 
@@ -37,13 +41,19 @@ export default function InitPoolDenom() {
         <div className={styles.denomination_switch}>
             <button
                 className={!isDenomBase ? styles.active_button : styles.non_active_button}
-                onClick={() => dispatch(toggleDidUserFlipDenom())}
+                onClick={() => {
+                    invertInitialPrice();
+                    setIsDenomBase(!isDenomBase);
+                }}
             >
                 {baseTokenSymbol}
             </button>
             <button
                 className={isDenomBase ? styles.active_button : styles.non_active_button}
-                onClick={() => dispatch(toggleDidUserFlipDenom())}
+                onClick={() => {
+                    invertInitialPrice();
+                    setIsDenomBase(!isDenomBase);
+                }}
             >
                 {quoteTokenSymbol}
             </button>
