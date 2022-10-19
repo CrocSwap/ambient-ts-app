a867ef5f9dd1798f5d9ba38c59a345f5ad37c5ef
Jadessu <junioradessu@gmail.com>
2022-10-19 12:24:14 -0500

    add defaut imports to claim order

--- src/components/ClaimOrder/ClaimOrder.tsx
+++ src/components/ClaimOrder/ClaimOrder.tsx
@@ -1,5 +1,17 @@
 import styles from './ClaimOrder.module.css';
-
+import { useState, useEffect } from 'react';
+import { useProcessOrder } from '../../utils/hooks/useProcessOrder';
+import { ILimitOrderState } from '../../utils/state/graphDataSlice';
+import { CircleLoaderFailed } from '../Global/LoadingAnimations/CircleLoader/CircleLoader';
+import Button from '../Global/Button/Button';
+import Animation from '../Global/Animation/Animation';
+import completed from '../../assets/animations/completed.json';
+import { FiExternalLink } from 'react-icons/fi';
+import { formatAmount } from '../../utils/numbers';
+import { CrocEnv } from '@crocswap-libs/sdk';
+import { BigNumber } from 'ethers';
+import Toggle2 from '../Global/Toggle/Toggle2';
+import TooltipComponent from '../Global/TooltipComponent/TooltipComponent';
 export default function ClaimOrder() {
     return <div>claim</div>;
 }
