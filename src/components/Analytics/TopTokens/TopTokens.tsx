c5c85235f7e67cfea1cb4b657b6ae85e8a363015
Jadessu <junioradessu@gmail.com>
2022-11-01 15:04:35 -0500

    create top tokens component

--- src/components/Analytics/TopTokens/TopTokens.tsx
+++ src/components/Analytics/TopTokens/TopTokens.tsx
@@ -1,27 +1,30 @@
-// import TokenScrolls from '../Analytics/TokenScrolls/TokenScrolls';
 import styles from './TopTokens.module.css';
 import TopTokensCard from './TopTokensCard/TopTokensCard';
-// import TopTokensHeader from './TopTokensHeader/TopTokensHeader';
-// import { uniswapTokens } from '../../data/uniswapTokens'
+import TopTokensHeader from './TopTokensHeader/TopTokensHeader';
+import { uniswapTokens } from '../fakedata/uniswapTokens';
 
-// const tokenData = uniswapTokens[0].tokens
+const tokenData = uniswapTokens[0].tokens;
 
 export default function TopTokens() {
-    // const container = (
-    //     <div className={styles.item_container}>
-    //         {tokenData.slice(0, 12).map((token, idx) => (
-    //             <TopTokensCard name={token.name } img={token.logoURI} symbol={token.symbol} key={idx} number={idx + 1} />
-
-    //         ))}
-    //    </div>
-    // )
+    const container = (
+        <div className={styles.item_container}>
+            {tokenData.slice(0, 12).map((token, idx) => (
+                <TopTokensCard
+                    name={token.name}
+                    img={token.logoURI}
+                    symbol={token.symbol}
+                    key={idx}
+                    number={idx + 1}
+                />
+            ))}
+        </div>
+    );
     return (
         <div className={styles.main_container}>
             <p>All Tokens</p>
-            {/* <TokenRows /> */}
-            {/* <TokenScrolls/>
-          <TopTokensHeader/>
-          {container} */}
+
+            <TopTokensHeader />
+            {container}
         </div>
     );
 }
