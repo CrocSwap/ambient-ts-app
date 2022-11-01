889ed9387b782d2f04b148a86c207725a2f91d71
Jadessu <junioradessu@gmail.com>
2022-11-01 15:12:14 -0500

    create top pools content

---
+++ src/components/Analytics/TopPools/TopPools.tsx
@@ -0,0 +1,23 @@
+import styles from './TopPools.module.css';
+import TopPoolsCard from './TopPoolsCard/TopPoolsCard';
+import TopPoolsHeader from './TopPoolsHeader/TopPoolsHeader';
+import { uniswapPools } from '../fakedata/uniswapTokens';
+
+export default function TopPools() {
+    const container = (
+        <div className={styles.item_container}>
+            {uniswapPools.map((pair, idx) => (
+                <TopPoolsCard pair={pair} key={idx} number={idx + 1} />
+            ))}
+        </div>
+    );
+    return (
+        <div className={styles.main_container}>
+            <p>Trending Pools</p>
+            <p>All Pools</p>
+            <TopPoolsHeader />
+
+            {container}
+        </div>
+    );
+}
