b2c9fa4d01c124d9d1b25d7e87e62965f9ea0412
mhsmith321 <emily.drake.smith@gmail.com>
2022-11-01 15:51:57 -0400

    add comments to document code in useSoloSearch.ts file

--- src/components/Global/TokenSelectContainer/useSoloSearch.ts
+++ src/components/Global/TokenSelectContainer/useSoloSearch.ts
@@ -8,26 +8,37 @@ export const useSoloSearch = (
     TokenIF | null,
     Dispatch<SetStateAction<string>>
 ] => {
+    // raw input from the user
     const [input, setInput] = useState('');
 
+    // cleaned and validated version of raw user input
     const validatedInput = useMemo(() => {
+        // trim string and make it lower case
         const cleanInput = input.trim().toLowerCase();
+        // add '0x' to the front of the cleaned string if not present
         const fixedInput = cleanInput.startsWith('0x')
             ? cleanInput
             : '0x' + cleanInput;
+        // declare an output variable
         let output = '';
+        // check if string is a correctly-formed contract address
         if (
+            // check if string has 42 characters
             fixedInput.length === 42 &&
+            // check if string after '0x' is valid hexadecimal
             fixedInput.substring(2).match(/[0-9a-f]/g)
         ) {
+            // if fixed string is valid, assign it to the output variable
             output = fixedInput;
         }
-        // string starts with 0x then 40 hexadecimal characters
-        // string is 40 hexadecimal characters
+        // return output variable
         return output;
     }, [input]);
 
+    // data object for token matching user query
     const [token, setToken] = useState<TokenIF|null>(null);
+
+    // control flow to gatekeep secondary and tertiary calls
     const [isTokenFound, setIsTokenFound] = useState(false);
 
     useEffect(() => {
