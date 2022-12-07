ff1ed0bef69869e5ed4aa38f5871a1e9a7048bf7
mhsmith321 <emily.drake.smith@gmail.com>
2022-12-07 12:59:53 -0500

    refactor useToken.ts to check local storage recursively for token lists

--- src/App/hooks/useToken.ts
+++ src/App/hooks/useToken.ts
@@ -6,12 +6,14 @@ export const useToken = () => {
     // get allTokenLists from local storage
     // if it doesn't exist yet, check recurisvely until it does
     useEffect(() => {
-        console.log('woohoo');
-        localStorage.getItem('allTokenLists')
-            ? console.log('we have token lists')
-            : console.log('I do not see any lists yet');
-        const checkForTokenLists = () => {
+        const checkForTokenLists = (limiter=0) => {
             console.log('trigger function checForTokenLists()');
+            if (localStorage.getItem('allTokenLists')) {
+                console.log('we have token lists');
+            } else {
+                console.log('I do not see any lists yet');
+                setTimeout(() => checkForTokenLists(limiter + 1), 250);
+            }
         }
         checkForTokenLists();
     }, []);
