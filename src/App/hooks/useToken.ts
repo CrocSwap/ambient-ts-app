19f4829880ce69c06d754550a745ba6a29254d82
mhsmith321 <emily.drake.smith@gmail.com>
2022-12-07 12:38:02 -0500

    trigger useEffect() hook inside useToken.ts file

--- src/App/hooks/useToken.ts
+++ src/App/hooks/useToken.ts
@@ -1,3 +1,14 @@
+import { useEffect } from 'react';
+
 export const useToken = () => {
     console.log('triggered useToken() hook!');
+
+    // get allTokenLists from local storage
+    // if it doesn't exist yet, check recurisvely until it does
+    useEffect(() => {
+        console.log('woohoo');
+        localStorage.getItem('allTokenLists')
+            ? console.log('we have token lists')
+            : console.log('I do not see any lists yet');
+    }, []);
 }
