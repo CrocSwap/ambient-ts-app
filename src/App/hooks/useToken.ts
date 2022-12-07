6425c02939d7d2189ec019da6814ee995b43e54b
mhsmith321 <emily.drake.smith@gmail.com>
2022-12-07 13:05:02 -0500

    comment new code in useToken.ts file

--- src/App/hooks/useToken.ts
+++ src/App/hooks/useToken.ts
@@ -3,16 +3,21 @@ import { useEffect } from 'react';
 export const useToken = () => {
     console.log('triggered useToken() hook!');
 
-    // get allTokenLists from local storage
-    // if it doesn't exist yet, check recurisvely until it does
+    // get allTokenLists from local storage after initial render
     useEffect(() => {
+        // fn to check local storage for token lists with a recursion limiter
         const checkForTokenLists = (limiter=0) => {
-            console.log('trigger function checForTokenLists()');
+            // execute if local storage has token lists
             if (localStorage.getItem('allTokenLists')) {
                 console.log('we have token lists');
-            } else {
+            // call fn recursively if local storage does not have token lists
+            // limit recursive calls to 100
+            } else if (limiter < 100) {
                 console.log('I do not see any lists yet');
                 setTimeout(() => checkForTokenLists(limiter + 1), 250);
+            } else {
+                // console warning if max recursion depth is reached
+                console.warn('maximum recursion depth reached');
             }
         }
         checkForTokenLists();
