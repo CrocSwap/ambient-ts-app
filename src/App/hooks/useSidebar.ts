2cc443c217b9726ce63d5943cc9c7e082e50bde9
mhsmith321 <emily.drake.smith@gmail.com>
2022-12-08 20:11:38 -0500

    create useState() hook in useSidebar.ts to track user preference on sidebar in-session

--- src/App/hooks/useSidebar.ts
+++ src/App/hooks/useSidebar.ts
@@ -1,3 +1,10 @@
+import { useState } from 'react';
+
 export const useSidebar = () => {
     console.log('called custom hook useSidebar()');
+    const [sidebar, setSidebar] = useState(
+        JSON.parse(localStorage.getItem('user') as string).sidebar ?? 'open'
+    );
+    false && sidebar;
+    false && setSidebar;
 }
