9219195a4a37f7f4ee6d96e94b0942f5c3c39b64
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-04 07:38:03 -0500

    fix typo in hiddenPaths value in useSidebar.ts file

--- src/App/hooks/useSidebar.ts
+++ src/App/hooks/useSidebar.ts
@@ -44,7 +44,7 @@ export const useSidebar = (pathname: string): [
     // value whether to sidebar should be hidden on the current URL path
     const hidden = useMemo<boolean>(() => {
         // array of url paths on which to hide the sidebar
-        const hiddenPaths = ['/', 'swap'];
+        const hiddenPaths = ['/', '/swap'];
         // determine if the current URL path starts with any proscribed strings
         const isPathHidden = hiddenPaths.some(
             ((path: string) => pathname.startsWith(path))
