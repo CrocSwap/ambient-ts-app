0e2dc8e23ca969938880c607ff167dc35ee8a815
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-09 17:51:46 -0500

    port input validation logic from useSoloSearch.ts component to useSidebarSearch.ts

--- src/App/components/Sidebar/useSidebarSearch.ts
+++ src/App/components/Sidebar/useSidebarSearch.ts
@@ -1,22 +1,52 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useMemo, useState } from 'react';
 
 export const useSidebarSearch = (): [
     Dispatch<SetStateAction<string>>
 ] => {
     // raw user input from the DOM
     const [rawInput, setRawInput] = useState<string>('');
-    // sanitized version of DOM input
-    const [cleanInput, setCleanInput] = useState<string>('');
-    // make the linter happy until I use this value
-    false && cleanInput;
 
-    // run logic to sanitize raw input every time it changes
-    useEffect(() => {
-        // trim outer whitespace and convert to lower case
-        const fixedInput = rawInput.trim().toLowerCase();
-        // send sanitized input to local state
-        setCleanInput(fixedInput);
+    // search type ➜ '' or 'address' or 'nameOrAddress'
+    const [searchAs, setSearchAs] = useState<string|null>(null);
+    false && searchAs;
+
+    // cleaned and validated version of raw user input
+    const validatedInput = useMemo<string>(() => {
+        // trim string and make it lower case
+        const cleanInput = rawInput.trim().toLowerCase();
+        // action if input appears to be a contract address
+        if (
+            cleanInput.length === 42 ||
+            (cleanInput.length === 40 && !cleanInput.startsWith('0x'))
+        ) {
+            setSearchAs('address');
+            // if not an apparent token address, search name and symbol
+        } else if (cleanInput.length >= 2) {
+            setSearchAs('nameOrSymbol');
+            return cleanInput;
+            // otherwise treat as if there is no input entered
+        } else {
+            setSearchAs(null);
+            return '';
+        }
+        // add '0x' to the front of the cleaned string if not present
+        const fixedInput = cleanInput.startsWith('0x') ? cleanInput : '0x' + cleanInput;
+        // declare an output variable
+        let output = cleanInput;
+        // check if string is a correctly-formed contract address
+        if (
+            // check if string has 42 characters
+            fixedInput.length === 42 &&
+            // check if string after '0x' is valid hexadecimal
+            fixedInput.substring(2).match(/[0-9a-f]/g)
+        ) {
+            // if fixed string is valid, assign it to the output variable
+            output = fixedInput;
+        }
+        // return output variable
+        return output;
     }, [rawInput]);
+    false && validatedInput;
 
     return [
         setRawInput
