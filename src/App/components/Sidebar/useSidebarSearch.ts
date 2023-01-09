78150a2201215b0d7f480131d378d97cce02e1d0
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-09 17:36:59 -0500

    annotate new code in useSidebarSearch.ts file

--- src/App/components/Sidebar/useSidebarSearch.ts
+++ src/App/components/Sidebar/useSidebarSearch.ts
@@ -3,12 +3,18 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 export const useSidebarSearch = (): [
     Dispatch<SetStateAction<string>>
 ] => {
+    // raw user input from the DOM
     const [rawInput, setRawInput] = useState<string>('');
+    // sanitized version of DOM input
     const [cleanInput, setCleanInput] = useState<string>('');
+    // make the linter happy until I use this value
     false && cleanInput;
 
+    // run logic to sanitize raw input every time it changes
     useEffect(() => {
+        // trim outer whitespace and convert to lower case
         const fixedInput = rawInput.trim().toLowerCase();
+        // send sanitized input to local state
         setCleanInput(fixedInput);
     }, [rawInput]);
 
