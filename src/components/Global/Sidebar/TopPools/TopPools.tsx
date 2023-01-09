12ffdfd5587698a21d524e5f4b066ea33d03d1d9
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-09 17:10:05 -0500

    remove unnecessary useState import in TopPools.tsx component file

--- src/components/Global/Sidebar/TopPools/TopPools.tsx
+++ src/components/Global/Sidebar/TopPools/TopPools.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { PoolStatsFn } from '../../../../App/functions/getPoolStats';
 import { topPools } from '../../../../App/mockData';
 import { tradeData } from '../../../../utils/state/tradeDataSlice';
