5516451419bcba8925afe88f5e70f95b23feac2c
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-09 15:40:52 -0500

    create new global interface TempPoolIF to support current branch

---
+++ src/utils/interfaces/TempPoolIF.ts
@@ -0,0 +1,16 @@
+export interface TempPoolIF {
+    base: string,
+    baseDecimals: number,
+    baseSymbol: string,
+    blockFirstMint: number,
+    chainId: string,
+    curveStorageSlot: string,
+    id: string,
+    network: string,
+    poolHash: string,
+    poolIdx: number,
+    quote: string,
+    quoteDecimals: number,
+    quoteSymbol: string,
+    timeFirstMint: number
+}
