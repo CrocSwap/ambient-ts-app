c4288a2aa3ca59e737a3e7606bc5376b838342e7
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-10 13:48:31 -0500

    get current pathname in useClick.ts file via useLocation() hook

--- src/App/components/Sidebar/SidebarSearchResults/PoolsSearchResults/useClick.ts
+++ src/App/components/Sidebar/SidebarSearchResults/PoolsSearchResults/useClick.ts
@@ -1,3 +1,7 @@
+import { useLocation } from 'react-router-dom';
+
 export const useClick = () => {
+    const {pathname} = useLocation();
     console.log('fired custom hook useClick()');
+    console.log('current pathname is: ' + pathname);
 }
