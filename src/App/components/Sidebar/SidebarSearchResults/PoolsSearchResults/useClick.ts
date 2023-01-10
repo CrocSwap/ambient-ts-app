897136ed81376b2c27dc6cad6a7c2798e6a42bfc
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-10 13:54:22 -0500

    refactor logic to generate locationSlug in useClick.ts for better error handling

--- src/App/components/Sidebar/SidebarSearchResults/PoolsSearchResults/useClick.ts
+++ src/App/components/Sidebar/SidebarSearchResults/PoolsSearchResults/useClick.ts
@@ -1,7 +1,25 @@
+import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const useClick = () => {
     const {pathname} = useLocation();
-    console.log('fired custom hook useClick()');
-    console.log('current pathname is: ' + pathname);
+    const locationSlug = useMemo<string>(() => {
+        let slug: string;
+        try {
+            if (pathname.startsWith('/trade/market') || pathname.startsWith('/account')) {
+                slug = '/trade/market';
+            } else if (pathname.startsWith('/trade/limit')) {
+                slug = '/trade/limit';
+            } else if (pathname.startsWith('/trade/range')) {
+                slug = '/trade/range';
+            } else {
+                throw new Error('Could not identify the correct URL path for redirect. Using /trade/market as a fallback value. Refer to useClick.tsx for troubleshooting.');
+            }
+        } catch (err) {
+            console.warn(err);
+            slug = '/trade/market';
+        }
+        return slug;
+    }, [pathname]);
+    console.log({locationSlug});
 }
