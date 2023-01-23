2f28a40e1d1e7b5e7111d6caa8e235d7d7d7e0e9
Jadessu <junioradessu@gmail.com>
2023-01-23 16:21:14 -0600

    create time frame mobile dropdown

--- src/pages/Trade/TradeCharts/TradeChartsComponents/TimeFrame.tsx
+++ src/pages/Trade/TradeCharts/TradeChartsComponents/TimeFrame.tsx
@@ -1,5 +1,5 @@
 import styles from './TimeFrame.module.css';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 interface TimeFramePropsIF {
     activeTimeFrame: string;
@@ -8,6 +8,9 @@ interface TimeFramePropsIF {
 }
 export default function TimeFrame(props: TimeFramePropsIF) {
     const { setActiveTimeFrame, setActivePeriod, activeTimeFrame } = props;
+
+    const [showTimeFrameDropdown, setShowTimeFrameDropdown] = useState(false);
+
     const activeTimeFrameData = [
         { label: '1m', activePeriod: 60 },
         { label: '5m', activePeriod: 300 },
@@ -22,22 +25,57 @@ export default function TimeFrame(props: TimeFramePropsIF) {
         setActivePeriod(time);
     }
 
-    return (
-        <div className={styles.chart_overlay_container}>
-            {activeTimeFrameData.map((time, idx) => (
-                <div className={styles.main_time_frame_container} key={idx}>
-                    <button
-                        onClick={() => handleTimeFrameButtonClick(time.label, time.activePeriod)}
-                        className={
-                            time.label === activeTimeFrame
-                                ? styles.active_selected_button
-                                : styles.non_active_selected_button
-                        }
-                    >
-                        {time.label}
-                    </button>
-                </div>
-            ))}
+    const wrapperStyle = showTimeFrameDropdown
+        ? styles.dropdown_wrapper_active
+        : styles.dropdown_wrapper;
+
+    const timeFrameMobile = (
+        <div className={styles.dropdown_menu}>
+            <button
+                className={styles.time_frame_mobile_button}
+                onClick={() => setShowTimeFrameDropdown(!showTimeFrameDropdown)}
+            >
+                {activeTimeFrame}
+            </button>
+
+            <div className={wrapperStyle}>
+                {activeTimeFrameData.map((time, idx) => (
+                    <div className={styles.main_time_frame_container} key={idx}>
+                        <button
+                            onClick={() =>
+                                handleTimeFrameButtonClick(time.label, time.activePeriod)
+                            }
+                            className={
+                                time.label === activeTimeFrame
+                                    ? styles.active_selected_button
+                                    : styles.non_active_selected_button
+                            }
+                        >
+                            {time.label}
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
+    );
+
+    return (
+        // <div className={styles.chart_overlay_container}>
+        //     {activeTimeFrameData.map((time, idx) => (
+        //         <div className={styles.main_time_frame_container} key={idx}>
+        //             <button
+        //                 onClick={() => handleTimeFrameButtonClick(time.label, time.activePeriod)}
+        //                 className={
+        //                     time.label === activeTimeFrame
+        //                         ? styles.active_selected_button
+        //                         : styles.non_active_selected_button
+        //                 }
+        //             >
+        //                 {time.label}
+        //             </button>
+        //         </div>
+        //     ))}
+        // </div>
+        <>{timeFrameMobile}</>
     );
 }
