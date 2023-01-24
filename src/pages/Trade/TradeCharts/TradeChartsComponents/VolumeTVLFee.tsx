fe79b7bc9f4f0753d35fb300cc2c969a6cb26cd7
Jadessu <junioradessu@gmail.com>
2023-01-24 08:47:54 -0600

    create mobile dropdown for volume and tvl

--- src/pages/Trade/TradeCharts/TradeChartsComponents/VolumeTVLFee.tsx
+++ src/pages/Trade/TradeCharts/TradeChartsComponents/VolumeTVLFee.tsx
@@ -1,5 +1,6 @@
 import styles from './VolumeTVLFee.module.css';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
+import useMediaQuery from '../../../../utils/hooks/useMediaQuery';
 
 interface VolumeTVLFeePropsIF {
     setShowVolume: Dispatch<SetStateAction<boolean>>;
@@ -13,6 +14,10 @@ interface VolumeTVLFeePropsIF {
 export default function VolumeTVLFee(props: VolumeTVLFeePropsIF) {
     const { setShowVolume, setShowTvl, setShowFeeRate, showVolume, showTvl, showFeeRate } = props;
 
+    const [showVolumeTVLFeeDropdown, setShowVolumeTVLFeeDropdown] = useState(false);
+
+    const desktopView = useMediaQuery('(max-width: 768px)');
+
     const handleVolumeToggle = () => setShowVolume(!showVolume);
 
     const handleTvlToggle = () => setShowTvl(!showTvl);
@@ -23,6 +28,40 @@ export default function VolumeTVLFee(props: VolumeTVLFeePropsIF) {
         { name: 'TVL', selected: showTvl, action: handleTvlToggle },
         { name: 'Fee Rate', selected: showFeeRate, action: handleFeeRateToggle },
     ];
+
+    const wrapperStyle = showVolumeTVLFeeDropdown
+        ? styles.dropdown_wrapper_active
+        : styles.dropdown_wrapper;
+
+    const volumeTVLFeeMobile = (
+        <div className={styles.dropdown_menu}>
+            <button
+                className={styles.volume_tvl_fee_mobile_button}
+                onClick={() => setShowVolumeTVLFeeDropdown(!showVolumeTVLFeeDropdown)}
+            >
+                {showVolume ? 'Volume' : showTvl ? 'TVL' : showFeeRate ? 'Fee Rate' : ''}
+            </button>
+
+            <div className={wrapperStyle}>
+                {volumeTvlAndFeeData.map((button, idx) => (
+                    <div className={styles.volume_tvl_container} key={idx}>
+                        <button
+                            onClick={button.action}
+                            className={
+                                button.selected
+                                    ? styles.active_selected_button
+                                    : styles.non_active_selected_button
+                            }
+                        >
+                            {button.name}
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
+    if (desktopView) return volumeTVLFeeMobile;
 
     return (
         <div
