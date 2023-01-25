a641643c6a11acf49db532d6b9410145edc82f38
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-25 16:37:01 -0500

    render token symbols and position value in PositionLI.tsx component

--- src/App/components/Sidebar/SidebarSearchResults/PositionsSearchResults/PositionLI.tsx
+++ src/App/components/Sidebar/SidebarSearchResults/PositionsSearchResults/PositionLI.tsx
@@ -1,6 +1,7 @@
 import styles from '../SidebarSearchResults.module.css';
 import { PositionIF } from '../../../../../utils/interfaces/PositionIF';
 import getUnicodeCharacter from '../../../../../utils/functions/getUnicodeCharacter';
+import { formatAmountOld } from '../../../../../utils/numbers';
 
 interface propsIF {
     position: PositionIF;
@@ -28,11 +29,35 @@ export default function PositionLI(props: propsIF) {
             ? `${quoteTokenCharacter}${position?.lowRangeShortDisplayInBase}-${quoteTokenCharacter}${position?.highRangeShortDisplayInBase}`
             : `${baseTokenCharacter}${position?.lowRangeShortDisplayInQuote}-${baseTokenCharacter}${position?.highRangeShortDisplayInQuote}`;
 
+    const usdValueNum = position.totalValueUSD;
+
+    const usdValueTruncated = !usdValueNum
+        ? '0.00'
+        : usdValueNum < 0.0001
+        ? usdValueNum.toExponential(2)
+        : usdValueNum < 2
+        ? usdValueNum.toPrecision(3)
+        : usdValueNum >= 10000
+        ? formatAmountOld(usdValueNum, 1)
+        : // ? baseLiqDisplayNum.toExponential(2)
+            usdValueNum.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            });
+
+            
+
     return (
         <div className={styles.card_container}>
-            <div>Pool</div>
+            <div>
+                {
+                    isDenomBase
+                        ? `${position?.baseSymbol} / ${position?.quoteSymbol}`
+                        : `${position?.quoteSymbol} / ${position?.baseSymbol}`
+                }
+            </div>
             <div>{rangeDisplay}</div>
-            <div>Qty</div>
+            <div>{'$' + usdValueTruncated}</div>
         </div>
     );
 }
