09ac2f805fb03700d22af28c3563f6e872a80f6c
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-27 14:24:25 -0500

    add developmental notes to TxLI.tsx component

--- src/App/components/Sidebar/SidebarSearchResults/TxSearchResults/TxLI.tsx
+++ src/App/components/Sidebar/SidebarSearchResults/TxSearchResults/TxLI.tsx
@@ -8,6 +8,10 @@ interface propsIF {
 export default function TxLI(props: propsIF) {
     const { tx } = props;
 
+    // TODO:   @Emily  replace Price and Qty in template below with live
+    // TODO:   @Emily  ... data, refer to SidebarRecentTransactionsCard.tsx
+    // TODO:   @Emily  ... component file as a model to replicate logic
+
     return (
         <div className={styles.card_container}>
             <div>{tx.baseSymbol} / {tx.quoteSymbol}</div>
