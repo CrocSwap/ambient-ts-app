30d274619a6522a0777f68e34cc81a9c61cc6f96
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-27 14:55:03 -0500

    refactor getTxType.tsx for clearer control flow and to log + handle errors properly

--- src/App/components/Sidebar/SidebarSearchResults/TxSearchResults/functions/getTxType.ts
+++ src/App/components/Sidebar/SidebarSearchResults/TxSearchResults/functions/getTxType.ts
@@ -1,7 +1,22 @@
-export const getTxType = (entityType: string) => {
-    const output = entityType === 'swap'
-        ? 'Market' : entityType === 'limitOrder'
-        ? 'Limit'
-        : 'Range';
+export const getTxType = (entityType: string): string => {
+    let output: string;
+
+    const errorMessage = `Did not recognize value of arg <<entityType>> in function getTxType(). Received value <<${entityType}>> of type <<${typeof entityType}>>. Recognized values  are type <<string>> and include <<'swap'>>, <<'limitOrder'>>, and <<liqchange>>. Function will return value <<'Unknown'>> of type <<string>>. Please refer to file getTxType.ts for troubleshooting.`;
+
+    switch (entityType) {
+        case 'swap':
+            output = 'Market';
+            break;
+        case 'limitOrder':
+            output = 'Limit';
+            break;
+        case 'liqchange':
+            output = 'Range';
+            break;
+        default:
+            console.warn(errorMessage);
+            output = 'Unknown';
+    }
+
     return output;
 }
