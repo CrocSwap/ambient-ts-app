095404ba98cdd07aef4024b213350c8ff7c3d989
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-30 14:57:20 -0500

    add comments to PositionLI.tsx component file

--- src/App/components/Sidebar/SidebarSearchResults/PositionsSearchResults/PositionLI.tsx
+++ src/App/components/Sidebar/SidebarSearchResults/PositionsSearchResults/PositionLI.tsx
@@ -1,5 +1,3 @@
-import { Dispatch, SetStateAction, useMemo } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import styles from '../SidebarSearchResults.module.css';
 import { PositionIF } from '../../../../../utils/interfaces/exports';
 import { getRangeDisplay } from './functions/getRangeDisplay'
@@ -8,53 +6,24 @@ import { getValueUSD } from './functions/getValueUSD';
 interface propsIF {
     position: PositionIF;
     isDenomBase: boolean;
-    setOutsideControl: Dispatch<SetStateAction<boolean>>;
-    setSelectedOutsideTab: Dispatch<SetStateAction<number>>;
-    setCurrentPositionActive: Dispatch<SetStateAction<string>>;
-    setIsShowAllEnabled: Dispatch<SetStateAction<boolean>>;
+    handleClick: (position: PositionIF) => void;
 }
 
 export default function PositionLI(props: propsIF) {
     const {
         position,
         isDenomBase,
-        setOutsideControl,
-        setSelectedOutsideTab,
-        setCurrentPositionActive,
-        setIsShowAllEnabled,
+        handleClick
     } = props;
 
-    const { pathname } = useLocation();
-    const navigate = useNavigate();
-
-    const linkPath = useMemo<string>(() => {
-        let locationSlug = '';
-        if (pathname.startsWith('/trade/market') || pathname.startsWith('/account')) {
-            locationSlug = '/trade/market';
-        } else if (pathname.startsWith('/trade/limit')) {
-            locationSlug = '/trade/limit';
-        } else if (pathname.startsWith('/trade/range')) {
-            locationSlug = '/trade/range';
-        } else if (pathname.startsWith('/swap')) {
-            locationSlug = '/swap';
-        }
-        return locationSlug + '/chain=0x5&tokenA=' + position.base + '&tokenB=' + position.quote;
-    }, [pathname]);
-
-    function handleRangePositionClick(pos: PositionIF) {
-        setOutsideControl(true);
-        setSelectedOutsideTab(2);
-        setCurrentPositionActive(pos.positionStorageSlot);
-        setIsShowAllEnabled(false);
-        navigate(linkPath);
-    }
-
+    // fn to generate human-readable range output (from X to Y)
     const rangeDisplay = getRangeDisplay(position, isDenomBase);
 
+    // fn to generate human-readable version of total position value
     const positionValue = getValueUSD(position.totalValueUSD);
 
     return (
-        <div className={styles.card_container} onClick={() => handleRangePositionClick(position)}>
+        <div className={styles.card_container} onClick={() => handleClick(position)}>
             <div>
                 {isDenomBase
                     ? `${position?.baseSymbol} / ${position?.quoteSymbol}`
