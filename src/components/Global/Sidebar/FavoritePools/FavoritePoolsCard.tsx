a9547e1b6b0101650238d7204f5dce1d3efb66a2
mhsmith321 <emily.drake.smith@gmail.com>
2023-01-31 13:39:24 -0500

    abstract logic from FavoritePoolsCard.tsx to get volume and tvl for relevant pool into a custom hook

--- src/components/Global/Sidebar/FavoritePools/FavoritePoolsCard.tsx
+++ src/components/Global/Sidebar/FavoritePools/FavoritePoolsCard.tsx
@@ -1,10 +1,9 @@
 import styles from './FavoritePoolsCard.module.css';
 import { PoolIF } from '../../../../utils/interfaces/exports';
 import { PoolStatsFn } from '../../../../App/functions/getPoolStats';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { formatAmountOld } from '../../../../utils/numbers';
 import { useAppSelector } from '../../../../utils/hooks/reduxToolkit';
+import { usePoolStats } from './hooks/usePoolStats';
 
 interface propsIF {
     pool: PoolIF;
@@ -16,8 +15,8 @@ interface propsIF {
 export default function FavoritePoolsCard(props: propsIF) {
     const { pool, chainId, cachedPoolStatsFetch, lastBlockNumber } = props;
 
-    const [poolVolume, setPoolVolume] = useState<string | undefined>();
-    const [poolTvl, setPoolTvl] = useState<string | undefined>();
+    // hook to get human-readable values for pool volume and TVL
+    const [volume, tvl] = usePoolStats(pool, lastBlockNumber, cachedPoolStatsFetch);
 
     const { tokenB } = useAppSelector((state) => state.tradeData);
 
@@ -28,35 +27,13 @@ export default function FavoritePoolsCard(props: propsIF) {
 
     const linkPath = '/trade/market/chain=' + chainId + '&tokenA=' + addrTokenA + '&tokenB=' + addrTokenB
 
-    const fetchPoolStats = () => {
-        (async () => {
-            const poolStatsFresh = await cachedPoolStatsFetch(
-                pool.chainId,
-                pool.base.address,
-                pool.quote.address,
-                pool.poolId,
-                Math.floor(lastBlockNumber / 4),
-            );
-            const volume = poolStatsFresh?.volume;
-            const volumeString = volume ? '$' + formatAmountOld(volume) : undefined;
-            setPoolVolume(volumeString);
-            const tvl = poolStatsFresh?.tvl;
-            const tvlString = tvl ? '$' + formatAmountOld(tvl) : undefined;
-            setPoolTvl(tvlString);
-        })();
-    };
-
-    useEffect(() => {
-        fetchPoolStats();
-    }, [lastBlockNumber]);
-
     return (
         <Link className={styles.container} to={linkPath}>
             <div>
                 {pool.base.symbol} / {pool.quote.symbol}
             </div>
-            <div>{poolVolume ?? '…'}</div>
-            <div>{poolTvl ?? '…'}</div>
+            <div>{volume}</div>
+            <div>{tvl}</div>
         </Link>
     );
 }
