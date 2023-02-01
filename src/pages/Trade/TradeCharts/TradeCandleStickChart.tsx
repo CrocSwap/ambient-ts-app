0db40f6860ad73586e090249573cb4d4e276d7d3
zcelbir <zcelbir@proven.com.tr>
2023-02-01 11:19:31 +0300

    freezing chart problem

--- src/pages/Trade/TradeCharts/TradeCandleStickChart.tsx
+++ src/pages/Trade/TradeCharts/TradeCandleStickChart.tsx
@@ -237,15 +237,15 @@ export default function TradeCandleStickChart(props: ChartData) {
         });
     };
 
-    const standardDeviation = (arr: any, usePopulation = false) => {
-        const mean = arr.reduce((acc: any, val: any) => acc + val, 0) / arr.length;
-        return Math.sqrt(
-            arr
-                .reduce((acc: any, val: any) => acc.concat((val - mean) ** 2), [])
-                .reduce((acc: any, val: any) => acc + val, 0) /
-                (arr.length - (usePopulation ? 0 : 1)),
-        );
-    };
+    // const standardDeviation = (arr: any, usePopulation = false) => {
+    //     const mean = arr.reduce((acc: any, val: any) => acc + val, 0) / arr.length;
+    //     return Math.sqrt(
+    //         arr
+    //             .reduce((acc: any, val: any) => acc.concat((val - mean) ** 2), [])
+    //             .reduce((acc: any, val: any) => acc + val, 0) /
+    //             (arr.length - (usePopulation ? 0 : 1)),
+    //     );
+    // };
 
     // volume data
 
@@ -494,20 +494,20 @@ export default function TradeCandleStickChart(props: ChartData) {
 
                 liqAskData.sort((a: any, b: any) => b.liqPrices - a.liqPrices);
 
-                const liqAllAskPrices = liqAskData.map(({ liqPrices }) => liqPrices);
-                const liqAskDeviation = standardDeviation(liqAllAskPrices);
+                // const liqAllAskPrices = liqAskData.map(({ liqPrices }) => liqPrices);
+                // const liqAskDeviation = standardDeviation(liqAllAskPrices);
 
-                const liqAllBidPrices = liqBidData.map(({ liqPrices }) => liqPrices);
-                const liqBidDeviation = standardDeviation(liqAllBidPrices);
+                // const liqAllBidPrices = liqBidData.map(({ liqPrices }) => liqPrices);
+                // const liqBidDeviation = standardDeviation(liqAllBidPrices);
 
-                while (liqBidData[0].liqPrices < limitBoundary) {
-                    liqBidData.unshift({
-                        activeLiq: liqBidData[0].activeLiq,
-                        liqPrices: liqBidData[0].liqPrices + liqBidDeviation,
-                        deltaAverageUSD: 0,
-                        cumAverageUSD: 0,
-                    });
-                }
+                // while (liqBidData[0].liqPrices < limitBoundary) {
+                //     liqBidData.unshift({
+                //         activeLiq: liqBidData[0].activeLiq,
+                //         liqPrices: liqBidData[0].liqPrices + liqBidDeviation,
+                //         deltaAverageUSD: 0,
+                //         cumAverageUSD: 0,
+                //     });
+                // }
 
                 liqBidData.push({
                     activeLiq: liqBidData.find((liqData) => liqData.liqPrices < limitBoundary)
@@ -525,15 +525,15 @@ export default function TradeCandleStickChart(props: ChartData) {
                     cumAverageUSD: 0,
                 });
 
-                while (liqAskData[liqAskData.length - 1].liqPrices - liqAskDeviation > 0) {
-                    liqAskData.push({
-                        activeLiq: liqAskData[liqAskData.length - 1].activeLiq,
-                        liqPrices:
-                            liqAskData[liqAskData.length - 1].liqPrices - liqAskDeviation / 2,
-                        deltaAverageUSD: 0,
-                        cumAverageUSD: 0,
-                    });
-                }
+                // while (liqAskData[liqAskData.length - 1].liqPrices - liqAskDeviation > 0) {
+                //     liqAskData.push({
+                //         activeLiq: liqAskData[liqAskData.length - 1].activeLiq,
+                //         liqPrices:
+                //             liqAskData[liqAskData.length - 1].liqPrices - liqAskDeviation / 2,
+                //         deltaAverageUSD: 0,
+                //         cumAverageUSD: 0,
+                //     });
+                // }
 
                 liqAskData.push({
                     activeLiq: liqAskData[liqAskData.length - 1].activeLiq,
