eff497fcd57ef9f00b3fad03958db38ae195f001
Jadessu <junioradessu@gmail.com>
2023-02-01 13:23:16 -0600

    remove chart settings

--- src/pages/Trade/TradeCharts/TradeCharts.tsx
+++ src/pages/Trade/TradeCharts/TradeCharts.tsx
@@ -14,7 +14,7 @@ import {
     // AiOutlineCopy,
     // AiOutlineLink,
     // AiOutlineTwitter,
-    AiOutlineSetting,
+    // AiOutlineSetting,
 } from 'react-icons/ai';
 import { VscClose } from 'react-icons/vsc';
 
@@ -371,9 +371,9 @@ export default function TradeCharts(props: propsIF) {
                     <AiOutlineCamera size={20} />
                 </div>
             </DefaultTooltip>
-            <div onClick={() => setShowChartSettings(!showChartSettings)}>
+            {/* <div onClick={() => setShowChartSettings(!showChartSettings)}>
                 <AiOutlineSetting size={20} />
-            </div>
+            </div> */}
         </div>
     );
 
