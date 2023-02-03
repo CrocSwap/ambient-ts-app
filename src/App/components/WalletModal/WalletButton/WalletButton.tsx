b96599a682ea6123df6c0fe8bc3103b2a762f2da
Ben Wolski <benwolski@gmail.com>
2023-02-03 12:55:44 -0500

    disable unsupported connector buttons

--- src/App/components/WalletModal/WalletButton/WalletButton.tsx
+++ src/App/components/WalletModal/WalletButton/WalletButton.tsx
@@ -10,7 +10,12 @@ interface WalletButtonPropsIF {
 
 export default function WalletButton(props: WalletButtonPropsIF) {
     return (
-        <button onClick={() => props.action()} className={styles.container}>
+        <button
+            disabled={props.disabled}
+            onClick={() => props.action()}
+            className={styles.container}
+            style={props.disabled ? { cursor: 'default' } : {}}
+        >
             <img className={styles.icon} src={props.logo ? props.logo : ambientLogo} alt='' />
             <div className={styles.wallet_name}>{props.title}</div>
         </button>
