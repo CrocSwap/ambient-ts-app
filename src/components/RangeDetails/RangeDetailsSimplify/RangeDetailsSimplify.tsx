3b0032d14fa1bc37dd984a770d7a742470c4e7e4
Jadessu <junioradessu@gmail.com>
2023-02-07 09:09:03 -0600

    create range details and item row peops

--- src/components/RangeDetails/RangeDetailsSimplify/RangeDetailsSimplify.tsx
+++ src/components/RangeDetails/RangeDetailsSimplify/RangeDetailsSimplify.tsx
@@ -1,5 +1,19 @@
 import styles from './RangeDetailsSimplify.module.css';
+import { PositionIF } from '../../../utils/interfaces/exports';
 
-export default function RangeDetailsSimplify() {
+interface ItemRowPropsIF {
+    title: string;
+    // eslint-disable-next-line
+    content: any;
+    explanation: string;
+}
+
+interface RangeDetailsSimplifyPropsIF {
+    position: PositionIF;
+    account: string;
+}
+export default function RangeDetailsSimplify(props: RangeDetailsSimplifyPropsIF) {
+    const { account, position } = props;
+
     return <div></div>;
 }
