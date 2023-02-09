ce26402cdddbd69e0dce97a78e43cd1f70fba560
Jadessu <junioradessu@gmail.com>
2023-02-09 10:46:16 -0600

    refactor full chat

--- src/components/Chat/ChatPanel.tsx
+++ src/components/Chat/ChatPanel.tsx
@@ -352,6 +352,7 @@ export default function ChatPanel(props: ChatProps) {
                 showCurrentPoolButton={showCurrentPoolButton}
                 setShowCurrentPoolButton={setShowCurrentPoolButton}
                 currentPool={currentPool}
+                favePools={favePools}
             />
         );
 
