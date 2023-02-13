328905c50fab0865097eddff58a7d7c73466ffab
Ben Wolski <benwolski@gmail.com>
2023-02-13 12:17:57 -0500

    fix moderator icon display logic

--- src/components/Chat/ChatPanel.tsx
+++ src/components/Chat/ChatPanel.tsx
@@ -162,7 +162,7 @@ export default function ChatPanel(props: ChatProps) {
                         return result;
                     });
                 } else {
-                    currentUser === moderators[0].id ? setModerator(true) : setModerator(true);
+                    currentUser === moderators[0].id ? setModerator(true) : setModerator(false);
                     setCurrentUser(result.userData._id);
                     if (result.userData.ensName !== ensName) {
                         // eslint-disable-next-line
