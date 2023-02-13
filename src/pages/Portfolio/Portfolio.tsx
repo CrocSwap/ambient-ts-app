2b173dcfc125dcb8b5739b5edb51916bbebf4781
Ben Wolski <benwolski@gmail.com>
2023-02-13 15:34:18 -0500

    update env variables - bump SDK

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -29,12 +29,15 @@ import { useAccount, useEnsName } from 'wagmi';
 import useMediaQuery from '../../utils/hooks/useMediaQuery';
 import { SpotPriceFn } from '../../App/functions/querySpotPrice';
 
-const mainnetProvider = new ethers.providers.WebSocketProvider(
-    // 'wss://mainnet.infura.io/ws/v3/4a162c75bd514925890174ca13cdb6a2', // benwolski@gmail.com
-    // 'wss://mainnet.infura.io/ws/v3/170b7b65781c422d82a94b8b289ca605',
-    // 'wss://mainnet.infura.io/ws/v3/e0aa879e36fc4c9e91b826ad961a36fd',
-    'wss://mainnet.infura.io/ws/v3/360ea5fda45b4a22883de8522ebd639e', // croc labs #2
-);
+const infuraKey2 = process.env.REACT_APP_INFURA_KEY_2;
+
+const mainnetProvider = infuraKey2
+    ? new ethers.providers.WebSocketProvider(
+          'wss://mainnet.infura.io/ws/v3/' + infuraKey2, // croc labs #2
+      )
+    : new ethers.providers.WebSocketProvider(
+          'wss://mainnet.infura.io/ws/v3/360ea5fda45b4a22883de8522ebd639e', // croc labs #2
+      );
 
 interface propsIF {
     crocEnv: CrocEnv | undefined;
