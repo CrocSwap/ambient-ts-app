f9c22a06ca8687e92b764ac634a1d7883ccbae33
mhsmith321 <emily.drake.smith@gmail.com>
2023-02-17 16:13:15 -0500

    abstract logic to generate exit URL path from RepositionHeader.tsx component file

--- src/components/Trade/Reposition/RepositionHeader/RepositionHeader.tsx
+++ src/components/Trade/Reposition/RepositionHeader/RepositionHeader.tsx
@@ -1,7 +1,7 @@
-import { useMemo } from 'react';
+import { useRepoExitPath } from './useRepoExitPath';
 import ContentHeader from '../../../Global/ContentHeader/ContentHeader';
 import { RiSettings5Line } from 'react-icons/ri';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import trimString from '../../../../utils/functions/trimString';
 import styles from './RepositionHeader.module.css';
 import TransactionSettings from '../../../Global/TransactionSettings/TransactionSettings';
@@ -27,45 +27,10 @@ export default function RepositionHeader(props: propsIF) {
         toggleBypassConfirm,
     } = props;
 
-    const { params } = useParams();
-
-    // generate a nav path for clicking the exit button
-    // regenerate value every time the URL params change (virtually never)
-    const exitPath = useMemo<string>(() => {
-        // get URL parameters or empty string if undefined
-        const fixedParams = params ?? '';
-        // break URL param string into parameter tuples
-        const paramSets = fixedParams
-            // split the params string at the separator character
-            .split('&')
-            // remove any values missing an = symbol
-            .filter((par) => par.includes('='))
-            // split substrings at = symbols to make [key, value] tuples
-            .map((par) => par.split('='))
-            // remove empty strings created by extra = symbols
-            .map((par) => par.filter((e) => e !== ''))
-            // remove tuples with trisomy issues
-            .filter((par) => par.length === 2);
-        // fn to look up the value of any param
-        // return empty string if param is not found
-        const findParam = (name: string): string => {
-            // find param tuple with name provided as an arg
-            const paramTuple = paramSets.find((param) => param[0] === name);
-            // return value from tuple or empty string in tuple is not found
-            return paramTuple ? paramTuple[1] : '';
-        };
-        // generate and return nav path
-        return (
-            '/trade/range/chain=' +
-            findParam('chain') +
-            '&tokenA=' +
-            findParam('tokenA') +
-            '&tokenB=' +
-            findParam('tokenB')
-        );
-    }, [params]);
-
     const navigate = useNavigate();
+
+    // navpath for when user clicks the exit button
+    const exitPath = useRepoExitPath();
 
     const [isModalOpen, openModal, closeModal] = useModal();
 
