d77ed2557e9e18b143ae73be7b95b78cb3b92fda
mhsmith321 <emily.drake.smith@gmail.com>
2023-02-22 12:02:45 -0500

    get tradeData from RTK in useParamsFactory.ts file

--- src/utils/hooks/useParamsBuilder.ts
+++ src/utils/hooks/useParamsBuilder.ts
@@ -1,3 +1,7 @@
+import { useAppSelector } from './reduxToolkit';
+
 export const useParamsFactory = () => {
     console.log('ran custom hook useParamsBuilder()');
+    const { tradeData } = useAppSelector(state => state);
+    false && tradeData;
 }
