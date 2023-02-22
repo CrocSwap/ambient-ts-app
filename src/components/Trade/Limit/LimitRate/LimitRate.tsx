2830b3b9a65f80777899ae2ac9b6865599dfbd7f
Jadessu <junioradessu@gmail.com>
2023-02-21 18:44:19 -0600

    style top of book

--- src/components/Trade/Limit/LimitRate/LimitRate.tsx
+++ src/components/Trade/Limit/LimitRate/LimitRate.tsx
@@ -46,7 +46,7 @@ export default function LimitRate(props: propsIF) {
 
     const dispatch = useAppDispatch();
     const isDenomBase = useAppSelector((state) => state.tradeData).isDenomBase;
-    const limitTick = useAppSelector((state) => state.tradeData).limitTick;
+    // const limitTick = useAppSelector((state) => state.tradeData).limitTick;
 
     const initialLimitRateNonDisplay =
         (poolPriceNonDisplay || 0) * (isSellTokenBase ? 0.985 : 1.015);
@@ -126,17 +126,15 @@ export default function LimitRate(props: propsIF) {
                 style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
             >
                 <p>Price</p>
-                {limitTick !== pinnedInitialTick ? (
-                    <button
-                        className={styles.reset_limit_button}
-                        onClick={() => {
-                            dispatch(setLimitTick(pinnedInitialTick));
-                            // console.log({ displayPrice });
-                        }}
-                    >
-                        Top of Book
-                    </button>
-                ) : null}
+                <button
+                    className={styles.reset_limit_button}
+                    onClick={() => {
+                        dispatch(setLimitTick(pinnedInitialTick));
+                        // console.log({ displayPrice });
+                    }}
+                >
+                    Top of Book
+                </button>
             </span>
 
             <div className={`${styles.swap_input} `}>{rateInput}</div>
