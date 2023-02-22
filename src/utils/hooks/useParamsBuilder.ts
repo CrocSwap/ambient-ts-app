c4aa6660199f28a71673dd6f0bd446e0cf5d74b9
mhsmith321 <emily.drake.smith@gmail.com>
2023-02-22 12:28:26 -0500

    add logic to useParamsBuilder.ts to create a full param string by string key

--- src/utils/hooks/useParamsBuilder.ts
+++ src/utils/hooks/useParamsBuilder.ts
@@ -1,7 +1,40 @@
 import { useAppSelector } from './reduxToolkit';
 
-export const useParamsFactory = () => {
+export const useParamsBuilder = (): (
+    (destination: string) => string
+) => {
     console.log('ran custom hook useParamsBuilder()');
     const { tradeData } = useAppSelector(state => state);
     false && tradeData;
+
+    const makeParam = (paramKey: string): string => {
+        let value: string;
+        switch (paramKey) {
+            case 'tokenA':
+                value = tradeData.tokenA.address;
+                break;
+            case 'tokenB':
+                value = tradeData.tokenB.address;
+                break;
+            case 'lowTick':
+                value = tradeData.advancedLowTick.toString();
+                break;
+            case 'highTick':
+                value = tradeData.advancedHighTick.toString();
+                break;
+            case 'limitTick':
+                value = tradeData.limitTick?.toString() ?? '';
+                break;
+            default:
+                value = '';
+        }
+        const output = paramKey + '=' + value;
+        return output;
+    }
+
+    const getNavPath = (destination: string) => {
+        return destination;
+    }
+
+    return getNavPath;
 }
