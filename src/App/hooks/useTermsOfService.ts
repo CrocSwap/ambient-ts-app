f9bf10924ce6c2815e4f54859047e1d331403dde
mhsmith321 <emily.drake.smith@gmail.com>
2023-02-28 16:02:06 -0500

    refactor useTermsOfService.ts interfaces

--- src/App/hooks/useTermsOfService.ts
+++ src/App/hooks/useTermsOfService.ts
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react';
 
-export interface termsOfServiceIF {
+export interface sectionTermsIF {
     text: string;
     version: number;
     publishedOn: string | Date;
     acceptedOn?: string | Date;
 };
 
+export interface termsOfServiceIF {
+    transaction: sectionTermsIF,
+    chat: sectionTermsIF
+}
+
 export const useTermsOfService = () => {
-    const tos: termsOfServiceIF = {
+    const tos: sectionTermsIF = {
         text: 'Bacon ipsum dolor amet ham jowl pork belly, venison brisket cow meatloaf ball tip short ribs salami pork chop swine tail sausage. Hamburger doner frankfurter porchetta leberkas, chislic short ribs ribeye jerky ham hock meatball chuck. Frankfurter tail strip steak bacon picanha buffalo cupim venison tongue. Meatball shankle tongue, leberkas short loin sirloin doner pork chop strip steak landjaeger t-bone cow. Prosciutto turkey boudin, pork loin bacon pastrami fatback sirloin cow pork short ribs jerky short loin ground round pork belly. Tri-tip meatloaf spare ribs corned beef chicken. Rump hamburger prosciutto picanha, doner venison shankle meatball alcatra bacon boudin ham kevin.',
         version: 1,
         publishedOn: new Date('05 October 2019 14:48 UTC').toISOString()
     };
 
-    const getCurrentAgreement = (): (termsOfServiceIF|undefined) => {
+    const getCurrentAgreement = (): (sectionTermsIF|undefined) => {
         return JSON.parse(localStorage.getItem('termsOfService') as string);
     };
 
-    const [agreement, setAgreement] = useState<termsOfServiceIF|undefined>(
+    const [agreement, setAgreement] = useState<sectionTermsIF|undefined>(
         getCurrentAgreement()
     );
 
