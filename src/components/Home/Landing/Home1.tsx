a3ee66547319b0466fb28f4aadd8a2f608407392
Jadessu <junioradessu@gmail.com>
2023-02-28 15:07:38 -0600

    create first home slide

--- src/components/Home/Landing/Home1.tsx
+++ src/components/Home/Landing/Home1.tsx
@@ -2,6 +2,7 @@ import styles from './Home1.module.css';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import logoText from '../../../assets/images/logos/logo_text.svg';
+import TradeNowButton from './TradeNowButton/TradeNowButton';
 
 export default function Home1() {
     const { t } = useTranslation();
@@ -10,6 +11,7 @@ export default function Home1() {
         <div className={styles.main_container}>
             <div className={styles.content_container}>
                 <h1>Zero-to-One Decentralized Trading Protocol</h1>
+                <TradeNowButton />
             </div>
         </div>
     );
