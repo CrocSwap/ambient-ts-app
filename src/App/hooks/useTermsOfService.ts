8f9fbaff6e907c7faa8f5d1045643cfc7d5f757e
mhsmith321 <emily.drake.smith@gmail.com>
2023-02-28 16:06:50 -0500

    abstract terms of service raw data out of useTermsOfService.ts file

--- src/App/hooks/useTermsOfService.ts
+++ src/App/hooks/useTermsOfService.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { transactionToS } from '../../utils/data/termsOfService';
 
 export interface sectionTermsIF {
     text: string;
@@ -13,12 +14,6 @@ export interface termsOfServiceIF {
 }
 
 export const useTermsOfService = () => {
-    const tos: sectionTermsIF = {
-        text: 'Bacon ipsum dolor amet ham jowl pork belly, venison brisket cow meatloaf ball tip short ribs salami pork chop swine tail sausage. Hamburger doner frankfurter porchetta leberkas, chislic short ribs ribeye jerky ham hock meatball chuck. Frankfurter tail strip steak bacon picanha buffalo cupim venison tongue. Meatball shankle tongue, leberkas short loin sirloin doner pork chop strip steak landjaeger t-bone cow. Prosciutto turkey boudin, pork loin bacon pastrami fatback sirloin cow pork short ribs jerky short loin ground round pork belly. Tri-tip meatloaf spare ribs corned beef chicken. Rump hamburger prosciutto picanha, doner venison shankle meatball alcatra bacon boudin ham kevin.',
-        version: 1,
-        publishedOn: new Date('05 October 2019 14:48 UTC').toISOString()
-    };
-
     const getCurrentAgreement = (): (sectionTermsIF|undefined) => {
         return JSON.parse(localStorage.getItem('termsOfService') as string);
     };
@@ -35,10 +30,10 @@ export const useTermsOfService = () => {
     false && setAgreement;
 
     const output = {
-        currentToS: () => tos,
+        currentToS: () => transactionToS,
         getLastAgreement: () => agreement,
-        checkAgreement: () => agreement?.version === tos.version,
-        acceptAgreement: () => setAgreement({...tos, acceptedOn: new Date().toISOString()})
+        checkAgreement: () => agreement?.version === transactionToS.version,
+        acceptAgreement: () => setAgreement({...transactionToS, acceptedOn: new Date().toISOString()})
     };
 
     return output;
