a08581823383b8661c3c0af0eb8bb82ef1a5bf0b
mhsmith321 <emily.drake.smith@gmail.com>
2023-02-28 13:53:05 -0500

    create interface for termsOfService data object

--- src/App/hooks/useTermsOfService.ts
+++ src/App/hooks/useTermsOfService.ts
@@ -1,9 +1,15 @@
+export interface termsOfServiceIF {
+    text: string;
+    version: number;
+    published: string | Date;
+};
+
 export const useTermsOfService = () => {
-    console.log('fired custom hook useTermsOfService!');
-    const tos = {
+    const tos: termsOfServiceIF = {
         text: 'Bacon ipsum dolor amet ham jowl pork belly, venison brisket cow meatloaf ball tip short ribs salami pork chop swine tail sausage. Hamburger doner frankfurter porchetta leberkas, chislic short ribs ribeye jerky ham hock meatball chuck. Frankfurter tail strip steak bacon picanha buffalo cupim venison tongue. Meatball shankle tongue, leberkas short loin sirloin doner pork chop strip steak landjaeger t-bone cow. Prosciutto turkey boudin, pork loin bacon pastrami fatback sirloin cow pork short ribs jerky short loin ground round pork belly. Tri-tip meatloaf spare ribs corned beef chicken. Rump hamburger prosciutto picanha, doner venison shankle meatball alcatra bacon boudin ham kevin.',
         version: 1,
         published: new Date('05 October 2019 14:48 UTC').toISOString()
     };
-    
+
+    console.log(tos);
 };
