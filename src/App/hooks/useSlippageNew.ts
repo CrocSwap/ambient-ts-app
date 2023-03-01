164bd4620876af9b8d1fe2e8eca313adc5bbcb7b
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-01 15:58:47 -0500

    refactor logic in useSlippageNew.ts to return individual slippage values

--- src/App/hooks/useSlippageNew.ts
+++ src/App/hooks/useSlippageNew.ts
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export interface SlippageNewIF {
     stable: number,
     volatile: number
 }
 
+export interface SlippageMethodsIF {
+    stable: number;
+    volatile: number;
+}
+
 export const useSlippageNew = (
     slippageType: string,
     defaults: SlippageNewIF
-): void => {
-
+): SlippageMethodsIF => {
     const getSlippage = () => {
         const pair = JSON.parse(localStorage.getItem(`slippage_${slippageType}`) as string);
         return pair ?? defaults;
@@ -17,21 +21,13 @@ export const useSlippageNew = (
 
     const [slippage, setSlippage] = useState<SlippageNewIF>(getSlippage());
 
-    const checkSlippage = (type: string): number => {
-        let output: number;
-        switch (type) {
-            case 'stable':
-                output = slippage.stable;
-                break;
-            case 'volatile':
-                output = slippage.volatile;
-                break;
-            default:
-                output = 0;
-        }
-        return output;
-    }
+    const stable = useMemo<number>(() => slippage.stable, [slippage]);
+    const volatile = useMemo<number>(() => slippage.volatile, [slippage]);
 
     false && setSlippage;
-    false && checkSlippage;
+
+    return {
+        stable,
+        volatile
+    };
 }
