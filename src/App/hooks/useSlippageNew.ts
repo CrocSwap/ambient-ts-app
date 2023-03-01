a608724822553e03f2561e220cd87984da0a505c
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-01 16:26:47 -0500

    add logic to useSlippageNew.ts to update slippage values on command

--- src/App/hooks/useSlippageNew.ts
+++ src/App/hooks/useSlippageNew.ts
@@ -8,6 +8,8 @@ export interface SlippageNewIF {
 export interface SlippageMethodsIF {
     stable: number;
     volatile: number;
+    updateStable: (val: number) => void;
+    updateVolatile: (val: number) => void;
 }
 
 export const useSlippageNew = (
@@ -24,10 +26,26 @@ export const useSlippageNew = (
     const stable = useMemo<number>(() => slippage.stable, [slippage]);
     const volatile = useMemo<number>(() => slippage.volatile, [slippage]);
 
-    false && setSlippage;
+    const updateSlippage = (slipType: string, newVal: number) => {
+        const locSlippage = slippage;
+        let updated = false;
+        if (slipType === 'stable') {
+            locSlippage.stable = newVal;
+            updated = true;
+        } else if (slipType === 'volatile') {
+            locSlippage.volatile = newVal;
+            updated = true;
+        };
+        if (updated) {
+            setSlippage(locSlippage);
+            localStorage.setItem(`slippage_${slippageType}`, JSON.stringify(locSlippage));
+        }
+    }
 
     return {
         stable,
-        volatile
+        volatile,
+        updateStable: (val: number) => updateSlippage('stable', val),
+        updateVolatile: (val: number) => updateSlippage('volatile', val)
     };
 }
