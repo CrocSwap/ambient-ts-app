66b28be861899d065d238a8720f24ce798e3f22d
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-02 12:43:32 -0500

    fix bug preventing useSlippageNew() from updating local storage properly

--- src/App/hooks/useSlippageNew.ts
+++ src/App/hooks/useSlippageNew.ts
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface SlippageNewIF {
     stable: number,
@@ -16,29 +16,39 @@ export const useSlippageNew = (
     slippageType: string,
     defaults: SlippageNewIF
 ): SlippageMethodsIF => {
-    const getSlippage = () => {
-        const pair = JSON.parse(localStorage.getItem(`slippage_${slippageType}`) as string);
-        return pair ?? defaults;
+    const getSlippage = (whichOne: string): number => {
+        const pair: SlippageNewIF|null = JSON.parse(
+            localStorage.getItem(`slippage_${slippageType}`) as string
+        );
+        let output: number;
+        switch (whichOne) {
+            case 'stable':
+                output = pair?.stable ?? defaults.stable;
+                break;
+            case 'volatile':
+                output = pair?.volatile ?? defaults.volatile;
+                break;
+            default:
+                output = 0;
+        };
+        return output;
     }
 
-    const [slippage, setSlippage] = useState<SlippageNewIF>(getSlippage());
+    const [stable, setStable] = useState<number>(getSlippage('stable'));
+    const [volatile, setVolatile] = useState<number>(getSlippage('volatile'));
 
-    const stable = useMemo<number>(() => slippage.stable, [slippage]);
-    const volatile = useMemo<number>(() => slippage.volatile, [slippage]);
+    useEffect(() => {
+        localStorage.setItem(`slippage_${slippageType}`, JSON.stringify({stable, volatile}));
+    }, [stable, volatile]);
 
     const updateSlippage = (slipType: string, newVal: number) => {
-        const locSlippage = slippage;
-        let updated = false;
-        if (slipType === 'stable') {
-            locSlippage.stable = newVal;
-            updated = true;
-        } else if (slipType === 'volatile') {
-            locSlippage.volatile = newVal;
-            updated = true;
-        };
-        if (updated) {
-            setSlippage(locSlippage);
-            localStorage.setItem(`slippage_${slippageType}`, JSON.stringify(locSlippage));
+        switch (slipType) {
+            case 'stable':
+                setStable(newVal);
+                break;
+            case 'volatile':
+                setVolatile(newVal);
+                break;
         }
     }
 
