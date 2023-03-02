ba5ab44603453972b51c1e012588dd2c0a53772c
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-02 13:49:00 -0500

    add logic to useChartSettings.ts to hold subchart preferences in local state

--- src/App/hooks/useChartSettings.ts
+++ src/App/hooks/useChartSettings.ts
@@ -1,3 +1,44 @@
+import { useState } from 'react';
+
 export const useChartSettings = () => {
-    console.log('fired custom hook useChartSettings()');
+    const getPreference = (subchart: string) => {
+        const allPreferences = JSON.parse(
+            localStorage.getItem('chartSettings') as string
+        );
+        let output: boolean;
+        switch (subchart) {
+            case 'volume':
+                output = allPreferences.volume;
+                break;
+            case 'tvl':
+                output = allPreferences.tvl;
+                break;
+            case 'feeRate':
+                output = allPreferences.feeRate;
+                break;
+            default:
+                return;
+        }
+        return output;
+    };
+
+    const [
+        volumeSubchartEnabled,
+        setVolumeSubchartEnabled
+    ] = useState<boolean>(getPreference('volume') ?? true);
+    const [
+        tvlSubchartEnabled,
+        setTvlSubchartEnabled
+    ] = useState<boolean>(getPreference('tvl') ?? false);
+    const [
+        feeRateSubchartEnabled,
+        setFeeRateSubchartEnabled
+    ] = useState<boolean>(getPreference('feeRate') ?? false);
+
+    false && volumeSubchartEnabled;
+    false && setVolumeSubchartEnabled;
+    false && tvlSubchartEnabled;
+    false && setTvlSubchartEnabled;
+    false && feeRateSubchartEnabled;
+    false && setFeeRateSubchartEnabled;
 }
