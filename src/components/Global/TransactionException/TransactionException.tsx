7a03d6ef64432b3da034765c09086efec5151bbf
Ben Wolski <ben@crocodilelabs.io>
2023-03-08 12:42:30 -0500

    add possible explanation to tx exception modal when ETH is minted as secondary

--- src/components/Global/TransactionException/TransactionException.tsx
+++ src/components/Global/TransactionException/TransactionException.tsx
@@ -4,6 +4,9 @@ import styles from './TransactionException.module.css';
 // import { CircleLoaderFailed } from '../LoadingAnimations/CircleLoader/CircleLoader';
 // import { Dispatch, SetStateAction } from 'react';
 import Button from '../Button/Button';
+import Divider from '../Divider/Divider';
+import { useAppSelector } from '../../../utils/hooks/reduxToolkit';
+import { ZERO_ADDRESS } from '../../../constants';
 
 // interface TransactionSubmittedProps {
 //     hash: string;
@@ -22,14 +25,51 @@ interface TransactionSubmittedProps {
 export default function TransactionException(props: TransactionSubmittedProps) {
     const { resetConfirmation, initiateTx } = props;
 
+    const rangeModuleActive = location.pathname.includes('/trade/range');
+    const tradeData = useAppSelector((state) => state.tradeData);
+
+    const isEthSecondary =
+        (tradeData.isTokenAPrimaryRange && tradeData.tokenB.address === ZERO_ADDRESS) ||
+        (!tradeData.isTokenAPrimaryRange && tradeData.tokenA.address === ZERO_ADDRESS);
+
+    const primaryTokenSymbol = tradeData.isTokenAPrimaryRange
+        ? tradeData.tokenA.symbol
+        : tradeData.tokenB.symbol;
+
     return (
         <div className={styles.removal_pending}>
             <h2>Transaction Exception</h2>
 
-            <p>
-                Check the Metamask extension in your browser for notifications, or click &quot;Try
-                Again&quot;.
-            </p>
+            {rangeModuleActive && isEthSecondary ? (
+                <div>
+                    <p>
+                        A preliminary simulation of your transaction has failed. We apologize for
+                        this inconvenience.
+                    </p>
+                    <Divider />
+                    <p>
+                        This may have occurred due to an insufficient ETH balance to cover potential
+                        slippage.
+                    </p>
+                    <Divider />
+                    <p>
+                        Please try entering a specific amount of ETH, rather than
+                        {' ' + primaryTokenSymbol}.
+                    </p>
+                </div>
+            ) : (
+                <div>
+                    <p>
+                        A preliminary simulation of your transaction has failed. We apologize for
+                        this inconvenience.
+                    </p>
+                    <Divider />
+                    <p>
+                        Check the Metamask extension in your browser for notifications, or click
+                        &quot;Try Again&quot;.
+                    </p>
+                </div>
+            )}
             <Button
                 title='Try Again'
                 action={() => {
