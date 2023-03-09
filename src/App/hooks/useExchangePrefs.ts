1b2d3370c36323464209ed1ff8948dc880e60ad6
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-09 11:09:06 -0500

    add logic to useExchangePrefs to track user preference for dex balance in local state

--- src/App/hooks/useExchangePrefs.ts
+++ src/App/hooks/useExchangePrefs.ts
@@ -1,3 +1,12 @@
+import { useState } from 'react';
+
 export const useExchangePrefs = (txType: string) => {
     console.log('exchange balance pref for txType: ' + txType);
+
+    const [outputToDexBal, setOutputToDexBal] = useState<boolean>(false);
+    const [drawFromDexBal, setDrawFromDexBal] = useState<boolean>(false);
+    false && outputToDexBal;
+    false && setOutputToDexBal;
+    false && drawFromDexBal;
+    false && setDrawFromDexBal;
 }
