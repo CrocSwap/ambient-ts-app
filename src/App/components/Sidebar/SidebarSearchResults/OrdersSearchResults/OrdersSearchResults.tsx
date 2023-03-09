c61618c5eccb22d43116872065ac94f7968cc470
Jadessu <junioradessu@gmail.com>
2023-03-08 18:50:06 -0600

    refactor position li to be in position search results

--- src/App/components/Sidebar/SidebarSearchResults/OrdersSearchResults/OrdersSearchResults.tsx
+++ src/App/components/Sidebar/SidebarSearchResults/OrdersSearchResults/OrdersSearchResults.tsx
@@ -1,8 +1,9 @@
 import { Dispatch, SetStateAction } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LimitOrderLI from './LimitOrderLI';
 import styles from '../SidebarSearchResults.module.css';
 import { LimitOrderIF } from '../../../../../utils/interfaces/exports';
+import getUnicodeCharacter from '../../../../../utils/functions/getUnicodeCharacter';
+import { getDisplayPrice, getValueUSD } from './functions/exports';
 
 interface propsIF {
     chainId: string;
@@ -12,6 +13,36 @@ interface propsIF {
     setSelectedOutsideTab: Dispatch<SetStateAction<number>>;
     setCurrentPositionActive: Dispatch<SetStateAction<string>>;
     setIsShowAllEnabled: Dispatch<SetStateAction<boolean>>;
+}
+interface limitOrderPropsIF {
+    limitOrder: LimitOrderIF;
+    isDenomBase: boolean;
+    handleClick: (limitOrder: LimitOrderIF) => void;
+}
+
+function LimitOrderLI(props: limitOrderPropsIF) {
+    const { limitOrder, isDenomBase, handleClick } = props;
+
+    const symbols = {
+        base: limitOrder.baseSymbol ? getUnicodeCharacter(limitOrder.baseSymbol) : '',
+        quote: limitOrder.quoteSymbol ? getUnicodeCharacter(limitOrder.quoteSymbol) : '',
+    };
+
+    const displayPrice = isDenomBase
+        ? getDisplayPrice(symbols.quote, limitOrder.invLimitPriceDecimalCorrected)
+        : getDisplayPrice(symbols.base, limitOrder.limitPriceDecimalCorrected);
+
+    const valueUSD = getValueUSD(limitOrder.totalValueUSD);
+
+    return (
+        <li className={styles.card_container} onClick={() => handleClick(limitOrder)}>
+            <p>
+                {limitOrder.baseSymbol} / {limitOrder.quoteSymbol}
+            </p>
+            <p style={{ textAlign: 'center' }}>{displayPrice}</p>
+            <p style={{ textAlign: 'center' }}>{valueUSD}</p>
+        </li>
+    );
 }
 
 export default function OrdersSearchResults(props: propsIF) {
@@ -34,25 +65,25 @@ export default function OrdersSearchResults(props: propsIF) {
         setIsShowAllEnabled(false);
         navigate(
             '/trade/limit/chain=' +
-            chainId +
-            '&tokenA=' +
-            limitOrder.base +
-            '&tokenB=' +
-            limitOrder.quote
+                chainId +
+                '&tokenA=' +
+                limitOrder.base +
+                '&tokenB=' +
+                limitOrder.quote,
         );
-    }
+    };
 
     return (
         <div>
-            <div className={styles.card_title}>My Limit Orders</div>
+            <h6 className={styles.card_title}>My Limit Orders</h6>
             {searchedLimitOrders.length ? (
                 <>
-                    <div className={styles.header}>
+                    <header className={styles.header}>
                         <div>Pool</div>
                         <div>Price</div>
                         <div>Value</div>
-                    </div>
-                    <div className={styles.main_result_container}>
+                    </header>
+                    <ol className={styles.main_result_container}>
                         {searchedLimitOrders.slice(0, 4).map((limitOrder: LimitOrderIF) => (
                             <LimitOrderLI
                                 key={`order-search-result-${JSON.stringify(limitOrder)}`}
@@ -61,7 +92,7 @@ export default function OrdersSearchResults(props: propsIF) {
                                 handleClick={handleClick}
                             />
                         ))}
-                    </div>
+                    </ol>
                 </>
             ) : (
                 <h5 className={styles.not_found_text}>No Orders Found</h5>
