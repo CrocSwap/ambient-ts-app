dae52b3355aa14f178c6ae8bbbc736d0d08e9b6e
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-16 14:28:01 -0400

    abstract code from HarvestPositionSettings.ts into hook useHarvestSlippage.ts file

--- src/components/HarvestPosition/HarvestPositionSettings/HarvestPositionSettings.tsx
+++ src/components/HarvestPosition/HarvestPositionSettings/HarvestPositionSettings.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import styles from './HarvestPositionSettings.module.css';
 import { useHarvestSlippage } from './useHarvestSlippage';
 
@@ -11,23 +11,10 @@ interface propsIF {
 export default function HarvestPositionSettings(props: propsIF) {
     const { persistedSlippage, setCurrentSlippage, presets } = props;
 
-    // this layer is necessary to make the `<input />` responsive to change
-    // future Emily this is past Emily yes you're going to hate this
-    // ... implementation but please trust me it really is necessary
-    const [slip, setSlip] = useState<string>(persistedSlippage.toString());
-    function takeNewSlippage(val: string | number): void {
-        // setSlip() needs a string
-        // setCurrentSlippage needs a number (float)
-        if (typeof val === 'string') {
-            setSlip(val);
-            setCurrentSlippage(parseFloat(val));
-        } else if (typeof val === 'number') {
-            setSlip(val.toString());
-            setCurrentSlippage(val);
-        }
-    }
-
-    useHarvestSlippage();
+    const [slip, takeNewSlippage] = useHarvestSlippage(
+        persistedSlippage,
+        setCurrentSlippage,
+    );
 
     return (
         <div className={styles.main_container}>
