ee0934ab8fdb95f865d30fb31ac1f0ede09ab09c
Ben Wolski <benwolski@gmail.com>
2023-03-17 13:49:52 -0400

    comment getMoneynessRank.ts

--- src/utils/functions/getMoneynessRank.ts
+++ src/utils/functions/getMoneynessRank.ts
@@ -1,4 +1,17 @@
 export const getMoneynessRank = (addr: string): number => {
+    /* 
+        This 'moneyness' rank is intended to reflect an average user's expectation 
+        of default price denomination, e.g. the price of the ETH/USDC pool should be 
+        denominated by default in USDC for an appearance like 1,600, rather than 0.0006. 
+        
+        Comparing the moneyness rank value of USDC with the rank of ETH allows the front-end
+        to set the default denomination to USDC, as it has a higher moneyness rank.
+
+        The user can override this default denomination on /trade, but not /account.
+
+        The current hardcoded ranking values are ordered according the dev team's intuition, 
+        but otherwise arbitrary.
+    */
     const moneynessMap = new Map<string, number>([
         [
             '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c_0x5', // 'USDC' on Goerli
