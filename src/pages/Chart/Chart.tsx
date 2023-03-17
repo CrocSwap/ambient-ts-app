409528ea3d81e363a9310d5634a511fc32ba9425
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2023-03-17 17:21:02 +0300

    no ghost zone converted svg to canvas

--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -213,6 +213,7 @@ export default function Chart(props: ChartData) {
     const d3CanvasBar = useRef(null);
     const d3CanvasLiqBid = useRef(null);
     const d3CanvasLiqAsk = useRef(null);
+    const d3CanvasNoGoZone = useRef(null);
 
     const d3Xaxis = useRef(null);
     const d3Yaxis = useRef(null);
@@ -5528,17 +5529,21 @@ export default function Chart(props: ChartData) {
     // NoGoZone
     useEffect(() => {
         if (scaleData !== undefined) {
+            const canvas = d3
+                .select(d3CanvasNoGoZone.current)
+                .select('canvas')
+                .node() as any;
+            const ctx = canvas.getContext('2d');
+
             const limitNoGoZone = d3fc
-                .annotationSvgBand()
+                .annotationCanvasBand()
                 .xScale(scaleData.xScale)
                 .yScale(scaleData.yScale)
                 .fromValue((d: any) => d[0])
                 .toValue((d: any) => d[1])
                 .decorate((selection: any) => {
-                    selection
-                        .select('path')
-                        .attr('fill', 'rgba(235, 235, 255, 0.1)');
-                    selection.enter().style('visibility', 'hidden');
+                    selection.fillStyle = 'rgba(235, 235, 255, 0.1)';
+                    // selection.fillStyle = 'rgba(235, 235, 255, 0.1)';
                 });
 
             setLimitNoGoZone(() => {
@@ -5552,6 +5557,36 @@ export default function Chart(props: ChartData) {
             });
         }
     }, [scaleData]);
+
+    useEffect(() => {
+        const canvas = d3
+            .select(d3CanvasNoGoZone.current)
+            .select('canvas')
+            .node() as any;
+        const ctx = canvas.getContext('2d');
+
+        if (limitNoGoZone) {
+            d3.select(d3CanvasNoGoZone.current)
+                .on('draw', () => {
+                    limitNoGoZone(noGoZoneBoudnaries);
+                })
+                .on('measure', () => {
+                    limitNoGoZone.context(ctx);
+                });
+        }
+    }, [noGoZoneBoudnaries, limitNoGoZone]);
+
+    useEffect(() => {
+        if (isLineDrag) {
+            d3.select(d3CanvasNoGoZone.current)
+                .select('canvas')
+                .style('display', 'inline');
+        } else {
+            d3.select(d3CanvasNoGoZone.current)
+                .select('canvas')
+                .style('display', 'none');
+        }
+    }, [isLineDrag]);
 
     function noGoZone(poolPrice: any) {
         return [[poolPrice * 0.99, poolPrice * 1.01]];
@@ -6415,6 +6450,8 @@ export default function Chart(props: ChartData) {
                             )
                         ) {
                             onBlurLimitRate(newLimitValue);
+                        } else {
+                            flashNoGoZone();
                         }
                         // newLimitValue =
                         //     poolPriceDisplay !== undefined &&
@@ -6440,9 +6477,9 @@ export default function Chart(props: ChartData) {
                         horizontalBandJoin(svg, [horizontalBandData]).call(
                             horizontalBand,
                         );
-                        limitNoGoZoneJoin(svg, [noGoZoneBoudnaries]).call(
-                            limitNoGoZone,
-                        );
+                        // limitNoGoZoneJoin(svg, [noGoZoneBoudnaries]).call(
+                        //     limitNoGoZone,
+                        // );
 
                         targetsJoin(svg, [targets.ranges]).call(horizontalLine);
                         marketJoin(svg, [targets.market]).call(marketLine);
@@ -6885,6 +6922,54 @@ export default function Chart(props: ChartData) {
         }
     };
 
+    const flashNoGoZone = () => {
+        d3.select(d3CanvasNoGoZone.current)
+            .select('canvas')
+            .style('display', 'inline');
+
+        const { noGoZoneMin, noGoZoneMax } = getNoZoneData();
+        const center = (noGoZoneMax - noGoZoneMin) / 2;
+        const controlData = noGoZoneMin;
+        // while(controlData <= noGoZoneMax) {
+
+        // }
+        //     d3.select(d3CanvasNoGoZone.current)
+        //     .transition()
+        //     .duration(1000)
+        //     .tween('animate.circles', function() {
+        //       return function(t) {
+        //         noGoZoneBoudnaries[0].forEach(function(d:any,i:any){
+        //           d = d.trans.i(t);
+        //         });
+        //         // drawCircles('black');
+        //       };
+        //   });
+        const interval = setInterval(() => {
+            // d3.select(d3CanvasNoGoZone.current)
+            // .transition()
+            // .duration(2000)
+            // .attr('width', 200)
+            // .attr('height', 200)
+            // .on('end', function() {
+            //     console.log('Animasyon bitti!');
+            // })
+
+            d3.select(d3CanvasNoGoZone.current)
+                .select('canvas')
+                .style('display', 'inline');
+
+            setTimeout(() => {
+                d3.select(d3CanvasNoGoZone.current)
+                    .select('canvas')
+                    .style('display', 'none');
+            }, 200);
+        }, 500);
+
+        setTimeout(() => {
+            interval && clearInterval(interval);
+        }, 1000);
+    };
+
     const onBlurLimitRate = (newLimitValue: any) => {
         const limitPreviousData = limit[0].value;
         if (newLimitValue === undefined) {
@@ -7025,6 +7110,11 @@ export default function Chart(props: ChartData) {
                         ></d3fc-canvas>
                         <d3fc-canvas
                             ref={d3CanvasLiqAsk}
+                            className='plot-canvas'
+                        ></d3fc-canvas>
+
+                        <d3fc-canvas
+                            ref={d3CanvasNoGoZone}
                             className='plot-canvas'
                         ></d3fc-canvas>
 
