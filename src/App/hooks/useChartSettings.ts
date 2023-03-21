2462da84454651b831b5f587dd70298a9bf63b47
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-21 12:48:39 -0400

    annotate new code in useChartSettings.tsx file

--- src/App/hooks/useChartSettings.ts
+++ src/App/hooks/useChartSettings.ts
@@ -6,8 +6,8 @@ interface chartSettingsLocalStorageIF {
     isVolumeSubchartEnabled: boolean;
     isTvlSubchartEnabled: boolean;
     isFeeRateSubchartEnabled: boolean;
-    marketOverlay: string | null;
-    rangeOverlay: string | null;
+    marketOverlay: string;
+    rangeOverlay: string;
 }
 
 // interface for class to manage a given subchart setting
@@ -37,18 +37,23 @@ export interface chartSettingsMethodsIF {
 
 // hook to manage user preferences for chart settings
 export const useChartSettings = (): chartSettingsMethodsIF => {
+    // key for data held in local storage
     const localStorageKey = 'chart_settings';
 
-    const getDataFromLocalStorage = (): chartSettingsLocalStorageIF | null => {
-        return JSON.parse(localStorage.getItem(localStorageKey) as string);
-    };
+    // fn to retrieve and parse persisted data from local storage
+    // will return `null` if the key-val pair does not exist
+    const getDataFromLocalStorage = (): chartSettingsLocalStorageIF | null =>
+        JSON.parse(localStorage.getItem(localStorageKey) as string);
 
     // fn to check a user preference for any given subchart
     const getPreference = (subchart: string): boolean | undefined => {
-        // persisted data from local storage, returns undefined if not present
+        // persisted data from local storage, returns `null` if not present
         const chartSettings: chartSettingsLocalStorageIF | null =
             getDataFromLocalStorage();
+        // declare an output variable to be assigned in switch router
         let output: boolean | undefined;
+        // logic router to assign a value to output
+        // returns `undefined` if there is missing data or for invalid input
         switch (subchart) {
             case 'volume':
                 output = chartSettings?.isVolumeSubchartEnabled;
@@ -65,11 +70,17 @@ export const useChartSettings = (): chartSettingsMethodsIF => {
         return output;
     };
 
-    const getOverlay = (overlay: string): string | undefined => {
+    // fn to get user preference for overlay to display on the chart by module
+    // will return `undefined` if the value does not exist yet
+    // value of `undefined` will be handled downstream
+    const getOverlay = (overlayFor: string): string | undefined => {
         const chartSettings: chartSettingsLocalStorageIF | null =
             getDataFromLocalStorage();
+        // declare an output variable to be assigned in switch router
         let output: string | undefined;
-        switch (overlay) {
+        // logic router to assign a value to output
+        // returns `undefined` if there is missing data or for invalid input
+        switch (overlayFor) {
             case 'market':
             case 'limit':
                 output = chartSettings?.marketOverlay ?? 'depth';
@@ -122,7 +133,6 @@ export const useChartSettings = (): chartSettingsMethodsIF => {
     ]);
 
     // class definition for subchart setting and methods
-    // checked against subchartSettingsIF
     class Subchart implements subchartSettingsIF {
         // base value of the preference
         public readonly isEnabled: boolean;
@@ -150,11 +160,16 @@ export const useChartSettings = (): chartSettingsMethodsIF => {
         }
     }
 
+    // class definition for overlay setting and methods
     class Overlay implements overlayIF {
+        // base value
         public readonly pref: string;
+        // pre-loaded setter functions
         public readonly showDepth: () => void;
         public readonly showCurve: () => void;
         public readonly showNone: () => void;
+        // @param value ➡ current value from local state
+        // @param setterFn ➡ fn to update local state
         constructor(value: string, setterFn: Dispatch<SetStateAction<string>>) {
             this.pref = value;
             this.showDepth = () => setterFn('depth');
