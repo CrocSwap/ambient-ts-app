2d0c795ff9bf16c66198aa8b93c00428c3ffb85a
Ben Wolski <benwolski@gmail.com>
2023-03-24 14:01:44 -0400

    add netlify redirect based on country code to edge function

--- netlify/edge-functions/country-block.ts
+++ netlify/edge-functions/country-block.ts
@@ -3,6 +3,10 @@ import { Context } from 'https://edge.netlify.com';
 export default async (request: Request, context: Context) => {
     // if user not in blocked country, show website
     if (!['KP', 'IR', 'CU'].includes(context.geo.country.code)) {
+        if (['NL'].includes(context.geo.country.code)) {
+            Response.redirect('https://ambient-proven.netlify.app/', 302);
+            // return new URL('https://ambient-proven.netlify.app/', request.url);
+        }
         return;
     }
 
