3b31b5840b43295237eab1a64be853f9d31ccf0c
Jadessu <junioradessu@gmail.com>
2023-03-27 13:01:18 -0500

    add escape to navbar dropdown

--- src/App/hooks/useChartSettings.ts
+++ src/App/hooks/useChartSettings.ts
@@ -217,6 +217,7 @@ export const useChartSettings = (): chartSettingsMethodsIF => {
 
     class CandleTime implements candleTimeIF {
         time: number;
+        // eslint-disable-next-line
         changeTime: (_val: number) => void;
         defaults = [
             { readable: '1m', seconds: 60 },
