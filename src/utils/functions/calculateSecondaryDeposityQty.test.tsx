3247d22352ae35cbd28d9cd33a23f955414d5401
Paul McInnis <paulmcinnis99@gmail.com>
2023-03-27 16:01:23 -0400

    significantly expand quantity of tests

--- src/utils/functions/calculateSecondaryDeposityQty.test.tsx
+++ src/utils/functions/calculateSecondaryDeposityQty.test.tsx
@@ -1,33 +1,27 @@
 import { calculateSecondaryDepositQty } from './calculateSecondaryDepositQty';
-// import * as sdk from '@crocswap-libs/sdk';
-// const mockToDisplayPrice = jest.fn();
 
-jest.mock('@crocswap-libs/sdk', () => ({
-    toDisplayPrice: () => 0.0005809534616770911,
-}));
-
-// import service from './Service';
-// jest.mock('@crocswap-libs/sdk', () => jest.fn());
-// sdk.toDisplayPrice = jest.fn(() => { /*your mock*/ })
-
-const TEST_PRECISION = 4;
+const WBTC_DECIMALS = 8;
 const USDC_DECIMALS = 6;
 const ETH_DECIMALS = 18;
 
 describe('verify calculateSecondaryDepositQty output', () => {
     it.each([
-        // // 1 USDC to 0.000603 eth: http://localhost:3000/trade/range/chain=0x5&tokenA=0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C&tokenB=0x0000000000000000000000000000000000000000
-        // 580953461.6770911, // poolPriceNonDisplay: number, // the 'scaled' or 'wei' price of the pool
-        // 6, // tokenADecimals: number,
-        // 18, // tokenBDecimals: number,
-        // '1', // primaryInputValueStr: string, // the token quantity entered by the user
-        // true, // isTokenAPrimary: boolean,
-        // false, // isTokenABase: boolean,
-        // false, // isAmbientPosition: boolean,
-        // 1.038616454647909, // depositSkew?: number,
-        // 0.000603,
-
         [
+            // 1 USDC yielding 0.000603 eth
+            580953461.6770911, // poolPriceNonDisplay: number, 'scaled' or 'wei' price of the pool
+            6, // tokenADecimals: number,
+            18, // tokenBDecimals: number,
+            '1', // primaryInputValueStr: string, the token quantity entered by the user
+            true, // isTokenAPrimary: boolean,
+            false, // isTokenABase: boolean,
+            false, // isAmbientPosition: boolean,
+            1.038616454647909, // depositSkew?: number,
+            0.0005809534616770911, // mocked @crocswap-libs/sdk.toDisplayPrice return value.
+            0.000603, // expected value.
+            6, // precision for comparison of default vlaue.
+        ],
+        [
+            // 100 USDC yielding 0.0603 eth
             580953461.6770911,
             USDC_DECIMALS,
             ETH_DECIMALS,
@@ -36,7 +30,79 @@ describe('verify calculateSecondaryDepositQty output', () => {
             false,
             false,
             1.038616454647909,
+            0.0005809534616770911,
             0.060338782468249,
+            15,
+        ],
+        [
+            // 0.00001 USDC yielding a small amount of ETH, with updated pool value
+            585685851.9804151,
+            USDC_DECIMALS,
+            ETH_DECIMALS,
+            '0.00001',
+            true,
+            false,
+            false,
+            1.0093300287442797,
+            0.0005856858519804151,
+            5.911503178145104e-9,
+            15,
+        ],
+        [
+            // 1000 USDC to ~0.5 ETH, 52% Range Width
+            585685851.9804151,
+            USDC_DECIMALS,
+            ETH_DECIMALS,
+            '1000',
+            true,
+            false,
+            false,
+            1.0016912625001917,
+            0.0005856858519804151,
+            0.5866764004987624,
+            15,
+        ],
+        [
+            // 1000 USDC to ~0.5 ETH, 12% Range Width
+            585685851.9804151,
+            USDC_DECIMALS,
+            ETH_DECIMALS,
+            '1000',
+            true,
+            false,
+            false,
+            1.007850691212829,
+            0.0005856858519804151,
+            0.5902838907520361,
+            15,
+        ],
+        [
+            // 1 ETH to ~1600 USDC, with updated pool value
+            580949046.2755661,
+            ETH_DECIMALS,
+            USDC_DECIMALS,
+            '1',
+            true,
+            true,
+            false,
+            1.0384661373603,
+            0.000580949046275566,
+            1657.561372390969,
+            15,
+        ],
+        [
+            // 10 WBTC to , with updated pool value
+            157556275070.23358,
+            WBTC_DECIMALS,
+            ETH_DECIMALS,
+            '10',
+            true,
+            false,
+            false,
+            0.976255942594625,
+            15.755627507023359,
+            153.8152498303889,
+            15,
         ],
     ])(
         'input: %s',
@@ -49,9 +115,14 @@ describe('verify calculateSecondaryDepositQty output', () => {
             isTokenABase,
             isAmbientPosition,
             depositSkew,
+            mockToDisplayPrice,
             expected,
+            precision,
         ) => {
-            // sdk.toDisplayPrice.mockImplementation(() => 0.0005809534616770911);
+            // NOTE: I wanted to do the mock once, but I was running into issues with mockImplementation and our sdk.
+            jest.mock('@crocswap-libs/sdk', () => ({
+                toDisplayPrice: () => mockToDisplayPrice,
+            }));
             const qtyTokenB = calculateSecondaryDepositQty(
                 poolPriceNonDisplay,
                 tokenADecimals,
@@ -63,7 +134,7 @@ describe('verify calculateSecondaryDepositQty output', () => {
                 depositSkew,
             );
             expect(qtyTokenB).toBeDefined();
-            expect(expected).toBeCloseTo(qtyTokenB || -1, TEST_PRECISION);
+            expect(qtyTokenB || -1).toBeCloseTo(expected, precision);
         },
     );
 });
