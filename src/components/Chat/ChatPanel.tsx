3fdbe4310dba73a2b91db6335feee6851596f07e
usahan <usahan@proven.com.tr>
2023-03-28 09:00:39 +0300

    When moderator deleted message scroll bar should not scrolls down automatically

--- src/components/Chat/ChatPanel.tsx
+++ src/components/Chat/ChatPanel.tsx
@@ -237,6 +237,7 @@ export default function ChatPanel(props: propsIF) {
     useEffect(() => {
         if (isMessageDeleted === true) {
             getMsg();
+            window.scrollTo(0, 0);
         }
     }, [isMessageDeleted]);
 
