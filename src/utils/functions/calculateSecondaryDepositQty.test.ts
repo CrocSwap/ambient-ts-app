72c375b5e1fa9e6529d63607a34d65b2d37af789
Paul McInnis <paulmcinnis99@gmail.com>
2023-03-29 09:51:27 -0400

    capture the pool prices as constants + fix some test descriptions + consolidate the precision to 15

--- src/utils/functions/calculateSecondaryDepositQty.test.ts
+++ src/utils/functions/calculateSecondaryDepositQty.test.ts
@@ -3,12 +3,16 @@ import { calculateSecondaryDepositQty } from './calculateSecondaryDepositQty';
 const WBTC_DECIMALS = 8;
 const USDC_DECIMALS = 6;
 const ETH_DECIMALS = 18;
+const ETH_USDC_POOL_PRICE = 580953461.6770911;
+const ETH_WBTC_POOL_PRICE = 157556275070.23358;
+const USDC_WBTC_POOL_PRICE = 0.003724870738812943;
+const TEST_PRECISION = 15;
 
 describe('testing calculateSecondaryDepositQty', () => {
     it.each([
         [
             'Balanced 1 ETH-USDC Mint at ±10% Width',
-            580953461.6770911, // poolPriceNonDisplay: number, 'scaled' or 'wei' price of the pool
+            ETH_USDC_POOL_PRICE, // poolPriceNonDisplay: number, 'scaled' or 'wei' price of the pool
             6, // tokenADecimals: number,
             18, // tokenBDecimals: number,
             '1', // primaryInputValueStr: string, the token quantity entered by the user
@@ -16,12 +20,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             false, // isTokenABase: boolean,
             false, // isAmbientPosition: boolean,
             1.038616454647909, // depositSkew?: number,
-            0.000603, // expected value.
-            6, // precision for comparison of default vlaue.
+            0.0006033878246824903, // expected value.
         ],
         [
             'Balanced 100 USDC-ETH Mint at ±10% Width',
-            580953461.6770911,
+            ETH_USDC_POOL_PRICE,
             USDC_DECIMALS,
             ETH_DECIMALS,
             '100',
@@ -30,11 +33,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             1.038616454647909,
             0.060338782468249,
-            15,
         ],
         [
             'Balanced 0.00001 USDC-ETH Mint at ±10% Width',
-            585685851.9804151,
+            ETH_USDC_POOL_PRICE,
             USDC_DECIMALS,
             ETH_DECIMALS,
             '0.00001',
@@ -42,12 +44,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             false,
             1.0093300287442797,
-            5.911503178145104e-9,
-            15,
+            5.8637377417362725e-9,
         ],
         [
             'Balanced 1000 USDC-ETH Mint at ±52% Width',
-            585685851.9804151,
+            ETH_USDC_POOL_PRICE,
             USDC_DECIMALS,
             ETH_DECIMALS,
             '1000',
@@ -55,12 +56,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             false,
             1.0016912625001917,
-            0.5866764004987624,
-            15,
+            0.5819360064811822,
         ],
         [
             'Balanced 1000 USDC-ETH Mint at ±12% Width',
-            585685851.9804151,
+            ETH_USDC_POOL_PRICE,
             USDC_DECIMALS,
             ETH_DECIMALS,
             '1000',
@@ -68,12 +68,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             false,
             1.007850691212829,
-            0.5902838907520361,
-            15,
+            0.5855143479137421,
         ],
         [
             'Balanced 1000 USDC-ETH Mint at ±10% Width',
-            580949046.2755661,
+            ETH_USDC_POOL_PRICE,
             ETH_DECIMALS,
             USDC_DECIMALS,
             '1',
@@ -81,12 +80,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             true,
             false,
             1.0384661373603,
-            1657.561372390969,
-            15,
+            1657.5487744816794,
         ],
         [
             'Balanced 10 WBTC-ETH Mint at ±10% Width',
-            157556275070.23358,
+            ETH_WBTC_POOL_PRICE,
             WBTC_DECIMALS,
             ETH_DECIMALS,
             '10',
@@ -95,11 +93,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             0.976255942594625,
             153.8152498303889,
-            15,
         ],
         [
             'Balanced 10 WBTC-ETH Mint at Ambient Width',
-            157556275070.23358,
+            ETH_WBTC_POOL_PRICE,
             WBTC_DECIMALS,
             ETH_DECIMALS,
             '10',
@@ -108,11 +105,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             true,
             1,
             157.5562750702336,
-            15,
         ],
         [
             'Balanced 15.3 WBTC-ETH Mint, non-base token, at ±10% Width',
-            157556275070.23358,
+            ETH_WBTC_POOL_PRICE,
             WBTC_DECIMALS,
             ETH_DECIMALS,
             '15.3',
@@ -121,12 +117,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             0.976255942594625,
             0.9946998114212481,
-            15,
         ],
 
         [
             'Balanced 1 ETH-WBTC Mint, non-base token, at ±10% Width',
-            157556275070.23358,
+            ETH_WBTC_POOL_PRICE,
             ETH_DECIMALS,
             WBTC_DECIMALS,
             '1',
@@ -135,11 +130,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             true,
             1,
             0.06346938575148668,
-            15,
         ],
         [
             'Unbalanced 1000 USDC-ETH Mint, at [-46%, +12%] Width',
-            576602240.4394826,
+            ETH_USDC_POOL_PRICE,
             6,
             18,
             '1000',
@@ -147,12 +141,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             false,
             0.28913102208717334,
-            0.16671359511602166,
-            15,
+            0.16797166815977885,
         ],
         [
             'Unbalanced 1000 ETH-USDC Mint, at [-3%, +7%] Width',
-            576602240.4394826,
+            ETH_USDC_POOL_PRICE,
             18,
             6,
             '1000',
@@ -160,12 +153,11 @@ describe('testing calculateSecondaryDepositQty', () => {
             true,
             false,
             2.2668609959948887,
-            765065.7928064138,
-            15,
+            759335.6082986615,
         ],
         [
-            'Unbalanced 1000 WBTC-ETH Mint, at [-22%, +5%] Width',
-            0.003724870738812943,
+            'Unbalanced 1000 USDC-WBTC Mint, at [-22%, +5%] Width',
+            USDC_WBTC_POOL_PRICE,
             8,
             6,
             '1000',
@@ -174,11 +166,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             0.24864319098007026,
             107972267.73271404,
-            15,
         ],
         [
             'Unbalanced 1000 USDC-WBTC Mint, at [-19%, +8%] Width',
-            0.003724870738812943,
+            USDC_WBTC_POOL_PRICE,
             6,
             8,
             '1000',
@@ -187,11 +178,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             0.429637409474533,
             0.016003438148510827,
-            15,
         ],
         [
             'Unbalanced 1000 USDC-WBTC Mint, at [-38%, -113%] Width (Out of Range)',
-            0.003724870738812943,
+            USDC_WBTC_POOL_PRICE,
             6,
             8,
             '1000',
@@ -200,11 +190,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             0,
             0,
-            15,
         ],
         [
             'Unbalanced 1000 ETH-WBTC Mint, at [+4%, +5%] Width (Out of Range)',
-            157556275070.23358,
+            ETH_WBTC_POOL_PRICE,
             18,
             8,
             '1000',
@@ -213,11 +202,10 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             Infinity,
             0,
-            15,
         ],
         [
             'Unbalanced 2134142 ETH-USDC Mint, at [+2%, +1.83%] Width (Out of Range)',
-            576602240.4394826,
+            ETH_USDC_POOL_PRICE,
             18,
             6,
             '2134142.95',
@@ -226,7 +214,6 @@ describe('testing calculateSecondaryDepositQty', () => {
             false,
             0,
             0,
-            15,
         ],
     ])(
         'testing case: %s',
@@ -241,7 +228,6 @@ describe('testing calculateSecondaryDepositQty', () => {
             isAmbientPosition,
             depositSkew,
             expected,
-            precision,
         ) => {
             const qtyTokenB =
                 calculateSecondaryDepositQty(
@@ -255,7 +241,7 @@ describe('testing calculateSecondaryDepositQty', () => {
                     depositSkew,
                 ) ?? 0;
             expect(qtyTokenB).toBeDefined();
-            expect(qtyTokenB).toBeCloseTo(expected, precision);
+            expect(qtyTokenB).toBeCloseTo(expected, TEST_PRECISION);
         },
     );
 });
