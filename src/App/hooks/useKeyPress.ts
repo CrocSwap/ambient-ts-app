0b8bdf9f75ef3de821fe66dd06d53573f2456d9d
Jadessu <junioradessu@gmail.com>
2023-03-29 14:41:21 -0500

    add usade and documentation for useKeyPress

--- src/App/hooks/useKeyPress.ts
+++ src/App/hooks/useKeyPress.ts
@@ -32,3 +32,29 @@ export default function useKeyPress(targetKey: string): boolean {
 
     return keyPressed;
 }
+
+// Taken from : https://usehooks.com/useKeyPress/
+
+// How to Use:
+
+// The "useKeyPress" hook is a custom React hook that allows you to detect if a specific key is pressed or not.
+
+// To use the "useKeyPress" hook, you need to pass in a target key as a string to the hook. The hook returns a boolean value which is true if the target key is currently being pressed and false if it's not being pressed.
+
+// Example:
+
+// import React from 'react';
+// import useKeyPress from './useKeyPress';
+
+// function MyComponent() {
+//   const isKeyPressed = useKeyPress('Enter');
+
+//   return (
+//     <div>
+//       {isKeyPressed ? 'Enter key is pressed' : 'Enter key is not pressed'}
+//     </div>
+//   );
+// }
+// In this example, we're using the "useKeyPress" hook to detect if the "Enter" key is currently being pressed. The hook returns a boolean value which we're using to display a message to the user.
+
+// Note that the hook automatically adds event listeners to the window object for the "keydown" and "keyup" events. These event listeners are removed when the component unmounts.
