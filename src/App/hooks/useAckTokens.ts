03d19b859b07ccbaf036eaed7b6f3e51b9c8169d
mhsmith321 <emily.drake.smith@gmail.com>
2023-03-31 16:51:47 -0400

    build out basic logic in useAckTokens.ts file

--- src/App/hooks/useAckTokens.ts
+++ src/App/hooks/useAckTokens.ts
@@ -1,3 +1,35 @@
-export const useAckTokens = () => {
+import { useState } from 'react';
+import { TokenIF } from '../../utils/interfaces/exports';
+
+export interface ackTokensMethodsIF {
+    ackTokens: TokenIF[];
+    acknowledge: (newToken: TokenIF) => void;
+    check: (tkn: TokenIF) => boolean;
+}
+
+export const useAckTokens = (): ackTokensMethodsIF => {
     console.log('ran hook useAckTokens() in App.tsx file!');
+
+    const localStorageKey = 'acknowledgedTokens';
+
+    const [ackTokens, setAckTokens] = useState<TokenIF[]>(
+        JSON.parse(localStorage.getItem(localStorageKey) as string) ?? []
+    );
+
+    function acknowledgeToken(newTkn: TokenIF): void {
+        setAckTokens([newTkn, ...ackTokens]);
+    }
+
+    function checkToken(tkn: TokenIF): boolean {
+        return ackTokens.some((ackToken: TokenIF) => {
+            ackToken.address.toLowerCase() === tkn.address.toLowerCase() &&
+            ackToken.chainId === tkn.chainId
+        });
+    }
+
+    return {
+        ackTokens,
+        acknowledge: acknowledgeToken,
+        check: checkToken,
+    }
 }
