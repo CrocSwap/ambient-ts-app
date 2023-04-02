aa4b27d4ad15be160bbdfaa501706636be786f90
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-04-01 23:57:10 -0400

    Add comments to stable tokens list that identifies which token each line is for

--- src/utils/data/stablePairs.ts
+++ src/utils/data/stablePairs.ts
@@ -1,8 +1,8 @@
 // Arbitrary list of stable or stabl-ish USD pegged coins indexed by chain ID
 const STABLE_TOKENS_BY_CHAIN = {
     '0x5': [
-        '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60',
-        '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C',
+        '0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60', // DAI
+        '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C', // USDC
     ].map((x) => x.toLowerCase()),
 };
 
