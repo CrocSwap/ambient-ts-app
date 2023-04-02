a3159cf886ce6be9ff94349893077a62b652a983
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-04-02 00:51:55 -0400

    Add REACT environment variable to allow environment to define which chains should be supported in the network selector of the app.

--- src/utils/data/chains.ts
+++ src/utils/data/chains.ts
@@ -1,11 +1,16 @@
-// map of all chains supported by Moralis and recognized chain id values
-// all chains supported by Moralis are here regardless of Ambient support
-// keys === name of the network (human readable)
-
 import { CHAIN_SPECS } from '@crocswap-libs/sdk';
 
 // function to validate any given value as a proper id for a supported chain
 export function validateChainId(chainIdToValidate: string): boolean {
     CHAIN_SPECS;
     return chainIdToValidate in CHAIN_SPECS;
+}
+
+// Lists the chain IDs that app should be supported by network selectors and the like
+export function getSupportedChainIds(): Array<string> {
+    if (process.env.REACT_APP_CHAIN_IDS) {
+        return JSON.parse(process.env.REACT_APP_CHAIN_IDS) as [];
+    } else {
+        return ['0x5', '0x6eed'];
+    }
 }
