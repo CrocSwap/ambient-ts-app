02df49ebdc65a5ecf5e6fbe1fe3ca6e6c281e985
mhsmith321 <emily.drake.smith@gmail.com>
2023-04-04 12:40:16 -0400

    use data from defaultTokens.ts as source of truth in defaultTopPool.ts for arbitrum goerli pools

--- src/utils/data/defaultTopPools.ts
+++ src/utils/data/defaultTopPools.ts
@@ -1,3 +1,10 @@
+import {
+    arbGoerliETH,
+    arbGoerliDAI,
+    arbGoerliUSDC,
+    arbGoerliWBTC,
+} from './defaultTokens';
+
 export const defaultTopPools = [
     {
         name: 'ETH/USDC',
@@ -101,24 +108,8 @@ export const defaultTopPools = [
     },
     {
         name: 'ETH/USDC',
-        base: {
-            name: 'Native Ether',
-            address: '0x0000000000000000000000000000000000000000',
-            symbol: 'ETH',
-            decimals: 18,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-        },
-        quote: {
-            name: 'USDCoin',
-            address: '0xc944b73fba33a773a4a07340333a3184a70af1ae',
-            symbol: 'USDC',
-            decimals: 6,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
-        },
+        base: arbGoerliETH,
+        quote: arbGoerliUSDC,
         chainId: '0xa4b1',
         poolId: 36000,
         speed: 0,
@@ -127,24 +118,8 @@ export const defaultTopPools = [
 
     {
         name: 'ETH/WBTC',
-        base: {
-            name: 'Native Ether',
-            address: '0x0000000000000000000000000000000000000000',
-            symbol: 'ETH',
-            decimals: 18,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-        },
-        quote: {
-            name: 'Wrapped BTC',
-            address: '0x5263e9d82352b8098cc811164c38915812bfc1e3',
-            symbol: 'WBTC',
-            decimals: 8,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png',
-        },
+        base: arbGoerliETH,
+        quote: arbGoerliWBTC,
         chainId: '0xa4b1',
         poolId: 36000,
         speed: 0.5,
@@ -152,24 +127,8 @@ export const defaultTopPools = [
     },
     {
         name: 'WBTC/USDC',
-        base: {
-            name: 'Wrapped BTC',
-            address: '0x5263e9d82352b8098cc811164c38915812bfc1e3',
-            symbol: 'WBTC',
-            decimals: 8,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png',
-        },
-        quote: {
-            name: 'USDCoin',
-            address: '0xc944b73fba33a773a4a07340333a3184a70af1ae',
-            symbol: 'USDC',
-            decimals: 6,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
-        },
+        base: arbGoerliWBTC,
+        quote: arbGoerliUSDC,
         chainId: '0xa4b1',
         poolId: 36000,
         speed: -2,
@@ -177,24 +136,8 @@ export const defaultTopPools = [
     },
     {
         name: 'USDC/DAI',
-        base: {
-            name: 'USDCoin',
-            address: '0xc944b73fba33a773a4a07340333a3184a70af1ae',
-            symbol: 'USDC',
-            decimals: 6,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
-        },
-        quote: {
-            name: 'Dai Stablecoin',
-            address: '0xc52f941486978a25fad837bb701d3025679780e4',
-            symbol: 'DAI',
-            decimals: 18,
-            chainId: 0xa4b1,
-            logoURI:
-                'https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png',
-        },
+        base: arbGoerliUSDC,
+        quote: arbGoerliDAI,
         chainId: '0xa4b1',
         poolId: 36000,
         speed: -2,
