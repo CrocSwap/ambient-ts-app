4f341f93f1a48014f79ba13db77e66fd745e8b98
Ben Wolski <benwolski@gmail.com>
2023-04-03 23:50:48 -0400

    add arbitrum görli tokens to defaultTopPools

--- src/utils/data/defaultTopPools.ts
+++ src/utils/data/defaultTopPools.ts
@@ -100,4 +100,105 @@ export const defaultTopPools = [
         speed: -2,
         id: 4,
     },
+    {
+        name: 'ETH/USDC',
+        base: {
+            name: 'Native Ether',
+            address: '0x0000000000000000000000000000000000000000',
+            symbol: 'ETH',
+            decimals: 18,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+        },
+        quote: {
+            name: 'USDCoin',
+            address: '0xc944b73fba33a773a4a07340333a3184a70af1ae',
+            symbol: 'USDC',
+            decimals: 6,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+        },
+        chainId: '0xa4b1',
+        poolId: 36000,
+        speed: 0,
+        id: 1,
+    },
+
+    {
+        name: 'ETH/WBTC',
+        base: {
+            name: 'Native Ether',
+            address: '0x0000000000000000000000000000000000000000',
+            symbol: 'ETH',
+            decimals: 18,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+        },
+        quote: {
+            name: 'Wrapped BTC',
+            address: '0x5263e9d82352b8098cc811164c38915812bfc1e3',
+            symbol: 'WBTC',
+            decimals: 8,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png',
+        },
+        chainId: '0xa4b1',
+        poolId: 36000,
+        speed: 0.5,
+        id: 3,
+    },
+    {
+        name: 'WBTC/USDC',
+        base: {
+            name: 'Wrapped BTC',
+            address: '0x5263e9d82352b8098cc811164c38915812bfc1e3',
+            symbol: 'WBTC',
+            decimals: 8,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png',
+        },
+        quote: {
+            name: 'USDCoin',
+            address: '0xc944b73fba33a773a4a07340333a3184a70af1ae',
+            symbol: 'USDC',
+            decimals: 6,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+        },
+        chainId: '0xa4b1',
+        poolId: 36000,
+        speed: -2,
+        id: 4,
+    },
+    {
+        name: 'USDC/DAI',
+        base: {
+            name: 'USDCoin',
+            address: '0xc944b73fba33a773a4a07340333a3184a70af1ae',
+            symbol: 'USDC',
+            decimals: 6,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+        },
+        quote: {
+            name: 'Dai Stablecoin',
+            address: '0xc52f941486978a25fad837bb701d3025679780e4',
+            symbol: 'DAI',
+            decimals: 18,
+            chainId: 0xa4b1,
+            logoURI:
+                'https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png',
+        },
+        chainId: '0xa4b1',
+        poolId: 36000,
+        speed: -2,
+        id: 4,
+    },
 ];
