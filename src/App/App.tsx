50690979915374cc7490ed82e7851d535ec92449
mhsmith321 <emily.drake.smith@gmail.com>
2023-04-05 11:07:31 -0400

    disable code in PageHeader.tsx to manage document title

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -32,9 +32,7 @@ import {
     setDataLoadingStatus,
     resetConnectedUserDataLoadingStatus,
 } from '../utils/state/graphDataSlice';
-
 import { useAccount, useDisconnect, useProvider, useSigner } from 'wagmi';
-
 import useWebSocket from 'react-use-websocket';
 import {
     sortBaseQuoteTokens,
@@ -43,8 +41,8 @@ import {
     toDisplayQty,
 } from '@crocswap-libs/sdk';
 import { resetReceiptData } from '../utils/state/receiptDataSlice';
-
 import SnackbarComponent from '../components/Global/SnackbarComponent/SnackbarComponent';
+import { Helmet } from 'react-helmet-async';
 
 /** ***** Import JSX Files *******/
 import PageHeader from './components/PageHeader/PageHeader';
@@ -3134,6 +3132,7 @@ export default function App() {
 
     return (
         <>
+            <Helmet></Helmet>
             <div className={containerStyle} data-theme={theme}>
                 {isMobileSidebarOpen && <div className='blur_app' />}
                 <AppOverlay
