635daedf65618072b1a33ad78bd5a25e2f2e3a6c
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-04-09 12:35:07 -0400

    Reload entire window when app switches between valid chains. Not ideal, but fixes issues with mis-matched providers dragging performance after switch

--- src/App/hooks/useAppChain.ts
+++ src/App/hooks/useAppChain.ts
@@ -52,6 +52,7 @@ export const useAppChain = (
 
                     if (validateChainId(currentChain)) {
                         navigate('/');
+                        window.location.reload();
                     }
                 } else if (!validateChainId(chainId)) {
                     setIsChainSupported(false);
