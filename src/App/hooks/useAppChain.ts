063785f75193798e6a3e072c93f51f79f85de047
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-04-14 15:45:42 -0400

    Refactor useAppChain for better reliability and to be responsive to user-initiated network changes in wallet

--- src/App/hooks/useAppChain.ts
+++ src/App/hooks/useAppChain.ts
@@ -4,7 +4,7 @@ import { lookupChain } from '@crocswap-libs/sdk/dist/context';
 import { getDefaultChainId, validateChainId } from '../../utils/data/chains';
 import { useNetwork, useSwitchNetwork } from 'wagmi';
 import { setChainId } from '../../utils/state/tradeDataSlice';
-import { useAppDispatch } from '../../utils/hooks/reduxToolkit';
+import { useAppDispatch, useAppSelector } from '../../utils/hooks/reduxToolkit';
 import { useNavigate } from 'react-router-dom';
 
 export const useAppChain = (
@@ -22,23 +22,40 @@ export const useAppChain = (
         switchNetwork,
     } = useSwitchNetwork();
 
-    const { chain } = useNetwork();
+    const { chain: chainNetwork } = useNetwork();
+
+    function determineConnected(chainNetwork: any): string {
+        return chainNetwork
+            ? '0x' + chainNetwork.id.toString(16)
+            : defaultChain;
+    }
 
     const defaultChain = getDefaultChainId();
-    const chainId = chain ? '0x' + chain.id.toString(16) : defaultChain;
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
     // value tracking the current chain the app is set to use
     // initializes on the default chain parameter
     // we need this value so the app can be used without a wallet
-    const [currentChain, setCurrentChain] = useState(chainId);
+    const [currentChain, setCurrentChain] = useState(
+        determineConnected(chainNetwork),
+    );
+
+    // value trackng the chain the user or app is trying to switch to
+    // If valid, currentChain should converge to this value. For invalid chains
+    // the rest of the app should be gated, by not converging currentChain
+    const [nextChain, setNextChain] = useState(currentChain);
 
     // boolean representing if the current chain is supported by the app
     // we use this value to populate the SwitchNetwork.tsx modal
     const [isChainSupported, setIsChainSupported] = useState(
-        validateChainId(defaultChain),
+        validateChainId(currentChain),
     );
+
+    // If chain switches from wallet, propogate that into currentChain hook
+    useEffect(() => {
+        setNextChain(determineConnected(chainNetwork));
+    }, [chainNetwork]);
 
     // This is crude way to handle a chain switch, but without this there is a
     // a lot of dangling providers pointing to the wrong chain that will error and
@@ -53,50 +70,40 @@ export const useAppChain = (
     // gatekeeping also ensures app will not change to an unsupported network
     // TODO: plan for pathways supporting de-authentication
     useEffect(() => {
-        // If wallet is connected, don't worry about validating chain, should
-        // be correct be default
-        if (!isUserLoggedIn) {
-            return;
+        // Indicates that we're switching between valid chains. For now we reload
+        // the app, since there's a lot of downstream dependencies to the provider
+        if (
+            validateChainId(nextChain) &&
+            validateChainId(currentChain) &&
+            nextChain !== currentChain
+        ) {
+            nukeAndReloadApp();
         }
 
-        // Logic to run if the chain ID has switched
-        if (chainId && chainId !== currentChain) {
-            /* Core of this logic is that currentChain *only* updates when chain
-             * has switched to a supported chain. Three type of user-initiated chain
-             * chain switches are possible:
-             *
-             *    1) Valid -> valid chain switch: Reset entire app (see comments above) */
-            if (validateChainId(chainId) && validateChainId(currentChain)) {
-                nukeAndReloadApp();
-
-                /*    2) Invalid -> valid chain switch: Update currentChain, which cascades app
-                 *                  back to valid state */
-            } else if (validateChainId(chainId)) {
-                setCurrentChain(chainId);
-                setIsChainSupported(true);
-
-                /*    3) Valid/invalid -> invalid chain switch: Turn off isChainSupported, which
-                 *                        triggers blocking downstream */
-            } else {
-                setIsChainSupported(false);
-            }
+        // Only switch currentChain iif valid
+        if (validateChainId(nextChain)) {
+            setCurrentChain(nextChain);
+            dispatch(setChainId(nextChain));
+            setIsChainSupported(true);
+        } else {
+            setIsChainSupported(false);
         }
-    }, [chainId, currentChain, isUserLoggedIn]);
+    }, [nextChain, isUserLoggedIn]);
 
     // data from the SDK about the current chain
     // refreshed every time the the value of currentChain is updated
     const chainData = useMemo(() => {
+        console.log('chain data memo', nextChain, currentChain);
+
         let chn;
         try {
             chn = lookupChain(currentChain);
-            dispatch(setChainId(currentChain));
         } catch (err) {
             console.error(err);
-            setCurrentChain(defaultChain);
             chn = lookupChain(defaultChain);
         }
         return chn;
     }, [currentChain]);
 
-    return [chainData, isChainSupported, setCurrentChain, switchNetwork];
+    return [chainData, isChainSupported, setNextChain, switchNetwork];
 };
