7caaeefc7378e66f36263e82c27a0117f24d475f
Jadessu <junioradessu@gmail.com>
2023-04-21 15:45:59 -0500

    add documentation to fetchCOntractDetails

--- src/App/functions/fetchContractDetails.ts
+++ src/App/functions/fetchContractDetails.ts
@@ -11,13 +11,22 @@ export interface ContractDetails {
     symbol: string | undefined;
     name: string | undefined;
 }
-
+/**
+ * Fetches contract details for a given address using an Ethereum provider.
+ *
+ * @param provider The Ethereum provider to use for querying the blockchain.
+ * @param address The address of the contract to fetch details for.
+ * @param _chainId The ID of the chain to use. This parameter is unused.
+ * @returns A Promise that resolves to a TokenIF object representing the contract details.
+ */
 export const fetchContractDetails = async (
     provider: ethers.providers.Provider,
     address: string,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _chainId: string,
 ): Promise<TokenIF> => {
+    // If the address is the zero address, return details for the native Ether token
+
     if (address === ZERO_ADDRESS) {
         return {
             address: address,
@@ -29,20 +38,25 @@ export const fetchContractDetails = async (
             logoURI: '',
         };
     }
+    // Create a contract object using the given address and the ERC20 ABI
 
     const contract = new Contract(address, ERC20_ABI, provider);
 
     let decimals,
         symbol,
         name = undefined;
+    // Attempt to fetch the decimals, symbol, and name of the contract
 
     try {
         decimals = await contract.decimals();
         symbol = await contract.symbol();
         name = await contract.name();
     } catch (error) {
+        // Log a warning if an error occurs during the fetch
+
         console.warn({ error });
     }
+    // Return the contract details as a TokenIF object
 
     return {
         address: address,
