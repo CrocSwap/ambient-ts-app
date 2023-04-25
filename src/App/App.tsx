2511eee81557fab8725007fa1313e6e93924a239
mhsmith321 <emily.drake.smith@gmail.com>
2023-04-25 12:54:39 -0400

    stub up new Accessibility page at /accessibility URL route

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -177,6 +177,7 @@ import { ackTokensMethodsIF, useAckTokens } from './hooks/useAckTokens';
 import { topPoolIF, useTopPools } from './hooks/useTopPools';
 import { formSlugForPairParams } from './functions/urlSlugs';
 import useChatApi from '../components/Chat/Service/ChatApi';
+import Accessibility from '../pages/Accessibility/Accessibility';
 
 const cachedFetchAddress = memoizeFetchAddress();
 const cachedFetchNativeTokenBalance = memoizeFetchNativeTokenBalance();
@@ -332,6 +333,10 @@ export default function App() {
         onIdle,
         onActive,
         //    onAction,
+        //    onComet,
+        //    onCupid,
+        //    onDonner,
+        //    onBlitzen,
         timeout: 1000 * 60 * 60, // set user to idle after 60 minutes
         promptTimeout: 0,
         events: [
@@ -3449,6 +3454,10 @@ export default function App() {
                                     cachedPoolStatsFetch={cachedPoolStatsFetch}
                                 />
                             }
+                        />
+                        <Route
+                            path='accessibility'
+                            element={<Accessibility />}
                         />
                         <Route path='trade' element={<Trade {...tradeProps} />}>
                             <Route
---
+++ src/pages/Accessibility/Accessibility.tsx
@@ -0,0 +1,3 @@
+export default function Accessibility() {
+    return <h1>This is the Accessibility page!</h1>;
+}
