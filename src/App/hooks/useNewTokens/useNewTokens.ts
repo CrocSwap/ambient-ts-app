fcc1adaf310e16f9bacc93d8ea4a2b4d05c86ef2
mhsmith321 <emily.drake.smith@gmail.com>
2023-05-01 13:40:31 -0400

    add logic to useNewTokens.ts to import external token lists asynchronously

--- src/App/hooks/useNewTokens/useNewTokens.ts
+++ src/App/hooks/useNewTokens/useNewTokens.ts
@@ -1,3 +1,23 @@
+import { tokenListURIs } from '../../../utils/data/tokenListURIs';
+import uriToHttp from '../../../utils/functions/uriToHttp';
+import { TokenListIF } from '../../../utils/interfaces/TokenListIF';
+
 export const useNewTokens = () => {
     console.log('ran custom hook useNewTokens() in App.tsx file!');
+    // create an array of promises to fetch all token lists in the URIs file
+    const tokenLists: Promise<TokenListIF>[] = Object.values(tokenListURIs).map(
+        (uri) =>
+            fetch(uriToHttp(uri))
+                // process response as a JSOM
+                .then((response) => response.json())
+                // middleware to add internal-use data to each list
+                .then((response) => ({
+                    ...response,
+                    uri,
+                    dateRetrieved: new Date().toISOString(),
+                    userImported: false,
+                })),
+    );
+
+    console.log(tokenLists);
 };
