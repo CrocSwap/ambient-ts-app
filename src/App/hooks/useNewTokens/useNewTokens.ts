8e7174f129fc7c263837cb094e4710f91bd3a553
mhsmith321 <emily.drake.smith@gmail.com>
2023-05-01 13:51:03 -0400

    add logic to useNewTokens.ts to resolve promises asynchronously

--- src/App/hooks/useNewTokens/useNewTokens.ts
+++ src/App/hooks/useNewTokens/useNewTokens.ts
@@ -5,19 +5,22 @@ import { TokenListIF } from '../../../utils/interfaces/TokenListIF';
 export const useNewTokens = () => {
     console.log('ran custom hook useNewTokens() in App.tsx file!');
     // create an array of promises to fetch all token lists in the URIs file
-    const tokenLists: Promise<TokenListIF>[] = Object.values(tokenListURIs).map(
-        (uri) =>
-            fetch(uriToHttp(uri))
-                // process response as a JSOM
-                .then((response) => response.json())
-                // middleware to add internal-use data to each list
-                .then((response) => ({
-                    ...response,
-                    uri,
-                    dateRetrieved: new Date().toISOString(),
-                    userImported: false,
-                })),
+    const tokenListPromises: Promise<TokenListIF>[] = Object.values(
+        tokenListURIs,
+    ).map((uri) =>
+        fetch(uriToHttp(uri))
+            // process response as a JSOM
+            .then((response) => response.json())
+            // middleware to add internal-use data to each list
+            .then((response) => ({
+                ...response,
+                uri,
+                dateRetrieved: new Date().toISOString(),
+                userImported: false,
+            })),
     );
 
-    console.log(tokenLists);
+    Promise.allSettled(tokenListPromises).then((promises) =>
+        console.log(promises),
+    );
 };
