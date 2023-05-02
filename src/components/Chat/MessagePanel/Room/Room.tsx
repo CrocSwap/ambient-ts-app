d6d985c29048f05a2f1d1b2c8c4d41dc7aa36cb9
Ben Wolski <benwolski@gmail.com>
2023-05-02 13:06:39 -0400

    update padding and background color of chat dropdown

--- src/components/Chat/MessagePanel/Room/Room.tsx
+++ src/components/Chat/MessagePanel/Room/Room.tsx
@@ -69,7 +69,7 @@ export default function RoomDropdown(props: propsIF) {
     const defaultRooms = [
         {
             id: 100,
-            name: '   @Global',
+            name: 'Global',
             value: 'Global',
         },
     ];
@@ -77,7 +77,7 @@ export default function RoomDropdown(props: propsIF) {
     const isFullScreenDefaultRooms = [
         {
             id: 100,
-            name: '@Global',
+            name: 'Global',
             value: 'Global',
         },
     ];
