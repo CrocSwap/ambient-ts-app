9dd051b3bc2e00e50ecb4c7f11e5dd20afd31129
mhsmith321 <emily.drake.smith@gmail.com>
2023-05-03 11:44:50 -0400

    add logic to useNewTokens.ts to timestamp token lists for when the list should be refeteched

--- src/App/hooks/useNewTokens/useNewTokens.ts
+++ src/App/hooks/useNewTokens/useNewTokens.ts
@@ -6,7 +6,13 @@ import { TokenIF, TokenListIF } from '../../../utils/interfaces/exports';
 export const useNewTokens = () => {
     const tokenListsLocalStorageKey = 'tokenLists';
 
-    const [tokenLists, setTokenLists] = useState<TokenListIF[]>();
+    function getTokenListsFromLS(): TokenListIF[] {
+        return JSON.parse(localStorage.getItem(tokenListsLocalStorageKey) as string);
+    }
+
+    const [tokenLists, setTokenLists] = useState<TokenListIF[]>(
+        getTokenListsFromLS() ?? []
+    );
     
     useEffect(() => {
         // create an array of promises to fetch all token lists in the URIs file
@@ -14,16 +20,22 @@ export const useNewTokens = () => {
             tokenListURIs,
         ).map((uri: string) => fetchTokenList(uri, false));
         Promise.allSettled(tokenListPromises)
+            // format returned data into a useful form for the app
+            // 1st val ➡ indicates if second val is a value
+            // 2nd val ➡ value returned by promise
             .then((promises) => promises.flatMap((promise) => Object.entries(promise))
                 .filter((promise) => promise[0] === 'value')
                 .map((promise) => promise[1])
-            ).then((lists) => {
+            )
+            // middleware to add metadata used by the Ambient platform
+            .then((lists) => {
                 // indicate which list each token data object was imported with
-                lists.forEach((list) =>
+                lists.forEach((list) => {
+                    list.refreshAfter = Date.now() + 14400000;
                     list.tokens.forEach(
                         (token: TokenIF) => (token.fromList = list.uri),
-                    ),
-                );
+                    )
+                });
                 setTokenLists(lists);
                 localStorage.setItem(tokenListsLocalStorageKey, JSON.stringify(lists));
             });
