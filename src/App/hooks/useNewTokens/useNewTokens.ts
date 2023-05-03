31f1ca2dcb4aaeca507780a2bdcc6488e6335158
mhsmith321 <emily.drake.smith@gmail.com>
2023-05-03 11:49:36 -0400

    gatekeep logic in useNewTokens.ts to fetch all token lists only when data in local storage is completely absent

--- src/App/hooks/useNewTokens/useNewTokens.ts
+++ src/App/hooks/useNewTokens/useNewTokens.ts
@@ -11,35 +11,37 @@ export const useNewTokens = () => {
     }
 
     const [tokenLists, setTokenLists] = useState<TokenListIF[]>(
-        getTokenListsFromLS() ?? []
+        getTokenListsFromLS()
     );
     
     useEffect(() => {
-        // create an array of promises to fetch all token lists in the URIs file
-        const tokenListPromises: Promise<TokenListIF>[] = Object.values(
-            tokenListURIs,
-        ).map((uri: string) => fetchTokenList(uri, false));
-        Promise.allSettled(tokenListPromises)
-            // format returned data into a useful form for the app
-            // 1st val ➡ indicates if second val is a value
-            // 2nd val ➡ value returned by promise
-            .then((promises) => promises.flatMap((promise) => Object.entries(promise))
-                .filter((promise) => promise[0] === 'value')
-                .map((promise) => promise[1])
-            )
-            // middleware to add metadata used by the Ambient platform
-            .then((lists) => {
-                // indicate which list each token data object was imported with
-                lists.forEach((list) => {
-                    list.refreshAfter = Date.now() + 14400000;
-                    list.tokens.forEach(
-                        (token: TokenIF) => (token.fromList = list.uri),
-                    )
+        if (tokenLists === null) {
+            // create an array of promises to fetch all token lists in the URIs file
+            const tokenListPromises: Promise<TokenListIF>[] = Object.values(
+                tokenListURIs,
+            ).map((uri: string) => fetchTokenList(uri, false));
+            Promise.allSettled(tokenListPromises)
+                // format returned data into a useful form for the app
+                // 1st val ➡ indicates if second val is a value
+                // 2nd val ➡ value returned by promise
+                .then((promises) => promises.flatMap((promise) => Object.entries(promise))
+                    .filter((promise) => promise[0] === 'value')
+                    .map((promise) => promise[1])
+                )
+                // middleware to add metadata used by the Ambient platform
+                .then((lists) => {
+                    // indicate which list each token data object was imported with
+                    lists.forEach((list) => {
+                        list.refreshAfter = Date.now() + 14400000;
+                        list.tokens.forEach(
+                            (token: TokenIF) => (token.fromList = list.uri),
+                        )
+                    });
+                    setTokenLists(lists);
+                    localStorage.setItem(tokenListsLocalStorageKey, JSON.stringify(lists));
                 });
-                setTokenLists(lists);
-                localStorage.setItem(tokenListsLocalStorageKey, JSON.stringify(lists));
-            });
+        };
     }, []);
 
-    useEffect(() => console.log(tokenLists), [tokenLists]);
+    // useEffect(() => console.log(tokenLists), [tokenLists]);
 };
