123e8798f44a536f78fecf44b3188c7ad7ee3a4c
Ben Wolski <benwolski@gmail.com>
2023-05-09 13:27:06 -0400

    try to fetch NFT previews, if available

--- src/App/functions/getNFTs.ts
+++ src/App/functions/getNFTs.ts
@@ -15,8 +15,9 @@ export async function getNFTs(account: string) {
         const chain = EvmChain.ETHEREUM;
 
         const response = await Moralis.EvmApi.nft.getWalletNFTs({
-            address,
-            chain,
+            address: address,
+            chain: chain,
+            mediaItems: true,
         });
 
         const result = response?.result;
@@ -30,8 +31,65 @@ export async function getNFTs(account: string) {
             const imageLocalURLs: string[] = [];
             userEthNFTs.forEach((nft) => {
                 const metadata = nft.metadata;
+                const media = nft.media;
+                console.log({ media });
+                if (media) {
+                    const mediaCollection = media.mediaCollection;
+                    console.log({ mediaCollection });
+                    const highPreview = mediaCollection?.high;
+                    console.log({ highPreview });
+                    const highPreviewUrl = highPreview?.url;
+                    console.log({ highPreviewUrl });
 
-                if (metadata) {
+                    // const originalImageUrl = (metadata as unknown as metadata)
+                    //     .image;
+
+                    const imageGatewayURL = highPreviewUrl
+                        ? highPreviewUrl
+                        : undefined;
+
+                    console.log({ imageGatewayURL });
+                    // if (
+                    //     imageGatewayURL.includes('lotterynft') ||
+                    //     [
+                    //         '88279830972223375506659847640588682494880443313199730925423741164868176183297',
+                    //     ].includes(nft.result.tokenId.toString())
+                    // ) {
+                    //     return;
+                    // } else if (
+                    //     !nft.symbol ||
+                    //     ![
+                    //         'RARI',
+                    //         'Save Fud NFT Lab',
+                    //         'ICE',
+                    //         'OPENSTORE',
+                    //         // 'ToshimonMinter',
+                    //     ].includes(nft.symbol)
+                    // ) {
+                    //     return;
+                    // } else if (imageGatewayURL.startsWith('https://')) {
+                    //     imageGatewayURL = originalImageUrl;
+                    // } else if (originalImageUrl.startsWith('ipfs://')) {
+                    //     const imageUrlNoProtocol =
+                    //         originalImageUrl.substring(12);
+                    //     imageGatewayURL =
+                    //         'https://ipfs.io/ipfs/' + imageUrlNoProtocol;
+                    // }
+                    IS_LOCAL_ENV &&
+                        console.debug({ nftMatchingAllowList: nft });
+                    if (imageGatewayURL)
+                        fetch(imageGatewayURL)
+                            .then((response) => response.blob())
+                            .then((image) => {
+                                if (image.type.includes('image')) {
+                                    console.log({ image });
+                                    // Create a local URL of that image
+                                    const localUrl = URL.createObjectURL(image);
+                                    imageLocalURLs.push(localUrl);
+                                }
+                            })
+                            .catch(console.error);
+                } else if (metadata) {
                     let imageGatewayURL;
                     const imageUrl = (metadata as unknown as metadata).image;
                     if (
@@ -74,6 +132,7 @@ export async function getNFTs(account: string) {
                             .catch(console.error);
                 }
             });
+            console.log({ imageLocalURLs });
             return imageLocalURLs;
         }
     } catch (e) {
