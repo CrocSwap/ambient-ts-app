d71a2a104e2c3afed7ce4a3bf288d51b8a790d6b
Ben Wolski <benwolski@gmail.com>
2023-05-12 14:43:41 -0400

    add base for hexadecimal numbers as separate constant

--- src/App/functions/chainNumToString.ts
+++ src/App/functions/chainNumToString.ts
@@ -1,3 +1,5 @@
+const baseForHexNumbers = 16;
+
 export default function chainNumToString(chainNumber: number): string {
-    return '0x' + chainNumber.toString(16).toLowerCase();
+    return '0x' + chainNumber.toString(baseForHexNumbers).toLowerCase();
 }
