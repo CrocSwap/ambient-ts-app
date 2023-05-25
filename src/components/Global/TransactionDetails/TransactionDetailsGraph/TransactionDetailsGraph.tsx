25b4c778aa9bed504d89e4d6b1be5de2498b5014
Jadessu <junioradessu@gmail.com>
2023-05-25 16:04:26 -0500

    change timeout to 1000

--- src/components/Global/TransactionDetails/TransactionDetailsGraph/TransactionDetailsGraph.tsx
+++ src/components/Global/TransactionDetails/TransactionDetailsGraph/TransactionDetailsGraph.tsx
@@ -807,7 +807,7 @@ export default function TransactionDetailsGraph(
         if (!graphData) {
             const timeoutId = setTimeout(() => {
                 setIsDataEmpty(true);
-            }, 10000);
+            }, 1000);
 
             return () => {
                 clearTimeout(timeoutId);
