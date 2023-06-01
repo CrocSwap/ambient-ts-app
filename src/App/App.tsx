abf2786381c88eed584941abb9f8556af3cb0210
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-01 13:13:53 -0400

    remove code disabling sidebar on `/404` route

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -123,7 +123,6 @@ export default function App() {
     // Show sidebar on all pages except for home and swap
     const sidebarRender = currentLocation !== '/' &&
         currentLocation !== '/swap' &&
-        currentLocation !== '/404' &&
         !currentLocation.includes('/chat') &&
         !fullScreenChart &&
         isChainSupported && <Sidebar />;
