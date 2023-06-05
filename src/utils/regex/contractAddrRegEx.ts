28a7ed549461b08caef075dc9e2d5539fbf1a3db
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-05 15:19:31 -0400

    annotate new centralized RegExp files

--- src/utils/regex/contractAddrRegEx.ts
+++ src/utils/regex/contractAddrRegEx.ts
@@ -1,1 +1,3 @@
+// RegExp object for contract addresses
+// '0x' + 40 case-insensitive hexadecimal characters
 export const contractAddrRegEx = new RegExp('0x[0-9a-fA-F]{40}$');
--- src/utils/regex/exponentialNumRegEx.ts
+++ src/utils/regex/exponentialNumRegEx.ts
@@ -1,3 +1,11 @@
+// RegExp object for an exponential notation number
+// at least one 0-9 numeral
+// ... + an optional decimal
+// ... + more 0-9 numerals
+// ... + an optional 'E' or 'e'
+// ... + at least one 0-9 numeral
+// ... + an optional decimal
+// ... + more 0-9 numerals
 export const exponentialNumRegEx = new RegExp(
     '^[0-9,]*[.]?[0-9]*[Ee]?[+-]?[0-9]*[.]?[0-9]*$',
 );
