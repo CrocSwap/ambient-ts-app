2fe8d06cd65701ab30b09930b15da19ec9aee714
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-07 13:22:28 -0400

    increase explicit type usage in `useSortedTxs.ts` file

--- src/components/Trade/TradeTabs/useSortedTxs.ts
+++ src/components/Trade/TradeTabs/useSortedTxs.ts
@@ -2,54 +2,62 @@ import { Dispatch, SetStateAction, useMemo, useState } from 'react';
 import { TransactionIF } from '../../../utils/interfaces/exports';
 import { diffHashSig } from '../../../utils/functions/diffHashSig';
 
+export type TxSortType =
+    | 'time'
+    | 'wallet'
+    | 'walletid'
+    | 'pool'
+    | 'price'
+    | 'value'
+    | 'default';
+
 export const useSortedTxs = (
-    defaultSort: string,
+    defaultSort: TxSortType,
     transactions: TransactionIF[],
 ): [
-    string,
-    Dispatch<SetStateAction<string>>,
+    TxSortType,
+    Dispatch<SetStateAction<TxSortType>>,
     boolean,
     Dispatch<SetStateAction<boolean>>,
     TransactionIF[],
 ] => {
     // default sort function
-    const sortByUpdateTime = (unsortedData: TransactionIF[]) =>
+    const sortByUpdateTime = (unsortedData: TransactionIF[]): TransactionIF[] =>
         [...unsortedData].sort((a, b) => b.txTime - a.txTime);
     // sort by token pair
-    const sortByPool = (unsortedData: TransactionIF[]) =>
+    const sortByPool = (unsortedData: TransactionIF[]): TransactionIF[] =>
         [...unsortedData].sort((a, b) => {
             const poolA = a.baseSymbol + a.quoteSymbol;
             const poolB = b.baseSymbol + b.quoteSymbol;
             return poolA.localeCompare(poolB);
         });
     // sort by wallet or ens address
-    const sortByWallet = (unsortedData: TransactionIF[]) =>
+    const sortByWallet = (unsortedData: TransactionIF[]): TransactionIF[] =>
         [...unsortedData].sort((a, b) => {
             const usernameA: string = a.ensResolution ?? a.user;
             const usernameB: string = b.ensResolution ?? b.user;
             return usernameA.localeCompare(usernameB);
         });
     // sort by limit price
-    const sortByPrice = (unsortedData: TransactionIF[]) =>
+    const sortByPrice = (unsortedData: TransactionIF[]): TransactionIF[] =>
         [...unsortedData].sort((a, b) => b.limitPrice - a.limitPrice);
     // sort by value of limit order
-    const sortByValue = (unsortedData: TransactionIF[]) =>
+    const sortByValue = (unsortedData: TransactionIF[]): TransactionIF[] =>
         [...unsortedData].sort((a, b) => {
             const valueA = a.totalValueUSD;
             const valueB = b.totalValueUSD;
             if (!valueB) return -1;
-
             return Math.abs(valueB) - Math.abs(valueA);
         });
 
     // column the user wants the table sorted by
     // this is set when the user clicks a sortable column header
-    const [sortBy, setSortBy] = useState(defaultSort);
+    const [sortBy, setSortBy] = useState<TxSortType>(defaultSort);
     // whether the sort should be ascending or descending
-    const [reverseSort, setReverseSort] = useState(false);
+    const [reverseSort, setReverseSort] = useState<boolean>(false);
 
     // router to pass data through the appropriate sort function
-    const sortData = (data: TransactionIF[]) => {
+    const sortData = (data: TransactionIF[]): TransactionIF[] => {
         // variable to hold output
         let sortedData: TransactionIF[];
         // router to apply a specific sort function
