5a455a651addb09636ba5b045d545143069db2e3
surudhb <bsurudh@gmail.com>
2023-06-13 16:52:01 -0700

    type the exponentialNumRegEx

--- src/utils/regex/exponentialNumRegEx.ts
+++ src/utils/regex/exponentialNumRegEx.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-escape */
 // RegExp object for an exponential notation number
-let exponentialNumRegEx;
+let exponentialNumRegEx: RegExp;
 
 if (navigator.userAgent.includes('Safari')) {
     exponentialNumRegEx = /^[\d]+[.]?[,\d]*[eE]?[+\-]?[\d]*[.]?[\d]*/;
