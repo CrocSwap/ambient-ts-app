8e805163b3b137208fe542b7c7e99569b782c4b9
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-12 21:35:10 -0400

    apply new token sorting logic to token lists in Account module

--- src/components/Global/Account/AccountTabs/Exchange/Exchange.tsx
+++ src/components/Global/Account/AccountTabs/Exchange/Exchange.tsx
@@ -5,6 +5,9 @@ import { TokenIF } from '../../../../../utils/interfaces/exports';
 import Spinner from '../../../Spinner/Spinner';
 import { useAppSelector } from '../../../../../utils/hooks/reduxToolkit';
 import { TokenPriceFn } from '../../../../../App/functions/fetchTokenPrice';
+import { TokenContext } from '../../../../../contexts/TokenContext';
+import { useContext } from 'react';
+import { tokenListURIs } from '../../../../../utils/data/tokenListURIs';
 
 interface propsIF {
     resolvedAddressTokens: (TokenIF | undefined)[];
@@ -20,6 +23,8 @@ export default function Exchange(props: propsIF) {
         cachedFetchTokenPrice,
     } = props;
 
+    const { tokens } = useContext(TokenContext);
+
     const { nativeToken, erc20Tokens } = useAppSelector(
         (state) => state.userData.tokens,
     );
@@ -32,29 +37,77 @@ export default function Exchange(props: propsIF) {
     const ItemContent = () => {
         if (connectedAccountActive) {
             if (connectedUserTokens && connectedUserTokens.length > 0) {
-                return connectedUserTokens.map((item, idx) => (
+                return sequenceTokens(connectedUserTokens as TokenIF[]).map(
+                    (item, idx) => (
+                        <ExchangeCard
+                            key={idx}
+                            token={item}
+                            cachedFetchTokenPrice={cachedFetchTokenPrice}
+                        />
+                    ),
+                );
+            }
+        }
+        if (resolvedAddressTokens && resolvedAddressTokens[0]) {
+            return sequenceTokens(resolvedAddressTokens as TokenIF[]).map(
+                (item, idx) => (
                     <ExchangeCard
                         key={idx}
                         token={item}
                         cachedFetchTokenPrice={cachedFetchTokenPrice}
                     />
-                ));
-            }
-        }
-        if (resolvedAddressTokens && resolvedAddressTokens[0]) {
-            return resolvedAddressTokens.map((item, idx) => (
-                <ExchangeCard
-                    key={idx}
-                    token={item}
-                    cachedFetchTokenPrice={cachedFetchTokenPrice}
-                />
-            ));
+                ),
+            );
         }
         if (resolvedAddressTokens && !resolvedAddressTokens[0]) {
             return;
         }
         return spinnerElement;
     };
+
+    function sequenceTokens(tkns: TokenIF[]) {
+        const tokensWithOrigins: TokenIF[] = tkns.map((tkn: TokenIF) => {
+            return {
+                ...tkn,
+                listedBy: tokens.getTokenByAddress(tkn.address)?.listedBy ?? [
+                    'unknown',
+                ],
+            };
+        });
+        const sequencedTokens: TokenIF[] = tokensWithOrigins.sort(
+            (a: TokenIF, b: TokenIF) => {
+                // output value
+                let rank: number;
+                // decision tree to determine sort order
+                // sort ambient-listed token higher if only one is listed by us
+                // otherwise sort by number of lists featuring the token overall
+                if (isOnAmbientList(a) && isOnAmbientList(b)) {
+                    rank = comparePopularity();
+                } else if (isOnAmbientList(a)) {
+                    rank = -1;
+                } else if (isOnAmbientList(b)) {
+                    rank = 1;
+                } else {
+                    rank = comparePopularity();
+                }
+                // fn to determine if a given token is on the ambient list
+                function isOnAmbientList(t: TokenIF): boolean {
+                    return !!t.listedBy?.includes(tokenListURIs.ambient);
+                }
+                // fn to determine which of the two tokens is more popular
+                function comparePopularity(): number {
+                    const getPopularity = (tkn: TokenIF): number =>
+                        tkn.listedBy?.length ?? 1;
+                    const popularityTokenA: number = getPopularity(a);
+                    const popularityTokenB: number = getPopularity(b);
+                    return popularityTokenB - popularityTokenA;
+                }
+                // return the output variable
+                return rank;
+            },
+        );
+        return sequencedTokens;
+    }
 
     return (
         <div
--- src/components/Global/Account/AccountTabs/Wallet/Wallet.tsx
+++ src/components/Global/Account/AccountTabs/Wallet/Wallet.tsx
@@ -5,6 +5,9 @@ import { TokenIF } from '../../../../../utils/interfaces/exports';
 import { useAppSelector } from '../../../../../utils/hooks/reduxToolkit';
 import { TokenPriceFn } from '../../../../../App/functions/fetchTokenPrice';
 import Spinner from '../../../Spinner/Spinner';
+import { useContext } from 'react';
+import { TokenContext } from '../../../../../contexts/TokenContext';
+import { tokenListURIs } from '../../../../../utils/data/tokenListURIs';
 
 interface propsIF {
     resolvedAddressTokens: (TokenIF | undefined)[];
@@ -20,6 +23,8 @@ export default function Wallet(props: propsIF) {
         cachedFetchTokenPrice,
     } = props;
 
+    const { tokens } = useContext(TokenContext);
+
     const { nativeToken, erc20Tokens } = useAppSelector(
         (state) => state.userData.tokens,
     );
@@ -30,6 +35,50 @@ export default function Wallet(props: propsIF) {
     const userTokens = connectedAccountActive
         ? connectedUserTokens
         : resolvedAddressTokens;
+
+    function sequenceTokens(tkns: TokenIF[]) {
+        const tokensWithOrigins: TokenIF[] = tkns.map((tkn: TokenIF) => {
+            return {
+                ...tkn,
+                listedBy: tokens.getTokenByAddress(tkn.address)?.listedBy ?? [
+                    'unknown',
+                ],
+            };
+        });
+        const sequencedTokens: TokenIF[] = tokensWithOrigins.sort(
+            (a: TokenIF, b: TokenIF) => {
+                // output value
+                let rank: number;
+                // decision tree to determine sort order
+                // sort ambient-listed token higher if only one is listed by us
+                // otherwise sort by number of lists featuring the token overall
+                if (isOnAmbientList(a) && isOnAmbientList(b)) {
+                    rank = comparePopularity();
+                } else if (isOnAmbientList(a)) {
+                    rank = -1;
+                } else if (isOnAmbientList(b)) {
+                    rank = 1;
+                } else {
+                    rank = comparePopularity();
+                }
+                // fn to determine if a given token is on the ambient list
+                function isOnAmbientList(t: TokenIF): boolean {
+                    return !!t.listedBy?.includes(tokenListURIs.ambient);
+                }
+                // fn to determine which of the two tokens is more popular
+                function comparePopularity(): number {
+                    const getPopularity = (tkn: TokenIF): number =>
+                        tkn.listedBy?.length ?? 1;
+                    const popularityTokenA: number = getPopularity(a);
+                    const popularityTokenB: number = getPopularity(b);
+                    return popularityTokenB - popularityTokenA;
+                }
+                // return the output variable
+                return rank;
+            },
+        );
+        return sequencedTokens;
+    }
 
     // TODO:   @Junior  I don't think there's any reason for the header element in
     // TODO:   ... the return statement to be abstracted into its own file as it
@@ -44,7 +93,7 @@ export default function Wallet(props: propsIF) {
             <WalletHeader />
             <div className={styles.item_container}>
                 {userTokens && userTokens.length > 0 ? (
-                    userTokens.map((token) => (
+                    sequenceTokens(userTokens as TokenIF[]).map((token) => (
                         <WalletCard
                             key={JSON.stringify(token)}
                             token={token}
