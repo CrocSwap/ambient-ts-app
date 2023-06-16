9111fa3a367767dfc122d9b3ab19fb3110d00b96
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-16 12:03:08 -0400

    refactor `handleWETH.ts` and move from `/src/utils/functions` to `/src/utils/data` directory

---
+++ src/utils/data/handleWETH.ts
@@ -0,0 +1,15 @@
+export const handleWETH = {
+    check(userInput: string): boolean {
+        const wethStrings: string[] = [
+            'weth',
+            'wrapped ether',
+            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        ];
+        const isWeth: boolean = wethStrings.some((str: string) =>
+            userInput.includes(str),
+        );
+        return isWeth;
+    },
+    message:
+        'It looks like you want to use Wrapped Ether. Do you know that native Ether is cheaper on the Ambient platform?',
+};
