265f335bc6876b2a39ae0c931fd57f7b04113fa7
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-19 17:37:22 -0400

    change string matches in `handleWeth.ts` in consult with Doug

--- src/utils/data/handleWETH.ts
+++ src/utils/data/handleWETH.ts
@@ -2,7 +2,6 @@ export const handleWETH = {
     check(userInput: string): boolean {
         const wethStrings: string[] = [
             'weth',
-            'wrapped eth',
             '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
         ];
         const isWeth: boolean = wethStrings.some((str: string) =>
