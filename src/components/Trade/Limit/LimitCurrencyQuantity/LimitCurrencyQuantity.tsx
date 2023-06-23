41050b26883ce2c413365eb2005c806e5a73ceac
Enoch Wong <enwong10@gmail.com>
2023-06-23 19:20:55 -0400

    Fix lint

--- src/components/Trade/Limit/LimitCurrencyQuantity/LimitCurrencyQuantity.tsx
+++ src/components/Trade/Limit/LimitCurrencyQuantity/LimitCurrencyQuantity.tsx
@@ -2,7 +2,6 @@
 import styles from './LimitCurrencyQuantity.module.css';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { TokenIF } from '../../../../utils/interfaces/exports';
-import { decimalNumRegEx } from '../../../../utils/regex/exports';
 
 interface propsIF {
     disable?: boolean;
