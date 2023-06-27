571888a7a49dd853e670981b11f64fd3a2087482
mhsmith321 <emily.drake.smith@gmail.com>
2023-06-27 14:49:40 -0400

    remove unused webpack import statements from `useFreshParams.ts` file

--- src/utils/hooks/useLinkGen.ts
+++ src/utils/hooks/useLinkGen.ts
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface swapParamsIF {
     chain: string;
