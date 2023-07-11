7c98351af750db299e8e5a406165c496b542ace6
Jadessu <junioradessu@gmail.com>
2023-07-11 18:59:14 -0500

    add refresh button

--- src/pages/Analytics/Analytics.tsx
+++ src/pages/Analytics/Analytics.tsx
@@ -1,3 +1,4 @@
+import { FiRefreshCw } from 'react-icons/fi';
 import TopPools from '../../components/Global/Analytics/TopPools';
 
 export default function Analytics() {
@@ -6,8 +7,12 @@ export default function Analytics() {
             style={{ background: 'var(--dark2)', height: 'calc(100vh - 56px)' }}
             className=' p-4 space-y-4 flex flex-col'
         >
-            <p className='text-header1 leading-header1 text-text1'>
+            <p className='text-header1 leading-header1 text-text1 flex items-center justify-between flex-row px-1'>
                 Top Pools on Ambient
+                <button className='bg-dark3 rounded-md p-1'>
+                    {' '}
+                    <FiRefreshCw size={18} />
+                </button>
             </p>
 
             <TopPools />
