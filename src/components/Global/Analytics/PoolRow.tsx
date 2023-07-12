d187990853165f781ca6543dc16984e682456b35
Jadessu <junioradessu@gmail.com>
2023-07-12 13:14:14 -0500

    refactor table cell to use stylde components

--- src/components/Global/Analytics/PoolRow.tsx
+++ src/components/Global/Analytics/PoolRow.tsx
@@ -1,21 +1,67 @@
 import TokenIcon from '../TokenIcon/TokenIcon';
 import uriToHttp from '../../../utils/functions/uriToHttp';
 import { PoolDataIF } from '../../../contexts/AnalyticsContext';
+import styled, { css } from 'styled-components';
 
 interface propsIF {
     pool: PoolDataIF;
     goToMarket: (tknA: string, tknB: string) => void;
 }
+interface TableCellProps {
+    hidden?: boolean;
+    sm?: boolean;
+    lg?: boolean;
+    xl?: boolean;
+}
+// Media queries
+const media = {
+    sm: '(min-width: 640px)',
+    lg: '(min-width: 1024px)',
+    xl: '(min-width: 1280px)',
+};
+// Styles for media queries
+const mediaStyles = {
+    sm: css`
+        @media ${media.sm} {
+            display: table-cell;
+        }
+    `,
+    lg: css`
+        @media ${media.lg} {
+            display: table-cell;
+        }
+    `,
+    xl: css`
+        @media ${media.xl} {
+            display: table-cell;
+        }
+    `,
+};
+const TableCell = styled.td<TableCellProps>`
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    white-space: nowrap;
+
+    ${({ hidden }) =>
+        hidden &&
+        css`
+            display: none;
+        `}
+
+    ${({ sm }) => sm && mediaStyles.sm}
+${({ lg }) => lg && mediaStyles.lg}
+${({ xl }) => xl && mediaStyles.xl}
+`;
 
 export default function PoolRow(props: propsIF) {
     const { pool, goToMarket } = props;
 
     return (
         <tr
-            className='hover:bg-dark2 cursor-pointer analaytics-pools-table '
+            className='hover:bg-dark2 cursor-pointer  '
             onClick={() => goToMarket(pool.base.address, pool.quote.address)}
         >
-            <td className='px-6  whitespace-nowrap'>
+            <TableCell>
                 <div className='flex items-center'>
                     <div className='flex-shrink-0 flex items-center'>
                         <TokenIcon
@@ -33,31 +79,19 @@ export default function PoolRow(props: propsIF) {
                         <div className='text-text1'>{pool.name}</div>
                     </div>
                 </div>
-            </td>
-            <td
-                className='hidden sm:table-cell px-6  whitespace-nowrap'
-                style={{ height: '40px' }}
-            >
+            </TableCell>
+            <TableCell hidden sm>
                 <div className=' text-text1'>{pool.name}</div>
-            </td>
-            <td
-                className='hidden sm:table-cell px-6  whitespace-nowrap'
-                style={{ height: '40px' }}
-            >
+            </TableCell>
+            <TableCell hidden sm>
                 <div className=' text-accent5'>{pool.displayPrice}</div>
-            </td>
-            <td
-                className='hidden sm:table-cell px-6  whitespace-nowrap'
-                style={{ height: '40px' }}
-            >
+            </TableCell>
+            <TableCell hidden sm>
                 <div className=' text-text1'>
                     {!pool.tvl || pool.tvl.includes('NaN') ? '' : pool.tvl}
                 </div>
-            </td>
-            <td
-                className='hidden xl:table-cell px-6  whitespace-nowrap'
-                style={{ height: '40px' }}
-            >
+            </TableCell>
+            <TableCell hidden xl>
                 <div
                     className={`text-text1 ${
                         Number(pool.apy) > 0 ? 'text-positive' : 'text-negative'
@@ -65,14 +99,11 @@ export default function PoolRow(props: propsIF) {
                 >
                     {pool.apy}
                 </div>
-            </td>
-            <td className=' px-6  whitespace-nowrap' style={{ height: '40px' }}>
+            </TableCell>
+            <TableCell>
                 <div className=' text-text1'>{pool.volume}</div>
-            </td>
-            <td
-                className='hidden lg:table-cell px-6  whitespace-nowrap'
-                style={{ height: '40px' }}
-            >
+            </TableCell>
+            <TableCell hidden lg>
                 <div
                     className={`text-text1 ${
                         pool.priceChange.startsWith('-')
@@ -82,8 +113,8 @@ export default function PoolRow(props: propsIF) {
                 >
                     {pool.priceChange}
                 </div>
-            </td>
-            <td className='px-6 whitespace-nowrap' style={{ height: '40px' }}>
+            </TableCell>
+            <TableCell>
                 <div className='flex items-center justify-end h-full'>
                     <button
                         className='flex items-center justify-center text-bg-dark1 border-dark3 border rounded-full hover:text-accent1 hover:border-accent1'
@@ -95,7 +126,7 @@ export default function PoolRow(props: propsIF) {
                         <span className='px-2 py-1'>Trade</span>
                     </button>
                 </div>
-            </td>
+            </TableCell>
         </tr>
     );
 }
