497112065a92de0cceae73d918093087b92a34ca
Jadessu <junioradessu@gmail.com>
2023-07-13 16:11:55 -0500

    clean up testpage

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,12 +1,7 @@
-import 'intro.js/introjs.css';
-
 export default function TestPage() {
     return (
-        <section
-            style={{ background: 'var(--dark2)', height: 'calc(100vh - 56px)' }}
-            className=' p-4 space-y-4 flex flex-col'
-        >
-            <p className='text-header1 leading-header1 text-text1'>Test page</p>
+        <section>
+            <p>Test page</p>
         </section>
     );
 }
