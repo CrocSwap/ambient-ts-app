f288655d52a03d53f03d619cd915d8e651a751a8
Jadessu <junioradessu@gmail.com>
2023-07-17 13:58:40 -0500

    refactor pool display container

--- src/components/Global/Analytics/PoolDisplay.tsx
+++ src/components/Global/Analytics/PoolDisplay.tsx
@@ -1,13 +1,30 @@
-import styles from './PoolDisplay.module.css';
+import styled from 'styled-components';
 
 interface TokenNameData {
     token0: string;
     token1: string;
 }
+
+const PoolDisplayContainer = styled.section`
+    display: none;
+    color: rgba(235, 235, 255, 0.4);
+
+    @media only screen and (min-width: 768px) {
+        font-size: 10px;
+        line-height: 13px;
+    }
+
+    @media only screen and (min-width: 1200px) {
+        display: flex;
+        font-size: 12px;
+        line-height: 16px;
+    }
+`;
+
 export default function PoolDisplay(props: TokenNameData) {
     return (
-        <section className={styles.account_pool_display}>
+        <PoolDisplayContainer>
             {props.token0}/{props.token1}
-        </section>
+        </PoolDisplayContainer>
     );
 }
