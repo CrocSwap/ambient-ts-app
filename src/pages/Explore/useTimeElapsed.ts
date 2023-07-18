037b5d017fe028c78d1c392d385679234d69e7d4
mhsmith321 <emily.drake.smith@gmail.com>
2023-07-18 15:12:05 -0400

    simplify logic in `useTimeElapsed.ts` file

--- src/pages/Explore/useTimeElapsed.ts
+++ src/pages/Explore/useTimeElapsed.ts
@@ -16,25 +16,28 @@ export const useTimeElapsed = (
 
     // fn to update DOM string
     function updateTime(): void {
+        // time constants (ms per unit time)
+        const ONE_SECOND = 1000;
+        const ONE_MINUTE = 60000;
         // fn to turn ms into a human-readable value (minutes or seconds)
         const calcTime = (
             timeInMs: number,
             denom: 'minutes' | 'seconds',
-        ): string =>
-            Math.floor(
-                timeInMs / (denom === 'minutes' ? 60000 : 1000),
-            ).toString();
-        // constant defining 1s in ms
-        const ONE_SECOND = 1000;
+        ): string => {
+            // get divisor for specified time interval
+            const divisor = denom === 'minutes' ? ONE_MINUTE : ONE_SECOND;
+            // return number of wholse minutes or seconds as relevant
+            return Math.floor(timeInMs / divisor).toString();
+        };
         // run logic if the hook has a `retrieved` at value (or return default)
         if (retrievedAt) {
             // time elapsed since fetch in ms
             const elapsedTime: number = Date.now() - retrievedAt;
             // logic router to get human-readable string for DOM
             let elapsedText: string;
-            if (elapsedTime > ONE_SECOND * 1800) {
+            if (elapsedTime > ONE_MINUTE * 30) {
                 elapsedText = '> 30 min';
-            } else if (elapsedTime >= ONE_SECOND * 60) {
+            } else if (elapsedTime >= ONE_MINUTE) {
                 elapsedText = calcTime(elapsedTime, 'minutes') + ' min ago';
             } else if (elapsedTime >= ONE_SECOND) {
                 elapsedText = calcTime(elapsedTime, 'seconds') + ' sec ago';
