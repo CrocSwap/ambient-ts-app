782b7cd296a87642ff22053b7ca415d1508f5d4c
Jadessu <junioradessu@gmail.com>
2023-07-25 17:54:44 -0500

    create example of common styles

---
+++ src/css/common.styles.ts
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components';
+
+// Define the prop types for the GridContainer
+
+interface GridContainerProps {
+    numCols?: number;
+    numRows?: number;
+    gapSize?: number;
+    fullHeight?: boolean;
+}
+
+// Define the prop types for the FlexContainer
+interface FlexContainerProps {
+    gap?: number;
+    fullHeight?: boolean;
+    flexDirection?: 'row' | 'column';
+    justifyContent?: string;
+}
+
+//   ------------------------------ DISPLAY ---------------------------------------
+
+const GridContainer = styled.div<GridContainerProps>`
+    display: grid;
+    grid-template-columns: ${({ numCols }) =>
+        numCols ? `repeat(${numCols}, 1fr)` : 'auto'};
+    grid-template-rows: ${({ numRows }) =>
+        numRows ? `repeat(${numRows}, 1fr)` : 'auto'};
+    gap: ${({ gapSize }) => (gapSize ? `${gapSize}px` : '0')};
+    ${({ fullHeight }) => (fullHeight ? 'height: 100%;' : '')}
+`;
+const FlexContainer = styled.div<FlexContainerProps>`
+    display: flex;
+    flex-wrap: wrap;
+    ${({ gap }) => gap && `gap: ${gap}px`};
+    ${({ fullHeight }) => fullHeight && 'height: 100%;'};
+    flex-direction: ${({ flexDirection }) =>
+        flexDirection ? flexDirection : 'row'};
+    ${({ justifyContent }) =>
+        justifyContent && `justify-content: ${justifyContent}`};
+`;
+// ------------------------FONTS------------------------------
+// Example usage:
+// const CustomDiv = styled.div<{ fontSize: string }>`
+//     /* Other styles for your div go here */
+//   ${FontSize}
+// `;
+const FontSize = css<{ fontSize?: 'header1' | 'header2' | 'header' | 'body' }>`
+    ${({ fontSize }) =>
+        fontSize &&
+        `
+    font-size: var(--${fontSize}-size);
+    line-height: var(--${fontSize}-lh);
+  `}
+`;
+
+export { FlexContainer, GridContainer, FontSize };
--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,7 +1,28 @@
+import { styled } from 'styled-components';
+import { FlexContainer, FontSize } from '../../css/common.styles';
+
 export default function TestPage() {
+    const CustomDiv = styled.div<{ fontSize?: string }>`
+        /* Other styles for your div go here */
+        ${FontSize}
+    `;
     return (
         <section>
-            <p>Test page</p>
+            <FlexContainer
+                gap={20}
+                fullHeight
+                flexDirection='row'
+                justifyContent='space-between'
+            >
+                {/* Your content goes here */}
+                <CustomDiv fontSize='body'>Hello</CustomDiv>
+                <div>Item 1</div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+                <div>Item 4</div>
+                <div>Item 5</div>
+                <div>Item 6</div>
+            </FlexContainer>
         </section>
     );
 }
