1c51bc23d9c489d5290e523c75713088bafc123a
mishaHype <98827944+mishaHype@users.noreply.github.com>
2023-08-03 12:23:42 -0400

    State change on mouseClick

--- src/components/Global/RangeSlider/RangeSlider.tsx
+++ src/components/Global/RangeSlider/RangeSlider.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, MouseEventHandler } from 'react';
+import React, { ChangeEvent, MouseEventHandler, useState } from 'react';
 import styles from './RangeSlider.module.css'; // Import base CSS
 
 interface CustomInputProps {
@@ -25,23 +25,35 @@ const RangeSlider: React.FC<CustomInputProps> = ({
     step = 1,
     defaultValue = 0,
     onChange,
-    onClick,
     className = '',
-}) => (
-    <input
-        size={size}
-        aria-labelledby={ariaLabelledBy}
-        aria-label={ariaLabel}
-        id={id}
-        min={min}
-        max={max}
-        step={step}
-        defaultValue={defaultValue}
-        type='range'
-        className={`${styles.range_slider} ${className}`}
-        onChange={onChange}
-        onClick={onClick}
-    />
-);
+}) => {
+    const [isGrabbing, setIsGrabbing] = useState(false);
+
+    const handleClick: MouseEventHandler<HTMLInputElement> = (e) => {
+        setIsGrabbing(true);
+    };
+
+    const handleMouseUp: MouseEventHandler<HTMLInputElement> = (e) => {
+        setIsGrabbing(false);
+    };
+
+    return (
+        <input
+            size={size}
+            aria-labelledby={ariaLabelledBy}
+            aria-label={ariaLabel}
+            id={id}
+            min={min}
+            max={max}
+            step={step}
+            defaultValue={defaultValue}
+            type='range'
+            className={`${styles.range_slider} ${className} ${isGrabbing ? styles.range_slider_grabbing : styles.range_slider_grab}`}
+            onChange={onChange}
+            onMouseDown={handleClick}
+            onMouseUp={handleMouseUp}
+        />
+    );
+};
 
 export default RangeSlider;
