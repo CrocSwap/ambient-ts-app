6b86bfaf953b4659a29402e0fdae5335aee406a1
mhsmith321 <emily.drake.smith@gmail.com>
2023-08-08 16:12:44 -0400

    add goerli WETH to `handleWETH.ts` file

--- src/utils/data/handleWETH.ts
+++ src/utils/data/handleWETH.ts
@@ -1,6 +1,10 @@
 export const handleWETH = {
     check(userInput: string): boolean {
         const wethStrings: string[] = [
+            // goerli WETH
+            '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+            'b4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+            // mainnet WETH
             'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
             '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
         ];
