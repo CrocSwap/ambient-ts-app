8bf7ae6620e0a71f8351f57c902a64c02ae0a8fe
Enoch Wong <enwong10@gmail.com>
2023-08-11 14:57:57 -0400

    Fix lint

--- src/utils/hooks/useUrlParams.ts
+++ src/utils/hooks/useUrlParams.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch } from './reduxToolkit';
 import {
