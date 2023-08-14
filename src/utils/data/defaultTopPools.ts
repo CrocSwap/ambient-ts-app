78a378c58f80f1156ec3f26bf87376d59c11f7d6
Ben Wolski <benwolski@gmail.com>
2023-08-14 11:23:27 -0400

    replace ETH / USDC with USDT / USDC in Top Pools

--- src/utils/data/defaultTopPools.ts
+++ src/utils/data/defaultTopPools.ts
@@ -10,8 +10,9 @@ import {
     mainnetETH,
     mainnetUSDC,
     mainnetWBTC,
-    mainnetPEPE,
     mainnetDAI,
+    mainnetUSDT,
+    goerliUSDT,
 } from './defaultTokens';
 import { PoolIF, TokenIF } from '../interfaces/exports';
 import sortTokens from '../functions/sortTokens';
@@ -54,13 +55,13 @@ const defaultTopPools = {
         new TopPool(mainnetETH, mainnetUSDC, MAINNET_POOL_ID),
         new TopPool(mainnetETH, mainnetWBTC, MAINNET_POOL_ID),
         new TopPool(mainnetDAI, mainnetUSDC, MAINNET_POOL_ID),
-        new TopPool(mainnetETH, mainnetPEPE, MAINNET_POOL_ID),
+        new TopPool(mainnetUSDT, mainnetUSDC, MAINNET_POOL_ID),
     ],
     '0x5': [
         new TopPool(goerliETH, goerliUSDC, GOERLI_POOL_ID),
         new TopPool(goerliETH, goerliWBTC, GOERLI_POOL_ID),
         new TopPool(goerliUSDC, goerliDAI, GOERLI_POOL_ID),
-        new TopPool(goerliWBTC, goerliUSDC, GOERLI_POOL_ID),
+        new TopPool(goerliUSDT, goerliUSDC, GOERLI_POOL_ID),
     ],
     '0x66eed': [
         new TopPool(arbGoerliETH, arbGoerliUSDC, GOERLI_POOL_ID),
