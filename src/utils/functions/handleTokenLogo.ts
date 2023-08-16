427864b4060269913a020fedef2850bf3b65ea06
mhsmith321 <emily.drake.smith@gmail.com>
2023-08-16 07:27:45 -0400

    pseudocode processes in `handleTokenLogo.ts` file

--- src/utils/functions/handleTokenLogo.ts
+++ src/utils/functions/handleTokenLogo.ts
@@ -2,4 +2,6 @@ import * as localLogos from '../../assets/images/tokens/exports';
 
 export default function handleTokenLogo() {
     console.log(localLogos.ASTR);
+    // FIRST determine if the token logo is available locally in the codebase
+    // SECOND return the URI passed through the `uriToHttp()` fn
 }
