31204e281c5cd90bf48f6469aa58d6d080254b43
Jadessu <junioradessu@gmail.com>
2023-08-16 11:39:05 -0500

    refactor isPoolInitialized to take presedence

--- src/components/NoChartData/NoChartData.tsx
+++ src/components/NoChartData/NoChartData.tsx
@@ -7,6 +7,7 @@ import styles from './NoChartData.module.css';
 import { FiRefreshCcw } from 'react-icons/fi';
 import { useContext } from 'react';
 import { CandleContext } from '../../contexts/CandleContext';
+import { useSimulatedIsPoolInitialized } from '../../App/hooks/useSimulatedIsPoolInitialized';
 
 interface PropsIF {
     chainId: string;
@@ -19,6 +20,7 @@ interface PropsIF {
 export const NoChartData = (props: PropsIF) => {
     const { chainId, tokenA, tokenB, isCandleDataNull } = props;
     const { setIsManualCandleFetchRequested } = useContext(CandleContext);
+    const isPoolInitialized = useSimulatedIsPoolInitialized();
 
     const linkGenInitPool: linkGenMethodsIF = useLinkGen('initpool');
 
@@ -31,46 +33,48 @@ export const NoChartData = (props: PropsIF) => {
             <FiRefreshCcw size={22} color='var(--text1)' />
         </button>
     );
+    let title = '';
+    let subTitle = '';
+    let content = null;
+
+    if (!isPoolInitialized) {
+        title = 'This pool has not been initialized.';
+        subTitle = 'Do you want to initialize it?';
+        content = (
+            <Link
+                to={linkGenInitPool.getFullURL({
+                    chain: chainId,
+                    tokenA: tokenA.address,
+                    tokenB: tokenB.address,
+                })}
+                className={styles.initialize_link}
+            >
+                Initialize Pool
+                <TokenIcon
+                    src={uriToHttp(tokenA.logoURI)}
+                    alt={tokenA.symbol}
+                    size='m'
+                />
+                <TokenIcon
+                    src={uriToHttp(tokenB.logoURI)}
+                    alt={tokenB.symbol}
+                    size='m'
+                />
+            </Link>
+        );
+    } else if (isCandleDataNull) {
+        title = 'Candle data not available.';
+        content = refreshCandleButton;
+    }
 
     return (
         <div className={styles.pool_not_initialialized_container}>
             <div className={styles.pool_init_bg}>
                 <div className={styles.pool_not_initialialized_content}>
                     <div className={styles.pool_not_init_inner}>
-                        <h2>
-                            {isCandleDataNull
-                                ? 'Candle data not available.'
-                                : 'This pool has not been initialized.'}
-                        </h2>
-                        <h3>
-                            {isCandleDataNull
-                                ? ''
-                                : 'Do you want to initialize it?'}
-                        </h3>
-                        {isCandleDataNull ? (
-                            refreshCandleButton
-                        ) : (
-                            <Link
-                                to={linkGenInitPool.getFullURL({
-                                    chain: chainId,
-                                    tokenA: tokenA.address,
-                                    tokenB: tokenB.address,
-                                })}
-                                className={styles.initialize_link}
-                            >
-                                Initialize Pool
-                                <TokenIcon
-                                    src={uriToHttp(tokenA.logoURI)}
-                                    alt={tokenA.symbol}
-                                    size='m'
-                                />
-                                <TokenIcon
-                                    src={uriToHttp(tokenB.logoURI)}
-                                    alt={tokenB.symbol}
-                                    size='m'
-                                />
-                            </Link>
-                        )}
+                        <h2>{title}</h2>
+                        <h3>{subTitle}</h3>
+                        {content}
                     </div>
                 </div>
             </div>
