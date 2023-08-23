d37b0e650eedbbd46d1639ce126e2ca10aa51563
mhsmith321 <emily.drake.smith@gmail.com>
2023-08-23 18:03:24 -0400

    update WBTC logo from svg to png

--- src/assets/images/tokens/exports.ts
+++ src/assets/images/tokens/exports.ts
@@ -3,6 +3,6 @@ import ETH from './ETH.png';
 import PEPE from './PEPE.png';
 import USDC from './USDC.svg';
 import USDT from './USDT.svg';
-import WBTC from './WBTC.svg';
+import WBTC from './WBTC.png';
 
 export { DAI, ETH, PEPE, USDC, USDT, WBTC };
