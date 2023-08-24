86a29db2b0d3e3d155b4c7196b16c93101daf5ed
Ben Wolski <benwolski@gmail.com>
2023-08-24 13:42:28 -0400

    add hardcoded FRAX logo to add contrast with dark background

--- src/assets/images/tokens/exports.ts
+++ src/assets/images/tokens/exports.ts
@@ -4,5 +4,6 @@ import PEPE from './PEPE.png';
 import USDC from './USDC.svg';
 import USDT from './USDT.svg';
 import WBTC from './WBTC.png';
+import FRAX from './FRAX.png';
 
-export { DAI, ETH, PEPE, USDC, USDT, WBTC };
+export { DAI, ETH, PEPE, USDC, USDT, WBTC, FRAX };
