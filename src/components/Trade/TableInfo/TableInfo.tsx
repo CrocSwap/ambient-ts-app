11768634128c3c2adbeb4d3a7ca46fdd6c764f6e
Jadessu <junioradessu@gmail.com>
2023-09-01 15:47:29 -0500

    extract completed info ui

--- src/components/Trade/TableInfo/TableInfo.tsx
+++ src/components/Trade/TableInfo/TableInfo.tsx
@@ -1,46 +1,22 @@
 import { useContext } from 'react';
 import { GridContainer, ScrollContainer } from '../../../styled/Common';
-import { FiExternalLink, FiCopy } from 'react-icons/fi';
-import trimString from '../../../utils/functions/trimString';
+
 import { useAppSelector } from '../../../utils/hooks/reduxToolkit';
 import { TokenIF } from '../../../utils/interfaces/TokenIF';
-import {
-    MainSection,
-    BoxContainer,
-    FeaturedBoxInfoContainer,
-    InfoHeader,
-    FeaturedBoxInnerContainer,
-    FlexCenter,
-    TokenName,
-    TokenSymbol,
-    BoxInfoText,
-    TabPlaceholder,
-    DetailedBoxContainer,
-} from './TableInfo.styles';
-import TableInfoTabs from './TableInfoTabs';
+import { MainSection } from './TableInfo.styles';
 import { CrocEnvContext } from '../../../contexts/CrocEnvContext';
-import { getChainExplorer } from '../../../utils/data/chains';
 import useFetchPoolStats from '../../../App/hooks/useFetchPoolStats';
 import { PoolIF } from '../../../utils/interfaces/PoolIF';
 import { getFormattedNumber } from '../../../App/functions/getFormattedNumber';
-import { ZERO_ADDRESS } from '../../../constants';
-import TokenIcon from '../../Global/TokenIcon/TokenIcon';
-import IconWithTooltip from '../../Global/IconWithTooltip/IconWithTooltip';
-import useCopyToClipboard from '../../../utils/hooks/useCopyToClipboard';
-import { AppStateContext } from '../../../contexts/AppStateContext';
+
 import { DetailedBox } from './DetailedBox';
-interface FeaturedBoxPropsIF {
-    token: TokenIF;
-    balance: string;
-    value: string;
-}
+import { FeaturedBox } from './FeaturedBox';
 
 export default function TableInfo() {
     const { tradeData } = useAppSelector((state) => state);
     const {
-        chainData: { chainId, addrs },
+        chainData: { chainId },
     } = useContext(CrocEnvContext);
-    const blockExplorer = getChainExplorer(chainId);
 
     const denomInBase = tradeData.isDenomBase;
 
@@ -71,16 +47,7 @@ export default function TableInfo() {
         tradeData.baseToken,
         tradeData.quoteToken,
     ];
-    const detailedData = [
-        { label: 'Market Cap:', value: '$69m' },
-        { label: 'FDV:', value: '$690m' },
-        { label: '24h Volume:', value: '$6.93k' },
-        { label: 'TVL:', value: poolTvl },
-        { label: 'Total Fees:', value: '$6.93k' },
-        { label: 'Tick Liquidity:', value: '$500k' },
-        { label: 'Out of Range Liq:', value: '20%' },
-        { label: 'Pool Created:', value: '15/07/2022' },
-    ];
+
     const featuredData = [
         {
             token: topToken,
@@ -94,106 +61,6 @@ export default function TableInfo() {
         },
     ];
 
-    const [_, copy] = useCopyToClipboard();
-
-    const {
-        snackbar: { open: openSnackbar },
-    } = useContext(AppStateContext);
-
-    function FeaturedBox(props: FeaturedBoxPropsIF) {
-        const { token, balance, value } = props;
-
-        function handleCopyAddress() {
-            copy(token.address);
-            openSnackbar(`${token.address} copied`, 'info');
-        }
-        return (
-            <BoxContainer>
-                <FeaturedBoxInnerContainer>
-                    <FlexCenter>
-                        <TokenIcon token={token} size={'3xl'} />
-                        <TokenSymbol>{token.symbol}</TokenSymbol>
-                        <TokenName>{token.name}</TokenName>
-                    </FlexCenter>
-                    <FlexCenter style={{ gap: '8px' }}>
-                        <InfoHeader>
-                            {trimString(token.address, 5, 6, '…')}
-                        </InfoHeader>
-                        <IconWithTooltip
-                            title='Copy transaction hash to clipboard'
-                            placement='bottom'
-                        >
-                            <div onClick={handleCopyAddress}>
-                                <FiCopy size={16} color='var(--text3)' />
-                            </div>
-                        </IconWithTooltip>
-                        <IconWithTooltip
-                            title='View on Block Explorer'
-                            placement='bottom'
-                        >
-                            <a
-                                href={
-                                    token.address === ZERO_ADDRESS
-                                        ? `${blockExplorer}address/${addrs.dex}`
-                                        : `${blockExplorer}token/${token.address}`
-                                }
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FiExternalLink />
-                            </a>
-                        </IconWithTooltip>
-                    </FlexCenter>
-                    <FeaturedBoxInfoContainer>
-                        <InfoHeader>Balance</InfoHeader>
-                        <BoxInfoText>{balance}</BoxInfoText>
-                    </FeaturedBoxInfoContainer>
-                    <FeaturedBoxInfoContainer>
-                        <InfoHeader>Value</InfoHeader>
-                        <BoxInfoText>${value}</BoxInfoText>
-                    </FeaturedBoxInfoContainer>
-                </FeaturedBoxInnerContainer>
-            </BoxContainer>
-        );
-    }
-
-    const temp = true;
-    if (temp)
-        return (
-            <MainSection>
-                <ScrollContainer>
-                    <GridContainer numCols={2} gapSize={8} height={200}>
-                        <GridContainer numCols={2} gapSize={8}>
-                            {featuredData.map((data, idx) => (
-                                <FeaturedBox
-                                    key={idx}
-                                    token={data.token}
-                                    balance={data.balance}
-                                    value={data.value}
-                                />
-                            ))}
-                        </GridContainer>
-
-                        <GridContainer gapSize={28} customRows='46px 46px auto'>
-                            <GridContainer numCols={4} gapSize={8}>
-                                {/* first 4 row items go here */}
-                                <DetailedBox
-                                    label='TVL'
-                                    value={`$${poolTvl?.toString() || '...'}`}
-                                />
-                            </GridContainer>
-                            {/* second 4 row items go here */}
-                            <GridContainer
-                                numCols={4}
-                                gapSize={8}
-                            ></GridContainer>
-                        </GridContainer>
-                    </GridContainer>
-                </ScrollContainer>
-            </MainSection>
-        );
-
-    // Leave this as it is. This is the completed UI
     return (
         <MainSection>
             <ScrollContainer>
@@ -211,29 +78,14 @@ export default function TableInfo() {
 
                     <GridContainer gapSize={28} customRows='46px 46px auto'>
                         <GridContainer numCols={4} gapSize={8}>
-                            {detailedData.slice(0, 4).map((data, idx) => (
-                                <DetailedBox
-                                    label={data.label}
-                                    value={data.label}
-                                    key={idx}
-                                />
-                            ))}
+                            {/* first 4 row items go here */}
+                            <DetailedBox
+                                label='TVL'
+                                value={`$${poolTvl?.toString() || '...'}`}
+                            />
                         </GridContainer>
-                        <GridContainer numCols={4} gapSize={8}>
-                            {detailedData
-                                .slice(4, detailedData.length)
-                                .map((data, idx) => (
-                                    <DetailedBox
-                                        label={data.label}
-                                        value={data.label}
-                                        key={idx}
-                                    />
-                                ))}
-                        </GridContainer>
-
-                        <TabPlaceholder>
-                            <TableInfoTabs />
-                        </TabPlaceholder>
+                        {/* second 4 row items go here */}
+                        <GridContainer numCols={4} gapSize={8}></GridContainer>
                     </GridContainer>
                 </GridContainer>
             </ScrollContainer>
