8bfdbbfcb02147977641887c72ada4d1afbfec6a
Ben Wolski <ben@crocodilelabs.io>
2023-10-18 00:34:22 -0400

    re-add navigation to homepage when wallet not connected

--- src/App/hooks/useAppChain.ts
+++ src/App/hooks/useAppChain.ts
@@ -172,6 +172,11 @@ export const useAppChain = (): {
     function chooseNetwork(network: NetworkIF): void {
         localStorage.setItem(CHAIN_LS_KEY, network.chainId);
         setActiveNetwork(network);
+        const { pathname } = window.location;
+        if (pathname.includes('token')) {
+            // navigate to index page only if token pair in URL
+            linkGenIndex.navigate();
+        }
         window.location.reload();
     }
 
