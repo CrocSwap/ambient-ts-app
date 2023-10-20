4ff7b5d61186f14ef18bbfbde783993d7bd4c027
mhsmith321 <emily.drake.smith@gmail.com>
2023-10-20 12:01:37 -0400

    annotate new logic in `Home.tsx` file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,20 +12,30 @@ import { useEffect } from 'react';
 
 export default function Home() {
     const showMobileVersion = useMediaQuery('(max-width: 600px)');
+    // hook from wagmi to switch connected wallet in extension
     const { switchNetwork } = useSwitchNetwork();
+    // hook from wagmi indicating if user is connected
     const { isConnected } = useAccount();
-
+    // hook managing chain data between the app and external APIs
     const { chooseNetwork, chainData } = useAppChain();
+    // hook to consume and alter search params on the index page
     const [searchParams, setSearchParams] = useSearchParams();
-
+    // logic to consume chain param data from the URL
+    // runs once when the app initializes, again when wagmi finishes initializing
     useEffect(() => {
+        // search for param in URL by key 'chain' or 'network'
         const chainParam: string | null =
             searchParams.get('chain') ?? searchParams.get('network');
+        // logic to execute if a param is found (if not, do nothing)
         if (chainParam) {
+            // check if chain is supported and not the current chain in the app
+            // yes → trigger machinery to switch the current network
+            // no → no action except to clear the param from the URL
             if (
                 supportedNetworks[chainParam] &&
                 chainParam !== chainData.chainId
             ) {
+                // use wagmi if wallet is connected, otherwise use in-app toggle
                 if (switchNetwork) {
                     switchNetwork(parseInt(chainParam));
                 } else if (!isConnected) {
