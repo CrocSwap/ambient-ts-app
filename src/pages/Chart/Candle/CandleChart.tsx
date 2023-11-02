53e6ea924377e38b82490dd79b06f6385130b1dc
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2023-11-02 18:46:35 +0300

    bug fixed chart shift to left after 1 minute

--- src/pages/Chart/Candle/CandleChart.tsx
+++ src/pages/Chart/Candle/CandleChart.tsx
@@ -7,7 +7,10 @@ import {
     setCanvasResolution,
 } from '../ChartUtils/chartUtils';
 import { IS_LOCAL_ENV } from '../../../constants';
-import { diffHashSigScaleData } from '../../../utils/functions/diffHashSig';
+import {
+    diffHashSig,
+    diffHashSigScaleData,
+} from '../../../utils/functions/diffHashSig';
 import * as d3 from 'd3';
 import * as d3fc from 'd3fc';
 import { CandleData } from '../../../App/functions/fetchCandleSeries';
@@ -40,6 +43,8 @@ export default function CandleChart(props: candlePropsIF) {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [candlestick, setCandlestick] = useState<any>();
+
+    const [isFirstRender, setIsFirstRender] = useState(true);
     const selectedCandleColor = '#E480FF';
     const crocCandleLightColor = '#CDC1FF';
     const crocCandleBorderLightColor = '#CDC1FF';
@@ -63,19 +68,22 @@ export default function CandleChart(props: candlePropsIF) {
 
     useEffect(() => {
         IS_LOCAL_ENV && console.debug('re-rending chart');
-        if (tradeTableState === 'Expanded') return;
+        if (tradeTableState === 'Expanded' || isFirstRender) return;
         if (data && data.length > 0 && scaleData) {
             if (!showLatest) {
                 const domainLeft = scaleData?.xScale.domain()[0];
                 const domainRight = scaleData?.xScale.domain()[1];
-
                 scaleData?.xScale.domain([
                     domainLeft + period * 1000,
                     domainRight + period * 1000,
                 ]);
             }
         }
-    }, [tradeTableState, lastCandleData]);
+    }, [tradeTableState, lastCandleData?.time]);
+
+    useEffect(() => {
+        setIsFirstRender(false);
+    }, []);
 
     useEffect(() => {
         renderCanvasArray([d3CanvasCandle]);
