b40189764841c353ed1cbb10b40a8c2275087d09
mhsmith321 <emily.drake.smith@gmail.com>
2023-11-03 12:54:52 -0400

    add dev note to `useTopPools.ts` file

--- src/App/hooks/useTopPools.ts
+++ src/App/hooks/useTopPools.ts
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react';
 import { getDefaultTopPools } from '../../utils/data/defaultTopPools';
 import { PoolIF } from '../../utils/interfaces/exports';
 
+// TODO:    I'm pretty sure there's no reason for this to be a hook. all the data
+// TODO:    ... consumed is hardcoded in the front, we can probably eliminate this
+// TODO:    ... layer from the codebase     -Emily
+
 /* Hook to manage the arbitrairly defined top pools for the specific chain.
  * Consumed in sidebar and home page to give the user a convenient way to
  * see which pools they might want to check out. */
