9dc16515554b271a733a7beb71463e354ffc9149
surudhb <bsurudh@gmail.com>
2023-11-08 15:09:29 -0700

    remove submodule

--- src/contexts/ENSAddressContext.tsx
+++ src/contexts/ENSAddressContext.tsx
@@ -1,15 +1,16 @@
 import { createContext, useContext, useState, useRef, ReactNode } from 'react';
-import { getAddress } from 'ethers/lib/utils.js';
-import { TransactionIF } from '../utils/interfaces/TransactionIF';
-import { LimitOrderIF } from '../utils/interfaces/LimitOrderIF';
-import { PositionIF } from '../utils/interfaces/PositionIF';
-import { fetchEnsAddresses } from '../App/functions/fetchENSAddresses';
+import {
+    TransactionIF,
+    LimitOrderIF,
+    PositionIF,
+} from '../../ambient-utils/src/types';
+import { batchFetchAndCacheENSAddressData } from '../../ambient-utils/src/dataLayer';
 
 type TradeTableDataRow = TransactionIF | LimitOrderIF | PositionIF;
 
 type ENSAddressContextValue = {
     ensAddressMapping: Map<string, string>;
-    addData: (data: TradeTableDataRow[]) => void;
+    addData: (data: TradeTableDataRow[]) => Promise<void>;
 };
 
 const ENSAddressContext = createContext<ENSAddressContextValue | undefined>(
@@ -22,63 +23,22 @@ export const ENSAddressContextProvider = (props: { children: ReactNode }) => {
     >(new Map());
     const cacheRef = useRef<Map<string, string>>(new Map());
     const nullCacheRef = useRef<Map<string, number>>(new Map());
-    const DEADLINE = 1 * 60 * 60 * 1000; // 1 hour
 
-    const addData = (data: TradeTableDataRow[]) => {
-        const now = Date.now();
-
-        const uncachedAddresses = [
-            ...new Set(
-                data.map((row) =>
-                    (row.user ? getAddress(row.user) : '').toLowerCase(),
-                ),
-            ),
-        ].filter((addr) => {
-            const nullTimestamp = nullCacheRef.current.get(addr);
-            // Address is valid if it's not in the cache OR in the nullCache but was added more than DEADLINE ago
-            return (
-                addr &&
-                !cacheRef.current.has(addr) &&
-                (!nullTimestamp || now - nullTimestamp >= DEADLINE)
+    const addData = async (data: TradeTableDataRow[]) => {
+        const copiedCache = new Map([...cacheRef.current]);
+        const copiedNullCache = new Map([...nullCacheRef.current]);
+        const { updatedAddressesCache, updatedNullAddressesCache } =
+            await batchFetchAndCacheENSAddressData(
+                data,
+                copiedCache,
+                copiedNullCache,
             );
-        });
 
-        // If we have uncached addresses, we fetch them
-        if (uncachedAddresses.length > 0) {
-            (async () => {
-                const batchedEnsMap = await fetchEnsAddresses(
-                    uncachedAddresses,
-                );
-
-                if (batchedEnsMap && batchedEnsMap.size > 0) {
-                    // Separate the addresses that returned null
-                    const nullAddresses: string[] = [];
-
-                    batchedEnsMap.forEach(
-                        (value, key) =>
-                            value === 'null' &&
-                            nullAddresses.push(key.toLowerCase()) &&
-                            batchedEnsMap.delete(key),
-                    );
-
-                    // Update nullCache with the addresses that returned null and current timestamp
-                    nullAddresses.forEach((addr) =>
-                        nullCacheRef.current.set(addr, now),
-                    );
-
-                    // Update the state with the newly fetched addresses
-                    setEnsAddressMapping(
-                        (prev) => new Map([...prev, ...batchedEnsMap]),
-                    );
-
-                    // Update the cache with the newly fetched addresses
-                    cacheRef.current = new Map([
-                        ...cacheRef.current,
-                        ...batchedEnsMap,
-                    ]);
-                }
-            })();
-        }
+        cacheRef.current = new Map([...copiedCache, ...updatedAddressesCache]);
+        setEnsAddressMapping(
+            (prev) => new Map([...prev, ...updatedAddressesCache]),
+        );
+        nullCacheRef.current = updatedNullAddressesCache;
     };
 
     return (
