ce188aa2435a67cb259afd26034fd97c97620469
surudhb <bsurudh@gmail.com>
2023-11-14 17:58:37 -0700

    remove data layer as a lib

--- src/ambient-utils/types/tradeData.d.ts
+++
@@ -1,5 +0,0 @@
-import { TransactionIF } from './transaction';
-import { LimitOrderIF } from './limitOrder';
-import { PositionIF } from './rangePosition';
-
-export type TradeTableDataRow = TransactionIF | LimitOrderIF | PositionIF;
