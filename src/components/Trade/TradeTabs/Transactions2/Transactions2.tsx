194b35607c0dd9809d19871ca1df1bfb37319381
mhsmith321 <emily.drake.smith@gmail.com>
2023-11-21 12:16:48 -0500

    add sample data to front

--- src/components/Trade/TradeTabs/Transactions2/Transactions2.tsx
+++ src/components/Trade/TradeTabs/Transactions2/Transactions2.tsx
@@ -3,17 +3,24 @@ import { useEffect, useRef } from 'react';
 export default function Transactions2() {
     const containerRef = useRef<HTMLOListElement>(null);
 
+    // add an observer to watch for element to be re-sized
+    // later this should go to the parent so every table tab has it available
     useEffect(() => {
+        // fn to log the width of the element in the DOM (number of pixels)
         function logWidth() {
+            // make sure element exists
             if (containerRef.current) {
+                // log the width of the DOM element
                 const containerWidth = containerRef.current.clientWidth;
                 console.log(containerWidth);
             }
         }
 
+        // create an observer holding the width-logging function
         const resizeObserver: ResizeObserver = new ResizeObserver(logWidth);
         containerRef.current && resizeObserver.observe(containerRef.current);
 
+        // cleanup the observer from the DOM when component dismounts
         return () => {
             resizeObserver.disconnect();
         };
