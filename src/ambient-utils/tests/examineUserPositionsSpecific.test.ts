e183eba641d471a421c9e1eda24c870051a2f8be
JustinGirard <ec2-user@ip-172-31-12-164.us-west-2.compute.internal>
2023-12-18 21:21:14 +0000

    polished unit test, doing a deeper record compare

--- src/ambient-utils/tests/examineUserPositionsSpecific.test.ts
+++ src/ambient-utils/tests/examineUserPositionsSpecific.test.ts
@@ -32,7 +32,44 @@ const fetchDataForChain = async (recordType, userAddress, chainId) => {
     });
     return updatedLedger;
 };
+function validateLedgerSets(set1: Array<any>, set2: Array<any>): boolean {
+    if (set1.length !== set2.length) {
+        console.log(
+            `Length mismatch: Set1 length = ${set1.length}, Set2 length = ${set2.length}`,
+        );
+        return false;
+    }
 
+    const tolerance = 0.3; // 30 cents tolerance for totalValueUSD
+
+    const getIdField = (record: any) =>
+        record.positionId ? 'positionId' : 'limitOrderId';
+
+    for (let i = 0; i < set1.length; i++) {
+        const entry1 = set1[i];
+        const idField = getIdField(entry1);
+        const entry2 = set2.find((e) => e[idField] === entry1[idField]);
+
+        if (!entry2) {
+            console.log(
+                `No matching record found for ${idField} ${entry1[idField]} in Set2`,
+            );
+            return false;
+        }
+
+        const valueDifference = Math.abs(
+            entry1.totalValueUSD - entry2.totalValueUSD,
+        );
+        if (valueDifference > tolerance) {
+            console.log(
+                `Value mismatch for ${idField} ${entry1[idField]}: Set1 value = ${entry1.totalValueUSD}, Set2 value = ${entry2.totalValueUSD}`,
+            );
+            return false;
+        }
+    }
+
+    return true;
+}
 describe('Test fetchUserPositions Specific', () => {
     jest.setTimeout(30000);
     describe('userPositions', () => {
@@ -72,7 +109,15 @@ describe('Test fetchUserPositions Specific', () => {
                         userChain.userAddress,
                         userChain.chainId,
                     );
+                    // console.log(userPositions);
                     expect(userPositions.length).toBeGreaterThan(0);
+
+                    // Validate the sets
+                    const isValid = validateLedgerSets(
+                        userRecords,
+                        userPositions,
+                    );
+                    expect(isValid).toBeTruthy();
                 }
             }
         });
