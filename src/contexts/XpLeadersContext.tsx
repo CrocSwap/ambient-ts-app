e2cf4fd10557677aeffa1fe95ccd7382fca5e465
Ben Wolski <benwolski@gmail.com>
2024-01-29 15:41:48 -0500

    add weekly and byChain mock xp leaders data

--- src/contexts/XpLeadersContext.tsx
+++ src/contexts/XpLeadersContext.tsx
@@ -1,12 +1,20 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext } from 'react';
 import { UserXpIF } from '../ambient-utils/types';
 import { fetchXpLeadersData } from '../ambient-utils/api';
+import { CrocEnvContext } from './CrocEnvContext';
 
 interface XpLeadersContextIF {
-    xpLeadersData: XpLeadersDataIF;
+    xpLeaders: XpLeadersDataIF;
 }
 
 export interface XpLeadersDataIF {
+    global: XpLeaderboardDataIF;
+    byWeek: XpLeaderboardDataIF;
+    byChain: XpLeaderboardDataIF;
+    getXpLeaders: (xpLeaderboardType: string) => void;
+}
+
+export interface XpLeaderboardDataIF {
     dataReceived: boolean;
     data: Array<UserXpIF> | undefined;
 }
@@ -18,22 +26,62 @@ export const XpLeadersContext = createContext<XpLeadersContextIF>(
 export const XpLeadersContextProvider = (props: {
     children: React.ReactNode;
 }) => {
-    const [xpLeaders, setXpLeaders] = React.useState<XpLeadersDataIF>({
-        dataReceived: false,
-        data: undefined,
-    });
+    const {
+        chainData: { chainId },
+    } = useContext(CrocEnvContext);
 
-    React.useEffect(() => {
-        fetchXpLeadersData().then((data) => {
-            setXpLeaders({
-                dataReceived: true,
-                data: data,
-            });
+    const [xpLeadersGlobal, setXpLeadersGlobal] =
+        React.useState<XpLeaderboardDataIF>({
+            dataReceived: false,
+            data: undefined,
         });
-    }, []);
+    const [xpLeadersByWeek, setXpLeadersByWeek] =
+        React.useState<XpLeaderboardDataIF>({
+            dataReceived: false,
+            data: undefined,
+        });
+    const [xpLeadersByChain, setXpLeadersByChain] =
+        React.useState<XpLeaderboardDataIF>({
+            dataReceived: false,
+            data: undefined,
+        });
+
+    function getXpLeaders(xpLeaderboardType: string) {
+        switch (xpLeaderboardType) {
+            case 'Weekly':
+                fetchXpLeadersData('byWeek').then((data) => {
+                    setXpLeadersByWeek({
+                        dataReceived: true,
+                        data: data,
+                    });
+                });
+                break;
+            case 'Chain':
+                fetchXpLeadersData('byChain', chainId).then((data) => {
+                    setXpLeadersByChain({
+                        dataReceived: true,
+                        data: data,
+                    });
+                });
+                break;
+            default:
+                fetchXpLeadersData('global').then((data) => {
+                    setXpLeadersGlobal({
+                        dataReceived: true,
+                        data: data,
+                    });
+                });
+                break;
+        }
+    }
 
     const xpLeadersContext: XpLeadersContextIF = {
-        xpLeadersData: xpLeaders,
+        xpLeaders: {
+            global: xpLeadersGlobal,
+            getXpLeaders: getXpLeaders,
+            byWeek: xpLeadersByWeek,
+            byChain: xpLeadersByChain,
+        },
     };
 
     return (
