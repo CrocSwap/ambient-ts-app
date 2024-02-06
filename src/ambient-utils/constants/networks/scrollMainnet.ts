ec5b13e9d1024b455b815501f54752ac8d4a3202
Ben Wolski <benwolski@gmail.com>
2024-02-06 10:24:52 -0500

    add USDT/USDC and USDC/WBTC to Top Pools for Scroll

--- src/ambient-utils/constants/networks/scrollMainnet.ts
+++ src/ambient-utils/constants/networks/scrollMainnet.ts
@@ -1,5 +1,10 @@
 import { lookupChain } from '@crocswap-libs/sdk/dist/context';
-import { scrollETH, scrollUSDC } from '../defaultTokens';
+import {
+    scrollETH,
+    scrollUSDC,
+    scrollUSDT,
+    scrollWBTC,
+} from '../defaultTokens';
 import { NetworkIF } from '../../types/NetworkIF';
 import { TopPool } from './TopPool';
 import { Provider } from '@ethersproject/providers';
@@ -41,6 +46,8 @@ export const scrollMainnet: NetworkIF = {
     defaultPair: [scrollETH, scrollUSDC],
     topPools: [
         new TopPool(scrollETH, scrollUSDC, lookupChain('0x82750').poolIndex),
+        new TopPool(scrollUSDT, scrollUSDC, lookupChain('0x82750').poolIndex),
+        new TopPool(scrollETH, scrollWBTC, lookupChain('0x82750').poolIndex),
     ],
     getGasPriceInGwei: async (provider?: Provider) => {
         if (!provider) return 0;
