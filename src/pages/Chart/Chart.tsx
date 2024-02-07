f6b97d338d5488a86303610b1a39f1cd44957c18
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2024-02-07 13:56:57 +0300

    fixed :  chart is not updated when new fetch candles

--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -784,19 +784,12 @@ export default function Chart(props: propsIF) {
 
             domainMax = domainMax < minDate ? minDate : domainMax;
 
-            // const nowDate = Date.now();
+            const nowDate = Date.now();
+            const snapDiff = nowDate % (period * 1000);
+            const snappedTime = nowDate + (period * 1000 - snapDiff);
 
-            // const snapDiff = nowDate % (period * 1000);
-            // const snappedTime = nowDate + (period * 1000 - snapDiff);
-
-            // const isShowLatestCandle =
-            //     xDomain[0] < snappedTime * 1000 &&
-            //     snappedTime * 1000 < xDomain[1];
-
-            // reverting to previous logic until Proven team can confirm fix
             const isShowLatestCandle =
-                xDomain[0] < lastCandleData?.time * 1000 &&
-                lastCandleData?.time * 1000 < xDomain[1];
+                xDomain[0] < snappedTime && snappedTime < xDomain[1];
 
             setCandleScale((prev: CandleScaleIF) => {
                 return {
