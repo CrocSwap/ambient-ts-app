cc9941fe62860ed644292e97598360ce7f349954
Ben Wolski <ben@crocodilelabs.io>
2024-02-22 22:10:20 -0500

    update network logos in dropdown menu

--- src/styled/Components/Header.ts
+++ src/styled/Components/Header.ts
@@ -529,7 +529,7 @@ export const NetworkItem = styled(motion.li)`
     flex-direction: row;
     align-items: center;
     transition: background 500ms;
-    padding: 8px 0;
+    padding: 4px;
     cursor: pointer;
     text-decoration: none;
 
@@ -543,7 +543,7 @@ export const NetworkItem = styled(motion.li)`
 export const ChainNameStatus = styled.div<{ active: boolean }>`
     display: flex;
     align-items: center;
-    padding: 0.6rem 0;
+    padding: 8px 0;
     font-size: var(--header2-size);
     width: 100%;
     color: var(--text1);
