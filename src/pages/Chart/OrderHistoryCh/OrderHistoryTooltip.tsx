6aaab9103ce694cb73a26c9cc991510003d2e7a1
zcelbir <zcelbir@proven.com.tr>
2024-02-27 17:06:26 +0300

    fix selected tooltip close on oclick bug

--- src/pages/Chart/OrderHistoryCh/OrderHistoryTooltip.tsx
+++ src/pages/Chart/OrderHistoryCh/OrderHistoryTooltip.tsx
@@ -89,6 +89,7 @@ export default function OrderHistoryTooltip(props: {
 
                             return !prev;
                         });
+                        setHoverOHTooltip(false);
                     }}
                 >
                     <OrderHistoryHeader>
