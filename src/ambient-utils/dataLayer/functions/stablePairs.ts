5b3ca4fbec8b7e26be843a6870d18f842b5a2e64
Ben Wolski <benwolski@gmail.com>
2024-02-29 15:19:49 -0500

    add USDB to stable tokens and Blast WETH to exclusion list

--- src/ambient-utils/dataLayer/functions/stablePairs.ts
+++ src/ambient-utils/dataLayer/functions/stablePairs.ts
@@ -11,6 +11,8 @@ import {
     goerliUSDT,
     mainnetDAI,
     mainnetUSDC,
+    blastUSDB,
+    blastSepoliaUSDB,
     mainnetUSDT,
     scrollAxlUSDC,
     sepoliaUSDC,
@@ -43,6 +45,7 @@ export function isWethToken(addr: string): boolean {
 export const STABLE_USD_TOKENS = [
     mainnetDAI.address,
     mainnetUSDC.address,
+    blastUSDB.address,
     mainnetUSDT.address,
     goerliDAI.address,
     goerliUSDC.address,
@@ -50,16 +53,20 @@ export const STABLE_USD_TOKENS = [
     arbGoerliDAI.address,
     arbGoerliUSDC.address,
     scrollUSDC.address,
+    scrollUSDC.address,
     scrollAxlUSDC.address,
     sepoliaUSDC.address,
+    blastSepoliaUSDB.address,
     scrollSepoliaUSDC.address,
 ].map((x) => x.toLowerCase());
 
 export const USDC_TOKENS = [
     mainnetUSDC.address,
+    blastUSDB.address,
     goerliUSDC.address,
     arbGoerliUSDC.address,
     sepoliaUSDC.address,
+    blastSepoliaUSDB.address,
     scrollSepoliaUSDC.address,
     scrollUSDC.address,
 ].map((x) => x.toLowerCase());
@@ -69,4 +76,6 @@ export const WETH_TOKENS = [
     '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // Goerli
     '0x5300000000000000000000000000000000000004', // Scroll (test and main)
     '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14', // Sepolia
+    '0x4300000000000000000000000000000000000004', // Blast
+    '0x4200000000000000000000000000000000000023', // Blast Seploia
 ].map((x) => x.toLowerCase());
