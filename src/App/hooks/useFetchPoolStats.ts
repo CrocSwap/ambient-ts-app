9ebe8fb04a6047324a81138b8c11eab4a9d3d422
Ben Wolski <benwolski@gmail.com>
2024-03-02 19:07:18 -0500

    fix delay in showing pool stats

--- src/App/hooks/useFetchPoolStats.ts
+++ src/App/hooks/useFetchPoolStats.ts
@@ -293,14 +293,15 @@ const useFetchPoolStats = (pool: PoolIF): PoolStatIF => {
         tokenB: quoteAddr,
     });
 
-    const timeInMinutes = Math.floor(Date.now() / 1000 / 60);
+    const minuteInterval = Math.floor(Date.now() / 1000 / 60);
 
     useEffect(() => {
         if (isServerEnabled) fetchPoolStats();
     }, [
         isServerEnabled,
         shouldInvertDisplay,
-        timeInMinutes,
+        minuteInterval,
+        lastBlockNumber === 0,
         !!crocEnv,
         !!provider,
         poolIndex,
