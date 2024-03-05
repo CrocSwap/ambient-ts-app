0fb1dd8566cedc357fc9c714281ef9339072d72d
Ben Wolski <benwolski@gmail.com>
2024-03-04 22:14:09 -0500

    fix disappearing row highlighting when closing limit and range modals

--- src/components/Trade/TradeTabs/TableRows.tsx
+++ src/components/Trade/TradeTabs/TableRows.tsx
@@ -58,7 +58,6 @@ function TableRows({
         useState<RangeModalAction>('Harvest');
 
     const closeRangeModal = (modalType: 'action' | 'details') => {
-        setCurrentPositionActive('');
         modalType === 'action' ? closeActionModal() : closeDetailsModal();
         setActiveRecord(undefined);
     };
@@ -96,7 +95,6 @@ function TableRows({
         useState<LimitModalAction>('Remove');
 
     const closeLimitModal = (modalType: 'action' | 'details') => {
-        setCurrentLimitOrderActive('');
         modalType === 'action' ? closeActionModal() : closeDetailsModal();
         setActiveRecord(undefined);
     };
