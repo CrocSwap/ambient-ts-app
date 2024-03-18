81998dafbc9bf669cf4b7703095e2ff238e732c5
mhsmith321 <emily.drake.smith@gmail.com>
2024-03-18 11:04:32 -0400

    add error logging logic to `processLogoSrc.ts` file

--- src/components/Global/TokenIcon/processLogoSrc.ts
+++ src/components/Global/TokenIcon/processLogoSrc.ts
@@ -1,6 +1,7 @@
 import * as localLogos from '../../../assets/images/tokens/exports';
 import { uriToHttp } from '../../../ambient-utils/dataLayer';
 import { TokenIF } from '../../../ambient-utils/types';
+import { IS_LOCAL_ENV } from '../../../ambient-utils/constants';
 
 // all args are optional; calling fn without data is handled
 interface argsIF {
@@ -13,7 +14,14 @@ export default function processLogoSrc(args: argsIF): string {
     // consume URI from TokenIF obj preferentially, use string as fallback if missing
     const uri: string | undefined = args.token?.logoURI ?? args.sourceURI;
     // early return if no URI was received in args
-    if (!uri) return '';
+    if (!uri) {
+        IS_LOCAL_ENV &&
+            console.debug(
+                `Fn <<${processLogoSrc.name}>> was called without a valid URI. Refer to file processLogoSrc.ts for troubleshooting. Fn will return an empty string (''). Args received in fn call are: `,
+                args,
+            );
+        return '';
+    }
     // return a filepath (if local) or a URI string (if remote)
     return (
         localLogos[
