52defd2afa6998476b14ad2b8f3376462dc1585f
Ben Wolski <benwolski@gmail.com>
2024-03-21 23:48:48 -0400

    re-add filtering of tokens in selector to only ack'd wallet tokens

--- src/App/hooks/useTokenSearch.ts
+++ src/App/hooks/useTokenSearch.ts
@@ -121,15 +121,14 @@ export const useTokenSearch = (
                       )
                     : tokens.getTokensFromList(tokenListURIs.ambient);
 
-            // temporarily removing token universe verification
             // ERC-20 tokens from connected wallet subject to universe verification
-            // const verifiedWalletTokens: TokenIF[] = walletTokens.filter(
-            //     (tkn: TokenIF) => tokens.verify(tkn.address),
-            // );
+            const verifiedWalletTokens: TokenIF[] = walletTokens.filter(
+                (tkn: TokenIF) => tokens.verify(tkn.address),
+            );
             // array with tokens added to user's wallet (subject to verification)
             const withWalletTokens: TokenIF[] = patchLists(
                 baseTokenList,
-                walletTokens, // verifiedWalletTokens
+                verifiedWalletTokens,
             );
             // remove the wrapped native token (if present)
             const tknsNoWrappedNative = removeWrappedNative(
