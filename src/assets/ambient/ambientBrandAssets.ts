5f5e253e8c9685021ec4c8a0df9a9dad8628ab94
mhsmith321 <emily.drake.smith@gmail.com>
2024-03-29 14:34:13 -0400

    fix `brandAssetsIF` interface

--- src/assets/ambient/ambientBrandAssets.ts
+++ src/assets/ambient/ambientBrandAssets.ts
@@ -1,7 +1,8 @@
+import { chainIds } from '../../ambient-utils/types';
 import { skins } from '../../App/hooks/useSkin';
 
 export interface brandAssetsIF {
-    color: { [key: string]: skins };
+    color: Record<chainIds, skins>;
     platformName: string;
 }
 
@@ -11,7 +12,8 @@ export const ambientBrandAssets: brandAssetsIF = {
         '0x82750': 'purple_dark',
         '0x5': 'purple_dark',
         '0x8274f': 'purple_dark',
-        '0x13e31': 'purple_dark',
+        '0xaa36a7': 'purple_dark',
+        '0xa0c71fd': 'purple_dark',
     },
     platformName: 'ambient',
 };
