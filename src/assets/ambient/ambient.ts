a6196bc594bdf98fa9c7b6e12185e9b50db179ec
mhsmith321 <emily.drake.smith@gmail.com>
2024-03-29 13:50:02 -0400

    create new `brandAssetsIF` interface to describe shape of asset obj

--- src/assets/ambient/ambient.ts
+++ src/assets/ambient/ambient.ts
@@ -1,4 +1,11 @@
-export const ambient = {
+import { skins } from '../../App/hooks/useSkin';
+
+interface brandAssetsIF {
+    color: { [key: string]: skins };
+    platformName: string;
+}
+
+export const ambient: brandAssetsIF = {
     color: {
         '0x1': 'purple_dark',
         '0x82750': 'purple_dark',
@@ -6,4 +13,5 @@ export const ambient = {
         '0x8274f': 'purple_dark',
         '0x13e31': 'purple_dark',
     },
+    platformName: 'ambient',
 };
