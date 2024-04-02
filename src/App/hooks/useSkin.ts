94d703cac28a4b65710a5c77cc926c30cc403146
mhsmith321 <emily.drake.smith@gmail.com>
2024-04-02 15:54:34 -0400

    complete logic to persist user color preferences in local storage

--- src/App/hooks/useSkin.ts
+++ src/App/hooks/useSkin.ts
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import { chainIds } from '../../ambient-utils/types';
 import { chainColorScheme } from '../../assets/branding/types';
+import { IS_LOCAL_ENV } from '../../ambient-utils/constants';
 
 export type skins = 'purple_dark' | 'purple_light' | 'orange';
 
@@ -15,17 +16,53 @@ export const useSkin = (
 ): skinMethodsIF => {
     const LS_KEY = 'skin';
 
+    // fn to get current data object from local storage
+    function getFromLS(): Partial<chainColorScheme> | undefined {
+        // const fallback: chainColorScheme = colorDefaults;
+        const entry: string | null = localStorage.getItem(LS_KEY);
+        // process data retrieved from local storage
+        if (entry) {
+            try {
+                // parse data from local storage and assign to output variable
+                return JSON.parse(entry);
+            } catch {
+                // clear data from local storage and warn user if unable to parse
+                // assign `null` value to output variable
+                IS_LOCAL_ENV &&
+                    console.debug(
+                        'localStorage token lists corrupt, clearing data for',
+                        LS_KEY,
+                    );
+                localStorage.setItem(LS_KEY, JSON.stringify(colorDefaults));
+            }
+        }
+    }
+
+    // fn to sync current color setting into local storage
+    function updateLS(): void {
+        const persisted = getFromLS() ?? colorDefaults;
+        if (skin) persisted[chainId] = skin;
+        localStorage.setItem(LS_KEY, JSON.stringify(persisted));
+    }
+
+    // fn to check for a color preference persisted in local storage
+    function getColorScheme(chn: chainIds): skins | undefined {
+        const persisted: Partial<chainColorScheme> | undefined = getFromLS();
+        if (persisted && persisted[chn]) {
+            return persisted[chn];
+        }
+    }
+
     // name of the current skin in use by the app
     // defaults to value in local storage, uses value from params as fallback
     const [skin, setSkin] = useState<skins>(
-        // localStorage[LS_KEY] ??
-        colorDefaults[chainId],
+        getColorScheme(chainId) ?? colorDefaults[chainId],
     );
 
     // hook to hold a single color set for the app to return
     // updates local storage when needed as an accessory function
     useEffect(() => {
-        localStorage.setItem(LS_KEY, skin);
+        updateLS();
     }, [skin]);
 
     return useMemo(
