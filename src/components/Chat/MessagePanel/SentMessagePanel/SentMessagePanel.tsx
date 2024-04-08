1570363c9afa8e54a338e2ae10907ae08f8c23e3
Umay Şahan <umaysahan03@outlook.com>
2024-04-08 16:41:15 +0300

    -Link improvement has finished. -Message should split with speacial characters.

--- src/components/Chat/MessagePanel/SentMessagePanel/SentMessagePanel.tsx
+++ src/components/Chat/MessagePanel/SentMessagePanel/SentMessagePanel.tsx
@@ -528,8 +528,8 @@ function SentMessagePanel(props: SentMessageProps) {
     function buildMessageToken(word: string, mentFound: MentFoundParam) {
         let ret = <></>;
         if (
-            (isLinkInCrocodileLabsLinks(word) ||
-                isLinkInCrocodileLabsLinksForInput(word)) &&
+            isLinkInCrocodileLabsLinks(word) &&
+            isLinkInCrocodileLabsLinksForInput(word) &&
             isValidUrl(word)
         ) {
             ret = (
