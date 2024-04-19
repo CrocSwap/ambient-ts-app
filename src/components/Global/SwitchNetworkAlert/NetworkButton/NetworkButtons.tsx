00becf50bc9c61d67e66f8102fb1f94fdedef050
mhsmith321 <emily.drake.smith@gmail.com>
2024-04-19 18:13:34 -0400

    remove disabled code from `NetworkButtons.tsx` file

--- src/components/Global/SwitchNetworkAlert/NetworkButton/NetworkButtons.tsx
+++ src/components/Global/SwitchNetworkAlert/NetworkButton/NetworkButtons.tsx
@@ -1,35 +1,14 @@
-// import goerliLogo from '../../../../assets/images/networks/goerli.png';
-// https://github.com/goerli/goer-logo
-
-// import ethereumLogo from '../../../../assets/images/networks/ethereum.png';
-
 import { lookupChain } from '@crocswap-libs/sdk/dist/context';
 import { useSwitchNetwork } from 'wagmi';
-// import { getSupportedChainIds } from '../../../../ambient-utils/dataLayer';
 import NetworkButton from './NetworkButton';
 import { useContext } from 'react';
 import { BrandContext } from '../../../../contexts/BrandContext';
 import { chainIds } from '../../../../ambient-utils/types';
 
-// interface NetworkButtonsPropsIF {
-//     switchNetwork: (providedChainId: string) => Promise<void>;
-// }
 export default function NetworkButtons() {
     const { networks } = useContext(BrandContext);
 
-    const {
-        // chains, error, isLoading, pendingChainId,
-        switchNetwork,
-    } = useSwitchNetwork();
-
-    // const supportedChains = getSupportedChainIds().map((chainId) => {
-    //     return {
-    //         name: lookupChain(chainId).displayName,
-    //         id: chainId,
-    //         icon: '',
-    //         theme: '#36364a',
-    //     };
-    // });
+    const { switchNetwork } = useSwitchNetwork();
 
     const supportedChains = networks.map((network: chainIds) => {
         return {
