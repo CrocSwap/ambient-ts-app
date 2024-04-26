ef570046122898575e9886b19c2a8f7a758ff6b3
mhsmith321 <emily.drake.smith@gmail.com>
2024-04-26 14:09:21 -0400

    update color of points bar

--- src/components/Global/LevelLine/LevelLine.tsx
+++ src/components/Global/LevelLine/LevelLine.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-interface LevelLineProps {
+interface propsIF {
     percentage: number;
     width?: string;
     height?: string;
 }
 
-const Container = styled.div<LevelLineProps>`
+const Container = styled.div<propsIF>`
     width: ${(props) => (props.width ? props.width : '100%')};
     height: ${(props) => (props.height ? props.height : '6px')};
     flex-shrink: 0;
     border-radius: 10px;
-    background-color: rgba(115, 113, 252, 0.5);
+    background-color: var(--accent2);
     position: relative;
 `;
 
@@ -19,17 +19,17 @@ const FilledArea = styled.div<{ percentage: number }>`
     height: 100%;
     width: ${(props) => `${props.percentage}%`};
     border-radius: 10px;
-    background-color: rgba(115, 113, 252, 1);
+    background-color: var(--accent1);
     position: absolute;
     top: 0;
     left: 0;
 `;
-export default function LevelLine(props: LevelLineProps) {
+export default function LevelLine(props: propsIF) {
     const { percentage, width, height } = props;
 
     return (
         <Container width={width} height={height} percentage={percentage}>
-            <FilledArea percentage={20} />
+            <FilledArea percentage={percentage} />
         </Container>
     );
 }
