970d9a280cb462c71312be02cab8f318613d2b64
mhsmith321 <emily.drake.smith@gmail.com>
2024-05-01 16:34:58 -0400

    build ambient production config object per new spec

--- src/assets/branding/ambientProductionBrandAssets.ts
+++ src/assets/branding/ambientProductionBrandAssets.ts
@@ -1,6 +1,7 @@
 import { brandAssetsIF, brandIF } from './types';
 import large from '../images/logos/large.svg';
 import scrollLogo from '../../assets/images/logos/scroll_brand_logo.svg';
+import blastLogo from '../../assets/images/logos/blast_logo.svg';
 
 export const ambientProductionBrandAssets: brandAssetsIF = {
     networks: [
@@ -25,12 +26,23 @@ export const ambientProductionBrandAssets: brandAssetsIF = {
 
 export const ambientProductionBrandAssets2: brandIF = {
     networks: {
-        '0xaa36a7': {
-            color: 'orange_dark',
-            hero: ['futa', scrollLogo],
+        // ethereum mainnet
+        '0x1': {
+            color: 'purple_dark',
+            hero: ['ambient', scrollLogo],
+        },
+        // scroll mainnet
+        '0x82750': {
+            color: 'purple_dark',
+            hero: ['ambient', scrollLogo],
+        },
+        // blast mainnet
+        '0x13e31': {
+            color: 'purple_dark',
+            hero: ['ambient', blastLogo],
         },
     },
-    platformName: 'futa',
+    platformName: 'ambient',
     showPoints: true,
     showDexStats: true,
     headerImage: large as string,
