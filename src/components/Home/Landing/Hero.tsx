876cb5a30d7f127ac2ec92c3a5243ccc9dbb75cd
mhsmith321 <emily.drake.smith@gmail.com>
2024-05-03 17:10:27 -0400

    fix display bugs in `Hero.tsx` file

--- src/components/Home/Landing/Hero.tsx
+++ src/components/Home/Landing/Hero.tsx
@@ -1,4 +1,4 @@
-import { FlexContainer, Text } from '../../../styled/Common';
+import { FlexContainer } from '../../../styled/Common';
 import TradeNowButton from './TradeNowButton/TradeNowButton';
 import styles from './BackgroundImages.module.css';
 import { HeroContainer } from '../../../styled/Components/Home';
@@ -8,12 +8,16 @@ import { BrandContext } from '../../../contexts/BrandContext';
 import { heroItem } from '../../../assets/branding/types';
 
 export default function Hero() {
-    const smallScreen = useMediaQuery('(max-width: 1200px)');
+    const smallScreen: boolean = useMediaQuery('(max-width: 1200px)');
     const { hero, platformName } = useContext(BrandContext);
 
+    // recognized slugs for background image CSS classes
     type cssSlugs = 'purple_waves' | 'stars';
+    // slug to specify the desired background image
     const cssSlug = useMemo<cssSlugs>(() => {
+        // declare an output variable
         let slug: cssSlugs;
+        // router to map a background image from deployment config
         switch (platformName) {
             case 'futa':
                 slug = 'stars';
@@ -23,52 +27,57 @@ export default function Hero() {
                 slug = 'purple_waves';
                 break;
         }
+        // return output variable
         return slug;
     }, [platformName]);
 
-    function makeHeroJSX(h: heroItem) {
+    // fn to turn hero banner metadata into JSX for DOM
+    function makeHeroJSX(h: heroItem): JSX.Element {
+        // declare an output variable
         let jsxOutput: JSX.Element;
-        if (h.processAs === 'text') {
-            jsxOutput = (
-                <p
-                    className={styles.ambient_blast_logo}
-                    style={{ fontSize: '110px' }}
-                >
-                    {h.content}
-                </p>
-            );
-        } else if (h.processAs === 'image') {
-            jsxOutput = (
-                <img
-                    src={h.content}
-                    alt=''
-                    width='70px'
-                    style={{
-                        marginTop: '8px',
-                        maxWidth: '60%',
-                    }}
-                />
-            );
-        } else if (h.processAs === 'separator') {
-            jsxOutput = (
-                <Text
-                    fontWeight='200'
-                    // fontSize='800px'
-                    color='text1'
-                    align='center'
-                    style={{
-                        marginTop: '20px',
-                        marginLeft: '15px',
-                        marginRight: '15px',
-                        fontSize: '30px',
-                    }}
-                >
-                    {h.content}
-                </Text>
-            );
-        } else {
-            jsxOutput = <></>;
+        // router to apply the correct JSX template
+        switch (h.processAs) {
+            // process content as raw text
+            case 'text':
+                jsxOutput = (
+                    <p
+                        className={styles.ambient_blast_logo}
+                        style={{ fontSize: '110px' }}
+                    >
+                        {h.content}
+                    </p>
+                );
+                break;
+            // process content as an image
+            case 'image':
+                jsxOutput = (
+                    <img
+                        src={h.content}
+                        alt=''
+                        width='70px'
+                        style={{
+                            marginTop: '8px',
+                            maxWidth: '60%',
+                        }}
+                    />
+                );
+                break;
+            // process content as a separator character
+            case 'separator':
+                jsxOutput = (
+                    <img
+                        src={h.content}
+                        alt='separator character'
+                        width='70px'
+                        style={{
+                            marginTop: '8px',
+                            maxWidth: '60%',
+                        }}
+                    />
+                );
+                break;
         }
+        // return output variable with processed JSX
         return jsxOutput;
     }
 
