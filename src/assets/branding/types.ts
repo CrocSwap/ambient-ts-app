408702f4cf1043187d8f5643435dd11af8e2349f
mhsmith321 <emily.drake.smith@gmail.com>
2024-05-03 17:44:49 -0400

    remove developmental code from `types.ts` file

--- src/assets/branding/types.ts
+++ src/assets/branding/types.ts
@@ -1,23 +1,6 @@
 import { chainIds } from '../../ambient-utils/types';
 import { skins } from '../../App/hooks/useSkin';
 
-// export type chainColorScheme = Record<chainIds, skins>;
-
-// export interface brandAssetsIF {
-//     networks: chainIds[];
-//     color: chainColorScheme;
-//     platformName: string;
-//     headerImage: string;
-//     showPoints: boolean;
-//     showDexStats: boolean;
-//     hero?: Partial<Record<chainIds, [any, any]>>;
-// }
-
-// export interface networkPrefsIF {
-//     color: skins;
-//     hero: [string, string];
-// }
-
 export interface heroItem {
     content: string;
     processAs: 'text' | 'image' | 'separator';
