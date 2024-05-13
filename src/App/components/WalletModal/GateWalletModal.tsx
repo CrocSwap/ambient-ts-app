c91fe4e0dfe498217d7ea274cc9e3b51f3899469
bus <busimus@gmail.com>
2024-05-13 21:41:45 +0000

    Bring back restricted wallet gate

--- src/App/components/WalletModal/GateWalletModal.tsx
+++ src/App/components/WalletModal/GateWalletModal.tsx
@@ -1,5 +1,5 @@
 // START: Import React and Dongles
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 // START: Import Local Files
 import styles from './GateWalletModal.module.css';
@@ -9,29 +9,18 @@ import Button from '../../../components/Form/Button';
 import { useTermsAgreed } from '../../hooks/useTermsAgreed';
 import { AppStateContext } from '../../../contexts/AppStateContext';
 import { useWeb3Modal } from '@web3modal/ethers5/react';
+import { CircleLoaderFailed } from '../../../components/Global/LoadingAnimations/CircleLoader/CircleLoader';
 
 export default function GateWalletModal() {
     const {
-        walletModal: { isOpen: isModalOpen, close: closeModal },
+        walletModal: { close: closeModal },
     } = useContext(AppStateContext);
 
-    const defaultState = import.meta.env.VITE_VIEW_ONLY
-        ? 'notAvailable'
-        : 'wallets';
-
-    const [_, setPage] = useState(defaultState);
-
-    // reset the page everytime the modal is closed
-    useEffect(() => {
-        if (!isModalOpen) {
-            setPage(defaultState);
-        }
-    }, [isModalOpen]);
-
+    const notAvailable = import.meta.env.VITE_VIEW_ONLY;
     const [recordAgreed, __, termUrls] = useTermsAgreed();
     const { open: openW3Modal } = useWeb3Modal();
 
-    return (
+    return !notAvailable ? (
         <Modal onClose={closeModal} title='Welcome'>
             <div className={styles.main_container}>
                 <section className={styles.first_section}>
@@ -84,6 +73,22 @@ export default function GateWalletModal() {
                         }}
                     />
                 </section>
+            </div>
+        </Modal>
+    ) : (
+        <Modal onClose={closeModal} title='Not Available'>
+            <div className={styles.not_available_container}>
+                <CircleLoaderFailed size='48' />
+                <p>Ambient is not available in the United States.</p>
+                <p>If you think this is an error, contact the host.</p>
+                <Button
+                    idForDOM='acknowledge_ambient_not_available_in_US_button'
+                    flat
+                    title='Close'
+                    action={() => {
+                        closeModal();
+                    }}
+                />
             </div>
         </Modal>
     );
