adb1c3f92e93e4b8964994ea77db15b7f4033811
mhsmith321 <emily.drake.smith@gmail.com>
2024-05-24 16:26:54 -0400

    stub up basic content in `Auctions.tsx` file

--- src/App/futa/Auctions.tsx
+++ src/App/futa/Auctions.tsx
@@ -1,5 +1,16 @@
 import styles from './Auctions.module.css';
 
 export default function Auctions() {
-    return <section className={styles.auctions_main}>Auctions</section>;
+    return (
+        <section className={styles.auctions_main}>
+            Auctions
+            <header className={styles.auction_header}>
+                <div>Search Dropdown</div>
+                <button>Desc</button>
+                <button>Asc</button>
+                <input type='text' placeholder='Search...' />
+                <button>Create Token</button>
+            </header>
+        </section>
+    );
 }
