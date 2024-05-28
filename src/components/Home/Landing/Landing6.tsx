a7509aac0e1a9f1c6ef8a7e65f1043122179f40b
Jadessu <junioradessu@gmail.com>
2024-05-28 09:28:57 -0500

    change twitter icon and update links

--- src/components/Home/Landing/Landing6.tsx
+++ src/components/Home/Landing/Landing6.tsx
@@ -1,4 +1,4 @@
-import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa';
+import { FaDiscord, FaGithub } from 'react-icons/fa';
 import { FaMedium } from 'react-icons/fa6';
 
 import styles from './LandingStyles.module.css';
@@ -16,13 +16,28 @@ import { useTermsAgreed } from '../../../App/hooks/useTermsAgreed';
 export default function Landing6() {
     const [, , termsUrls] = useTermsAgreed();
 
+    const twitterSvg = (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='13'
+            height='13'
+            viewBox='0 0 1200 1227'
+            fill='none'
+        >
+            <path
+                d='M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z'
+                fill='var(--text2)'
+            />
+        </svg>
+    );
+
     const linksData = [
         {
-            label: 'Twitter',
+            label: 'X / Twitter',
             text: 'Follow the latest from Ambient Finance',
             link: TWITTER_LINK,
             linkLabel: '@ambient_finance',
-            icon: <FaTwitter />,
+            icon: twitterSvg,
         },
         {
             label: 'Discord',
