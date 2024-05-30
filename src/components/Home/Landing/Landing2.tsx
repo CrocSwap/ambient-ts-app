04fa6e19c1b85cc9aba628af3281035877285cc7
Jadessu <junioradessu@gmail.com>
2024-05-30 12:43:17 -0500

    add hover overlay to trade image

--- src/components/Home/Landing/Landing2.tsx
+++ src/components/Home/Landing/Landing2.tsx
@@ -1,6 +1,7 @@
 import { FlexContainer } from '../../../styled/Common';
 import styles from './LandingStyles.module.css';
 import tradeImage from '../../../assets/images/home/trade.svg';
+import TradeNowButton from './TradeNowButton/TradeNowButton';
 
 export default function Landing2() {
     return (
@@ -26,7 +27,12 @@ export default function Landing2() {
                         liquidity rewards, and a fairer trading experience.
                     </p>
                 </FlexContainer>
-                <img src={tradeImage} alt='' />
+                <div className={styles.trade_image_container}>
+                    <img src={tradeImage} alt='' />
+                    <div className={styles.trade_image_overlay}>
+                        <TradeNowButton fieldId='landing_overlay' />
+                    </div>
+                </div>
             </div>
         </div>
     );
