84efccefa63f20598e4bce507040023a10efea0e
mhsmith321 <emily.drake.smith@gmail.com>
2024-05-30 15:59:03 -0400

    attach click handler to new `<input />` field in `CreateField.tsx` file

--- src/App/futa/CreateField.tsx
+++ src/App/futa/CreateField.tsx
@@ -14,6 +14,11 @@ export default function CreateField(props: CreateFieldPropsIF) {
 
     const [length, setLength] = useState<number>(0);
 
+    function handleChange(input: string): void {
+        updateRef(input);
+        setLength(input.length);
+    }
+
     return (
         <div className={styles.form_item}>
             <div className={styles.form_item_top}>
@@ -21,15 +26,16 @@ export default function CreateField(props: CreateFieldPropsIF) {
                 <div className={styles.counter}>{charLimit - length}</div>
             </div>
             {rows === 1 ? (
-                <input type='text' className={styles.text_input} />
+                <input
+                    type='text'
+                    className={styles.text_input}
+                    onChange={(e) => handleChange(e.target.value)}
+                />
             ) : (
                 <textarea
                     id={inputId}
                     className={styles.text_input}
-                    onChange={(e) => {
-                        updateRef(e.target.value);
-                        setLength(e.target.value.length);
-                    }}
+                    onChange={(e) => handleChange(e.target.value)}
                     maxLength={charLimit}
                     rows={rows}
                     spellCheck={false}
