453ded6d7010a4e63968ffcc8e08dcd37a828c11
Ben Wolski <benwolski@gmail.com>
2024-06-03 11:53:21 -0400

    update top pools

--- src/ambient-utils/constants/networks/blastNetwork.ts
+++ src/ambient-utils/constants/networks/blastNetwork.ts
@@ -3,9 +3,9 @@ import {
     blastETH,
     blastUSDB,
     blastEzETH,
-    blastUSDPLUS,
     blastWrsETH,
     blastJUICE,
+    blastYES,
 } from '../defaultTokens';
 import { NetworkIF } from '../../types/NetworkIF';
 import { TopPool } from './TopPool';
@@ -38,8 +38,8 @@ export const blast: NetworkIF = {
         new TopPool(blastETH, blastUSDB, lookupChain('0x13e31').poolIndex),
         new TopPool(blastWrsETH, blastETH, lookupChain('0x13e31').poolIndex),
         new TopPool(blastEzETH, blastETH, lookupChain('0x13e31').poolIndex),
-        new TopPool(blastUSDB, blastJUICE, lookupChain('0x13e31').poolIndex),
-        new TopPool(blastUSDPLUS, blastUSDB, lookupChain('0x13e31').poolIndex),
+        new TopPool(blastETH, blastYES, lookupChain('0x13e31').poolIndex),
+        new TopPool(blastETH, blastJUICE, lookupChain('0x13e31').poolIndex),
     ],
     getGasPriceInGwei: async (provider?: Provider) => {
         if (!provider) return 0;
