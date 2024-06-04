9b680e43ac9a54cd586204a686ac9dace18cbe7f
Jadessu <junioradessu@gmail.com>
2024-06-04 09:26:24 -0500

    just for deploy preview

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -4,7 +4,5 @@
 import TokenDetails from '../../App/futa/TokenDetails/TokenDetails';
 
 export default function TestPage() {
-    // const { skin } = useContext(BrandContext);
-
     return <TokenDetails />;
 }
