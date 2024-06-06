bf6c15558f4e8c0bb1a36b4445c515fe4d557a1b
Jadessu <junioradessu@gmail.com>
2024-06-06 13:27:56 -0500

    clean up css

--- src/components/Futa/Navbar/Navbar.tsx
+++ src/components/Futa/Navbar/Navbar.tsx
@@ -1,26 +1,28 @@
+// Imports
+import { useCallback, useContext, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FiMoreHorizontal } from 'react-icons/fi';
+import { motion } from 'framer-motion';
 import NetworkSelector from '../../../App/components/PageHeader/NetworkSelector/NetworkSelector';
 import styles from './Navbar.module.css';
 import Logo from '../../../assets/futa/images/futaLogo.svg';
+import Button from '../../Form/Button';
 import {
     useWeb3ModalAccount,
     useSwitchNetwork,
 } from '@web3modal/ethers5/react';
-import { useCallback, useContext, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { UserDataContext } from '../../../contexts/UserDataContext';
-import Button from '../../Form/Button';
-import { AppStateContext } from '../../../contexts/AppStateContext';
 import useMediaQuery from '../../../utils/hooks/useMediaQuery';
-import { motion } from 'framer-motion';
+// import useOnClickOutside from '../../../utils/hooks/useOnClickOutside';
+import { UserDataContext } from '../../../contexts/UserDataContext';
+import { AppStateContext } from '../../../contexts/AppStateContext';
 import { CrocEnvContext } from '../../../contexts/CrocEnvContext';
 import { TradeTokenContext } from '../../../contexts/TradeTokenContext';
 import { TokenBalanceContext } from '../../../contexts/TokenBalanceContext';
 import { GraphDataContext } from '../../../contexts/GraphDataContext';
 import { ReceiptContext } from '../../../contexts/ReceiptContext';
 import { TradeTableContext } from '../../../contexts/TradeTableContext';
-import useOnClickOutside from '../../../utils/hooks/useOnClickOutside';
-// TODO: UNCOMMENT OUT ANIMATE PRESENCE IF WE WANT THE STAGGER ANIMATION TO ONLY HAPPEN THE FIRST TIME THE USER OPENS THE MENU AND STOP AFTER THAT
+
+// Animation Variants
 const dropdownVariants = {
     hidden: { height: 0, opacity: 0 },
     visible: {
@@ -43,10 +45,12 @@ const itemVariants = {
     hidden: { opacity: 0, y: -20 },
     visible: { opacity: 1, y: 0 },
 };
-export default function Navbar() {
-    const desktopScreen = useMediaQuery('(min-width: 768px)');
 
+export default function Navbar() {
+    // States
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    // Context
     const { isConnected } = useWeb3ModalAccount();
     const { isUserConnected, disconnectUser } = useContext(UserDataContext);
     const { setCrocEnv } = useContext(CrocEnvContext);
@@ -64,50 +68,18 @@ export default function Navbar() {
     const { resetUserGraphData } = useContext(GraphDataContext);
     const { resetReceiptData } = useContext(ReceiptContext);
     const { setShowAllData } = useContext(TradeTableContext);
-    const dropdownRef = useRef<HTMLDivElement>(null);
-    const clickOutsideHandler = () => {
-        setIsDropdownOpen(false);
-    };
-    useOnClickOutside(dropdownRef, clickOutsideHandler);
-
     const {
         walletModal: { open: openWalletModal },
     } = useContext(AppStateContext);
-    const switchNetwork = isConnected
-        ? useSwitchNetwork().switchNetwork
-        : undefined;
 
-    const dropdownData = [
-        {
-            label: 'docs',
-            link: '/docs',
-        },
-        {
-            label: 'twitter',
-            link: '#',
-        },
-        {
-            label: 'discord',
-            link: '#',
-        },
-        {
-            label: 'legal & privacy',
-            link: '#',
-        },
-        {
-            label: 'terms of service',
-            link: '#',
-        },
-    ];
-    const connectWagmiButton = (
-        <Button
-            idForDOM='connect_wallet_button_page_header'
-            title={desktopScreen ? 'Connect Wallet' : 'Connect'}
-            action={openWalletModal}
-            thin
-            flat
-        ></Button>
-    );
+    // Refs
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    // Handlers
+    // const clickOutsideHandler = () => {
+    //     setIsDropdownOpen(false);
+    // };
+
     const clickLogout = useCallback(async () => {
         setCrocEnv(undefined);
         setBaseTokenBalance('');
@@ -121,12 +93,38 @@ export default function Navbar() {
         disconnectUser();
     }, []);
 
+    // Custom Hooks
+    // useOnClickOutside(dropdownRef, clickOutsideHandler);
+    const desktopScreen = useMediaQuery('(min-width: 768px)');
+    const switchNetwork = isConnected
+        ? useSwitchNetwork().switchNetwork
+        : undefined;
+
+    // Data
+    const dropdownData = [
+        { label: 'docs', link: '/docs' },
+        { label: 'twitter', link: '#' },
+        { label: 'discord', link: '#' },
+        { label: 'legal & privacy', link: '#' },
+        { label: 'terms of service', link: '#' },
+    ];
+
+    // Components
+    const connectWagmiButton = (
+        <Button
+            idForDOM='connect_wallet_button_page_header'
+            title={desktopScreen ? 'Connect Wallet' : 'Connect'}
+            action={openWalletModal}
+            thin
+            flat
+        />
+    );
+
     return (
         <div className={styles.container}>
             <Link to='/'>
                 <img src={Logo} alt='futa logo' />
             </Link>
-
             <div className={styles.rightContainer}>
                 <NetworkSelector
                     switchNetwork={switchNetwork}
@@ -138,7 +136,6 @@ export default function Navbar() {
                         onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                     />
                     {/* <AnimatePresence> */}
-
                     {isDropdownOpen && (
                         <motion.div
                             className={styles.dropdownMenu}
@@ -153,6 +150,7 @@ export default function Navbar() {
                                     key={idx}
                                     variants={itemVariants}
                                     className={styles.linkContainer}
+                                    onClick={() => setIsDropdownOpen(false)}
                                 >
                                     <Link to={item.link}>{item.label}</Link>
                                 </motion.div>
@@ -170,7 +168,6 @@ export default function Navbar() {
                                         ? clickLogout
                                         : openWalletModal
                                 }
-                                // variants={itemVariants}
                             >
                                 {isUserConnected ? 'LOG OUT' : 'CONNECT WALLET'}
                             </motion.button>
