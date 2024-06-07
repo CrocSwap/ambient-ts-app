9500c10173f2fecb6f51388e612134074e3974ed
Ben Wolski <benwolski@gmail.com>
2024-06-07 18:01:47 -0400

    add link to example auction from /auctions

--- src/pages/platformFuta/Auctions/Auctions.tsx
+++ src/pages/platformFuta/Auctions/Auctions.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
 import styles from './Auctions.module.css';
 export default function Auctions() {
     return (
         <div className={styles.container}>
-            <p>auctions</p>
+            <p>auctions:</p>
+            <Link to='/auctions/foo'>FOO Token</Link>
         </div>
     );
 }
