0f95c4ac3f36548fccaa3784cf7d4b39811b6173
mhsmith321 <emily.drake.smith@gmail.com>
2024-06-10 16:39:01 -0400

    add functionality to clear input button on search input field

--- src/pages/platformFuta/Auctions/Auctions.tsx
+++ src/pages/platformFuta/Auctions/Auctions.tsx
@@ -1,6 +1,7 @@
 import styles from './Auctions.module.css';
 import { HiMiniMagnifyingGlass } from 'react-icons/hi2';
 import AuctionItem from './AuctionItem';
+import { useState } from 'react';
 
 export interface auctionDataIF {
     ticker: string;
@@ -67,18 +68,29 @@ export default function Auctions() {
         },
     ];
 
+    const INPUT_DOM_ID = 'ticker_auction_search_input';
+
+    const [searchInput, setSearchInput] = useState<string>('');
+    function clearInput(): void {
+        setSearchInput('');
+        document.getElementById(INPUT_DOM_ID)?.focus();
+    }
+
     return (
         <div className={styles.auctions_main}>
             <h3>AUCTIONS</h3>
             <div className={styles.search_box}>
                 <HiMiniMagnifyingGlass />
                 <input
+                    id={INPUT_DOM_ID}
                     type='text'
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
                     placeholder='Search...'
                     spellCheck={false}
                     autoComplete='off'
                 />
-                <button>×</button>
+                <button onClick={() => clearInput()}>×</button>
             </div>
             <div className={styles.sort_buttons}></div>
             <div className={styles.auctions_list}>
