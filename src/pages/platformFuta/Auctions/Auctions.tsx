dedd08965bbe15bcf6b91dfddaba271816ca68f6
mhsmith321 <emily.drake.smith@gmail.com>
2024-06-10 14:11:23 -0400

    apply basic styling to search box in `Auctions.tsx` file

--- src/pages/platformFuta/Auctions/Auctions.tsx
+++ src/pages/platformFuta/Auctions/Auctions.tsx
@@ -1,9 +1,12 @@
 import styles from './Auctions.module.css';
+import { HiMiniMagnifyingGlass } from 'react-icons/hi2';
+
 export default function Auctions() {
     return (
-        <div className={styles.container}>
+        <div className={styles.auctions_main}>
             <h3>AUCTIONS</h3>
             <div className={styles.search_box}>
+                <HiMiniMagnifyingGlass />
                 <input type='text' />
                 <button>X</button>
             </div>
