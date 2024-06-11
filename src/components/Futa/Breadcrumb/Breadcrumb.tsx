bfd17492da2273fa7f3c3b5a92c0b240db335e8e
Jadessu <junioradessu@gmail.com>
2024-06-11 09:49:20 -0500

    filter out version number from breadcrumb

--- src/components/Futa/Breadcrumb/Breadcrumb.tsx
+++ src/components/Futa/Breadcrumb/Breadcrumb.tsx
@@ -3,7 +3,7 @@ import styles from './Breadcrumb.module.css';
 export default function BreadCrumb() {
     const location = useLocation();
     const pathname = location.pathname;
-    const pathnames = pathname.split('/').filter((x) => x);
+    const pathnames = pathname.split('/').filter((x) => x && !/^v\d+$/.test(x));
     return (
         <nav aria-label='breadcrumb' className={styles.breadcrumbContainer}>
             <ol>
