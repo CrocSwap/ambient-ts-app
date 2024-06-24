4efa447857aa20e5295e5d63b984c679b14f3a27
Jadessu <junioradessu@gmail.com>
2024-06-24 11:33:52 -0500

    add no auctions content

--- src/pages/platformFuta/Account/Account.tsx
+++ src/pages/platformFuta/Account/Account.tsx
@@ -27,7 +27,6 @@ export default function Account() {
         <div className={styles.claimAllContainer}>
             <h3>CLAIM ALL</h3>
             <p>CLAIM ALL TOKENS FROM WINNING AUCTIONS AND UNUSED BIDS</p>
-
             <div className={styles.extraFeeContainer}>
                 <div className={styles.alignCenter}>
                     <p>NETWORK FEE</p>
@@ -35,35 +34,36 @@ export default function Account() {
                 </div>
                 <p style={{ color: 'var(--text2)' }}>~0.01</p>
             </div>
+            <button className={styles.claimButton}>CLAIM ALL</button>
         </div>
     );
 
-    const sorted: sortedAuctionsIF = useSortedAuctions(accountData.auctions);
     const connectWalletContent = (
         <div className={styles.connectWalletContent}>
             <Typewriter text='Connect your wallet to view your auctions' />
-
             <button onClick={openWalletModal}>Connect wallet</button>
         </div>
     );
+
     const noAuctionsContent = (
         <div className={styles.connectWalletContent}>
             <Typewriter text='No auctions found' />
             <p>Consider viewing all auctions</p>
-
             <Link to='/auctions'>All auctions</Link>
         </div>
     );
-
+    const sorted: sortedAuctionsIF = useSortedAuctions(accountData.auctions);
     const desktopScreen = useMediaQuery('(min-width: 1080px)');
 
-    const noAuctions = false;
+    if (!isUserConnected) {
+        return connectWalletContent;
+    }
 
-    const desktopVersion = !isUserConnected ? (
-        connectWalletContent
-    ) : noAuctions ? (
-        noAuctionsContent
-    ) : (
+    if (!sorted?.data?.length) {
+        return noAuctionsContent;
+    }
+
+    return desktopScreen ? (
         <div className={styles.desktopContainer}>
             <div className={styles.content}>
                 <SearchableTicker
@@ -76,28 +76,16 @@ export default function Account() {
             <div className={styles.rightLayout}>
                 <Divider count={2} />
                 {claimAllContainer}
-                <button className={styles.claimButton}>CLAIM ALL</button>
             </div>
         </div>
-    );
-
-    if (desktopScreen) return desktopVersion;
-
-    return isUserConnected ? (
-        noAuctions ? (
-            noAuctionsContent
-        ) : (
-            <div className={styles.container}>
-                <div className={styles.content}>
-                    <BreadCrumb />
-                    <h2>Account</h2>
-                    <SearchableTicker auctions={sorted} isAccount={true} />
-                </div>
-                {claimAllContainer}
-                <button className={styles.claimButton}>CLAIM ALL</button>
-            </div>
-        )
     ) : (
-        connectWalletContent
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <BreadCrumb />
+                <h2>Account</h2>
+                <SearchableTicker auctions={sorted} isAccount={true} />
+            </div>
+            {claimAllContainer}
+        </div>
     );
 }
