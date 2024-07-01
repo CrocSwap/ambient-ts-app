9774d8f96023a0941ca8edce64363c44ecb8057b
Jadessu <junioradessu@gmail.com>
2024-07-01 10:46:06 -0500

    build desktop landing page

--- src/pages/platformFuta/Home/Home.tsx
+++ src/pages/platformFuta/Home/Home.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './Home.module.css';
 import bgVideo from '../../../assets/futa/landingPageCropped.mp4';
 import bgVideoTrimmed from '../../../assets/futa/landingPageTrimmed.mp4';
+import futaIntro from '../../../assets/futa/futaIntro.mp4';
+import futaTrimmed from '../../../assets/futa/futaIntroCropped.mp4';
 import { Link } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
+import useMediaQuery from '../../../utils/hooks/useMediaQuery';
 
 const containerVariants = {
     hidden: { opacity: 0 },
@@ -27,68 +30,111 @@ interface PropsIF {
 }
 
 function Home(props: PropsIF) {
+    const showMobileVersion = useMediaQuery('(max-width: 560px)');
+
     const { hasVideoPlayedOnce, setHasVideoPlayedOnce } = props;
     const videoRef = useRef<HTMLVideoElement>(null);
+    const videoRefDesktop = useRef<HTMLVideoElement>(null);
+    const [isActionButtonVisible, setIsActionButtonVisible] = useState(false);
 
+    const videoRefToUse = showMobileVersion ? videoRef : videoRefDesktop;
+
+    const handleTimeUpdate = useCallback(() => {
+        if (
+            videoRefToUse.current &&
+            videoRefToUse.current.currentTime >= (showMobileVersion ? 15 : 11)
+        ) {
+            setHasVideoPlayedOnce(true);
+        }
+    }, [setHasVideoPlayedOnce]);
     useEffect(() => {
-        const handleTimeUpdate = () => {
-            if (videoRef.current && videoRef.current.currentTime >= 15) {
-                setHasVideoPlayedOnce(true);
-            }
-        };
-
-        if (videoRef.current) {
-            videoRef.current.addEventListener('timeupdate', handleTimeUpdate);
+        if (videoRefToUse.current) {
+            videoRefToUse.current.addEventListener(
+                'timeupdate',
+                handleTimeUpdate,
+            );
 
             return () => {
-                if (videoRef.current) {
-                    videoRef.current.removeEventListener(
+                videoRefToUse.current &&
+                    videoRefToUse.current.removeEventListener(
                         'timeupdate',
                         handleTimeUpdate,
                     );
-                }
             };
         }
-    }, [setHasVideoPlayedOnce]);
+    }, [handleTimeUpdate]);
+
+    useEffect(() => {
+        if (hasVideoPlayedOnce) setIsActionButtonVisible(true);
+        const timer = setTimeout(() => {
+            setIsActionButtonVisible(true);
+        }, 11000);
+
+        return () => clearTimeout(timer);
+    }, [hasVideoPlayedOnce]);
+
+    const mobileVideo = !hasVideoPlayedOnce ? (
+        <video
+            ref={videoRef}
+            autoPlay
+            muted
+            loop
+            className={styles.backgroundVideo}
+        >
+            <source src={bgVideo} type='video/mp4' />
+            Your browser does not support the video tag.
+        </video>
+    ) : (
+        <video autoPlay muted loop className={styles.backgroundVideo}>
+            <source src={bgVideoTrimmed} type='video/mp4' />
+            Your browser does not support the video tag.
+        </video>
+    );
+
+    const desktopVideo = !hasVideoPlayedOnce ? (
+        <video
+            ref={videoRefDesktop}
+            autoPlay
+            muted
+            loop
+            className={styles.backgroundVideo}
+        >
+            <source src={futaIntro} type='video/mp4' />
+            Your browser does not support the video tag.
+        </video>
+    ) : (
+        <video autoPlay muted loop className={styles.backgroundVideo}>
+            <source src={futaTrimmed} type='video/mp4' />
+            Your browser does not support the video tag.
+        </video>
+    );
 
     return (
         <div className={styles.container}>
-            <h1>FUTA</h1>
-            {!hasVideoPlayedOnce ? (
-                <video
-                    ref={videoRef}
-                    autoPlay
-                    muted
-                    loop
-                    className={styles.backgroundVideo}
-                >
-                    <source src={bgVideo} type='video/mp4' />
-                    Your browser does not support the video tag.
-                </video>
-            ) : (
-                <video autoPlay muted loop className={styles.backgroundVideo}>
-                    <source src={bgVideoTrimmed} type='video/mp4' />
-                    Your browser does not support the video tag.
-                </video>
-            )}
+            {showMobileVersion ? mobileVideo : desktopVideo}
             <AnimatePresence>
-                <motion.div
-                    className={styles.content}
-                    initial='hidden'
-                    animate='visible'
-                    variants={containerVariants}
-                >
-                    <motion.div variants={itemVariants}>
-                        <Link to='/auctions' className={styles.exploreButton}>
-                            Explore
-                        </Link>
+                {isActionButtonVisible && (
+                    <motion.div
+                        className={styles.content}
+                        initial='hidden'
+                        animate='visible'
+                        variants={containerVariants}
+                    >
+                        <motion.div variants={itemVariants}>
+                            <Link
+                                to='/auctions'
+                                className={styles.exploreButton}
+                            >
+                                Explore
+                            </Link>
+                        </motion.div>
+                        <motion.div variants={itemVariants}>
+                            <Link to='/learn' className={styles.learnButton}>
+                                Learn
+                            </Link>
+                        </motion.div>
                     </motion.div>
-                    <motion.div variants={itemVariants}>
-                        <Link to='/learn' className={styles.learnButton}>
-                            Learn
-                        </Link>
-                    </motion.div>
-                </motion.div>
+                )}
             </AnimatePresence>
         </div>
     );
