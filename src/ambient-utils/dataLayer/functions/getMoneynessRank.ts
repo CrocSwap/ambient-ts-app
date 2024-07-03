0827d82adaa8b790273c73bdc04696239af029e5
Ben Wolski <benwolski@gmail.com>
2024-07-02 20:19:24 -0400

    add goonUSD to moneyness scale

--- src/ambient-utils/dataLayer/functions/getMoneynessRank.ts
+++ src/ambient-utils/dataLayer/functions/getMoneynessRank.ts
@@ -29,6 +29,7 @@ export const getMoneynessRank = (tokenSymbol: string): number => {
         LUSD: 95,
         USDPLUS: 95,
         USDE: 95,
+        GOONUSD: 95,
         DAI: 90,
         USDT: 80,
         FRAX: 70,
