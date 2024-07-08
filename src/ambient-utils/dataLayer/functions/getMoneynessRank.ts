69354558feed257c2b11a3806382981905f04c12
Ben Wolski <benwolski@gmail.com>
2024-07-08 09:21:23 -0400

    decrease moneyness of sUSDe relative to USDe

--- src/ambient-utils/dataLayer/functions/getMoneynessRank.ts
+++ src/ambient-utils/dataLayer/functions/getMoneynessRank.ts
@@ -27,7 +27,7 @@ export const getMoneynessRank = (tokenSymbol: string): number => {
         LUSD: 95,
         USDPLUS: 95,
         USDE: 95,
-        SUSDE: 95,
+        SUSDE: 90,
         DAI: 90,
         USDT: 80,
         FRAX: 70,
