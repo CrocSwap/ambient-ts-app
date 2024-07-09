173630ab909c50b79d5288c84c1a116d7fef0dd5
Ben Wolski <benwolski@gmail.com>
2024-07-09 16:00:28 -0400

    add rpcNodeStatus to ChainDataContext

--- src/components/Futa/Footer/DesktopFooter.tsx
+++ src/components/Futa/Footer/DesktopFooter.tsx
@@ -4,10 +4,8 @@ import styles from './Footer.module.css';
 import { ChainDataContext } from '../../../contexts/ChainDataContext';
 import { getFormattedNumber } from '../../../ambient-utils/dataLayer';
 export default function DesktopFooter() {
-    // const isStatusPositive = true;
-
     const { chainData } = useContext(TradeDataContext);
-    const { nativeTokenUsdPrice, lastBlockNumber } =
+    const { nativeTokenUsdPrice, lastBlockNumber, rpcNodeStatus } =
         useContext(ChainDataContext);
 
     if (location.pathname === '/') return null;
@@ -25,6 +23,9 @@ export default function DesktopFooter() {
             <div className={styles.leftContainer}>
                 <p className={styles.price}>
                     ETH PRICE: {nativeTokenPriceFormatted}
+                </p>
+                <p className={styles.price}>
+                    RPC STATUS: {rpcNodeStatus.toString().toUpperCase()}
                 </p>
                 <p className={styles.blockNumber}>{lastBlockNumber}</p>
             </div>
