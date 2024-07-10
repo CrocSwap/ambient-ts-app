7169dc64e95ce7f3205630c67f64dd12556d5bf2
Jadessu <junioradessu@gmail.com>
2024-07-09 19:50:35 -0500

    remove swap top

--- src/pages/platformFuta/SwapFuta/SwapFuta.tsx
+++ src/pages/platformFuta/SwapFuta/SwapFuta.tsx
@@ -1,9 +1,16 @@
 import Swap from '../../platformAmbient/Trade/Swap/Swap';
+import FlashingSvg from '../Home/Animations/FlashingSvg';
+import TerminalAnimation from '../Home/Animations/TerminalAnimation';
 import styles from './SwapFuta.module.css';
+
+import logo from '../../../assets/futa/logos/homeLogo.svg';
+
 function SwapFuta() {
     return (
         <section className={styles.mainSection}>
-            <div className={styles.chartSection}></div>
+            <div className={styles.chartSection}>
+                <img src={logo} alt='' />
+            </div>
             <Swap isOnTradeRoute />
         </section>
     );
