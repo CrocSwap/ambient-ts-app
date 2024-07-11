e367cbc165e424ddeedb565c49bf37283b3ed14a
Ben Wolski <benwolski@gmail.com>
2024-07-11 14:40:25 -0400

    update comment section verbiage

--- src/components/Futa/Comments/Comments.tsx
+++ src/components/Futa/Comments/Comments.tsx
@@ -56,7 +56,8 @@ function Comments() {
         getMsgWithRestWithPagination,
     } = useCommentsWS(room ? room : '', fetchListener, '');
     const [userId, setUserId] = useState('');
-    const { userAddress, ensName } = useContext(UserDataContext);
+    const { userAddress, ensName, isUserConnected } =
+        useContext(UserDataContext);
     const { selectedNetwork } = useContext(CrocEnvContext);
     const { saveUser } = useChatApi();
 
@@ -199,8 +200,9 @@ function Comments() {
                     >
                         {messages.length == 0 ? (
                             <span className={styles.no_comment_section}>
-                                {' '}
-                                No comment for this ticker
+                                {isUserConnected
+                                    ? `Start the ${ticker} comment thread below`
+                                    : `Connect your wallet to comment on ${ticker}`}
                             </span>
                         ) : (
                             <>
