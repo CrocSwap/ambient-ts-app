077d0ebe2e2a89c2ad49d28bf55a7fd8d625c318
Tuğçenur Coşkun <tcoskun@proven.com.tr>
2024-07-22 16:28:22 +0300

    fixed scatter chart flickering issue

--- src/pages/platformAmbient/Chart/ChartUtils/chartUtils.ts
+++ src/pages/platformAmbient/Chart/ChartUtils/chartUtils.ts
@@ -172,8 +172,6 @@ export function setCanvasResolution(canvas: HTMLCanvasElement) {
         canvas.width = width * ratio;
         canvas.height = height * ratio;
 
-        canvas.style.width = width + 'px';
-        canvas.style.height = height + 'px';
         context.scale(ratio, ratio);
     }
 }
