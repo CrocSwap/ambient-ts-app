ad1f56967e034f77e59224b823403e8079498f45
Jadessu <junioradessu@gmail.com>
2024-08-12 16:54:55 -0500

    remove mobile double scrolls

--- src/styled/Components/Analytics.ts
+++ src/styled/Components/Analytics.ts
@@ -25,13 +25,8 @@ export const ScrollableContainer = styled.div`
 `;
 
 export const ShadowBox = styled.div`
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-        0 2px 4px -1px rgba(0, 0, 0, 0.06);
-    border-radius: 0.375rem;
     background-color: var(--dark1);
     height: 100%;
-
-    padding: 0 1rem;
 `;
 
 export const PoolDisplayContainer = styled.section`
