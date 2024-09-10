6f785a4c41bddd6ef78f479e8c2a16a456e58fcd
mhsmith321 <emily.drake.smith@gmail.com>
2024-09-10 18:27:00 -0400

    add logic to `CSSDebug.tsx` to initialize color picker on a given color

--- src/pages/common/CSSDebug/CSSDebug.tsx
+++ src/pages/common/CSSDebug/CSSDebug.tsx
@@ -7,14 +7,18 @@ export default function CSSDebug() {
     function toggleColorPicker(): void {
         setShowColorPicker(!showColorPicker);
     }
+    function getCSSCustomPropertyValue (property: string): string {
+        const root = document.documentElement;
+        const value = getComputedStyle(root).getPropertyValue(property).trim();
+        return value || '';
+    };
+
     return (
         <>
             <button onClick={() => document.documentElement.style.setProperty('--text1', 'green')}>Green!</button>
             <button onClick={() => toggleColorPicker()}>Show Picker</button>
             {showColorPicker && <SketchPicker
-                // color={
-                //     selectedColorObj.selectedColor
-                // }
+                color={getCSSCustomPropertyValue('--text1')}
                 // width={'170px'}
                 // onChange={(
                 //     color,
