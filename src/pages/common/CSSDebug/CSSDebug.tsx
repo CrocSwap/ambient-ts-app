0b901eea3ee222fa7c0393ffb1b450fc1b35b01d
mhsmith321 <emily.drake.smith@gmail.com>
2024-09-10 18:10:40 -0400

    render color picker in DOM on user click

--- src/pages/common/CSSDebug/CSSDebug.tsx
+++ src/pages/common/CSSDebug/CSSDebug.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import { SketchPicker } from 'react-color';
 import Swap from '../../platformAmbient/Swap/Swap';
 
 export default function CSSDebug() {
+    const [showColorPicker, setShowColorPicker] = useState<boolean>(false);
+    function toggleColorPicker(): void {
+        setShowColorPicker(!showColorPicker);
+    }
     return (
         <>
-            
+            <button onClick={() => toggleColorPicker()}>Show Picker</button>
+            {showColorPicker && <SketchPicker
+                // color={
+                //     selectedColorObj.selectedColor
+                // }
+                // width={'170px'}
+                // onChange={(
+                //     color,
+                //     event,
+                // ) => {
+                //     event.stopPropagation();
+                //     item.action(
+                //         selectedColorObj.replaceSelector,
+                //         color,
+                //     );
+                // }}
+            />}
             <Swap />
         </>
     );
