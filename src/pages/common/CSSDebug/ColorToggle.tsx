c56df4e396c79ec387d585ba1af2ceb745c347ae
mhsmith321 <emily.drake.smith@gmail.com>
2024-09-24 11:14:28 -0400

    run CSS debug state management logic in DOM

--- src/pages/common/CSSDebug/ColorToggle.tsx
+++ src/pages/common/CSSDebug/ColorToggle.tsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { ColorResult, SketchPicker } from 'react-color';
 import styles from './ColorToggle.module.css';
 import { allColorsIF, cssColorIF } from './CSSDebug';
 import { RxReset } from 'react-icons/rx';
+import { UserPreferenceContext } from '../../../contexts';
 
 interface propsIF {
     cssProperty: cssColorIF;
@@ -13,15 +14,19 @@ interface propsIF {
 export default function ColorToggle(props: propsIF) {
     const { cssProperty, sampleText, allColors } = props;
 
+    const { cssDebug } = useContext(UserPreferenceContext);
+
     const [color, setColor] = useState<string>(getCssCustomPropertyValue(cssProperty.name));
 
     function getCssCustomPropertyValue(p: string): string {
+        const cached: string|undefined = cssDebug.check(p);
         const root: HTMLElement = document.documentElement;
         const value: string = getComputedStyle(root).getPropertyValue(p).trim();
-        return value;
+        return cached ?? value;
     }
 
     function handleChange(c: string): void {
+        cssDebug.cache(cssProperty.name, c);
         setColor(c);
         document.documentElement.style.setProperty(cssProperty.name, c);
     }
