42ae4f67f5686612c67c3f1b402d687e328816ce
Jadessu <junioradessu@gmail.com>
2024-10-07 12:16:39 -0500

    create skeleton for pools

---
+++ src/components/Global/Explore/PoolRow/PoolRowSkeleton.tsx
@@ -0,0 +1,61 @@
+import useMediaQuery from '../../../../utils/hooks/useMediaQuery';
+import styles from './PoolRow.module.css';
+
+export default function PoolRowSkeleton() {
+    const tempItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    const desktopView = useMediaQuery('(min-width: 768px)');
+
+    const displayItems = [
+        !desktopView
+            ? null
+            : {
+                  element: <div className={styles.tokenIcon}>...</div>,
+              },
+        {
+            element: <div>...</div>,
+
+            classname: styles.poolName,
+        },
+        {
+            element: <div>...</div>,
+        },
+
+        {
+            element: <div>...</div>,
+        },
+        {
+            element: <div>...</div>,
+        },
+        desktopView
+            ? {
+                  element: <div>...</div>,
+              }
+            : null,
+        {
+            element: <div>...</div>,
+        },
+        {
+            element: <div>...</div>,
+
+            classname: styles.tradeButton,
+        },
+    ];
+
+    return (
+        
+        <div className={styles.gridContainer}>
+            {displayItems
+                .filter((item) => item !== null) // Filter out null values
+                .map((item, idx) => (
+                    <div
+                        key={idx}
+                        className={`${styles.gridItem} ${item?.classname} ${styles.skeletonItem}`}
+                        // style={{background: 'yellow'}}
+                    >
+                        {item?.element}
+                    </div>
+                ))}
+        </div>
+    );
+}
