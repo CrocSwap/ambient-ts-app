39459b8c96a3e614d126a4e6136414b78e17e428
Ridvan Altunel <raltunel@proven.com.tr>
2024-10-09 14:30:45 +0300

    disable table opacity effect to test on iphone

--- src/components/Trade/TradeTabs/TableRowsInfiniteScroll.tsx
+++ src/components/Trade/TradeTabs/TableRowsInfiniteScroll.tsx
@@ -145,11 +145,11 @@ function TableRowsInfiniteScroll({
 
     const bindTableReadyState = (newState: boolean) => {
         if(newState === true){
-            setTransactionTableOpacity('1');
+            // setTransactionTableOpacity('1');
             setIsTableReady(true);
         }
         else{
-            setTransactionTableOpacity('.5');
+            // setTransactionTableOpacity('.5');
             setIsTableReady(false);
         }
     }
@@ -174,11 +174,11 @@ function TableRowsInfiniteScroll({
         }
     };
     
-    const setTransactionTableOpacity = (val: string) => {
-        if(wrapperEl){
-            wrapperEl.style.opacity = val;
-        }
-    };
+    // const setTransactionTableOpacity = (val: string) => {
+    //     if(wrapperEl){
+    //         wrapperEl.style.opacity = val;
+    //     }
+    // };
 
 
     const triggerAutoScroll = (
