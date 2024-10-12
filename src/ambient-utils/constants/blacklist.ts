adc351cabc2162b5c1c5e630ae4a17b1509339bb
Ben Wolski <benwolski@gmail.com>
2024-10-12 12:59:26 -0400

    annotated new excluded SCR token

--- src/ambient-utils/constants/blacklist.ts
+++ src/ambient-utils/constants/blacklist.ts
@@ -163,7 +163,7 @@ export const checkBlacklist = (addr: string) => {
 };
 
 export const excludedTokenAddresses = [
-    '0xd294412741ee08aa3a35ac179ff0b4d9d7fefb27',
+    '0xd294412741ee08aa3a35ac179ff0b4d9d7fefb27', // fake SCR
 ];
 
 // if blacklist is not already lowercase
