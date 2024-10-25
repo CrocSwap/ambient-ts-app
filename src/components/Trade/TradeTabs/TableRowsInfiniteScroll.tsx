6f384984f10f97e2c3170a22cda9f3d6e8a7dbaf
Ridvan Altunel <raltunel@proven.com.tr>
2024-10-25 16:42:46 +0300

    enable debugging

--- src/components/Trade/TradeTabs/TableRowsInfiniteScroll.tsx
+++ src/components/Trade/TradeTabs/TableRowsInfiniteScroll.tsx
@@ -94,7 +94,7 @@ function TableRowsInfiniteScroll({
     //     'div:nth-child(2)';
     const txSpanSelectorForBindMethod =  'div[data-label="hidden-id"]';
 
-    const debugMode = false;
+    const debugMode = true;
     const markRows = false;
     const[manualMode, setManualMode] = useState(false);
     const manualModeRef = useRef<boolean>();
