55760eda980a573a7f98b720335bf6aae178621e
Rıdvan Altunel <ridvan.altunel@gmail.com>
2024-10-28 14:39:07 +0300

    add timeout for only reload after network change

--- src/App/hooks/useAppChain.ts
+++ src/App/hooks/useAppChain.ts
@@ -246,7 +246,9 @@ export const useAppChain = (): {
             console.log('>>> choose network > default');
                 linkGenCurrent.navigate();
         }
-        //     window.location.reload();
+        setTimeout(() => {
+            window.location.reload();
+        }, 100);
     }
 
     // data from the SDK about the current chain in the connected wallet
