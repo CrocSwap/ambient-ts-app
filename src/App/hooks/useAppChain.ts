6fca67b6db775357bed13cf4463e9c8ffd69d0aa
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-04 19:26:49 -0500

    remove unused webpack import from `useAppChain.ts` file

--- src/App/hooks/useAppChain.ts
+++ src/App/hooks/useAppChain.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ChainSpec } from '@crocswap-libs/sdk';
 import { lookupChain } from '@crocswap-libs/sdk/dist/context';
 import { useWeb3ModalAccount, useSwitchNetwork } from '@web3modal/ethers/react';
