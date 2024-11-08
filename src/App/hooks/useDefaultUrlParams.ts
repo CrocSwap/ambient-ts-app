7d0f4ac01f2773b051e57675934c8e38278c1bb8
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-08 17:32:16 -0500

    delete unused `useDefaultUrlParams.ts` hook

--- src/App/hooks/useDefaultUrlParams.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { CrocEnvContext } from '../../contexts/CrocEnvContext';
-
-const useDefaultUrlParams = () => {
-    const { defaultUrlParams } = useContext(CrocEnvContext);
-    return defaultUrlParams;
-};
-
-export default useDefaultUrlParams;
