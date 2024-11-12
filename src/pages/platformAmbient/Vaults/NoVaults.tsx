8823854c69002e5260920846cd54509510dbb07d
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-12 13:13:45 -0500

    generate nav buttons in `<NoVaults />` programmatically

--- src/pages/platformAmbient/Vaults/NoVaults.tsx
+++ src/pages/platformAmbient/Vaults/NoVaults.tsx
@@ -1,12 +1,10 @@
+import { useContext } from 'react';
 import styles from './NoVaults.module.css';
 import Button from '../../../components/Form/Button';
 import { AppStateContext } from '../../../contexts';
-import { useContext, useEffect } from 'react';
-import {
-    scrollMainnet,
-    vaultSupportedNetworks,
-} from '../../../ambient-utils/constants';
+import { vaultSupportedNetworks } from '../../../ambient-utils/constants';
 import { useSwitchNetwork, useWeb3ModalAccount } from '@web3modal/ethers/react';
+import { NetworkIF } from '../../../ambient-utils/types';
 
 export default function NoVaults() {
     const { chooseNetwork } = useContext(AppStateContext);
@@ -14,26 +12,29 @@ export default function NoVaults() {
     const { isConnected } = useWeb3ModalAccount();
     const { switchNetwork } = useSwitchNetwork();
 
-    async function goToScroll(): Promise<void> {
+    async function changeNetwork(n: NetworkIF): Promise<void> {
         isConnected
-            ? await switchNetwork(parseInt(scrollMainnet.chainId))
-            : chooseNetwork(scrollMainnet);
+            ? await switchNetwork(parseInt(n.chainId))
+            : chooseNetwork(n);
     }
-
-    useEffect(() => {
-        console.log({ vaultSupportedNetworks });
-    }, [vaultSupportedNetworks]);
-
-    const BUTTON_DOM_ID = 'change_network_to_scroll';
 
     return (
         <div className={styles.no_vaults}>
             <h3>To use Vaults please change network to Scroll.</h3>
-            <Button
-                idForDOM={BUTTON_DOM_ID}
-                title='Change to Scroll'
-                action={() => goToScroll()}
-            />
+            {
+                Object.values(vaultSupportedNetworks).map(
+                    (vsn: NetworkIF) => {
+                        const KEY_SLUG = 'button_for_vaults_on_'
+                        return (<Button
+                            key={KEY_SLUG + JSON.stringify(vsn)}
+                            idForDOM={KEY_SLUG + vsn.displayName}
+                            title={`Change to ${vsn.displayName}`}
+                            action={() => changeNetwork(vsn)}
+                        />
+                        )
+                    }
+                )
+            }
         </div>
     );
 }
