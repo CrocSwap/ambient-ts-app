e2d31320f7548842be8197fab5a305212669a497
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-12 13:42:56 -0500

    separate `vaultServerIF` and `vaultIF` data interfaces

--- src/ambient-utils/types/vaults/vaultServerIF.ts
+++ src/ambient-utils/types/vaults/vaultServerIF.ts
@@ -1,28 +1,10 @@
+import { vaultIF } from './vaultIF';
+
 export interface vaultServerIF {
     code: number;
     message: 'Successfully';
     data: {
-        vaults: {
-            id: string,
-            createdAt: number,
-            updatedAt: number,
-            createdBy: string,
-            updatedBy: string,
-            chainId: number,
-            address: `0x${string}`,
-            strategy: string,
-            token0Address: `0x${string}`,
-            token1Address: `0x${string}`,
-            token0Decimals: number,
-            token1Decimals: number,
-            mainAsset: `0x${string}`,
-            initTime: number,
-            poolFee: string,
-            koSize: number,
-            tvlUsd: string,
-            tvlMainAsset: string,
-            apr: string
-        }[];
+        vaults: vaultIF[];
     };
     pagination: {
         totalItems: number;
