3d067bf8d0c5634763b42e9926859cdc9b10abc5
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-12 13:38:45 -0500

    expand `vaultServerIF` interface for full response shape

--- src/ambient-utils/types/vaults/vaultServerIF.ts
+++ src/ambient-utils/types/vaults/vaultServerIF.ts
@@ -1,21 +1,30 @@
 export interface vaultServerIF {
-    id: string,
-    createdAt: number,
-    updatedAt: number,
-    createdBy: string,
-    updatedBy: string,
-    chainId: number,
-    address: `0x${string}`,
-    strategy: string,
-    token0Address: `0x${string}`,
-    token1Address: `0x${string}`,
-    token0Decimals: number,
-    token1Decimals: number,
-    mainAsset: `0x${string}`,
-    initTime: number,
-    poolFee: string,
-    koSize: number,
-    tvlUsd: string,
-    tvlMainAsset: string,
-    apr: string
-}
+    code: number;
+    message: 'Successfully';
+    data: {
+        vaults: {
+            id: string,
+            createdAt: number,
+            updatedAt: number,
+            createdBy: string,
+            updatedBy: string,
+            chainId: number,
+            address: `0x${string}`,
+            strategy: string,
+            token0Address: `0x${string}`,
+            token1Address: `0x${string}`,
+            token0Decimals: number,
+            token1Decimals: number,
+            mainAsset: `0x${string}`,
+            initTime: number,
+            poolFee: string,
+            koSize: number,
+            tvlUsd: string,
+            tvlMainAsset: string,
+            apr: string
+        }[];
+    };
+    pagination: {
+        totalItems: number;
+    };
+};
