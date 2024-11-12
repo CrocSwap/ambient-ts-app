3cb1b27532a6cbc12bb3413f02ba7871c1786efd
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-12 13:25:34 -0500

    create `vaultServerIF` data interface

---
+++ src/ambient-utils/types/vaults/vaultServerIF.ts
@@ -0,0 +1,21 @@
+export interface VaultServerIF {
+    id: string,
+    createdAt: number,
+    updatedAt: number,
+    createdBy: string,
+    updatedBy: string,
+    chainId: number,
+    address: `0x${string}`,
+    strategy: string,
+    token0Address: `0x${string}`,
+    token1Address: `0x${string}`,
+    token0Decimals: number,
+    token1Decimals: number,
+    mainAsset: `0x${string}`,
+    initTime: number,
+    poolFee: string,
+    koSize: number,
+    tvlUsd: string,
+    tvlMainAsset: string,
+    apr: string
+}
