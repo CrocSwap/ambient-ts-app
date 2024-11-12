2597d21324950594435f2b8d0319ef4900b5bdda
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-12 13:57:45 -0500

    interface data handling in `Vaults.tsx` component

--- src/pages/platformAmbient/Vaults/Vaults.tsx
+++ src/pages/platformAmbient/Vaults/Vaults.tsx
@@ -5,7 +5,10 @@ import styles from './Vaults.module.css';
 import VaultRow from './VaultRow/VaultRow';
 
 function Vaults() {
-    const vaultData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // !important:  once we have mock data, change the type on this
+    // !important:  ... value to `vaultIF[]` and then fix linter
+    // !important:  ... warnings which manifest in response
+    const vaultData: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     
     const vaultHeader = (
         <div className={styles.vaultHeader}>
@@ -13,28 +16,33 @@ function Vaults() {
             <span className={styles.poolName}></span>
             <span className={styles.tvl}>TVL</span>
             <span className={styles.depositContainer}>
-                 My Deposit
+                My Deposit
             </span>
             <span className={styles.apyDisplay}>APY</span>
             <span className={styles.actionButtonContainer} />
         </div>
     );
-  
 
     return (
         <div data-testid={'portfolio'} className={styles.container}>
             <div className={styles.content}>
                 <h3 className={styles.mainTitle}>Vaults</h3>
-            {vaultHeader}
+                {vaultHeader}
                 <div className={`${styles.scrollableContainer} custom_scroll_ambient`}>
-
-
-            {vaultData.map((data, idx) => (
-                <VaultRow key={idx} />
-            ))}
+                    {
+                        vaultData.map((vault: number) => {
+                            const KEY_SLUG = 'vault_row_';
+                            return (
+                                <VaultRow
+                                    key={KEY_SLUG + JSON.stringify(vault)}
+                                    idForDOM={KEY_SLUG + vault.toString()}
+                                    vault={vault}
+                                />
+                            );
+                        })
+                    }
+                </div>
             </div>
-            </div>
-            
         </div>
     );
 }
