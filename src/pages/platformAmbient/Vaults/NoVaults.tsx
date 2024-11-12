30f9d7647393629b8a56f68407282cbedd3b277a
mhsmith321 <emily.drake.smith@gmail.com>
2024-11-12 12:07:43 -0500

    stub up content in `<NoVaults />` component

--- src/pages/platformAmbient/Vaults/NoVaults.tsx
+++ src/pages/platformAmbient/Vaults/NoVaults.tsx
@@ -1,5 +1,17 @@
+import styles from './NoVaults.module.css';
+import Button from '../../../components/Form/Button';
+
 export default function NoVaults() {
+    const BUTTON_DOM_ID = 'change_network_to_scroll'
+
     return (
-        <h1>No vaults!</h1>
+        <div className={styles.no_vaults}>
+            <h3>To use Vaults please change network to Scroll.</h3>
+            <Button
+                idForDOM={BUTTON_DOM_ID}
+                title='Change to Scroll'
+                action={() => console.log('wowzers')}
+            />
+        </div>
     );
 }
