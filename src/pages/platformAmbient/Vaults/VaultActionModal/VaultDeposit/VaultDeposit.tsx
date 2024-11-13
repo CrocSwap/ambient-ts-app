6978a60254cf565864bdce7f2b677b56ed55dfdd
Jadessu <junioradessu@gmail.com>
2024-11-13 11:00:58 -0600

    add zap toggle to deposit

--- src/pages/platformAmbient/Vaults/VaultActionModal/VaultDeposit/VaultDeposit.tsx
+++ src/pages/platformAmbient/Vaults/VaultActionModal/VaultDeposit/VaultDeposit.tsx
@@ -38,6 +38,7 @@ import {
     isTransactionReplacedError,
     isTransactionFailedError,
 } from '../../../../../utils/TransactionError';
+import Toggle from '../../../../../components/Form/Toggle';
 
 interface Props {
     mainAsset: TokenIF;
@@ -53,6 +54,8 @@ export default function VaultDeposit(props: Props) {
         string | undefined
     >();
     const [displayValue, setDisplayValue] = useState<string>('');
+    const [displayValueSecondary, setDisplayValueSecondary] =
+        useState<string>('');
     const [depositBigint, setDepositBigint] = useState<bigint | undefined>();
     const [mainAssetPrice, setMainAssetPrice] = useState<number | undefined>();
     const [mainAssetBalanceBigint, setMainAssetBalanceBigint] = useState<
@@ -316,57 +319,83 @@ export default function VaultDeposit(props: Props) {
                 isDexSelected={false}
                 onToggleDex={() => console.log('handleToggleDex')}
                 onMaxButtonClick={() =>
-                    handleTokenInputEvent(walletBalanceDisplay)
+                    handleTokenInputEvent(walletBalanceDisplay, 'main')
                 }
             />
         </>
     );
-    const handleTokenInputEvent = (input: string) => {
-        setDisplayValue(input);
+    const handleTokenInputEvent = (
+        input: string,
+        type: 'main' | 'secondary',
+    ) => {
+        if (type === 'main') {
+            setDisplayValue(input);
+        } else {
+            setDisplayValueSecondary(input);
+        }
+
         if (input === '') {
             setDepositBigint(undefined);
             return;
         }
+
         const inputBigint = BigInt(
             parseFloat(input) * 10 ** mainAsset.decimals,
         );
         setDepositBigint(inputBigint);
     };
+
     const token = mainAsset;
 
-    const fieldId = 'vault_deposit_input';
-    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const onChange = (
+        event: ChangeEvent<HTMLInputElement>,
+        type: 'main' | 'secondary',
+    ) => {
         let inputStringNoCommas = event.target.value
             .replace(/,/g, '.') // Replace commas with dots
             .replace(/\s+/g, ''); // Remove any spaces
 
         if (inputStringNoCommas === '.') inputStringNoCommas = '0.';
 
-        const inputStringNoUnfinishedExponent = isNaN(+inputStringNoCommas)
-            ? inputStringNoCommas.replace(
+        // Handle different logic based on 'main' or 'secondary'
+        const adjustedString =
+            type === 'secondary'
+                ? inputStringNoCommas.replace(/\./g, ',') // Example modification
+                : inputStringNoCommas;
+
+        const inputStringNoUnfinishedExponent = isNaN(+adjustedString)
+            ? adjustedString.replace(
                   /e[+-]?(?!\d)/gi, // Match 'e', 'e-' or 'e+' only if NOT followed by a number
                   '',
               )
-            : inputStringNoCommas;
+            : adjustedString;
 
         const isPrecisionGreaterThanDecimals =
-            precisionOfInput(inputStringNoCommas) > token.decimals;
+            precisionOfInput(adjustedString) > token.decimals;
 
         if (
             !isPrecisionGreaterThanDecimals &&
             !isNaN(+inputStringNoUnfinishedExponent)
         ) {
-            handleTokenInputEvent(inputStringNoCommas);
+            handleTokenInputEvent(adjustedString, type); // Call the unified function with `type`
         }
     };
 
-    const input = (
+    const renderInput = (type: 'main' | 'secondary') => (
         <input
             className={styles.tokenQuantityInput}
-            id={fieldId ? `${fieldId}_qty` : undefined}
-            placeholder={showConfirmation ? displayValue : '0.0'}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => onChange(event)}
-            value={displayValue}
+            id={type === 'main' ? 'vault_main_qty' : 'vault_secondary_qty'}
+            placeholder={
+                showConfirmation
+                    ? type === 'main'
+                        ? displayValue
+                        : displayValueSecondary
+                    : '0.0'
+            }
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                onChange(event, type)
+            }
+            value={type === 'main' ? displayValue : displayValueSecondary}
             type='string'
             step='any'
             inputMode='decimal'
@@ -377,96 +406,76 @@ export default function VaultDeposit(props: Props) {
             disabled={showConfirmation}
         />
     );
-
-    const tokenSymbol =
-        token?.symbol?.length > 4 ? (
+    // ---------------------------------------------------
+    
+    const renderTokenSymbol = (symbol?: string) => (
+        symbol && symbol.length > 4 ? (
             <DefaultTooltip
-                title={token.symbol}
-                placement={'top'}
-                arrow
-                enterDelay={700}
-                leaveDelay={200}
+            title={symbol}
+            placement={'top'}
+            arrow
+            enterDelay={700}
+            leaveDelay={200}
             >
-                <>{token.symbol}</>
+                <>{symbol}</>
             </DefaultTooltip>
         ) : (
-            <>{token.symbol}</>
-        );
-
+            <>{symbol}</>
+        )
+    );
+    
+    // ---------------------------------------------------
     const tokenSelectRef = useRef(null);
-
-    const tokenSelectButton = (
+    const renderTokenSelectButton = (
+        id: string,
+        token: TokenIF,  
+        tokenSymbol: React.ReactNode
+    ) => (
         <button
-            className={`${styles.tokenSelectButton} ${styles.justDisplay}
-            }`}
-            id={fieldId ? `${fieldId}_token_selector` : undefined}
-            onClick={undefined}
-            tabIndex={0}
-            aria-label='Open swap sell token modal.'
-            ref={tokenSelectRef}
-            style={{
-                borderRadius: '50px',
-            }}
+        className={`${styles.tokenSelectButton} ${styles.justDisplay}`}
+        id={id}
+        onClick={undefined}
+        tabIndex={0}
+        aria-label='Open swap sell token modal.'
+        ref={tokenSelectRef}
+        style={{
+            borderRadius: '50px',
+        }}
         >
             <TokenIcon
                 token={token}
                 src={uriToHttp(token.logoURI)}
                 alt={token.symbol}
                 size='2xl'
-            />
+                />
             {tokenSymbol}
         </button>
     );
+    // ---------------------------------------------------
+    
+    const inputMain = renderInput('main');
+    const inputSecondary = renderInput('secondary');
 
-    // const extraInfo = [
-    //     {
-    //         title: 'Expected Output',
-    //         tooltipTitle:
-    //             'Expected Conversion Rate After Price Impact and Provider Fee',
-    //         data: '...',
-    //         placement: 'bottom',
-    //     },
-    //     {
-    //         title: 'Price Impact',
-    //         tooltipTitle: 'Expected Pool Price After Swap',
-    //         data: '...',
-    //         placement: 'bottom',
-    //     },
-    //     {
-    //         title: 'Slippage Tolerance',
-    //         tooltipTitle: 'Expected Pool Price After Swap',
-    //         data: '...',
-    //         placement: 'bottom',
-    //     },
-    //     {
-    //         title: 'Liquidity Provider Fee',
-    //         tooltipTitle: 'Expected Pool Price After Swap',
-    //         data: '...',
-    //         placement: 'bottom',
-    //     },
-    // ];
+    
+    const tokenSymbolMain = renderTokenSymbol(mainAsset?.symbol);
+    const tokenSymbolSecondary = renderTokenSymbol(secondaryAsset?.symbol);
+    
 
-    // const confirmationDetails = (
-    //     <div className={styles.confContainer}>
+ 
+    const tokenSelectButtonMain = renderTokenSelectButton(
+        'main_vault_deposit_token_selector',
+        mainAsset,
+        tokenSymbolMain
+    );
+    
+    const tokenSelectButtonSecondary = renderTokenSelectButton(
+        'secondary_vault_deposit_token_selector',
+        secondaryAsset,
+        tokenSymbolSecondary
+    );
+    
 
-    //     <div className={styles.confDetails}>
-    //         <div className={styles.confRow}>
-    //             <p>Current Price</p>
-    //             <p>1,690</p>
-    //         </div>
-    //         <div className={styles.confRow}>
-    //             <p>Price Limit</p>
-    //             <p>1,690</p>
-    //         </div>
-    //         <div className={styles.confRow}>
-    //             <p>Slippage</p>
-    //             <p>0.3%</p>
-    //         </div>
-    //         </div>
 
-    //         <p>Output is estimated. You will swap up to 1.00ETH for USDC. You may swap less than 1.00ETH if the price moves beyond the limit shown above. You can increase the likelihood of swapping the full amound by increasing your slippage tolerance in the settings.</p>
-    //     </div>
-    // );
     const submittedButtonTitle = (
         <div className={styles.loading}>
             Submitting
@@ -477,6 +486,7 @@ export default function VaultDeposit(props: Props) {
     const displayMinDeposit = minDepositBigint
         ? toDisplayQty(minDepositBigint, mainAsset.decimals)
         : '…';
+    const [isZapOn, setIsZapOn] = useState(true);
 
     const includeWallet = true;
     return (
@@ -491,16 +501,42 @@ export default function VaultDeposit(props: Props) {
                 <p
                     className={styles.disclaimer}
                 >{`This deposit function uses "Zap Mode" to convert ${props.mainAsset.symbol} deposit into vault position which holds both ${props.mainAsset.symbol} and ${props.mainAsset.symbol}. The value of a vault deposit will fluctuate with the value of both these tokens and their exchange rate.`}</p>
+
+                <div className={styles.zapContainer}>
+                    <p>Zap In</p>
+
+                    <Toggle
+                        isOn={isZapOn}
+                        handleToggle={() => setIsZapOn(!isZapOn)}
+                        Width={36}
+                        id='zap_toggle_vault_deposit'
+                        disabled={false}
+                    />
+                </div>
                 <div className={styles.content}>
                     <div
                         className={styles.tokenQuantityContainer}
                         style={{ marginBottom: !includeWallet ? '8px' : '0' }}
                     >
-                        {input}
-                        {tokenSelectButton}
+                        {inputMain}
+                        {tokenSelectButtonMain}
                     </div>
                     {includeWallet && !showConfirmation && walletContent}
                 </div>
+                {!isZapOn && (
+                    <div className={styles.content}>
+                        <div
+                            className={styles.tokenQuantityContainer}
+                            style={{
+                                marginBottom: !includeWallet ? '8px' : '0',
+                            }}
+                        >
+                            {inputSecondary}
+                            {tokenSelectButtonSecondary}
+                        </div>
+                        {includeWallet && !showConfirmation && walletContent}
+                    </div>
+                )}
 
                 <div className={styles.gas_row}>
                     <FaGasPump size={15} /> {depositGasPriceinDollars ?? '…'}
