e887b1af3f2fae8e88ba48fcafa1587fdf82359f
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-11-28 13:08:26 -0500

    add remaining network definitions

--- src/assets/branding/defaultBrandAssets.ts
+++ src/assets/branding/defaultBrandAssets.ts
@@ -1,3 +1,4 @@
+import baseLogo from '../../assets/images/logos/Base_Wordmark_White.svg';
 import blastLogo from '../../assets/images/logos/blast_logo.svg';
 import plumeLogo from '../../assets/images/logos/plume_brand_logo.svg';
 import scrollLogo from '../../assets/images/logos/scroll_brand_logo.svg';
@@ -63,6 +64,17 @@ export const defaultBrandAssets: brandIF = {
                 { content: 'ambient', processAs: 'text' },
                 { content: '×', processAs: 'separator' },
                 { content: swellLogo, processAs: 'image' },
+            ],
+        },
+        // base sepolia
+        '0x14a34': {
+            // first value in array is default color scheme
+            color: ['purple_dark', 'purple_light', 'futa_dark'],
+            premiumColor: [],
+            hero: [
+                { content: 'ambient', processAs: 'text' },
+                { content: '×', processAs: 'separator' },
+                { content: baseLogo, processAs: 'image' },
             ],
         },
         // blast sepolia
