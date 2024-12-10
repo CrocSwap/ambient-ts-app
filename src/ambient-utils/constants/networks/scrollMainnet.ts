e6f4d8d8863789aa85416ff35e0a84d5fd725b03
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-12-10 15:00:05 -0500

    update Top Pools and fallback vaults list

--- src/ambient-utils/constants/networks/scrollMainnet.ts
+++ src/ambient-utils/constants/networks/scrollMainnet.ts
@@ -5,10 +5,9 @@ import { NetworkIF } from '../../types/NetworkIF';
 import {
     scrollETH,
     scrollSCR,
+    scrollSTONE,
     scrollUSDC,
     scrollUSDT,
-    scrollWBTC,
-    scrollWrsETH,
 } from '../defaultTokens';
 import { GCGO_SCROLL_URL } from '../gcgo';
 import { TopPool } from './TopPool';
@@ -45,9 +44,9 @@ export const scrollMainnet: NetworkIF = {
     topPools: [
         new TopPool(scrollETH, scrollUSDC, chainSpecFromSDK.poolIndex),
         new TopPool(scrollSCR, scrollETH, chainSpecFromSDK.poolIndex),
+        new TopPool(scrollSTONE, scrollETH, chainSpecFromSDK.poolIndex),
         new TopPool(scrollETH, scrollUSDT, chainSpecFromSDK.poolIndex),
-        new TopPool(scrollETH, scrollWBTC, chainSpecFromSDK.poolIndex),
-        new TopPool(scrollWrsETH, scrollETH, chainSpecFromSDK.poolIndex),
+        new TopPool(scrollUSDT, scrollUSDC, chainSpecFromSDK.poolIndex),
     ],
     getGasPriceInGwei: async (provider?: Provider) => {
         if (!provider) return 0;
