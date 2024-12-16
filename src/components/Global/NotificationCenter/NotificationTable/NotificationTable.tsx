2cea2687f79ac229cd3e7d36f384de1cbe97d1c8
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-12-16 10:31:52 -0600

    update parsedReceipts name to sessionReceipts since no longer parsing

--- src/components/Global/NotificationCenter/NotificationTable/NotificationTable.tsx
+++ src/components/Global/NotificationCenter/NotificationTable/NotificationTable.tsx
@@ -30,9 +30,7 @@ const NotificationTable = (props: NotificationTableProps) => {
     const { resetReceiptData, transactionsByType, sessionReceipts } =
         useContext(ReceiptContext);
 
-    const parsedReceipts = sessionReceipts.map((receipt) => receipt);
-
-    const parsedReceiptsDisplay = parsedReceipts.map((receipt, idx) => (
+    const sessionReceiptsDisplay = sessionReceipts.map((receipt, idx) => (
         <ReceiptDisplay
             key={idx}
             status={receipt?.status === 1 ? 'successful' : 'failed'}
@@ -74,7 +72,7 @@ const NotificationTable = (props: NotificationTableProps) => {
 
                 <div className={styles.content}>
                     {pendingTransactionsDisplay}
-                    {parsedReceiptsDisplay}
+                    {sessionReceiptsDisplay}
                 </div>
 
                 <FlexContainer justifyContent='center' margin='auto'>
