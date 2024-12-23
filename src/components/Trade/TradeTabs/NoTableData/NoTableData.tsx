dbe6d08d95fb1059ce4deca98b36d9cd6da6cee5
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-12-23 11:09:37 -0500

    fix no transactions in candle prompts

--- src/components/Trade/TradeTabs/NoTableData/NoTableData.tsx
+++ src/components/Trade/TradeTabs/NoTableData/NoTableData.tsx
@@ -2,7 +2,7 @@ import styles from './NoTableData.module.css';
 // import{ AiFillFolderOpen } from 'react-icons/ai';
 import { Dispatch, memo, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { IS_LOCAL_ENV } from '../../../../ambient-utils/constants';
+import { CandleContext } from '../../../../contexts';
 import { TradeTableContext } from '../../../../contexts/TradeTableContext';
 
 interface NoTableDataPropsIF {
@@ -22,11 +22,11 @@ function NoTableData(props: NoTableDataPropsIF) {
     } = props;
 
     const { showAllData, setShowAllData } = useContext(TradeTableContext);
+    const { isCandleSelected, setIsCandleSelected } = useContext(CandleContext);
 
     const navigate = useNavigate();
 
     const toggleAllEnabled = () => {
-        IS_LOCAL_ENV && console.debug('setting show all to true');
         setShowAllData(true);
         setSelectedDate && setSelectedDate(undefined);
     };
@@ -46,31 +46,43 @@ function NoTableData(props: NoTableDataPropsIF) {
             !activeUserPositionsLength) ? null : (
             <>
                 <p>
-                    {activeUserPositionsLength
-                        ? `Click to view your ${type}`
-                        : activeUserPositionsByPoolLength
-                          ? `Click to view all your ${type}`
-                          : `Consider turning on all ${type}`}
+                    {isCandleSelected
+                        ? showAllData
+                            ? `Click to view all ${type}`
+                            : `Click to view your ${type}`
+                        : activeUserPositionsLength
+                          ? `Click to view your ${type}`
+                          : activeUserPositionsByPoolLength
+                            ? `Click to view all your ${type}`
+                            : `Consider turning on all ${type}`}
                 </p>
                 <button
                     onClick={
-                        activeUserPositionsByPoolLength
-                            ? () => setShowAllData(false)
-                            : activeUserPositionsLength
-                              ? navigateToLiquidityTabOnAccount
-                              : toggleAllEnabled
+                        isCandleSelected
+                            ? () => setIsCandleSelected(false)
+                            : activeUserPositionsByPoolLength
+                              ? () => setShowAllData(false)
+                              : activeUserPositionsLength
+                                ? navigateToLiquidityTabOnAccount
+                                : toggleAllEnabled
                     }
                 >
-                    {activeUserPositionsByPoolLength
-                        ? `My ${type}`
-                        : activeUserPositionsLength
-                          ? `All My ${type}`
-                          : `All ${type}`}
+                    {isCandleSelected
+                        ? showAllData
+                            ? `All ${type}`
+                            : `My ${type}`
+                        : activeUserPositionsByPoolLength
+                          ? `My ${type}`
+                          : activeUserPositionsLength
+                            ? `All My ${type}`
+                            : `All ${type}`}
                 </button>
             </>
         );
 
-    const message = activeUserPositionsByPoolLength ? (
+    const message = isCandleSelected ? (
+        'NO TRANSACTIONS FOUND'
+    ) : activeUserPositionsByPoolLength ? (
         `YOU HAVE ${activeUserPositionsByPoolLength} ${type === 'liquidity' ? (activeUserPositionsByPoolLength > 1 ? 'POSITIONS' : 'POSITION') : type === 'limits' ? (activeUserPositionsByPoolLength > 1 ? 'LIMITS' : 'LIMIT') : activeUserPositionsByPoolLength > 1 ? 'TRANSACTIONS' : 'TRANSACTION'} IN THIS POOL`
     ) : activeUserPositionsLength && activeUserPositionsLength > 0 ? (
         `YOU HAVE ${activeUserPositionsLength} ${type === 'liquidity' ? (activeUserPositionsLength > 1 ? 'POSITIONS' : 'POSITION') : type === 'limits' ? (activeUserPositionsLength > 1 ? 'LIMITS' : 'LIMIT') : activeUserPositionsLength > 1 ? 'TRANSACTIONS' : 'TRANSACTION'} IN OTHER POOLS`
