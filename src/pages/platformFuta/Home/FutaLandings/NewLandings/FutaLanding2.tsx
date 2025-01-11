328aac8cdff0a688971937563c54e98f334dab82
Jadessu <junioradessu@gmail.com>
2025-01-11 11:41:13 +0000

    rearrange sections

--- src/pages/platformFuta/Home/FutaLandings/NewLandings/FutaLanding2.tsx
+++ src/pages/platformFuta/Home/FutaLandings/NewLandings/FutaLanding2.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import circleSvg from './Circle.svg';
 import styles from './FutaLanding2.module.css';
 export default function FutaLanding2() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        // Function to update the width state
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove event listener
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
+                <p>{width > 1600 ? 'monitor' : 'laptop'}</p>
+                <h3> width: {width}</h3>
+
                 <h3>/FAIR AUCTIONS</h3>
 
                 <p>Tickers are auctioned over a set period.</p>
