16b5c113fb06d0c88f339372f720d51a1c2eefce
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-01-13 13:49:42 -0500

    comment all code in `useMediaQuery.ts` file

--- src/utils/hooks/useMediaQuery.ts
+++ src/utils/hooks/useMediaQuery.ts
@@ -3,6 +3,7 @@ import { maxWidth, minWidth } from '../../ambient-utils/types/mediaQueries';
 
 type centralQueries = maxWidth | minWidth;
 
+// all atomic screen range sizes and their query strings
 interface BaseMediaQueryStringsIF {
     isWidescreen: string;
     isDesktop: string;
@@ -12,23 +13,32 @@ interface BaseMediaQueryStringsIF {
     isCellphone: string;
 }
 
+// booleans for all atomic screen range sizes and union sizes
 export interface MediaQueryResultsIF
     extends Record<keyof BaseMediaQueryStringsIF, boolean> {
+    // isWidescreen || isDesktop || isLaptop
     isComputer: boolean;
+    // isTabletWide || isTabletNarrow || isCellphone
     isMobile: boolean;
 }
 
+// fn signature with no argument (will return all standard query results)
 export function useMediaQuery(): MediaQueryResultsIF;
+// fn signature with a query string provided (will return that query only)
 export function useMediaQuery(query: centralQueries | string): boolean;
 
+// overload function definition
 export function useMediaQuery(
     query?: centralQueries | string,
 ): boolean | MediaQueryResultsIF {
+    // fn to build a query string from template
     function makeQueryString(low: number, high?: number): string {
         let output = `(min-width: ${low.toString()}px)`;
         if (high) output += `and (max-width: ${high.toString()}px)`;
         return output;
     }
+
+    // all atomic media queries (does not have union data)
     const defaultQueries: BaseMediaQueryStringsIF = {
         isWidescreen: makeQueryString(1921),
         isDesktop: makeQueryString(1281, 1920),
@@ -38,31 +48,37 @@ export function useMediaQuery(
         isCellphone: makeQueryString(1, 480),
     };
 
+    // state variable holding data to output from the hook
     const [matches, setMatches] = useState<boolean | MediaQueryResultsIF>(() =>
         runQueries(query),
     );
 
-    const handleChange = () => {
-        setMatches(runQueries(query));
-    };
-
+    // fn to run media queries depending on data used to instantiate hook
     function runQueries(q?: string): boolean | MediaQueryResultsIF {
-        const getMatches = (query: string): boolean => {
+        // fn to run a given media query
+        function getMatches(query: string): boolean {
+            // return media query result if `window` obj exists
             if (typeof window !== 'undefined') {
                 return window.matchMedia(query).matches;
             }
+            // return `false` if window obj does not exist
             return false;
-        };
+        }
+        // output variable
         let output: boolean | MediaQueryResultsIF;
+        // if a query string is provided, process that query string only
+        // if no query string is provided, run all standard queries
         if (q) {
             output = getMatches(q);
         } else {
+            // result of all base (atomic) media queries
             const rawOutput = (
                 Object.keys(defaultQueries) as (keyof BaseMediaQueryStringsIF)[]
             ).reduce(function (acc, key) {
                 acc[key] = getMatches(defaultQueries[key]);
                 return acc;
             }, {} as MediaQueryResultsIF);
+            // deconstruct data from basic media inquiries
             const {
                 isWidescreen,
                 isDesktop,
@@ -71,16 +87,21 @@ export function useMediaQuery(
                 isTabletNarrow,
                 isCellphone,
             } = rawOutput;
-            const processedOutput = {
+            // decorate data from basic inquiries with union values
+            output = {
                 ...rawOutput,
                 isComputer: isWidescreen || isDesktop || isLaptop,
                 isMobile: isTabletWide || isTabletNarrow || isCellphone,
             };
-            output = processedOutput;
         }
+        // return output variable
         return output;
     }
 
+    // event listener for DOM
+    const handleChange = () => setMatches(runQueries(query));
+
+    // logic to handle event listener in DOM
     useEffect(() => {
         if (query) {
             const matchMedia = window.matchMedia(query);
@@ -126,5 +147,6 @@ export function useMediaQuery(
         }
     }, [query]);
 
+    // return data from queries for instantiation of the hook
     return matches;
 }
