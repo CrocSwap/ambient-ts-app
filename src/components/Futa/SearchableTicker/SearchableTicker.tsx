a57638c10ef9460469f041bd543bc8a62fb6f43b
tcoskun <tcoskun@proven.com.tr>
2025-01-21 10:32:25 +0300

    fixed : Account Page height for Futa design

--- src/components/Futa/SearchableTicker/SearchableTicker.tsx
+++ src/components/Futa/SearchableTicker/SearchableTicker.tsx
@@ -597,7 +597,7 @@ export default function SearchableTicker(props: propsIF) {
             className={styles.searchable_ticker}
             style={{
                 gridTemplateRows: isAccount ? 'auto 100%' : '',
-                height: isAccount ? '' : '100%',
+                height: '100%',
             }}
             ref={canvasRef}
         >
