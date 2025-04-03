6eebcf15ebacbc5ea2a72c19b12c1823eaaf0f10
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-04-03 19:33:16 -0400

    update fallback vaults list

--- src/ambient-utils/types/vaults/allVaultsServerIF.ts
+++ src/ambient-utils/types/vaults/allVaultsServerIF.ts
@@ -17,6 +17,8 @@ export interface AllVaultsServerIF {
     vaultDecimals: number;
     vaultShow: boolean;
     mainAsset: `0x${string}`;
+    performanceUnitTokenAddress: string;
+    rewardApr: string;
     aprUnitTokenAddress: string;
     aprShow: string;
     initTime: number;
