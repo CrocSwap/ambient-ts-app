7df7e272a21f0436e15d58ded2adf35ebba7235e
Jadessu <junioradessu@gmail.com>
2025-06-13 16:22:07 -0500

    add fast lane protection tooltip and fix z index issue

--- src/components/Global/FastLaneProtectionControl/FastLaneProtectionControl.tsx
+++ src/components/Global/FastLaneProtectionControl/FastLaneProtectionControl.tsx
@@ -1,6 +1,10 @@
-import { Dispatch, SetStateAction, useId } from 'react';
+import { Dispatch, SetStateAction, useContext, useId } from 'react';
 import Toggle from '../../Form/Toggle';
 import styles from './FastLaneProtectionControl.module.css';
+import { FlexContainer } from '../../../styled/Common';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+import { AppStateContext } from '../../../contexts';
+import { ExplanationButton } from '../../Form/Icons/Icons.styles';
 
 interface propsIF {
     tempEnableFastLane: boolean;
@@ -9,19 +13,42 @@ interface propsIF {
 }
 
 export default function FastLaneProtectionControl(props: propsIF) {
-    const { tempEnableFastLane, setTempEnableFastLane, displayInSettings } = props;
+    const { tempEnableFastLane, setTempEnableFastLane, displayInSettings } =
+        props;
     const compKey = useId();
     const toggleAriaLabel = `${
         tempEnableFastLane ? 'disable' : 'enable'
     } MEV protection by Fastlane`;
 
+    const {
+        globalPopup: { open: openGlobalPopup },
+    } = useContext(AppStateContext);
+
     return (
         <div className={styles.main_container}>
-            {displayInSettings ? (
-                <p tabIndex={0}>{'Enable MEV-Protection by Fastlane'}</p>
-            ) : (
+            <FlexContainer alignItems='center' gap={8}>
                 <p tabIndex={0}>Enable MEV-Protection by Fastlane</p>
-            )}
+
+                <ExplanationButton
+                    onClick={() =>
+                        openGlobalPopup(
+                            <div>
+                                MEV-Protection by Fastlane helps protect your
+                                transactions from Maximal Extractable Value
+                                (MEV) attacks, such as front-running and
+                                sandwich attacks, by routing trades through a
+                                secure network.
+                            </div>,
+                            'MEV protection by Fastlane',
+                            'right',
+                        )
+                    }
+                    aria-label='Open range width explanation popup.'
+                >
+                    <AiOutlineInfoCircle color='var(--text2)' />
+                </ExplanationButton>
+            </FlexContainer>
+
             <Toggle
                 key={compKey}
                 isOn={tempEnableFastLane}
